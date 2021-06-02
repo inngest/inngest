90687ea04b89a21f8315dde71df41ed0ed7cf37a
Tony Holdstock-Brown <tonyhb@gmail.com>
2021-06-02 14:34:11 -0700

    Update homepage

--- pages/index.js
+++ pages/index.js
@@ -73,23 +73,32 @@ export default function Home() {
         <meta property="og:title" content="Inngest" />
         <meta property="og:url" content="https://www.inngest.com" />
         <meta property="og:image" content="/logo.svg" />
-        <meta property="og:description" content="An event-driven, code automation platform for developers" />
+        <meta property="og:description" content="An event-driven code automation platform for developers" />
         <script src="/inngest-sdk.js"></script>
       </Head>
 
       <div className={styles.content}>
         <img className={styles.logo} src="/logo.svg" alt="Inngest logo" />
-        <div><b>the first event-driven, code automation platform for developers</b></div>
-        <br/>
+
+
+        <hgroup>
+          <h1><b>serverless event driven infrastructure</b> for engineers</h1>
+          <h2>Build and deploy 25x faster on Inngest</h2>
+        </hgroup>
+
+
         <div>
           event subscriptions <br /> 
           event coordination <br /> 
-          scheduling <br /> 
-          DAG workflows <br/>
-          workflow functions-as-a-service<br/>
+          workflow functions-as-a-service, solo or as a DAG<br/>
+          scheduled DAG workflows <br /> 
         </div>
+
         <br/>
+
         <b>Sign up for updates</b>
+        <br/>
+
         <div>
           <input onKeyDown={onInputKey} type="email" placeholder="Your email here" value={email} onChange={onChange} />
           <button disabled={email === lastSubmitted} className={styles.submit} onClick={onSubmit}>{buttonText}</button>
