fa27a596875ca04a40ae59891181edf588fa6129
Tony Holdstock-Brown <tonyhb@gmail.com>
2021-07-13 12:54:55 -0700

    WIP: New site

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styled from "@emotion/styled";
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
@@ -10,101 +11,151 @@ export const INGEST_KEY = 'BIjxBrM6URqxAu0XgIAae5HgBCv8l_LodmdGonFCfngjhwIgQEbvb
 // export const INGEST_KEY = 'MnzaTCk7Se8i74hA141bZGS-NY9P39RSzYFbxanIHyV2VDNu1fwrns2xBQCEGdIb9XRPtzbp0zdRPjtnA1APTQ';
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [buttonText, setButtonText] = useState("Submit");
-  const [lastSubmitted, setLastSubmitted] = useState(null);
-  const [error, setError] = useState(null);
-
-  const onChange = (e) => {
-    setEmail(e.target.value);
-    setError(null);
-    setButtonText("Submit");
-  }
-
-  const isEmailValid = () => {
-    // stolen from https://www.w3resource.com/javascript/form/email-validation.php
-    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
-  }
-
-  const onSubmit = () => {
-    if (!isEmailValid()) {
-      setError("Is that a valid email address?");
-      return;
-    };
-
-    // prevent users from spamming the submit button
-    if (lastSubmitted === email) {
-      return;
-    }
-
-    setError(null);
-    setLastSubmitted(email);
-
-    const Inngest = globalThis.Inngest;
-
-    if (!Inngest) return;
-
-    Inngest.init(INGEST_KEY);
-    Inngest.event({
-      name: "marketing.signup",
-      data: {
-        email,
-      },
-      user: {
-        email,
-      }
-    });
-
-    setButtonText("Done!");
-  }
-
-  const onInputKey = (e) => {
-    // enter key
-    if (e.keyCode === 13) {
-      onSubmit();
-    }
-  }
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inngest</title>
-        <link rel="icon" href="/favicon.png" />
-        <meta property="og:title" content="Inngest" />
-        <meta property="og:url" content="https://www.inngest.com" />
-        <meta property="og:image" content="/logo.svg" />
-        <meta property="og:description" content="An event-driven code automation platform for developers" />
-        <script src="/inngest-sdk.js"></script>
-      </Head>
+    <>
+      <Nav>
+        <img src="/logo-blue.svg" alt="Inngest logo" />
+      </Nav>
 
-      <div className={styles.content}>
-        <img className={styles.logo} src="/logo.svg" alt="Inngest logo" />
+      <Hero className="text-center">
+        <h1>Automate your workflows</h1>
+        <p>
+          Build real time, event driven workflows in minutes with our serverless platform. <br />
+          Define workflows as code or via a UI, utilize pre-built integrations, or run your own code:<br />
+          it's <u>made for builders</u>, <u>designed for operators</u>.
+        </p>
 
+        <div />
+      </Hero>
 
-        <hgroup>
-          <h2>a platform for serverless, event driven systems</h2>
-          <h4>build and deploy 25x faster on inngest</h4>
-        </hgroup>
+      <HIW>
+        <Content>
+          <h5>Introducing Inngest</h5>
 
+          <p>Inngest is an <strong>automation platform</strong> which <strong>runs workflows on a schedule</strong> or <strong>in real-time after events happen</strong>. Design&nbsp;<strong>complex operational flows</strong> and <strong>run any code</strong> - including pre-built integrations or your own code - with <strong>zero&nbsp;infrastructure&nbsp;and&nbsp;maintenance</strong>.</p>
 
-        <div>
-          event subscriptions <br /> 
-          event coordination <br /> 
-          workflow functions-as-a-service, solo or as a DAG<br/>
-          scheduled DAG workflows <br /> 
-        </div>
+          <HIWGrid>
+            <div>
+              <h2>Workflow management</h2>
+              <p>Build, manage, and operate your product and ops flows end-to-end.  Complete with out-of-the-box integrations for rapid development, and the ability to run your own serverless code for full&nbsp;flexibility</p>
+            </div>
+            <div>
+              <h2>Change management</h2>
+              <p>Version every workflow complete with history, schedule workflows to go live, and handle workflow approvals within your account - it’s everything you need for a fully compliant&nbsp;solution</p>
+            </div>
+            <div>
+              <h2>Transparency &amp; debugging</h2>
+              <p>Drill down into every workflow run, including which users ran through which versions of a workflow and each workflow’s&nbsp;logs.</p>
+            </div>
+          </HIWGrid>
+        </Content>
+      </HIW>
 
-        <br/>
+      <Content>
+        <Callout className="text-center">
+          <div>
+            <span>35x</span>
+            <strong>faster implementation</strong>
+            <span>using our platform and integrations</span>
+          </div>
 
-        <b>Sign up for updates</b>
-        <br/>
+          <div>
+            <span>35x</span>
+            <strong>faster implementation</strong>
+            <span>using our platform and integrations</span>
+          </div>
 
-        <div>
-          <input onKeyDown={onInputKey} type="email" placeholder="Your email here" value={email} onChange={onChange} />
-          <button disabled={email === lastSubmitted} className={styles.submit} onClick={onSubmit}>{buttonText}</button>
-        </div>
-        {error && <div style={{ color: 'red', fontSize: '12px', marginTop: "5px" }}>{error}</div>}
-      </div>
-    </div>
+          <div>
+            <span>35x</span>
+            <strong>faster implementation</strong>
+            <span>using our platform and integrations</span>
+          </div>
+        </Callout>
+      </Content>
+
+      <Footer />
+    </>
   )
 }
+
+const Content = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`
+
+const Nav = styled(Content)`
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+
+  img {
+    max-height: 40px;
+  }
+`
+
+
+const Hero = styled(Content)`
+  font-size: 1.3125rem;
+  padding: 80px 0 0;
+  position: relative;
+
+  > div {
+    box-shadow: 0 10px 50px rgba(0, 0, 0, 0.1);
+    background: #FDFBF6;
+    width: 100%;
+    height: 500px;
+    margin: 100px 0 0;
+    position: relative;
+    z-index: 2;
+  }
+`
+
+const HIW = styled.div`
+  box-shadow: inset 0 0 0 20px #fff;
+  background: linear-gradient(180deg, rgba(243,245,245,1) 20%, rgba(249,251,254,1) 100%);;
+  padding: 450px 40px 140px 40px;
+  margin-top: -400px;
+
+  h5 + p {
+    font-size: 1.3125rem;
+  }
+`
+
+const HIWGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 100px;
+  padding: 30px 0 0;
+`
+
+const Callout = styled.div`
+  max-width: 80%;
+  margin: -80px auto 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 40px;
+
+  background: #FDFBF6;
+  padding: 40px;
+  box-shadow: 0 10px 50px rgba(0, 0, 0, 0.1);
+
+  strong, span {
+    display: block;
+    margin: 4px 0;
+  }
+
+  span:first-of-type {
+    font-size: 2.6rem;
+    margin: 0 0 6px;
+  }
+
+  span:last-of-type {
+    color: #737885;
+  }
+`;
+
+const Footer = styled.div`
+  margin-top: 100px;
+`
