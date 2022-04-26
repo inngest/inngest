84e4dd753e052e9bc8cc121dff5ac5912fb7f6c0
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-04-26 14:55:13 -0400

    Fix issue with Next.js build

--- utils/docs.ts
+++ utils/docs.ts
@@ -1,4 +1,18 @@
-import { TOC } from "../pages/docs/_docs/_toc";
+/**
+ * Docs Table of Contents
+ *
+ * This is the basic order of categories for the docs navigation.
+ */
+
+const TOC = {
+  "What is Inngest?": 0,
+  "Getting started": 1,
+  "Sending Data": 2,
+  "Working with Events": 3,
+  Functions: 4,
+  CLI: 5,
+  "Managing workflows": 10,
+};
 
 export type DocScope = {
   // If the slug contains a forward slahs (eg. foo/bar), this page will automatically
