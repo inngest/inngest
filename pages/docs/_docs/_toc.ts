f72ac1f1a7b6274297aace288b9f076282de7038
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-04-26 12:30:04 -0400

    Add TOC order file

---
+++ pages/docs/_docs/_toc.ts
@@ -0,0 +1,15 @@
+/**
+ * Docs Table of Contents
+ *
+ * This is the basic order of categories for the docs navigation.
+ */
+
+export const TOC = {
+  "What is Inngest?": 0,
+  "Getting started": 1,
+  "Sending Data": 2,
+  "Working with Events": 3,
+  Functions: 4,
+  CLI: 5,
+  "Managing workflows": 10,
+};
