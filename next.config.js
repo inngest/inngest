aa1b032137b09e0ce7d7c1199f7176fb219b0a59
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-06-09 11:31:02 -0400

    Redirect /docs to what is inngest

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ module.exports = {
         destination: "https://discord.gg/EuesV2ZSnX",
         permanent: true,
       },
+      {
+        source: "/docs",
+        destination: "/docs/what-is-inngest",
+        permanent: false,
+      },
     ];
   },
 };
