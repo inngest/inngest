04de84cc47aafcb178669d7149e1a30c9722802e
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-07-12 18:55:58 -0700

    Wait for queue to be created

--- tests/configs/sqs/start.sh
+++ tests/configs/sqs/start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-set -ex
+set -e
 
 docker-compose -f ./opt/compose.yml up -d
 
-# Wait for queues to be made...
-sleep 5
+end=$((SECONDS+60))
+
+while [ $SECONDS -lt $end ]; do
+	echo "check"
+	# There shold be 6 lines with 2 queues made.
+	num=$(docker exec -ti localstack_main sh -c 'awslocal sqs list-queues' | wc -l)
+	if [ $num -gt 5 ];
+	then
+		exit
+	fi;
+done
