e73ac5d186b5f845d347fb7ccf14cabfdebda8f1
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-07-20 21:15:26 -0400

    Add tests using embedded-postgres

--- pkg/coredata/postgres/postgres.go
+++ pkg/coredata/postgres/postgres.go
@@ -43,7 +43,7 @@ type ReadWriter struct {
 	db *sql.DB
 }
 
-func New(ctx context.Context, URI string) (coredata.ReadWriter, error) {
+func New(ctx context.Context, URI string) (*ReadWriter, error) {
 	rw := &ReadWriter{}
 	db, err := pg.Open(ctx, URI)
 	if err != nil {
@@ -57,6 +57,10 @@ func New(ctx context.Context, URI string) (coredata.ReadWriter, error) {
 	return rw, nil
 }
 
+func (rw ReadWriter) Close() error {
+	return rw.db.Close()
+}
+
 // TODO Add method to close the db connection
 
 var (
@@ -65,21 +69,31 @@ var (
 		SELECT action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at
 		FROM action_versions
 		WHERE action_dsn = $1 and version_major = $2 and version_minor = $3`
-	sqlFindLatestMajorActionVersion string = `
+	sqlFindLatestValidMajorActionVersion string = `
 		SELECT action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at
 		FROM action_versions
-		WHERE action_dsn = $1 and version_major = $2
+		WHERE action_dsn = $1 and version_major = $2 and valid_from is not null and valid_to is null
 		ORDER BY version_minor DESC
 		LIMIT 1`
-	sqlFindLatestActionVersion string = `
+	sqlFindLatestValidActionVersion string = `
 		SELECT action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at
 		FROM action_versions
-		WHERE action_dsn = $1
+		WHERE action_dsn = $1 and valid_from is not null and valid_to is null
 		ORDER BY version_major, version_minor DESC
 		LIMIT 1`
 	sqlInsertActionVersion string = `
 		INSERT INTO action_versions (action_dsn, version_major, version_minor, config)
 		VALUES ($1, $2, $3, $4)
+		RETURNING action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at`
+	sqlUpdateActionVersionValidFrom string = `
+		UPDATE action_versions
+		SET valid_from = $4
+		WHERE action_dsn = $1 and version_major = $2 and version_minor = $3
+		RETURNING action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at`
+	sqlUpdateActionVersionValidTo string = `
+		UPDATE action_versions
+		SET valid_to = $4
+		WHERE action_dsn = $1 and version_major = $2 and version_minor = $3
 		RETURNING action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at`
 
 	// functions
@@ -93,13 +107,12 @@ var (
 		FROM functions f
 		JOIN function_versions fv on f.function_id = fv.function_id
 		WHERE fv.valid_from is not null and fv.valid_to is null`
-	sqlFindAllScheduledFunctions string = `
-		SELECT f.function_id, fv.version, fv.config
-		FROM functions f
-		JOIN function_versions fv on f.function_id = fv.function_id
-		JOIN function_triggers ft on f.function_id = ft.function_id
-		WHERE ft.schedule is not null fv.valid_from is not null and fv.valid_to is null`
-	sqlFindAllValidFunctionsByEvent string = `
+	sqlFindAllLiveScheduledFunctions string = `
+		SELECT fv.function_id, fv.version, fv.config
+		FROM function_triggers ft
+		JOIN function_versions fv on fv.function_id = ft.function_id and fv.version = ft.version
+		WHERE ft.schedule is not null and fv.valid_from is not null and fv.valid_to is null`
+	sqlFindAllLiveFunctionsByEvent string = `
 		SELECT fv.function_id, fv.version, fv.config
 		FROM function_triggers ft
 		JOIN function_versions fv on fv.function_id = ft.function_id and fv.version = ft.version
@@ -132,10 +145,6 @@ var (
 // CreateFunctionVersion creates the function, ensures function_triggers are up to date,
 // and creates a new function version, setting any prior version no longer valid.
 func (rw *ReadWriter) CreateFunctionVersion(ctx context.Context, f function.Function, live bool, env string) (function.FunctionVersion, error) {
-	// NOTE - We currently have no "draft" functions in the open source Inngest, this is for future draft functionality
-	// Every new function version deployed is assumed to be live
-	live = true
-
 	var existingFunctionID string
 	var existingVersion int
 	now := time.Now()
@@ -155,7 +164,7 @@ func (rw *ReadWriter) CreateFunctionVersion(ctx context.Context, f function.Func
 	if err != nil {
 		return function.FunctionVersion{}, err
 	}
-	defer tx.Rollback()
+	defer func() { _ = tx.Rollback() }()
 
 	// We confirm there is no existing row in the functions table before creating one
 	if existingFunctionID == "" {
@@ -165,20 +174,9 @@ func (rw *ReadWriter) CreateFunctionVersion(ctx context.Context, f function.Func
 		}
 	}
 
-	// Create all function_triggers for the new version
-	for _, trigger := range f.Triggers {
-		var err error
-		if trigger.EventTrigger != nil {
-			_, err = tx.ExecContext(ctx, sqlInsertEventTrigger, f.ID, newFunctionVersion, trigger.Event)
-		} else if trigger.CronTrigger != nil {
-			_, err = tx.ExecContext(ctx, sqlInsertScheduleTrigger, f.ID, newFunctionVersion, trigger.Cron)
-		}
-		if err != nil {
-			return function.FunctionVersion{}, err
-		}
-	}
-
 	// For live functions, we must make the previous version as no longer valid
+	// NOTE - We currently have no "draft" functions in the open source Inngest, this is for future draft functionality
+	// Every new function version deployed is assumed to be live
 	if live && existingVersion != 0 {
 		_, err := tx.ExecContext(ctx, sqlUpdateFunctionVersionValidTo, f.ID, existingVersion, now)
 		if err != nil {
@@ -187,7 +185,7 @@ func (rw *ReadWriter) CreateFunctionVersion(ctx context.Context, f function.Func
 	}
 
 	// Create the function version
-	config, err := function.MarshalJSON(f)
+	config, err := function.MarshalCUE(f)
 	if err != nil {
 		return function.FunctionVersion{}, err
 	}
@@ -205,6 +203,19 @@ func (rw *ReadWriter) CreateFunctionVersion(ctx context.Context, f function.Func
 		Scan(&fv.CreatedAt, &fv.UpdatedAt)
 	if err != nil || err == sql.ErrNoRows {
 		return function.FunctionVersion{}, err
+	}
+
+	// Create all function_triggers for the new version
+	for _, trigger := range f.Triggers {
+		var err error
+		if trigger.EventTrigger != nil {
+			_, err = tx.ExecContext(ctx, sqlInsertEventTrigger, f.ID, newFunctionVersion, trigger.Event)
+		} else if trigger.CronTrigger != nil {
+			_, err = tx.ExecContext(ctx, sqlInsertScheduleTrigger, f.ID, newFunctionVersion, trigger.Cron)
+		}
+		if err != nil {
+			return function.FunctionVersion{}, err
+		}
 	}
 
 	if err = tx.Commit(); err != nil {
@@ -247,7 +258,7 @@ func (rw *ReadWriter) Functions(ctx context.Context) ([]function.Function, error
 	return rowsToFunctions(ctx, rows)
 }
 func (rw *ReadWriter) FunctionsScheduled(ctx context.Context) ([]function.Function, error) {
-	rows, err := rw.db.QueryContext(ctx, sqlFindAllScheduledFunctions)
+	rows, err := rw.db.QueryContext(ctx, sqlFindAllLiveScheduledFunctions)
 	if err != nil {
 		return []function.Function{}, err
 	}
@@ -255,7 +266,7 @@ func (rw *ReadWriter) FunctionsScheduled(ctx context.Context) ([]function.Functi
 	return rowsToFunctions(ctx, rows)
 }
 func (rw *ReadWriter) FunctionsByTrigger(ctx context.Context, eventName string) ([]function.Function, error) {
-	rows, err := rw.db.QueryContext(ctx, sqlFindAllValidFunctionsByEvent, eventName)
+	rows, err := rw.db.QueryContext(ctx, sqlFindAllLiveFunctionsByEvent, eventName)
 	if err != nil {
 		return []function.Function{}, err
 	}
@@ -269,11 +280,11 @@ func (rw *ReadWriter) ActionVersion(ctx context.Context, dsn string, version *in
 
 	var row *sql.Row
 	if version.Major == nil && version.Minor == nil {
-		// No version constraint - get the latest
-		row = rw.db.QueryRowContext(ctx, sqlFindLatestActionVersion, dsn)
+		// No version constraint - get the latest valid
+		row = rw.db.QueryRowContext(ctx, sqlFindLatestValidActionVersion, dsn)
 	} else if version.Major != nil && version.Minor == nil {
-		// No minor version constraint - get the latest matching the major version
-		row = rw.db.QueryRowContext(ctx, sqlFindLatestMajorActionVersion, dsn, version.Major)
+		// No minor version constraint - get the latest valid matching the major version
+		row = rw.db.QueryRowContext(ctx, sqlFindLatestValidMajorActionVersion, dsn, version.Major)
 	} else if version.Major != nil && version.Minor != nil {
 		// Exact constraint - get the exact match
 		row = rw.db.QueryRowContext(ctx, sqlFindExactMatchingActionVersion, dsn, version.Major, version.Minor)
@@ -314,6 +325,10 @@ func (rw *ReadWriter) CreateActionVersion(ctx context.Context, av inngest.Action
 		ActionVersion: av,
 	}
 
+	if created.Version == nil {
+		return client.ActionVersion{}, errors.New("version must not be empty")
+	}
+
 	// NOTE - We do not allow valid_from to be set when creating a version as the client needs to push a container image
 	// to the registry before calling UpdateActionVersion
 	err = rw.db.QueryRowContext(ctx, sqlInsertActionVersion, av.DSN, av.Version.Major, av.Version.Minor, config).
@@ -336,25 +351,30 @@ func (rw *ReadWriter) UpdateActionVersion(ctx context.Context, dsn string, versi
 	if err != nil {
 		return client.ActionVersion{}, errors.New("no existing action version to update")
 	}
-	// If it has already been enabled, the operation has already been performed
-	if existing.ValidFrom != nil {
+	// if it's already been enabled, or we should not enable, just return
+	if (existing.ValidFrom != nil && enabled) || (existing.ValidFrom == nil && !enabled) {
 		return existing, nil
 	}
 
-	sqlUpdateActionVersionValidFrom := `
-		UPDATE action_versions
-		SET valid_from = $4
-		WHERE action_dsn = $1 and version_major = $2 and version_minor = $3
-		RETURNING action_dsn, version_major, version_minor, config, valid_from, valid_to, created_at`
 	av := client.ActionVersion{}
 	v := inngest.VersionInfo{}
 
-	err = rw.db.QueryRowContext(ctx, sqlUpdateActionVersionValidFrom, dsn, version.Major, version.Minor, time.Now()).
-		Scan(&av.DSN, &v.Major, &v.Minor, &av.Config, &av.ValidFrom, &av.ValidTo, &av.CreatedAt)
-	if err != nil {
-		return client.ActionVersion{}, err
+	// Set the valid from or valid to depending on enabled
+	if existing.ValidFrom == nil && enabled {
+		err = rw.db.QueryRowContext(ctx, sqlUpdateActionVersionValidFrom, dsn, version.Major, version.Minor, time.Now()).
+			Scan(&av.DSN, &v.Major, &v.Minor, &av.Config, &av.ValidFrom, &av.ValidTo, &av.CreatedAt)
+		if err != nil {
+			return client.ActionVersion{}, err
+		}
+	} else if existing.ValidFrom != nil && !enabled {
+		err = rw.db.QueryRowContext(ctx, sqlUpdateActionVersionValidTo, dsn, version.Major, version.Minor, time.Now()).
+			Scan(&av.DSN, &v.Major, &v.Minor, &av.Config, &av.ValidFrom, &av.ValidTo, &av.CreatedAt)
+		if err != nil {
+			return client.ActionVersion{}, err
+		}
 	}
-	av.Version = &v
 
+	// Set the Version
+	av.Version = &v
 	return av, nil
 }
