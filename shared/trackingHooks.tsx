4a94087cfa4cec3cddef7fa2fe15b87294d87b4f
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-08-02 12:24:37 +0100

    Add some comments to `trackingHooks`

--- shared/trackingHooks.tsx
+++ shared/trackingHooks.tsx
@@ -2,11 +2,17 @@ import deterministicSplit from "deterministic-split";
 import { useMemo } from "react";
 import { useLocalStorage } from "react-use";
 
+/**
+ * AB experiments with keys as experiment names and values as the variants.
+ */
 const abExperiments = {
   header: ["kill-queues-headline", "event-driven-headerline"],
   footer: ["removed", "highlighted"],
 } as const;
 
+/**
+ * Fetch and return the user's anonymous ID.
+ */
 export const useAnonId = () => {
   const [anonId] = useLocalStorage<string>("inngest-anon-id");
 
@@ -15,7 +21,13 @@ export const useAnonId = () => {
   };
 };
 
+/**
+ * Fetch the user's current variant for a particular AB experiment.
+ */
 export const useAbTest = <T extends keyof typeof abExperiments>(
+  /**
+   * The experiment name to return the variant of.
+   */
   experimentName: T
 ) => {
   const { anonId } = useAnonId();
