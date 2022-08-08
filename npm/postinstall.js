30627cddcff099ca1ad2c5b34d2ef2b1db443be5
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-08-08 11:39:00 -0700

    Update release.yml for npm dir

--- npm/postinstall.js
+++
@@ -1,6 +0,0 @@
-/**
- * Stub postinstall.js file to allow `npm install` to return successfully during
- * development.
- *
- * This file should be overwritten before publishing via `npm run build`.
- */
