749b89d2a6cd618ce2774f0d4bd8b8688c847172
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-08-12 08:57:29 -0400

    Fix tracking demo viewed

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,9 @@ export default function Home() {
   const [demo, setDemo] = useState(false);
 
   useEffect(() => {
-    trackDemoView();
+    if (demo === true) {
+      trackDemoView();
+    }
   }, [demo]);
 
   return (
