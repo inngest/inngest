c89fa14a459580468f7bdca43c894b8032030475
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-08-26 17:19:54 +0100

    Add comments to code within `google-process-images` example

--- examples/google-process-images/steps/alert/src/index.ts
+++ examples/google-process-images/steps/alert/src/index.ts
@@ -1,11 +1,21 @@
 import type { Args } from "./types";
 
+/**
+ * The function to run for this step.
+ */
 export async function run({
   event: {
     data: { url },
     user,
   },
 }: Args) {
+  /**
+   * Here, we return that the user uploaded an unsafe image along with their
+   * user details and the URL in question.
+   *
+   * In this area, you might want to notify moderators of your platform or flag
+   * the account for review.
+   */
   return {
     status: 200,
     body: {
--- examples/google-process-images/steps/process/src/index.ts
+++ examples/google-process-images/steps/process/src/index.ts
@@ -5,6 +5,10 @@ import { Writable } from "stream";
 import { ulid } from "ulid";
 import type { Args } from "./types";
 
+/**
+ * A representation of a thumbnail of a given `size` that will be uploaded to
+ * Google Cloud Storage.
+ */
 interface Thumbnail {
   size: number;
   storageRef: File;
@@ -12,23 +16,44 @@ interface Thumbnail {
   resizer: Writable;
 }
 
+/**
+ * The stubs for the thumbnails we'll be creating of the given image.
+ */
 const baseThumbnails: (Partial<Thumbnail> & Pick<Thumbnail, "size">)[] = [
   { size: 50 },
   { size: 100 },
   { size: 250 },
 ];
 
-const gcs = new Storage({
+/**
+ * Create clients for interacting with Google Cloud Storage.
+ *
+ * `process.env.GOOGLE_SERVICE_ACCOUNT` is a stringified JSON secret that you
+ * would add to your Inngest Cloud account.
+ *
+ * With a JSON Google Service Account file, you could create a stringified
+ * version with:
+ *
+ *     node -e 'console.log(JSON.stringify(require("./key.json")))' > secret
+ *
+ * @link https://www.inngest.com/docs/functions/secrets
+ */
+const bucket = new Storage({
   credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT as string),
-});
+}).bucket("example_bucket_inngest");
 
-const bucket = gcs.bucket("example_bucket_inngest");
-
+/**
+ * The function to run for this step.
+ */
 export async function run({
   event: {
     data: { url },
   },
 }: Args) {
+  /**
+   * Try and fetch the image using the `url` from the event. Don't use the body
+   * yet, but ensure it's a successful response before continuing.
+   */
   const res = await new Promise<Response>(async (resolve, reject) => {
     fetch(url, {
       redirect: "follow",
@@ -54,10 +79,15 @@ export async function run({
    * the request through.
    */
   const thumbnails: Thumbnail[] = baseThumbnails.map(({ size }) => {
+    // The location we'll save the file in our bucket.
     const storageRef = bucket.file(`${ulid()}_${size}x${size}.png`);
+
+    // An writable stream to Google Cloud Storage, ready to send our thumbnail
     const uploadStream = storageRef.createWriteStream({
       metadata: { contentType: "image/png" },
     });
+
+    // Use Sharp to resize our streamed image on the fly
     const resizer = sharp()
       .resize({
         width: size,
