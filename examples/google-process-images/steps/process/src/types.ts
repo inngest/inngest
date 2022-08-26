82989e3cae3b65b8df373e394d22ca921d9553e3
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-08-26 12:46:47 +0100

    Duplicate `types.ts` for all steps
    This needs improving

--- examples/google-process-images/steps/process/src/types.ts
+++ examples/google-process-images/steps/process/src/types.ts
@@ -1,4 +1,21 @@
-export type EventTriggers = { [key: string]: any };
+export interface UserProfilePhotoUploaded {
+  name: "user/profile.photo.uploaded";
+  data: {
+    /**
+     * A signed public URL to access the profile photo the user wants to use.
+     *
+     * This is the un-optimized, un-checked version of the image.
+     */
+    url: string;
+  };
+  user: {
+    email: string;
+  };
+  v?: string;
+  ts?: number;
+}
+
+export type EventTriggers = UserProfilePhotoUploaded;
 
 export type Args = {
   event: EventTriggers;
