6e989cb0fe24419a426ac944d417c135b9686838
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-09-23 10:45:51 -0700

    Update next guide with typegen

--- utils/code.ts
+++ utils/code.ts
@@ -7,4 +7,10 @@ import remarkShikiTwoslash from 'remark-shiki-twoslash';
 //     remarkPlugins: [highlight],
 //   },
 // });
-export const highlight: any = [remarkShikiTwoslash]
+
+export const highlight: any = [remarkShikiTwoslash, {
+  // See: https://github.com/shikijs/twoslash/blob/main/packages/shiki-twoslash/README.md#user-settings
+  defaultOptions: {
+    showEmit: false
+  }
+}]
