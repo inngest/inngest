f6c96bbf48ac7f6b7dc1844e55924df89adaf3df
Ed Poole <ed@edpoole.me>
2022-11-11 17:44:24 +0000

    Fix copy button active state as it was getting stuck

--- ui/src/components/CodeBlock/CopyButton.tsx
+++ ui/src/components/CodeBlock/CopyButton.tsx
@@ -5,24 +5,21 @@ export default function CopyButton({ btnAction }) {
   const [clickedState, setClickedState] = useState(false)
 
   const handleClick = () => {
-    console.log('clicked')
     setClickedState(true)
     btnAction()
-  }
-
-  const handleTransitionEnd = () => {
-    setClickedState(false)
+    setTimeout(() => {
+      setClickedState(false)
+    }, 1000)
   }
 
   return (
     <button
       onClick={() => handleClick()}
-      onTransitionEnd={() => handleTransitionEnd()}
       className={classNames(
         clickedState
-          ? `bg-emerald-500 translate-x-0 border-transparent duration-1000`
+          ? `bg-emerald-500 border-transparent`
           : `bg-slate-700/50 hover:bg-slate-700/80 border-slate-700/50`,
-        `flex gap-1.5 items-center  border text-xs  rounded-sm px-2.5 py-1 text-slate-100  transition-transform duration-1000`
+        `flex gap-1.5 items-center  border text-xs  rounded-sm px-2.5 py-1 text-slate-100 transition-all duration-150`
       )}
     >
       {clickedState ? 'Copied!' : 'Copy'}
