6bbd1496cf25d749ef29b3a8fe6c75839e4c87e8
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-21 12:42:07 +0000

    Set better time tooltips

--- ui/src/components/Time.tsx
+++ ui/src/components/Time.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "preact/hooks";
 import TimeAgo, { Formatter, ReactTimeagoProps } from "react-timeago";
 
 interface TimeProps extends ReactTimeagoProps {
@@ -16,5 +17,7 @@ const formatter: Formatter = (value, unit, suffix, ms, next) => {
  * TODO Add tooltip on hover with full date.
  */
 export const Time = ({ date }: TimeProps) => {
-  return <TimeAgo date={date} formatter={formatter} />;
+  const tooltip = useMemo(() => new Date(date).toLocaleString(), [date]);
+
+  return <TimeAgo date={date} formatter={formatter} title={tooltip} />;
 };
