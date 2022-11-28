8bba09e708c30dd9fcf4a0bcd849d481c43dc389
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-11-28 10:53:10 -0800

    Add extending lease with tests

---
+++ pkg/execution/state/redis_state/lua/queue/extendLease.lua
@@ -0,0 +1,41 @@
+--[[
+
+Output:
+  0: Successfully leased item
+  1: Queue item not found
+  2: Queue item has no lease
+  3: Lease ID doesn't match (indicating someone else took the lease)
+
+]]
+
+local queueKey      = KEYS[1]
+local queueIndexKey = KEYS[2]
+
+local queueID         = ARGV[1]
+local currentLeaseKey = ARGV[2]
+local newLeaseKey     = ARGV[3]
+
+-- $include(decode_ulid_time.lua)
+
+-- Grab the current time from the new lease key.
+local nextTime = decode_ulid_time(newLeaseKey)
+
+-- Look up the current queue item.  We need to see if the queue item is already leased.
+local item = cjson.decode(redis.call("HGET", queueKey, queueID))
+if item == nil then
+	return 1
+end
+if item.leaseID == nil then
+	return 2
+end
+if item.leaseID ~= currentLeaseKey then
+	return 3
+end
+
+item.leaseID = newLeaseKey
+-- Update the item's lease key.
+redis.call("HSET", queueKey, queueID, cjson.encode(item))
+-- Update the item's score in our sorted index.
+redis.call("ZADD", queueIndexKey, math.floor(nextTime / 1000), item.id)
+
+return 0
