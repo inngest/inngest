6483f72fa825dce1c21dbe03a827a64fad887288
Ed Poole <ed@edpoole.me>
2022-11-30 18:01:51 +0000

    Remove Inngest logo from deployments

--- shared/Home/FitsYourWorkflow.tsx
+++ shared/Home/FitsYourWorkflow.tsx
@@ -132,10 +132,10 @@ export default function EventDriven() {
               </p>
             </div>
             <div className="flex items-center justify-evenly xl:justify-between mt-6 mb-8 flex-wrap m-auto px-8">
-              <div className="flex items-end lg:flex-row justify-evenly xl:justify-between w-full m-auto xl:w-1/2">
+              <div className="flex items-end lg:flex-row justify-evenly xl:justify-between w-full m-auto flex-wrap">
                 <a
                   href="/docs/deploy/vercel?ref=homepage-fits-your-workflow"
-                  className="flex w-1/2 max-w-[140px] hover:scale-110 transition-all duration-150"
+                  className="flex w-1/2 md:w-1/3 max-w-[140px] hover:scale-110 transition-all duration-150"
                 >
                   <img
                     className="max-w-[140px]"
@@ -144,27 +144,16 @@ export default function EventDriven() {
                 </a>
                 <a
                   href="/docs/deploy/netlify?ref=homepage-fits-your-workflow"
-                  className="flex w-1/2 max-w-[140px] hover:scale-110 transition-all duration-150"
+                  className="flex w-1/2 md:w-1/3 max-w-[140px] hover:scale-110 transition-all duration-150"
                 >
                   <img
                     className="max-w-[140px]"
                     src="/assets/homepage/send-events/netlify.png"
                   />
                 </a>
-              </div>
-              <div className="flex items-start lg:flex-row justify-evenly xl:justify-between w-full m-auto xl:w-1/2">
-                <a
-                  href="/docs/deploy/inngest-cloud?ref=homepage-fits-your-workflow"
-                  className="flex w-1/2 max-w-[140px] hover:scale-110 transition-all duration-150"
-                >
-                  <img
-                    className="max-w-[140px]"
-                    src="/assets/homepage/send-events/inngest.png"
-                  />
-                </a>
                 <a
                   href="/docs/deploy/cloudflare?ref=homepage-fits-your-workflow"
-                  className="flex w-1/2 max-w-[140px] hover:scale-110 transition-all duration-150"
+                  className="flex w-1/2 md:w-1/3 max-w-[140px] hover:scale-110 transition-all duration-150"
                 >
                   <img
                     className="max-w-[140px]"
