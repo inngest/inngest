1685ede92dddc40f13e8ad8f2d45b83baa6ecc42
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2022-11-30 18:02:42 +0000

    Add `<BlankSlate>` component for rendering empty states in the UI

---
+++ ui/src/components/Blank/index.tsx
@@ -0,0 +1,55 @@
+import { IconBook, IconFeed } from "../../icons";
+
+interface BlankSlateProps {
+  imageUrl?: string;
+  title?: string;
+  subtitle?: string;
+  link?: {
+    text: string;
+    url: string;
+  };
+  button?: { text: string; onClick: () => void };
+}
+
+export const BlankSlate = ({
+  imageUrl,
+  title,
+  subtitle,
+  button,
+  link,
+}: BlankSlateProps) => {
+  return (
+    <div className="w-full h-full flex items-center justify-center text-white">
+      <div className="max-w-[24rem] flex flex-col items-center justify-center text-center space-y-3">
+        {imageUrl ? (
+          <div className="w-48 mb-4">
+            <img src={imageUrl} />
+          </div>
+        ) : null}
+
+        {title ? <div className="font-semibold text-lg">{title}</div> : null}
+        {subtitle ? <div>{subtitle}</div> : null}
+
+        {link ? (
+          <a
+            href={link.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-2 bg-slate-1000/80 border border-slate-700 rounded-sm px-3 py-2 flex flex-row items-center space-x-2 justify-center leading-none"
+          >
+            <div>{link.text}</div>
+            <IconFeed />
+          </a>
+        ) : button ? (
+          <button
+            onClick={button.onClick}
+            className="mt-2 bg-slate-1000/80 border border-slate-700 rounded-sm px-3 py-2 flex flex-row items-center space-x-2 justify-center leading-none"
+          >
+            <div>{button.text}</div>
+            <IconBook />
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+};
