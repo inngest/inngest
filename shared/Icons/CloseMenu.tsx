dcbda6e9b0e961f786218a752b6d944ccb885f41
Ed Poole <ed@edpoole.me>
2022-11-30 14:43:47 +0000

    Fix clipRule casing in close menu svg

--- shared/Icons/CloseMenu.tsx
+++ shared/Icons/CloseMenu.tsx
@@ -9,7 +9,7 @@ export default function CloseMenu() {
       <path
         fillRule="evenodd"
         d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
