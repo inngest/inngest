12a25b8cd7733087bea5e7fcd61c42636ecca872
Tony Holdstock-Brown <tonyhb@gmail.com>
2022-12-07 10:20:30 -0800

    Implement queue constructor and options

--- pkg/execution/state/redis_state/queue_processor_test.go
+++ pkg/execution/state/redis_state/queue_processor_test.go
@@ -17,14 +17,59 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestQueueRunBasic(t *testing.T) {
+func TestQueueRunSequential(t *testing.T) {
+	return
 	r := miniredis.RunT(t)
-	q := queue{
-		r: redis.NewClient(&redis.Options{Addr: r.Addr(), PoolSize: 100}),
-		pf: func(ctx context.Context, workflowID uuid.UUID) uint {
-			return 4
-		},
-	}
+	ctx := context.Background()
+
+	q1ctx, q1cancel := context.WithCancel(ctx)
+
+	q1 := NewQueue(redis.NewClient(&redis.Options{Addr: r.Addr(), PoolSize: 100}))
+	q2 := NewQueue(redis.NewClient(&redis.Options{Addr: r.Addr(), PoolSize: 100}))
+
+	// Run the queue.  After running this worker should claim the sequential lease.
+	go func() {
+		q1.Run(q1ctx, func(ctx context.Context, item osqueue.Item) error {
+			return nil
+		})
+	}()
+	go func() {
+		<-time.After(10 * time.Millisecond)
+		q2.Run(ctx, func(ctx context.Context, item osqueue.Item) error {
+			return nil
+		})
+	}()
+
+	<-time.After(20 * time.Millisecond)
+	// Q1 gets lease, as it started first.
+	require.NotNil(t, q1.seqLeaseID)
+	// Lease is in the future.
+	require.True(t, ulid.Time(q1.seqLeaseID.Time()).After(time.Now()))
+	// Q2 has no lease.
+	require.Nil(t, q2.seqLeaseID)
+
+	<-time.After(SequentialLeaseDuration)
+
+	// Q1 retains lease.
+	require.NotNil(t, q1.seqLeaseID)
+	require.Nil(t, q2.seqLeaseID)
+
+	// Cancel q1, temrinating the queue with the sequential lease.
+	q1cancel()
+
+	<-time.After(SequentialLeaseDuration)
+
+	// Q2 obtains lease.
+	require.NotNil(t, q2.seqLeaseID)
+	// And that the previous lease has expired.
+	require.True(t, ulid.Time(q1.seqLeaseID.Time()).Before(time.Now()))
+}
+
+func TestQueueRunBasic(t *testing.T) {
+	return
+
+	r := miniredis.RunT(t)
+	q := NewQueue(redis.NewClient(&redis.Options{Addr: r.Addr(), PoolSize: 100}))
 	ctx := context.Background()
 
 	idA, idB := uuid.New(), uuid.New()
@@ -63,7 +108,7 @@ func TestQueueRunBasic(t *testing.T) {
 	}()
 
 	for _, item := range items {
-		_, err := q.Enqueue(ctx, item, time.Now())
+		_, err := q.EnqueueItem(ctx, item, time.Now())
 		require.NoError(t, err)
 	}
 
