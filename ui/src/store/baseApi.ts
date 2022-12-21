c7f4d80d9218a32319ba5aa2e2691f3a0f952449
Dan Farrelly <djfarrelly@users.noreply.github.com>
2022-12-21 06:10:36 -0500

    Use current hostname for GraphQL requests (#322)
    * Use current hostname for GraphQL requests
    
    If the user decides to run Inngest on another ip or host, the UI should make requests to the core api on that same host.
    
    Fixes #303.
    
    * Revert `index.html` built change
    
    Co-authored-by: Jack Williams <jack@inngest.com>
    Co-authored-by: Jack Williams <1736957+jpwilliams@users.noreply.github.com>

--- ui/src/store/baseApi.ts
+++ ui/src/store/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 import { GraphQLClient } from "graphql-request";
 
-export const client = new GraphQLClient("http://localhost:8300/gql");
+const hostname = window.location.hostname;
+
+export const client = new GraphQLClient(`http://${hostname}:8300/gql`);
 
 export const api = createApi({
   baseQuery: graphqlRequestBaseQuery({ client }),
