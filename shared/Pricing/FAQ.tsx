85679a643c0851f12d25032dbcc5ac4a2ef83ca7
Ed Poole <ed@edpoole.me>
2023-01-12 22:21:47 +0000

    Update FAQ spacing

--- shared/Pricing/FAQ.tsx
+++ shared/Pricing/FAQ.tsx
@@ -1,6 +1,6 @@
 export function FAQRow({ question, children }) {
   return (
-    <div className="items-start justify-between py-12">
+    <div className="items-start justify-between py-6 lg:py-12">
       <h3 className="mb-4 text-lg xl:text-xl font-semibold tracking-tight text-white">
         {question}
       </h3>
