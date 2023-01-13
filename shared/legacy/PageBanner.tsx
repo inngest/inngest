73903dfb6de024d81ee508e8508fd3a8205f4fa1
Ed Poole <ed@edpoole.me>
2023-01-13 13:58:20 +0000

    Add hover state to page banner

--- shared/legacy/PageBanner.tsx
+++ shared/legacy/PageBanner.tsx
@@ -8,7 +8,7 @@ type Props = {
 const PageBanner: React.FC<Props> = ({ href, children }) => (
   <a
     href={href}
-    className="page-banner bg-indigo-500 text-sm block text-center w-full py-2 text-white font-medium"
+    className="page-banner bg-indigo-500 text-sm block text-center w-full py-2 text-white font-medium hover:bg-indigo-600 transition-all"
   >
     {children}
     <span className="text-white"> &rsaquo;</span>
