d96834a400fd1d16dc898c9147df692dba57e4a5
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-04-08 15:49:05 -0400

    Quick roadmap refresh

--- shared/Home/Roadmap.tsx
+++ shared/Home/Roadmap.tsx
@@ -5,7 +5,7 @@ export default function Roadmap() {
     <>
       <Container className="mt-40">
         <SectionHeader
-          title="Inngest SDK Roadmap"
+          title="Inngest Roadmap"
           lede="What we've built and what's up next."
         />
       </Container>
@@ -15,31 +15,19 @@ export default function Roadmap() {
           <h4 className="text-white text-xl font-medium ml-4 mb-4">Future</h4>
           <ul className="flex flex-col gap-3 border border-slate-600/10 p-3 xl:p-4 rounded-xl">
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Additional platform support (AWS Lambda, Supabase, Deno)
+              Dev server function debugging
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Additional framework support
+              Improved Webhook DX
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Local event schema management
+              Function replay/redrive tools
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Dev server replay
+              Function replay/redrive tools
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              SDK APIs for testing
-            </li>
-            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Dashboards for function metrics
-            </li>
-            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Event schema mismatch quarantine
-            </li>
-            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Event stream warehousing
-            </li>
-            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              User-level debugging
+              Function observability metrics
             </li>
           </ul>
         </div>
@@ -47,16 +35,16 @@ export default function Roadmap() {
           <h4 className="text-white text-xl font-medium ml-4 mb-4">Now</h4>
           <ul className="flex flex-col gap-3 border border-slate-600/10 p-3 xl:p-4 rounded-xl">
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Scheduled function cancellation
+              Webapp redesign
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Step parallelization
+              Branch Environments w/ Vercel support
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Automatic error functions
+              Deploy monitoring
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Function concurrency limits
+              Function and step-level custom error handling functions
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
               Function Throttling
@@ -69,34 +57,23 @@ export default function Roadmap() {
         <div className="w-full lg:w-1/3">
           <h4 className="text-white text-xl font-medium ml-4 mb-4">Launched</h4>
           <ul className="flex flex-col gap-3 border border-slate-600/10 p-3 xl:p-4 rounded-xl">
-            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base overflow-hidden">
-              <div className="flex items-center px-6 py-4">
-                Step functions{" "}
-                <span className="px-1.5 py-1 font-medium leading-none text-white bg-indigo-500 rounded text-xs ml-2">
-                  New
-                </span>
-              </div>
-              <div className="flex flex-wrap px-4 py-2 bg-slate-800/40">
-                <span className="bg-cyan-600 text-slate-200 text-xs font-medium leading-none px-2 py-1 rounded-full">
-                  Frameworks
-                </span>
-              </div>
+            <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
+              Scheduled function cancellation{" "}
+              <span className="px-1.5 py-1 font-medium leading-none text-white bg-indigo-500 rounded text-xs ml-2">
+                New
+              </span>
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Create event-driven and scheduled functions
+              Function concurrency limits{" "}
+              <span className="px-1.5 py-1 font-medium leading-none text-white bg-indigo-500 rounded text-xs ml-2">
+                New
+              </span>
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              Send events
+              Parallel steps
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
-              TypeScript: Event Type generation and sync (
-              <a
-                className="text-indigo-400"
-                href="/docs/typescript?ref=features-sdk-roadmap"
-              >
-                docs
-              </a>
-              )
+              Step functions
             </li>
             <li className="text-slate-200 bg-slate-900 rounded text-sm xl:text-base px-6 py-4">
               Typescript support, including generics
@@ -108,13 +85,41 @@ export default function Roadmap() {
                   href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-next-js"
                 >
                   Next.js
-                </a>{" "}
-                &amp;{" "}
+                </a>
+                ,{" "}
                 <a
                   className="text-indigo-400"
                   href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-express"
                 >
                   Express.js
+                </a>
+                ,{" "}
+                <a
+                  className="text-indigo-400"
+                  href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-aws-lambda"
+                >
+                  AWS Lambda
+                </a>
+                ,{" "}
+                <a
+                  className="text-indigo-400"
+                  href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-redwood"
+                >
+                  RedwoodJS
+                </a>
+                ,{" "}
+                <a
+                  className="text-indigo-400"
+                  href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-remix"
+                >
+                  Remix
+                </a>
+                , &amp;{" "}
+                <a
+                  className="text-indigo-400"
+                  href="/docs/sdk/serve?ref=features-sdk-roadmap#framework-fresh-deno"
+                >
+                  Fresh (Deno)
                 </a>{" "}
                 support
               </div>
