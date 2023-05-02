8b8caad8d5714e594fff78a7eb7cfea9a60c0b27
Ed Poole <ed@edpoole.me>
2023-05-02 19:01:29 +0100

    Tweak customer quote visuals

--- shared/CustomerQuote.tsx
+++ shared/CustomerQuote.tsx
@@ -27,12 +27,12 @@ export default function CustomerQuote({
 }: CustomerQuoteProps) {
   return (
     <aside
-      className={`${className} max-w-5xl mx-auto px-4 flex flex-col md:flex-row items-center`}
+      className={`${className} max-w-[1200px] mx-auto px-4 flex flex-col md:flex-row items-center`}
     >
       <div className="relative max-w-[80%]">
-        <div className="absolute z-0 w-full h-full rounded-lg backdrop-blur bg-white/5"></div>
+        <div className="absolute z-0 w-full h-full rounded-[14px] backdrop-blur bg-white/5"></div>
         <div
-          className="relative z-10 m-5 py-5 px-14 h-36 md:h-96 md:w-72 flex items-center rounded-lg"
+          className="relative z-10 m-5 py-5 px-14 h-36 md:h-96 md:w-72 flex items-center rounded"
           style={{
             backgroundColor: `hsla(235,79%,63%,1)`,
             backgroundImage: MeshGradient,
@@ -45,8 +45,8 @@ export default function CustomerQuote({
           />
         </div>
       </div>
-      <div className="p-5 -mt-8 md:mt-0 md:-ml-16 border border-slate-200/30 rounded-lg">
-        <div className="p-6 pt-8 md:pt-6 md:pl-24 flex flex-col items-start	gap-6 rounded-lg text-white bg-slate-1000/50">
+      <div className="p-5 -mt-8 md:mt-0 md:-ml-16 border border-slate-600/30 rounded-[24px]">
+        <div className="p-6 pt-8 md:py-10 md:pl-24 md:pr-16 flex flex-col items-start	gap-6 rounded-[10px] text-white bg-slate-900">
           <p className="text-lg lg:text-xl">&ldquo;{text}&rdquo;</p>
           <Button href={cta.href} variant="secondary" arrow="right">
             {cta.text}
