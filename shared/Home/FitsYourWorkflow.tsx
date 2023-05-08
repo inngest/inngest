c3652612f841cf11ee9e59a3a88dd974733f611c
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-05-08 15:30:04 -0700

    Update weight of copy

--- shared/Home/FitsYourWorkflow.tsx
+++ shared/Home/FitsYourWorkflow.tsx
@@ -25,7 +25,9 @@ export default function EventDriven() {
             </h3>
             <p className="text-slate-400 text-sm max-w-lg leading-5 lg:leading-7 lg:text-base">
               Use the Inngest SDK to define functions that are triggered by
-              events from your app or anywhere on the internet.
+              events from your app or anywhere on the internet.  We call your
+              functions by HTTP at the right time, resuming your function with
+              the right state &mdash; using normal Typescript.
             </p>
             <div className="bg-slate-800/50 mt-4  backdrop-blur-md border border-slate-700/30 inline-flex rounded text-sm text-slate-200 shadow-lg">
               <pre className=" pl-4 pr-4 py-2">
--- shared/Home/Hero.tsx
+++ shared/Home/Hero.tsx
@@ -13,8 +13,8 @@ export default function Hero() {
             Reliable functions on any platform, in minutes
           </h1>
           <p className="text-sm md:text-base text-slate-300 max-w-xl leading-6 md:leading-7">
-            Use TypeScript to write and test <strong>durable functions driven by events</strong> or a schedule.{" "}
-            <strong>Deploy to any platform using your current flow</strong> in minutes, <strong>zero infrastructure or learning needed</strong>.
+            Use TypeScript to write and test durable functions driven by events or a schedule.{" "}
+            Deploy to any platform using your current flow in minutes, zero infrastructure needed.
           </p>
 
           <ul className="text-sm md:text-base text-slate-300 max-w-xl leading-5 md:leading-6 py-8 grid grid-cols-2 gap-x-4 gap-y-3">
