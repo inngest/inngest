5266248ac3c84b0584a6bd8aac6f2bd288174cc7
Tony Holdstock-Brown <tonyhb@gmail.com>
2023-05-08 15:12:28 -0700

    Update hero copy with suggested bullet points

--- shared/Home/Hero.tsx
+++ shared/Home/Hero.tsx
@@ -8,16 +8,53 @@ export default function Hero() {
     <div className="relative">
       <Container className="pt-20 pb-16 md:pt-36 md:pb-28 lg:pt-36 lg:pb-40 xl:pt-40 xl:pb-32  2xl:pt-56 2xl:pb-48  flex items-center">
         <HeroImg />
-        <div className="max-w-[760px] relative pr-10 lg:px-auto m-x-auto py-10 rounded-lg">
-          <h1 className="text-4xl leading-[48px] sm:text-5xl sm:leading-[58px] lg:text-6xl font-semibold lg:leading-[68px] tracking-[-2px] text-slate-50 mb-5">
-            Ship Background Jobs, Crons, Webhooks, and Reliable Workflows in
-            record time
+        <div className="max-w-[800px] relative pr-10 lg:px-auto m-x-auto py-10 rounded-lg">
+          <h1 className="text-4xl leading-[48px] sm:text-5xl sm:leading-[58px] lg:text-6xl font-semibold lg:leading-[68px] tracking-[-2px] text-slate-50 mb-5 font-extrabold">
+            Reliable functions on any platform, in minutes
           </h1>
           <p className="text-sm md:text-base text-slate-300 max-w-xl leading-6 md:leading-7">
-            Use TypeScript to build, test, and deploy serverless functions
-            driven by events or a schedule to any platform in seconds, with zero
-            infrastructure.
+            Use TypeScript to write and test <strong>durable functions driven by events</strong> or a schedule.{" "}
+            <strong>Deploy to any platform using your current flow</strong> in minutes, zero infrastructure or learning needed.
           </p>
+
+          <ul className="text-sm md:text-base text-slate-300 max-w-xl leading-5 md:leading-6 py-8 grid grid-cols-2 gap-x-4 gap-y-3">
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >Serverless queues</li>
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >Background jobs</li>
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >Scheduled functions</li>
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >Durable functions with memory</li>
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >Complex user-defined workflows</li>
+            <li
+              className="pl-8"
+              style={{
+                background: "url(/assets/check-white.svg) no-repeat 0 1px"
+              }}
+            >AI & LLM chains</li>
+          </ul>
           <div className="flex flex-col items-start lg:flex-row gap-4 mt-6 lg:mt-12 lg:items-center">
             <a
               href="sign-up?ref=homepage-hero"
