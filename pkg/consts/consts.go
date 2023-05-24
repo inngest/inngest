4fba697a79484b4dcf2c3ec7d95472fb3ac4b8e2
Tony Holdstock-Brown <tony@inngest.com>
2023-05-24 16:44:07 -0700

    Use max function timeouts as per docs for dev server (#451)
    This moves the dev server in line with the cloud.  Internally these
    consts will be used to keep parity.

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -1,6 +1,13 @@
 package consts
 
+import "time"
+
 const (
 	// DefaultRetryCount is used when no retry count for a step is specified.
 	DefaultRetryCount = 3
+
+	MaxFunctionTimeout = 2 * time.Hour
+
+	// MaxBodySize is the maximum payload size read on any HTTP response.
+	MaxBodySize = 1024 * 1024 * 4
 )
