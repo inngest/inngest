505f017cea3f322ea887a5dea988021396460e92
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-13 13:27:48 -0700

    INN-1401: Accept batch configs from registration

--- pkg/inngest/batch.go
+++ pkg/inngest/batch.go
@@ -1,5 +1,31 @@
 package inngest
 
+import "fmt"
+
+func NewEventBatchConfig(data map[string]any) (*EventBatchConfig, error) {
+	if data == nil {
+		return nil, nil
+	}
+
+	config := &EventBatchConfig{}
+
+	size, ok := data["maxSize"].(int)
+	if !ok {
+		return nil, fmt.Errorf("unexpected type for MaxSize: %v", size)
+	}
+	config.MaxSize = size
+
+	timeout, ok := data["timeout"].(string)
+	if !ok {
+		return nil, fmt.Errorf("unexpected type for Timeout: %v", timeout)
+	}
+	config.Timeout = timeout
+
+	// TODO: validate timeout expression
+
+	return config, nil
+}
+
 // EventBatchConfig represents how a function would expect
 // a list of events to look like for consumption
 //
--- pkg/inngest/batch_test.go
+++ pkg/inngest/batch_test.go
@@ -1,10 +1,63 @@
 package inngest
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestNewEventBatchConfig(t *testing.T) {
+	tests := []struct {
+		name          string
+		data          map[string]any
+		expected      *EventBatchConfig
+		expectedError error
+	}{
+		{
+			name: "should return config with valid data",
+			data: map[string]any{
+				"maxSize": 10,
+				"timeout": "2s",
+			},
+			expected: &EventBatchConfig{
+				MaxSize: 10,
+				Timeout: "2s",
+			},
+		},
+		{
+			name:     "should return nil without errors if data is empty",
+			data:     nil,
+			expected: nil,
+		},
+		{
+			name: "should return error with invalid size type",
+			data: map[string]any{
+				"maxSize": "yolo",
+			},
+			expectedError: errors.New("unexpected type for MaxSize:"),
+		},
+		{
+			name: "should return error with invalid timeout type",
+			data: map[string]any{
+				"maxSize": 10,
+				"timeout": 10,
+			},
+			expectedError: errors.New("unexpected type for Timeout:"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			config, err := NewEventBatchConfig(test.data)
+			require.Equal(t, test.expected, config)
+
+			if err != nil {
+				require.ErrorContains(t, err, test.expectedError.Error())
+			}
+		})
+	}
+}
 
 func TestEventBatchIsEnabled(t *testing.T) {
 	tests := []struct {
