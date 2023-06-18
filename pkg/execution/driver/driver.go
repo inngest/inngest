831868865865bd10482855b58223ee1cebee5fda
Darwin D Wu <darwin67@users.noreply.github.com>
2023-06-17 20:00:28 -0700

    INN-1401: Add tests for SDK request struct for checking batch sizes

--- pkg/execution/driver/driver.go
+++ pkg/execution/driver/driver.go
@@ -4,12 +4,9 @@ import (
 	"context"
 	"encoding/json"
 
-	"github.com/google/uuid"
 	"github.com/gowebpki/jcs"
-	"github.com/inngest/inngest/pkg/consts"
 	"github.com/inngest/inngest/pkg/execution/state"
 	"github.com/inngest/inngest/pkg/inngest"
-	"github.com/oklog/ulid/v2"
 )
 
 type Driver interface {
@@ -32,11 +29,11 @@ type FunctionStack struct {
 
 // MarshalV1 marshals state as an input to driver runtimes.
 func MarshalV1(ctx context.Context, s state.State, step inngest.Step, stackIndex int, env string) ([]byte, error) {
-	req := &SDKInvokeRequest{
+	req := &SDKRequest{
 		Events:  s.Events(),
 		Event:   s.Event(),
 		Actions: s.Actions(),
-		Context: &SDKInvokeRequestContext{
+		Context: &SDKRequestContext{
 			FunctionID: s.Function().ID,
 			Env:        env,
 			StepID:     step.ID,
@@ -59,41 +56,4 @@ func MarshalV1(ctx context.Context, s state.State, step inngest.Step, stackIndex
 	}
 
 	return jcs.Transform(j)
-}
-
-type SDKInvokeRequest struct {
-	Event   map[string]any           `json:"event,omitempty"`
-	Events  []map[string]any         `json:"events,omitempty"`
-	Actions map[string]any           `json:"steps"`
-	Context *SDKInvokeRequestContext `json:"ctx"`
-	BatchID *ulid.ULID               `json:"batch_id,omitempty"`
-}
-
-func (req *SDKInvokeRequest) IsBatchSizeTooLarge() bool {
-	byt, err := json.Marshal(req.Events)
-	if err != nil {
-		// log error
-		return false
-	}
-	return len(byt) > consts.MaxBodySize
-}
-
-type SDKInvokeRequestContext struct {
-	// FunctionID is used within entrypoints to SDK-based functions in
-	// order to specify the ID of the function to run via RPC.
-	FunctionID uuid.UUID `json:"fn_id"`
-
-	// Env is the name of the environment that the function is running in.
-	// though this is self-discoverable most of the time, for static envs
-	// the SDK has no knowledge of the name as it only has a signing key.
-	Env string `json:"env"`
-
-	// StepID is used within entrypoints to SDK-based functions in
-	// order to specify the step of the function to run via RPC.
-	StepID string `json:"step_id"`
-
-	// XXX: Pass in opentracing context within ctx.
-	RunID ulid.ULID `json:"run_id"`
-
-	Stack *FunctionStack `json:"stack"`
 }
---
+++ pkg/execution/driver/request.go
@@ -0,0 +1,46 @@
+package driver
+
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+	"github.com/inngest/inngest/pkg/consts"
+	"github.com/oklog/ulid/v2"
+)
+
+type SDKRequest struct {
+	Event   map[string]any     `json:"event,omitempty"`
+	Events  []map[string]any   `json:"events,omitempty"`
+	Actions map[string]any     `json:"steps"`
+	Context *SDKRequestContext `json:"ctx"`
+	BatchID *ulid.ULID         `json:"batch_id,omitempty"`
+}
+
+func (req *SDKRequest) IsBatchSizeTooLarge() bool {
+	byt, err := json.Marshal(req.Events)
+	if err != nil {
+		// log error
+		return false
+	}
+	return len(byt) >= consts.MaxBodySize
+}
+
+type SDKRequestContext struct {
+	// FunctionID is used within entrypoints to SDK-based functions in
+	// order to specify the ID of the function to run via RPC.
+	FunctionID uuid.UUID `json:"fn_id"`
+
+	// Env is the name of the environment that the function is running in.
+	// though this is self-discoverable most of the time, for static envs
+	// the SDK has no knowledge of the name as it only has a signing key.
+	Env string `json:"env"`
+
+	// StepID is used within entrypoints to SDK-based functions in
+	// order to specify the step of the function to run via RPC.
+	StepID string `json:"step_id"`
+
+	// XXX: Pass in opentracing context within ctx.
+	RunID ulid.ULID `json:"run_id"`
+
+	Stack *FunctionStack `json:"stack"`
+}
