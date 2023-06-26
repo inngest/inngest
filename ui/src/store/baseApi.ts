505ef53f0b4f632165c3ce9acfa5f90b2c2d935b
Igor Gassmann <igor@inngest.com>
2023-06-26 19:37:11 +0200

    fix(ui): don't use window when not available

--- ui/src/store/baseApi.ts
+++ ui/src/store/baseApi.ts
@@ -2,9 +2,7 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlRequestBaseQuery } from "@rtk-query/graphql-request-base-query";
 import { GraphQLClient } from "graphql-request";
 
-const hostname = process.env.NODE_ENV === 'development' ? "localhost:8288" : window.location.host;
-
-export const client = new GraphQLClient(`http://${hostname}/v0/gql`);
+export const client = new GraphQLClient('/v0/gql');
 
 export const api = createApi({
   baseQuery: graphqlRequestBaseQuery({ client }),
--- ui/src/store/devApi.ts
+++ ui/src/store/devApi.ts
@@ -1,39 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { api, type Event } from './generated';
 
-const devUrl = new URL(window.location.href);
-devUrl.pathname = "";
-
 export interface EventPayload {
   name: string;
 }
 
 export const devApi = createApi({
   reducerPath: "devApi",
-  baseQuery: fetchBaseQuery({ baseUrl: devUrl.href }),
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
   endpoints: (builder) => ({
     sendEvent: builder.mutation<void, { id: string, name: string; ts: number, data?: object, user?: object }>({
-      query: (event) => {
-        let url = "/e/dev_key";
-        /**
-         * In dev mode, always assume that the dev server API is available at 8288. This
-         * allows us to use a separate hot-reloading port for the UI when developing.
-         *
-         * NOTE: This has been removed to allow people to run `inngest dev --port 8111`:
-         * users run multiple copies of inngest - one for dev, one for tests - as mock
-         * environments.
-         */
-        if (process.env.NODE_ENV === 'development') {
-          const localDevUrl = new URL(url, devUrl);
-          url = localDevUrl.href;
-        }
-
-        return {
-          url,
-          method: "POST",
-          body: event,
-        };
-      },
+      query: (event) => ({
+        url: "/e/dev_key",
+        method: "POST",
+        body: event,
+      }),
       onQueryStarted(event, { dispatch, queryFulfilled }) {
         // Optimistically add the event to the `GetEventQuery` cache so that it shows up in the UI
         // immediately.
