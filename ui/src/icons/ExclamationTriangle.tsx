48c5ce6e6609d2ee9ffcc84e3e9fcc5ae58360df
Ana <anafilipadealmeida@gmail.com>
2023-07-05 19:05:55 +0100

    UI adjustments

--- ui/src/icons/ExclamationTriangle.tsx
+++ ui/src/icons/ExclamationTriangle.tsx
@@ -1,6 +1,7 @@
-export function IconExclamationTriangle() {
+export function IconExclamationTriangle({ className }: { className?: string }) {
   return (
     <svg
+      className={className}
       width="18"
       height="19"
       viewBox="0 0 18 19"
@@ -10,7 +11,7 @@ export function IconExclamationTriangle() {
       <g>
         <path
           d="M8.99971 7.25005V10.0625M2.02227 12.5943C1.37323 13.7193 2.18516 15.125 3.48396 15.125H14.5155C15.8143 15.125 16.6262 13.7193 15.9771 12.5943L10.4614 3.03364C9.812 1.90802 8.18742 1.90802 7.53802 3.03364L2.02227 12.5943ZM8.99971 12.3125H9.00533V12.3182H8.99971V12.3125Z"
-          stroke="white"
+          stroke="currentColor"
           strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
