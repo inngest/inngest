045f502b6cfd108a94b0cb82536080a133a6ec9a
Ana <anafilipadealmeida@gmail.com>
2023-07-05 13:51:40 +0100

    Add disabled state to buttons

--- ui/src/components/Button.tsx
+++ ui/src/components/Button.tsx
@@ -4,26 +4,29 @@ interface ButtonProps {
   kind?: "primary" | "secondary" | "text";
   label?: React.ReactNode;
   icon?: React.ReactNode;
+  disabled?: boolean;
   btnAction?: () => void;
 }
 
 export default function Button({
   label,
   icon,
+  disabled,
   btnAction,
   kind = "primary",
 }: ButtonProps) {
   return (
     <button
       className={classNames(
-        "flex gap-1.5 items-center border text-xs rounded-sm px-2.5 py-1 text-slate-100 transition-all duration-150",
+        "flex gap-1.5 items-center border text-xs rounded-sm px-2.5 py-1 text-slate-100 transition-all duration-150 disabled:text-slate-500",
         kind === "primary"
-          ? "bg-slate-700/50 border-slate-700/50 hover:bg-slate-700/80"
+          ? "bg-slate-700/50 border-slate-700/50 hover:bg-slate-700/80 disabled:hover:bg-slate-700/50"
           : kind === "text"
-          ? "text-slate-400 border-transparent hover:text-white"
-          : "bg-slate-800/20 border-slate-700/50 hover:bg-slate-800/40"
+          ? "text-slate-400 border-transparent hover:text-white disabled:hover:text-slate-40"
+          : "bg-slate-800/20 border-slate-700/50 hover:bg-slate-800/40 disabled:hover:bg-slate-800/20"
       )}
       onClick={btnAction}
+      disabled={disabled}
     >
       {label && label}
       {icon && icon}
