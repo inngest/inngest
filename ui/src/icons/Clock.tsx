85342d150e8a087c007f7bc784be81b8861ed550
Ana <anafilipadealmeida@gmail.com>
2023-07-12 17:26:10 +0100

    Add Clock icon

---
+++ ui/src/icons/Clock.tsx
@@ -0,0 +1,16 @@
+export function IconClock({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
