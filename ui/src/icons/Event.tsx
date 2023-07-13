df2855ddb92b8b5f42e374a664d5867dab95fcbf
Ana Filipa de Almeida <anafilipadealmeida@gmail.com>
2023-07-13 20:43:17 +0100

    INN-1526 Revamp function list as a table (#504)
    * Add Clock icon
    
    * Create skeleton
    
    * Create Tag component
    
    * Add Event icon
    
    * Replace list by table
    
    * Remove z-index
    
    * Add trigger button for events (#507)

---
+++ ui/src/icons/Event.tsx
@@ -0,0 +1,14 @@
+export function IconEvent({ className }: { className?: string }) {
+  return (
+    <svg className={className} viewBox="0 0 18 8" xmlns="http://www.w3.org/2000/svg">
+      <g fill="currentColor" fillRule="evenodd">
+        <path d="M2.987 1.28c.3556 0 .6967.0583 1.013.1651-.5943.732-.9467 1.6449-.9467 2.6349s.3524 1.903.946 2.6345A3.1434 3.1434 0 0 1 2.987 6.88C1.3373 6.88 0 5.6264 0 4.08c0-1.5464 1.3373-2.8 2.987-2.8Z" />
+        <circle cx="13.44" cy="4" r="4" />
+        <path
+          d="M7.9228.8c.3046 0 .599.0447.8772.128C8.0624 1.7306 7.6105 2.8112 7.6105 4s.4519 2.2694 1.1892 3.0714a3.0284 3.0284 0 0 1-.8769.1286C6.1981 7.2 4.8 5.7673 4.8 4S6.1981.8 7.9228.8Z"
+          fillRule="nonzero"
+        />
+      </g>
+    </svg>
+  );
+}
--- ui/src/views/FunctionList.tsx
+++ ui/src/views/FunctionList.tsx
@@ -1,58 +1,176 @@
-import { useGetFunctionsQuery, type Function } from "../store/generated";
-import FuncCard from "../components/Function/FuncCard";
-import { BlankSlate } from "../components/Blank";
-import { useAppDispatch } from "../store/hooks";
-import { showDocs } from "../store/global";
-import { FunctionStatus } from "../utils/statusStyles";
+import { useGetFunctionsQuery } from '@/store/generated';
+import { BlankSlate } from '@/components/Blank';
+import { useAppDispatch } from '@/store/hooks';
+import { showDocs, showEventSendModal } from '@/store/global';
+import { IconEvent, IconClock } from '@/icons';
+import Skeleton from '@/components/Skeleton';
+import Tag from '@/components/Tag';
+import classNames from '@/utils/classnames';
+import Button from '@/components/Button';
+
+const cellStyles = 'pl-6 pr-2 py-3';
+
+const HeaderCell = ({
+  children,
+  colSpan,
+}: {
+  children?: React.ReactNode;
+  colSpan: number;
+}) => {
+  return (
+    <th
+      className={classNames(
+        'w-fit whitespace-nowrap text-left text-xs font-semibold text-white',
+        cellStyles
+      )}
+      colSpan={colSpan}
+    >
+      {children}
+    </th>
+  );
+};
+
+const TableSkeleton = () => {
+  return (
+    <>
+      {[...Array(8)].map((_, index) => (
+        <tr key={index}>
+          <td className={classNames(cellStyles, 'max-h-5')} colSpan={3}>
+            <Skeleton className="block h-5 w-32" />
+          </td>
+          <td className={classNames(cellStyles, 'max-h-5')} colSpan={2}>
+            <Skeleton className="block h-5 w-32" />
+          </td>
+          <td className={classNames(cellStyles, 'max-h-5')} colSpan={3}>
+            <Skeleton className="block h-5 w-48" />
+          </td>
+          <td className={classNames(cellStyles, 'max-h-5')} colSpan={1} />
+        </tr>
+      ))}
+    </>
+  );
+};
 
 export const FunctionList = () => {
   const dispatch = useAppDispatch();
 
-  const { data, isFetching } = useGetFunctionsQuery();
+  const { data, isFetching } = useGetFunctionsQuery(undefined, {
+    refetchOnMountOrArgChange: true,
+  });
   const functions = data?.functions || [];
 
   return (
-    <div className="px-10 py-6 h-full flex flex-col overflow-y-scroll">
-      <header className="mb-8">
-        <h1 className="text-lg mb-2 text-slate-50">Functions</h1>
-        <p>This is a list of all detected functions</p>
-      </header>
-
-      <div className="flex-1">
-        {isFetching ? (
-          <>Loading...</>
-        ) : functions?.length ? (
-          <div className="flex flex-col items-center basis-[36px] gap-4 max-w-xl">
-            {functions.map((f, idx) => {
-              const triggers = f.triggers?.map((t) => t.value).join(", ");
-              const cleanUrl = new URL(f.url || "");
-              cleanUrl.search = "";
-              return (
-                <FuncCard
-                  key={f?.id || idx}
-                  title={f?.name || "Missing function name"}
-                  id={f?.id || "Invalid ID generated"}
-                  status={FunctionStatus.Registered}
-                  badge={triggers}
-                  contextualBar={
-                    <div className="text-3xs">{cleanUrl.toString()}</div>
-                  }
+    <main className="flex min-h-0 flex-col overflow-y-auto">
+      <table className="border-b border-slate-700/30 bg-slate-800/30 w-full table-fixed">
+        <thead className="sticky top-0 shadow bg-slate-950">
+          <tr>
+            <HeaderCell colSpan={3}>Function Name</HeaderCell>
+            <HeaderCell colSpan={2}>Triggers</HeaderCell>
+            <HeaderCell colSpan={3}>App URL</HeaderCell>
+            <HeaderCell colSpan={1} />
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-slate-800/30">
+          {isFetching ? (
+            <TableSkeleton />
+          ) : functions?.length === 0 ? (
+            <tr>
+              <td className="p-10" colSpan={9}>
+                <BlankSlate
+                  title="Inngest has not detected any functions"
+                  subtitle="Read our documentation to learn how to serve your functions"
+                  imageUrl="/images/no-results.png"
+                  button={{
+                    text: 'Serving Functions',
+                    onClick: () => dispatch(showDocs('/sdk/serve')),
+                  }}
                 />
-              );
-            })}
-          </div>
-        ) : (
-          <BlankSlate
-            title="Inngest has not detected any functions"
-            subtitle="Read our documentation to learn how to serve your functions"
-            imageUrl="/images/no-results.png"
-            button={{
-              text: "Serving Functions",
-              onClick: () => dispatch(showDocs("/sdk/serve")),
-            }}
-          />
-        )}
-      </div>
-    </div>
+              </td>
+            </tr>
+          ) : (
+            <>
+              {functions.map((func) => {
+                const getFirstEventValue = () => {
+                  const eventTrigger = func?.triggers?.find(
+                    (trigger) => trigger.type === 'EVENT'
+                  );
+                  return eventTrigger ? eventTrigger.value : null;
+                };
+                const cleanUrl = new URL(func.url || '');
+                cleanUrl.search = '';
+                return (
+                  <tr key={func.id}>
+                    {/* Function Name */}
+                    <td className="whitespace-nowrap" colSpan={3}>
+                      <p
+                        title={func.name}
+                        className={classNames(
+                          'pl-6 px-2 py-3 text-sm font-medium leading-7 text-ellipsis overflow-hidden',
+                          cellStyles
+                        )}
+                      >
+                        {func.name}
+                      </p>
+                    </td>
+                    {/* Triggers */}
+                    <td
+                      className={classNames(cellStyles, 'whitespace-nowrap')}
+                      colSpan={2}
+                    >
+                      {func.triggers?.map((trigger, index) => {
+                        return (
+                          <Tag key={index}>
+                            <div className="flex items-center gap-2">
+                              {trigger.type === 'EVENT' && (
+                                <IconEvent className="h-2" />
+                              )}
+                              {trigger.type === 'CRON' && (
+                                <IconClock className="h-3" />
+                              )}
+                              {trigger.value}
+                            </div>
+                          </Tag>
+                        );
+                      })}
+                    </td>
+                    {/* App URL */}
+                    <td
+                      className={classNames(cellStyles, 'whitespace-nowrap')}
+                      colSpan={3}
+                    >
+                      {cleanUrl.toString()}
+                    </td>
+                    {/* Trigger Button */}
+                    {getFirstEventValue() && (
+                      <td
+                        className={classNames(cellStyles, 'whitespace-nowrap')}
+                        colSpan={1}
+                      >
+                        <Button
+                          kind="secondary"
+                          label="Trigger"
+                          btnAction={() => {
+                            dispatch(
+                              showEventSendModal({
+                                show: true,
+                                data: JSON.stringify({
+                                  name: getFirstEventValue(),
+                                  data: {},
+                                  user: {},
+                                }),
+                              })
+                            );
+                          }}
+                        />
+                      </td>
+                    )}
+                  </tr>
+                );
+              })}
+            </>
+          )}
+        </tbody>
+      </table>
+    </main>
   );
 };
