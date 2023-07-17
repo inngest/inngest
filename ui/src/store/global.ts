4e7b45b8d94d2e918e54f4df8be5c1164a112992
Ana <anafilipadealmeida@gmail.com>
2023-07-17 19:55:46 +0100

    Clear not needed data stored

--- ui/src/store/global.ts
+++ ui/src/store/global.ts
@@ -2,51 +2,26 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState: {
-  contentView: "feed" | "functions" | "docs" | "apps";
   docsPath: string | null;
-  sidebarTab: "events" | "functions";
   selectedEvent: string | null;
   selectedRun: string | null;
-  showingSendEventModal: boolean;
-  sendEventModalData: string | null;
 } = {
-  contentView: "feed",
   docsPath: null,
-  sidebarTab: "events",
   selectedEvent: null,
   selectedRun: null,
-  showingSendEventModal: false,
-  sendEventModalData: null,
 };
 
 const globalState = createSlice({
   name: "global",
   initialState,
   reducers: {
-    setSidebarTab(
-      state,
-      action: PayloadAction<typeof initialState["sidebarTab"]>
-    ) {
-      state.sidebarTab = action.payload;
-    },
     selectEvent(state, action: PayloadAction<string | null>) {
       state.selectedEvent = action.payload;
     },
     selectRun(state, action: PayloadAction<string | null>) {
       state.selectedRun = action.payload;
     },
-    showFeed(state) {
-      state.contentView = "feed";
-    },
-    showApps(state) {
-      state.contentView = "apps";
-    },
-    showFunctions(state) {
-      state.contentView = "functions";
-    },
     showDocs(state, action: PayloadAction<`/${string}` | null | undefined>) {
-      state.contentView = "docs";
-
       if (typeof action.payload !== "undefined") {
         state.docsPath = action.payload || null;
       }
@@ -57,10 +32,6 @@ const globalState = createSlice({
 export const {
   selectEvent,
   selectRun,
-  setSidebarTab,
   showDocs,
-  showFunctions,
-  showFeed,
-  showApps,
 } = globalState.actions;
 export default globalState.reducer;
