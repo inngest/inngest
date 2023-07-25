35c6bc0905a795eb98740a2177c34b85c7756d52
Ana <anafilipadealmeida@gmail.com>
2023-07-25 16:27:31 +0100

    Add mock data

---
+++ ui/mock/triggerStream.ts
@@ -0,0 +1,56 @@
+import { FunctionRunStatus, FunctionTriggerTypes } from "../src/store/generated";
+
+export const triggerStream = [
+    {
+      id: 'id1',
+      name: 'payment.intent.created',
+      type: FunctionTriggerTypes.Event,
+      startedAt: '2023-07-17T00:08:42.725+01:00',
+      source: {
+        type: 'webhook',
+        name: 'Stripe',
+      },
+      test: true,
+      functions: [
+        {
+          id: 'function1',
+          name: 'Email: Make Payment',
+          status: FunctionRunStatus.Completed,
+        },
+        {
+          id: 'function2',
+          name: 'Discord: Send Notification',
+          status: FunctionRunStatus.Running,
+        },
+      ],
+    },
+    {
+      id: 'id2',
+      name: 'password.created',
+      type: FunctionTriggerTypes.Event,
+      startedAt: '2023-07-17T00:07:42.725+01:00',
+      source: {
+        type: 'app',
+        name: 'Dashboard',
+      },
+      functions: [],
+    },
+    {
+      id: 'id3',
+      name: 'TZ=Europe/Paris 0 12 * * 5',
+      type: FunctionTriggerTypes.Cron,
+      startedAt: '2023-07-17T00:07:42.725+01:00',
+      source: {
+        type: 'manual',
+      },
+      functions: [
+        {
+          id: 'function3',
+          name: 'Email: Changed Timezone',
+          status: FunctionRunStatus.Failed,
+        },
+      ],
+    },
+  ];
+
+  
