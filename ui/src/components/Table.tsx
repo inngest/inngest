87960bd64f5c19b3b84bc65e074c27fec21bc8d9
Ana <anafilipadealmeida@gmail.com>
2023-07-25 14:22:25 +0100

    Create Table component

---
+++ ui/src/components/Table.tsx
@@ -0,0 +1,39 @@
+import { useReactTable, flexRender } from '@tanstack/react-table';
+
+const cellStyles = 'pl-6 pr-2 py-3';
+
+export default function Table({ options }) {
+  const table = useReactTable(options);
+
+  return (
+    <table className="w-full bg-slate-950 border-b border-slate-700/30">
+      <thead className="bg-slate-900 text-left">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th className={cellStyles} key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody className="divide-y divide-slate-800/30">
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td className={cellStyles} key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
