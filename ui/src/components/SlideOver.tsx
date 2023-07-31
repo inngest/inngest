10da1c5a654c4b1086013e0df9d1bf93c9b1a89a
Ana <anafilipadealmeida@gmail.com>
2023-07-31 13:06:53 +0100

    Add SlideOver

---
+++ ui/src/components/SlideOver.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+type SlideOverProps = {
+  children?: React.ReactNode;
+  isOpen?: boolean;
+  onClose: () => void;
+};
+
+export default function SlideOver({ children, isOpen = true, onClose = () => {} }: SlideOverProps) {
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as="div"
+          enter="ease-in-out duration-500"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in-out duration-500"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            className="fixed inset-0 bg-[#04060C]/90 transition-opacity z-10"
+            aria-hidden="true"
+          />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-hidden z-10">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex w-3/4">
+              <Transition.Child
+                as="div"
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto relative w-screen h-full">
+                  <Transition.Child
+                    as="div"
+                    enter="ease-in-out duration-500"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-500"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  />
+                  <div className="flex h-full flex-col bg-slate-1000 shadow-xl w-3/4">
+                    {children}
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
