11f2e277693c3e37b1bdc13df30ed7f4645f5019
Ana <anafilipadealmeida@gmail.com>
2023-08-07 05:11:01 +0100

    Add search icon

---
+++ ui/src/icons/MagnifyingGlass.tsx
@@ -0,0 +1,18 @@
+export function IconMagnifyingGlass({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2.5"
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      />
+    </svg>
+  );
+}
