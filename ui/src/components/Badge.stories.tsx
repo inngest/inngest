e624d283159b1675ef75b539c2b8069da6045236
Ana <anafilipadealmeida@gmail.com>
2023-08-10 23:06:12 +0100

    Add Badge story

---
+++ ui/src/components/Badge.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Badge from './Badge';
+
+const meta = {
+  title: 'Components/Badge',
+  component: Badge,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    children: 'Badge',
+  },
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Outlined: Story = {
+  args: {
+    kind: 'outlined',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    kind: 'error',
+  },
+};
+
+export const Solid: Story = {
+  args: {
+    kind: 'solid',
+    className: 'text-orange-400 bg-orange-400/10',
+  },
+};
