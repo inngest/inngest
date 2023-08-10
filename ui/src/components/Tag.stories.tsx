75dba44eb8bfd1aa33f7fff39529dc2b7501a962
Ana <anafilipadealmeida@gmail.com>
2023-08-10 23:11:52 +0100

    Add Tag story

---
+++ ui/src/components/Tag.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Tag from './Tag';
+
+const meta = {
+  title: 'Components/Tag',
+  component: Tag,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: {
+    children: 'Tag',
+  },
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    className: 'text-white',
+  },
+};
+
+export const WithLink: Story = {
+  args: {
+    className: 'text-white',
+    href: new URL('http://ingest.com'),
+  },
+};
