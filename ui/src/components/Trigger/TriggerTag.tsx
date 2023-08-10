70710003a9e47cbc30ffbda5bb67907f51346e4a
Ana <anafilipadealmeida@gmail.com>
2023-08-10 23:28:08 +0100

    Add TriggerTag story

--- ui/src/components/Trigger/TriggerTag.tsx
+++ ui/src/components/Trigger/TriggerTag.tsx
@@ -10,7 +10,7 @@ type TriggerTagProps = {
 export default function TriggerTag({ value, type }: TriggerTagProps) {
   return (
     <Tag>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 text-slate-400">
         {type === FunctionTriggerTypes.Event && <IconEvent className="h-2" />}
         {type === FunctionTriggerTypes.Cron && <IconClock className="h-4" />}
         {value}
