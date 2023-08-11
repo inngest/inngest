932e4607af84856101f74567e27335573c7b7905
Ana <anafilipadealmeida@gmail.com>
2023-08-11 12:44:25 +0100

    Add Header story

---
+++ ui/src/components/Header.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Header from './Header';
+
+const meta = {
+  title: 'Layout/Header',
+  component: Header,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Header>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <p className='text-white pr-4'>This is the header's middle component</p>,
+  },
+};
