8ef3ca3b189a936e04b3888e4e8c221c29b81a1a
Ana <anafilipadealmeida@gmail.com>
2023-08-14 15:05:15 +0100

    Extract search input

---
+++ ui/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,36 @@
+import { IconMagnifyingGlass } from '@/icons';
+import classNames from '@/utils/classnames';
+
+interface SearchInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  className?: string;
+  onChange: (e) => void;
+  debouncedSearch: () => void;
+}
+
+export default function SearchInput({
+  value,
+  onChange,
+  debouncedSearch,
+  className,
+  ...props
+}: SearchInputProps) {
+  return (
+    <div
+      className={classNames('relative text-slate-400 flex items-center bg-slate-950', className)}
+    >
+      <input
+        type="text"
+        className="text-slate-100 w-96 placeholder-slate-400 py-1 pl-4 bg-slate-950"
+        placeholder={props?.placeholder ?? 'Search...'}
+        value={value ?? ''}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          onChange(e.target.value);
+          debouncedSearch();
+        }}
+        {...props}
+      />
+      <IconMagnifyingGlass className="absolute left-0 h-3 w-3 text--slate-400" />
+    </div>
+  );
+}
