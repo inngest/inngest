a9e1fdd9b26064aacb83e3dca22b88bd693f7ac4
Jack Williams <jack@inngest.com>
2023-08-18 15:51:29 +0100

    Fix CLI streaming issue and pin Windows to 0.14.6 (#575)
    * Switch to adm-zip from unzipper to fix CLI streaming
    
    * Add visible console log when pinning Windows version
    
    * Ensure Windows pin log is a warning
    
    * Add tests for installing npm CLI locally
    
    * fetch-depth: 0 for npm tests
    
    * Fix bad run statement in npm test
    
    * Fix build issues with npm testing
    
    * Add bin directory for npm installs
    
    * Use local installation for npm tests
    
    * Properly set up Node before running npm tests
    
    * Alternate installation method for npm tests
    
    * run binary directly; test only postinstall for npm
    
    * Build tweaks for PowerShell and CMD compatibility

--- npm/postinstall.ts
+++ npm/postinstall.ts
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+import AdmZip from "adm-zip";
+import Debug from "debug";
 import fetch, { Response } from "node-fetch";
 import path from "path";
 import tar from "tar";
-import unzipper from "unzipper";
 import { URL } from "url";
+
+const rootDebug = Debug("inngest:cli");
 
 /**
  * A map of Node's `process.arch` to Golang's `$GOARCH` equivalent.
@@ -42,17 +46,35 @@ const platformMap: Partial<
  * Fetches the supposed URL of the binary on GitHub for this version.
  */
 async function getBinaryUrl(): Promise<URL> {
-  const version = process.env.npm_package_version?.trim();
+  const debug = rootDebug.extend("getBinaryUrl");
+
+  const { arch, platform } = getArchPlatform();
+
+  debug({ arch, platform });
+
+  let version = process.env.npm_package_version?.trim();
+  debug("npm_package_version:", version);
+  /**
+   * Windows builds are currently not working past v0.14.6, so let's pin to that
+   * version here if we're on Windows.
+   */
+  if (platform.platform === "windows") {
+    debug("Windows detected; pinning to v0.14.6");
+    console.warn(
+      "Windows detected; pinning to last known working version 0.14.6"
+    );
+    version = "0.14.6";
+  }
 
   if (!version) {
     throw new Error("Could not find package version to install binary");
   }
 
-  const { arch, platform } = getArchPlatform();
-
   const targetUrl = new URL(
     `https://cli.inngest.com/artifact/v${version}/inngest_${version}_${platform.platform}_${arch}${platform.extension}`
   );
+
+  debug("targetUrl:", targetUrl.href);
 
   return targetUrl;
 }
@@ -64,8 +86,17 @@ async function getBinaryUrl(): Promise<URL> {
  * @throws when the arch or platform are not supported by this script.
  */
 function getArchPlatform() {
+  const debug = rootDebug.extend("getArchPlatform");
+
   const arch = archMap[process.arch];
   const platform = platformMap[process.platform];
+
+  debug({
+    arch,
+    platform,
+    "process.arch": process.arch,
+    "process.platform": process.platform,
+  });
 
   if (!arch) {
     throw new Error(`Unsupported architecture: ${process.arch}`);
@@ -73,6 +104,16 @@ function getArchPlatform() {
 
   if (!platform) {
     throw new Error(`Unsupported platform: ${process.platform}`);
+  }
+
+  /**
+   * Windows x64 builds pre v0.15 had an 'x86_64' suffix instead of 'amd64'.
+   * We are explicitly pinning Windows versions to 0.14.6 for now, so handle
+   * this problem here.
+   */
+  if (platform.platform === "windows" && arch === "amd64") {
+    debug("Old Windows x64 build; using 'x86_64' to access binary");
+    return { arch: "x86_64", platform };
   }
 
   return { arch, platform };
@@ -90,11 +131,15 @@ function downloadBinary(
    */
   url: URL
 ): Promise<Response> {
+  const debug = rootDebug.extend("downloadBinary");
+
   /**
    * Using `new Promise()` here so that we can use listeners when dealing with
    * the request in order to ensure it's valid without using the body.
    */
   return new Promise(async (resolve, reject) => {
+    debug("downloading binary from:", url.href);
+
     fetch(url.href, {
       redirect: "follow",
     })
@@ -133,12 +178,15 @@ function pipeBinaryToInstallLocation(
    */
   originalUrl: URL
 ): Promise<void> {
+  const debug = rootDebug.extend("pipeBinaryToInstallLocation");
+
   return new Promise<void>((resolve, reject) => {
     if (!res.body) {
       return reject(new Error("No body to pipe"));
     }
 
     const targetPath = path.resolve("./bin");
+    debug("targetPath:", targetPath);
 
     /**
      * A different unpacking strategy will be needed for every extension, so
@@ -149,6 +197,7 @@ function pipeBinaryToInstallLocation(
      */
     const strategies: Record<KnownExtension, () => void> = {
       [KnownExtension.Tar]: () => {
+        debug("unpacking using tar strategy");
         const untar = tar.extract({ cwd: targetPath });
 
         untar.on("error", reject);
@@ -157,12 +206,15 @@ function pipeBinaryToInstallLocation(
         res.body?.pipe(untar);
       },
       [KnownExtension.Zip]: () => {
-        const unzip = unzipper.Extract({ path: targetPath });
-
-        unzip.on("error", reject);
-        unzip.on("close", () => resolve());
-
-        res.body?.pipe(unzip);
+        debug("unpacking using zip strategy");
+        res
+          .buffer()
+          .then((buffer) => {
+            const zip = new AdmZip(buffer);
+            zip.extractAllTo(targetPath, true);
+            resolve();
+          })
+          .catch(reject);
       },
     };
 
@@ -182,10 +234,13 @@ function pipeBinaryToInstallLocation(
 }
 
 (async () => {
+  rootDebug("postinstall started");
+
   /**
    * Allow skipping this step for builds and local dev.
    */
   if (process.env.SKIP_POSTINSTALL) {
+    rootDebug("SKIP_POSTINSTALL was defined; skipping postinstall");
     process.exit(0);
   }
 
@@ -193,6 +248,7 @@ function pipeBinaryToInstallLocation(
     const binaryUrl = await getBinaryUrl();
     const req = await downloadBinary(binaryUrl);
     await pipeBinaryToInstallLocation(req, binaryUrl);
+    rootDebug("postinstall complete");
   } catch (err) {
     console.error(err);
     process.exit(1);
