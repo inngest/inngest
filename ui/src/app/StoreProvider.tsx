33bb100b58dbb37f22c11935253493f2b8ccd6ce
Ana <anafilipadealmeida@gmail.com>
2023-08-27 15:19:12 +0100

    Add react query

--- ui/src/app/StoreProvider.tsx
+++ ui/src/app/StoreProvider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 
 import { store } from '@/store/store';
@@ -9,5 +10,10 @@ type StoreProviderProps = {
 };
 
 export default function StoreProvider({ children }: StoreProviderProps) {
-  return <Provider store={store}>{children}</Provider>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+    </QueryClientProvider>
+  );
 }
