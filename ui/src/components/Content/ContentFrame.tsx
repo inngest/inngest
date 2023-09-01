677338ef05bc2599528a4a90f755b4ae196f206e
Ana <anafilipadealmeida@gmail.com>
2023-09-01 19:36:52 +0100

    Remove unused files

--- ui/src/components/Content/ContentFrame.tsx
+++
@@ -1,7 +0,0 @@
-export default function ContentFrame({ children }) {
-  return (
-    <main className="flex flex-1 overflow-hidden row-start-3 col-start-2">
-      <div className="flex gap-3 p-3 w-full min-w-0">{children}</div>
-    </main>
-  );
-}
