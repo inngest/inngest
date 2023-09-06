e145c8ff5ad8db6096f16bf0923300440110af83
Ana <anafilipadealmeida@gmail.com>
2023-09-06 03:37:46 +0100

    Add tooltip to metadata

--- ui/src/components/Metadata/MetadataItem.tsx
+++ ui/src/components/Metadata/MetadataItem.tsx
@@ -1,3 +1,5 @@
+import Tooltip from '@/components/Tooltip/Tooltip';
+import { IconInfo } from '@/icons';
 import classNames from '@/utils/classnames';
 
 export type MetadataItemProps = {
@@ -11,7 +13,12 @@ export default function MetadataItem({ value, label, tooltip }: MetadataItemProp
   return (
     <div className={classNames('flex flex-col p-1.5 bg-slate-950')}>
       <span className="text-white">{value}</span>
-      <span className="text-slate-500 capitalize">{label}</span>
+      <span className="flex items-center gap-1">
+        <span className="text-slate-500 capitalize">{label}</span>
+        {tooltip && (
+          <Tooltip children={<IconInfo className="text-slate-400 icon-lg" />} content={tooltip} />
+        )}
+      </span>
     </div>
   );
 }
