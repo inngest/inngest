26f72bea1d00e3c247110d192fd475c82f4500da
Ana <anafilipadealmeida@gmail.com>
2023-09-26 10:50:07 +0100

    Combine parser into one file

--- ui/src/components/TimelineV2/TimelineNode/TimelineNode.tsx
+++ ui/src/components/TimelineV2/TimelineNode/TimelineNode.tsx
@@ -1,37 +1,18 @@
 import TimelineItemHeader from '@/components/AccordionTimeline/TimelineItemHeader';
 import { type HistoryNode } from '../historyParser/index';
-import { renderMetadata } from './Metadata';
-import { renderIcon } from './Icon';
-import { renderName } from './Name';
+import renderTimelineNode from './TimelineNodeRenderer';
 
 type Props = {
   node: HistoryNode;
 };
 
-// type StepKind = 'sleep' | 'waitForEvent';
-
 export function TimelineNode({ node }: Props) {
-  // let durationMS: number | undefined = undefined;
-  // if (node.scope === 'step' && node.startedAt && node.endedAt) {
-  //   durationMS = node.endedAt.getTime() - node.startedAt.getTime();
-  // }
-
-  let stepKind: string | undefined;
-  if (node.scope === 'function') {
-    stepKind = 'Function';
-  } else if (node.sleepConfig) {
-    stepKind = 'Sleep';
-  } else if (node.waitForEventConfig) {
-    stepKind = 'Wait For Event';
-  }
-  const name = renderName({ node: node });
-  const metadata = renderMetadata({ node: node });
-  const icon = renderIcon({status: node.status})
+  const { icon, badge, name, metadata } = renderTimelineNode(node);
 
   return (
     <TimelineItemHeader
       icon={icon}
-      badge={stepKind}
+      badge={badge}
       title={name}
       metadata={metadata}
     />
