a62667377d843fc6a464a7b1b1542acf93e4d5cf
Ana <anafilipadealmeida@gmail.com>
2023-09-30 00:13:28 +0100

    Add extra run status

--- ui/src/utils/constants.ts
+++ ui/src/utils/constants.ts
@@ -1,1 +1,5 @@
 export const maxRenderedOutputSizeBytes = 1024 * 1024; // This prevents larger outputs from crashing the browser.
+export enum FunctionRunExtraStatus {
+  WaitingFor = 'WAITINGFOR',
+  Sleeping = 'SLEEPING',
+}
