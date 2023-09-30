1887abcfdb1d8c0f2590b1050ea7eb1e5e76032f
Ana <anafilipadealmeida@gmail.com>
2023-10-01 00:45:35 +0100

    Adjust stream output

--- ui/src/app/(dashboard)/stream/OutputList.tsx
+++ ui/src/app/(dashboard)/stream/OutputList.tsx
@@ -1,44 +1,57 @@
-  import {
-    useGetFunctionRunOutputQuery,
-    type FunctionRun,
-  } from '@/store/generated';
-  
-  export default function OutputList({ functionRuns }) {
-    return (
-      <>
-        {!functionRuns || functionRuns.length < 1 ? (
-          <p className="text-slate-600" />
-        ) : (
+import { useGetFunctionRunOutputQuery, type FunctionRun } from '@/store/generated';
+import { maxRenderedOutputSizeBytes } from '@/utils/constants';
 
-          <ul className="flex flex-col space-y-4">
-            {functionRuns &&
-              functionRuns.map((functionRun) => {
-                return <OutputItem functionRunID={functionRun.id} />;
-              })}
-          </ul>
+export default function OutputList({ functionRuns }) {
+  return (
+    <>
+      {!functionRuns || functionRuns.length < 1 ? (
+        <p className="text-slate-600" />
+      ) : (
+        <ul className="flex flex-col space-y-4">
+          {functionRuns &&
+            functionRuns.map((functionRun) => {
+              return <OutputItem functionRunID={functionRun.id} />;
+            })}
+        </ul>
+      )}
+    </>
+  );
+}
 
-        )}
-      </>
-    );
+type FunctionRunStatusSubset = Pick<FunctionRun, 'id' | 'output'>;
+
+export function OutputItem({ functionRunID }) {
+  const { data } = useGetFunctionRunOutputQuery({ id: functionRunID }, { pollingInterval: 1500 });
+  const functionRun = (data?.functionRun as FunctionRunStatusSubset) || {};
+
+  if (!functionRun || !functionRun?.output) {
+    return null;
   }
-  
-  type FunctionRunStatusSubset = Pick<FunctionRun, 'id' | 'output'>;
-  
-  export function OutputItem({ functionRunID }) {
-    const { data } = useGetFunctionRunOutputQuery({ id: functionRunID }, { pollingInterval: 1500 });
-    const functionRun = (data?.functionRun as FunctionRunStatusSubset) || {};
-  
-    if (!functionRun || !functionRun?.output) {
-      return null;
+
+  let message = '';
+  let errorName = '';
+  const isOutputTooLarge = functionRun.output?.length > maxRenderedOutputSizeBytes;
+  if (functionRun.output && !isOutputTooLarge) {
+    const parsedOutput = JSON.parse(functionRun.output);
+
+    if (parsedOutput.body && typeof parsedOutput.body === 'object') {
+      message = parsedOutput.body?.message;
+      errorName = parsedOutput.body?.name;
+    } else if (parsedOutput.body && typeof parsedOutput.body === 'string') {
+      const parsedBody = JSON.parse(parsedOutput.body);
+      message = parsedBody?.message;
+      errorName = parsedBody?.name;
     }
-    const parsedOutput = JSON.parse(functionRun.output)
-    const wasSuccessful = parsedOutput.body?.success || (parsedOutput.status.toString().startsWith('2'));
-  
-    return (
-      <li key={functionRun?.id} data-key={functionRun?.id} className="flex gap-2 font-mono">
-        <span className={wasSuccessful ? 'text-teal-300' : 'text-rose-500'}>{parsedOutput.status}</span>
-        <span className='text-xs'>{parsedOutput.body?.message}</span>
-      </li>
-    );
   }
-  
+
+  return (
+    <li
+      key={functionRun?.id}
+      data-key={functionRun?.id}
+      className="flex items-center gap-2 font-mono"
+    >
+      {errorName && <span className={'font-bold text-rose-500'}>{errorName}</span>}
+      <span className="text-xs truncate">{message}</span>
+    </li>
+  );
+}
