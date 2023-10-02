7b27a234f843d57af2c11e3a8f1d3a51ac7d8ab1
Aaron Harper <aaron@inngest.com>
2023-10-02 16:47:21 -0400

    Stop assuming output is map

--- pkg/execution/state/driver_response.go
+++ pkg/execution/state/driver_response.go
@@ -261,37 +261,31 @@ func (r DriverResponse) Retryable() bool {
 		return false
 	}
 
-	// Convert output into a map to check whether this responds with our
-	// suggested JSON response
-	mapped, ok := r.Output.(map[string]any)
-	if !ok {
-		// This doesn't contain the response, so default to retrying.
-		return true
-	}
-
 	status := r.StatusCode
 	if status == 0 {
-		// Fall back to statusCode for AWS Lambda compatibility in
-		// an attempt to use this field.
-		v, ok := mapped["statusCode"]
-		if !ok {
-			// If actions don't return a status, we assume that they're
-			// always retryable.
-			return true
-		}
+		if mapped, ok := r.Output.(map[string]any); ok {
+			// Fall back to statusCode for AWS Lambda compatibility in
+			// an attempt to use this field.
+			v, ok := mapped["statusCode"]
+			if !ok {
+				// If actions don't return a status, we assume that they're
+				// always retryable.
+				return true
+			}
 
-		switch val := v.(type) {
-		case float64:
-			status = int(val)
-		case int64:
-			status = int(val)
-		case int:
-			status = val
-		default:
-			slog.Default().Error(
-				"unexpected status code type",
-				"type", fmt.Sprintf("%T", v),
-			)
+			switch val := v.(type) {
+			case float64:
+				status = int(val)
+			case int64:
+				status = int(val)
+			case int:
+				status = val
+			default:
+				slog.Default().Error(
+					"unexpected status code type",
+					"type", fmt.Sprintf("%T", v),
+				)
+			}
 		}
 	}
 
