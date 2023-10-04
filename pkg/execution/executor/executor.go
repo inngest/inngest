451df723719f01ed4adc463621e75a6807df79c6
Aaron Harper <aaron@inngest.com>
2023-10-04 11:27:06 -0400

    Only create new group ID on parallel steps

--- pkg/execution/executor/executor.go
+++ pkg/execution/executor/executor.go
@@ -875,14 +875,17 @@ func (e *executor) HandleGeneratorResponse(ctx context.Context, resp *state.Driv
 	// Ensure that we process waitForEvents first, as these are highest priority.
 	sortOps(resp.Generator)
 
+	isParallel := len(resp.Generator) > 1
 	eg := errgroup.Group{}
 	for _, op := range resp.Generator {
 		copied := *op
 
-		// Give each op its own group ID, as we want to track each step
-		// individually.
 		newItem := item
-		newItem.GroupID = uuid.New().String()
+		if isParallel {
+			// Give each opcode its own group ID, since we want to track each
+			// parellel step individually.
+			newItem.GroupID = uuid.New().String()
+		}
 
 		eg.Go(func() error { return e.HandleGenerator(ctx, copied, newItem) })
 	}
