fe4ee088ae8927b795d94b66fbcb23d021752887
Darwin <5746693+darwin67@users.noreply.github.com>
2023-10-06 16:44:25 -0700

    feedback: use time.Duration instead of string for granularity (#676)
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

--- pkg/usage/metrics_test.go
+++ pkg/usage/metrics_test.go
@@ -56,52 +56,52 @@ func TestMetricsRequestGranularity(t *testing.T) {
 	tests := []struct {
 		name     string
 		req      *MetricsRequest
-		expected string
+		expected time.Duration
 	}{
 		{
-			name:     "empty req defaults to 5m",
+			name:     "empty req defaults to 1m",
 			req:      &MetricsRequest{},
-			expected: "5m",
+			expected: time.Minute,
 		},
 		{
-			name:     ">= 1h range returns 5m granularity",
-			req:      &MetricsRequest{From: time.Now().Add(-2 * time.Hour), To: time.Now()},
-			expected: "5m",
+			name:     ">= 3h range returns 5m granularity",
+			req:      &MetricsRequest{From: time.Now().Add(-4 * time.Hour), To: time.Now()},
+			expected: 5 * time.Minute,
 		},
 		{
 			name:     ">= 6h range returns 10m granularity",
 			req:      &MetricsRequest{From: time.Now().Add(-7 * time.Hour), To: time.Now()},
-			expected: "10m",
+			expected: 10 * time.Minute,
 		},
 		{
 			name:     ">= 12h range returns 15m granularity",
 			req:      &MetricsRequest{From: time.Now().Add(-12 * time.Hour), To: time.Now()},
-			expected: "15m",
+			expected: 15 * time.Minute,
 		},
 		{
 			name:     ">= 1d range returns 30m granularity",
 			req:      &MetricsRequest{From: time.Now().Add(-24 * time.Hour), To: time.Now()},
-			expected: "30m",
+			expected: 30 * time.Minute,
 		},
 		{
 			name:     ">= 3d range returns 1h granularity",
 			req:      &MetricsRequest{From: time.Now().Add(4 * -24 * time.Hour), To: time.Now()},
-			expected: "1h",
+			expected: time.Hour,
 		},
 		{
 			name:     ">= 7d returns 3h granularity",
 			req:      &MetricsRequest{From: time.Now().Add(8 * -24 * time.Hour), To: time.Now()},
-			expected: "3h",
+			expected: 3 * time.Hour,
 		},
 		{
 			name:     ">= 14d range returns 6h granularity",
 			req:      &MetricsRequest{From: time.Now().Add(15 * -24 * time.Hour), To: time.Now()},
-			expected: "6h",
+			expected: 6 * time.Hour,
 		},
 		{
 			name:     ">= 30d range returns 12h granularity",
 			req:      &MetricsRequest{From: time.Now().Add(31 * -24 * time.Hour), To: time.Now()},
-			expected: "12h",
+			expected: 12 * time.Hour,
 		},
 	}
 
