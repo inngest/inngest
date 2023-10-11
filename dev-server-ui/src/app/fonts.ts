38e346b2ce9ce2b137125ff488bd7018a34de254
Igor Gassmann <igor@inngest.com>
2023-10-11 13:37:01 +0200

    refactor: remove unneeded display config
    The default display config is already "swap": https://nextjs.org/docs/app/api-reference/components/font#display

--- dev-server-ui/src/app/fonts.ts
+++ dev-server-ui/src/app/fonts.ts
@@ -2,12 +2,10 @@ import { Inter, Roboto_Mono } from 'next/font/google';
 
 export const inter = Inter({
   subsets: ['latin'],
-  display: 'swap',
   variable: '--font-inter',
 });
 
 export const roboto_mono = Roboto_Mono({
   subsets: ['latin'],
-  display: 'swap',
   variable: '--font-roboto-mono',
 });
