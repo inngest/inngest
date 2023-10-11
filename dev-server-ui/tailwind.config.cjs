7612bc622cdaa405f46b412f4318426b55837944
Igor Gassmann <igor@inngest.com>
2023-10-11 11:50:39 +0200

    config(tailwind): align content lookup

--- dev-server-ui/tailwind.config.cjs
+++ dev-server-ui/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{ts,tsx}'],
   safelist: [
     'text-white',
     'text-slate-100',
