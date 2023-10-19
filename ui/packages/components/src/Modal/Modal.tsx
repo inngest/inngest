60a52f7b36efe856596930ccd476a6d5a8af5bab
Ana <anafilipadealmeida@gmail.com>
2023-10-19 15:34:37 +0100

    Remove unnecessary parent layer

--- ui/packages/components/src/Modal/Modal.tsx
+++ ui/packages/components/src/Modal/Modal.tsx
@@ -33,45 +33,44 @@ export function Modal({
           </Dialog.Overlay>
           {/* Full-screen container to center the panel */}
           <div className="fixed inset-0 z-50 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-6">
-              <motion.div
-                initial={{ y: -20, opacity: 0.2 }}
-                animate={{ y: 0, opacity: 1 }}
-                exit={{
-                  y: -20,
-                  opacity: 0.2,
-                  transition: { duration: 0.2, type: 'tween' },
-                }}
-                transition={{
-                  duration: 0.15,
-                  type: 'tween',
-                }}
+            <motion.div
+              className="flex min-h-full w-full items-center justify-center p-6"
+              initial={{ y: -20, opacity: 0.2 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{
+                y: -20,
+                opacity: 0.2,
+                transition: { duration: 0.2, type: 'tween' },
+              }}
+              transition={{
+                duration: 0.15,
+                type: 'tween',
+              }}
+            >
+              <Dialog.Content
+                className={classNames(
+                  className,
+                  'dark:bg-slate-910 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all'
+                )}
               >
-                <Dialog.Content
-                  className={classNames(
-                    className,
-                    'dark:bg-slate-910 transform overflow-hidden rounded-lg bg-white shadow-xl transition-all'
-                  )}
-                >
-                  {(title || description) && (
-                    <div className="dark:bg-slate-910 border-b border-slate-200 bg-slate-900 p-6 dark:border-slate-800">
-                      <Dialog.Title className="text-xl font-semibold text-white">
-                        {title}
-                      </Dialog.Title>
-                      <Dialog.Description className="text-sm font-medium text-white dark:text-slate-400">
-                        {description}
-                      </Dialog.Description>
-                    </div>
-                  )}
-                  {children}
-                  {footer && (
-                    <div className="border-t border-slate-200 p-6 dark:border-slate-800">
-                      {footer}
-                    </div>
-                  )}
-                </Dialog.Content>
-              </motion.div>
-            </div>
+                {(title || description) && (
+                  <div className="dark:bg-slate-910 border-b border-slate-200 bg-slate-900 p-6 dark:border-slate-800">
+                    <Dialog.Title className="text-xl font-semibold text-white">
+                      {title}
+                    </Dialog.Title>
+                    <Dialog.Description className="text-sm font-medium text-white dark:text-slate-400">
+                      {description}
+                    </Dialog.Description>
+                  </div>
+                )}
+                {children}
+                {footer && (
+                  <div className="border-t border-slate-200 p-6 dark:border-slate-800">
+                    {footer}
+                  </div>
+                )}
+              </Dialog.Content>
+            </motion.div>
           </div>
         </Dialog.Portal>
       </AnimatePresence>
