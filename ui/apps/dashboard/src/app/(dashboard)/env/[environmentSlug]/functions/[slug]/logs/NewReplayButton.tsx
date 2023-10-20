7818259cb045c4c7d7a3f6557de13eeb112b9fc9
Igor Gassmann <igor@inngest.com>
2023-10-20 17:09:19 +0200

    feat(dashboard): function replay modal (#763)

---
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/NewReplayButton.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Button } from '@inngest/components/Button';
+import { IconReplay } from '@inngest/components/icons/Replay';
+
+import NewReplayModal from '@/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/NewReplayModal';
+
+type NewReplayButtonProps = {
+  environmentSlug: string;
+  functionSlug?: string;
+};
+
+export default function NewReplayButton({ environmentSlug, functionSlug }: NewReplayButtonProps) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  return (
+    <>
+      <Button
+        label="New Replay"
+        kind="primary"
+        btnAction={() => setIsModalVisible(true)}
+        icon={<IconReplay className="h-5 w-5 text-white" />}
+      />
+      <NewReplayModal
+        isOpen={isModalVisible}
+        environmentSlug={environmentSlug}
+        functionSlug={functionSlug}
+        onClose={() => setIsModalVisible(false)}
+      />
+    </>
+  );
+}
