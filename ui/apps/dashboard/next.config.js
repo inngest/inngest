b225d1e731b3604f74c87e90b00761f0a14b6861
Igor Gassmann <igor@inngest.com>
2023-11-06 15:01:52 +0100

    chore: remove deprecated experimental.appDir config

--- ui/apps/dashboard/next.config.js
+++ ui/apps/dashboard/next.config.js
@@ -4,7 +4,6 @@ const { withSentryConfig } = require('@sentry/nextjs');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     typedRoutes: true,
   },
   images: {
