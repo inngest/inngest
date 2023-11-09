ce0add01f290724063142f25e9efebf914b5f05f
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2023-11-09 15:49:12 +0000

    dashboard: Query for invocation details in history items

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/[runId]/layout.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/[runId]/layout.tsx
@@ -51,6 +51,17 @@ const GetFunctionRunDetailsDocument = graphql(`
               eventID
               timeout
             }
+            invokeFunction {
+              eventID
+              functionID
+              correlationID
+              timeout
+            }
+            invokeFunctionResult {
+              eventID
+              timeout
+              runID
+            }
           }
           id
           status
