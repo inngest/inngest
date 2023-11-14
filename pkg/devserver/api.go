6bbbf983c74afaa27a4388de37da56a9d5b8beb8
Jack Williams <jack@inngest.com>
2023-11-14 15:39:35 +0000

    Don't return empty if lock failed when fetching `/dev` from Dev Server (#840)

--- pkg/devserver/api.go
+++ pkg/devserver/api.go
@@ -104,9 +104,7 @@ func (a devapi) UI(w http.ResponseWriter, r *http.Request) {
 
 // Info returns information about the dev server and its registered functions.
 func (a devapi) Info(w http.ResponseWriter, r *http.Request) {
-	if !a.devserver.handlerLock.TryLock() {
-		return
-	}
+	a.devserver.handlerLock.Lock()
 	defer a.devserver.handlerLock.Unlock()
 
 	all, _ := a.devserver.data.GetFunctions(r.Context())
