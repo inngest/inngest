156e31ec8bc9a4968a738c3007ff70ca3450627c
Ana <anafilipadealmeida@gmail.com>
2023-11-21 15:34:26 +0000

    Linting

--- ui/packages/components/src/Link/Link.tsx
+++ ui/packages/components/src/Link/Link.tsx
@@ -1,20 +1,27 @@
+import type { UrlObject } from 'url';
+import type { Route } from 'next';
 import NextLink from 'next/link';
 import { IconArrowRight } from '@inngest/components/icons/ArrowRight';
 import { IconArrowTopRightOnSquare } from '@inngest/components/icons/ArrowTopRightOnSquare';
 import { classNames } from '@inngest/components/utils/classNames';
 
-interface LinkProps {
+type LinkProps<PassedHref extends string> = {
   internalNavigation?: boolean;
   children: React.ReactNode;
   className?: string;
-  href: URL | string;
-}
+  href?: Route<PassedHref> | UrlObject;
+};
 
 const defaultLinkStyles =
   'group text-indigo-400 hover:decoration-indigo-400 decoration-transparent decoration-2 underline underline-offset-4 cursor-pointer transition-color duration-300 flex items-center gap-1';
 
-export function Link({ href, children, className, internalNavigation = false }: LinkProps) {
-  if (internalNavigation) {
+export function Link<PassedHref extends string>({
+  href,
+  children,
+  className,
+  internalNavigation = false,
+}: LinkProps<PassedHref>) {
+  if (internalNavigation && href) {
     return (
       <NextLink href={href} className={classNames(className, defaultLinkStyles)}>
         {children}
@@ -23,7 +30,12 @@ export function Link({ href, children, className, internalNavigation = false }: 
     );
   } else if (typeof href === 'string') {
     return (
-      <a className={classNames(className, defaultLinkStyles)} target="_blank" href={href}>
+      <a
+        className={classNames(className, defaultLinkStyles)}
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href}
+      >
         {children}
         {<IconArrowTopRightOnSquare />}
       </a>
