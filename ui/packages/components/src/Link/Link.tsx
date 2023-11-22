cc4382d989ccedaae5f43f56c84f952ec5769724
Ana <anafilipadealmeida@gmail.com>
2023-11-22 18:09:06 +0000

    Adjust type

--- ui/packages/components/src/Link/Link.tsx
+++ ui/packages/components/src/Link/Link.tsx
@@ -5,27 +5,22 @@ import { IconArrowRight } from '@inngest/components/icons/ArrowRight';
 import { IconArrowTopRightOnSquare } from '@inngest/components/icons/ArrowTopRightOnSquare';
 import { classNames } from '@inngest/components/utils/classNames';
 
-type LinkProps<PassedHref extends string> = {
+type LinkProps = {
   internalNavigation?: boolean;
   children: React.ReactNode;
   className?: string;
-  href?: Route<PassedHref> | UrlObject;
+  href?: Route | UrlObject;
 };
 
 const defaultLinkStyles =
   'group text-indigo-400 hover:decoration-indigo-400 decoration-transparent decoration-2 underline underline-offset-4 cursor-pointer transition-color duration-300 flex items-center gap-1';
 
-export function Link<PassedHref extends string>({
-  href,
-  children,
-  className,
-  internalNavigation = false,
-}: LinkProps<PassedHref>) {
+export function Link({ href, children, className, internalNavigation = false }: LinkProps) {
   if (internalNavigation && href) {
     return (
       <NextLink href={href} className={classNames(className, defaultLinkStyles)}>
         {children}
-        <IconArrowRight className="h-3 w-3 -translate-x-3 text-indigo-600 opacity-0 transition-all group-hover:translate-x-0 group-hover:opacity-100" />
+        <IconArrowRight className="h-3 w-3 -translate-x-3 text-indigo-400 opacity-0 transition-all group-hover:translate-x-0 group-hover:opacity-100" />
       </NextLink>
     );
   } else if (typeof href === 'string') {
