1b0643508da5205da86c722e36322402c1661073
Ana Filipa de Almeida <anafilipadealmeida@gmail.com>
2023-11-22 21:14:46 +0000

    Components for run list (#866)
    * Add text size to table
    
    * Adjust Link styles
    
    * Add validation
    
    * Linting
    
    * Add radix slideover to shared package
    
    * Render SlideOver
    
    * Adjust type
    
    * Make href mandatory

---
+++ ui/packages/components/src/icons/ArrowRight.tsx
@@ -0,0 +1,19 @@
+export function IconArrowRight({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+      height="16"
+      width="16"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+      />
+    </svg>
+  );
+}
