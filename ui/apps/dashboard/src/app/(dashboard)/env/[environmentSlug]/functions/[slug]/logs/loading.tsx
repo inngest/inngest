2fb229217f3c24be47dbffb6041c26ff5656d1bd
Ana <anafilipadealmeida@gmail.com>
2023-11-30 03:11:18 +0000

    Remove layout file

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingIcon from '@/icons/LoadingIcon';
-
-export default function FunctionRunsLoading() {
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <LoadingIcon />
-    </div>
-  );
-}
