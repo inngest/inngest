fc904a452908608d1092822af8ecf2b97dd7de95
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-12-02 14:00:37 -0500

    Use client-side queries to decouple requests
    This creates a better experience if a billing component fails with an error. The user should be able to see the error and it will be easier to debug in production. Additionally, this decouples the billable usage charge requests from the rest of the page. The billable usage can take a long time to load or it may fail in some instances which would previously cause the entire page to not load.

--- ui/apps/dashboard/src/app/(dashboard)/settings/billing/Payments.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/settings/billing/Payments.tsx
@@ -1,6 +1,11 @@
+'use client';
+
+import type React from 'react';
+import { useQuery } from 'urql';
+
+import Placeholder from '@/components/Placeholder';
 import Table from '@/components/Table';
 import { graphql } from '@/gql';
-import graphqlAPI from '@/queries/graphqlAPI';
 import { day } from '@/utils/date';
 import PaymentIcon from './PaymentIcons';
 
@@ -18,30 +23,58 @@ const GetPaymentIntentsDocument = graphql(`
   }
 `);
 
-export default async function Invoices() {
-  const response = await graphqlAPI.request(GetPaymentIntentsDocument);
-  const payments = response.account.paymentIntents;
+type TableRow = {
+  status: React.ReactNode;
+  description: React.ReactNode | string;
+  createdAt: React.ReactNode | string;
+  amount: React.ReactNode;
+  url: React.ReactNode;
+};
 
-  const paymentTableData = payments.map((payment) => ({
-    status: <PaymentIcon status={payment.status} />,
-    description: payment.description,
-    createdAt: day(payment.createdAt),
-    amount:
-      payment.status === 'canceled' ? (
-        <span className="text-slate-400">{payment.amountLabel}</span>
-      ) : (
-        payment.amountLabel
-      ),
-    url: payment.invoiceURL ? (
-      <a
-        href={payment.invoiceURL}
-        target="_blank"
-        className="font-semibold text-indigo-500 hover:text-indigo-800 hover:underline"
-      >
-        View
-      </a>
-    ) : null,
-  }));
+const loadingPlaceholder = (
+  <div className="flex">
+    <Placeholder className="mx-1 my-1 h-2 w-full max-w-[120px] bg-slate-200" />
+  </div>
+);
+
+const loadingRows: TableRow[] = [1, 2, 3].map(() => ({
+  status: <></>,
+  description: loadingPlaceholder,
+  createdAt: loadingPlaceholder,
+  amount: loadingPlaceholder,
+  url: loadingPlaceholder,
+}));
+
+export default function Payments() {
+  const [{ data, fetching }] = useQuery({
+    query: GetPaymentIntentsDocument,
+  });
+  const payments = data?.account.paymentIntents || [];
+
+  const paymentTableData = fetching
+    ? loadingRows
+    : payments.map(
+        (payment): TableRow => ({
+          status: <PaymentIcon status={payment.status} />,
+          description: payment.description,
+          createdAt: day(payment.createdAt),
+          amount:
+            payment.status === 'canceled' ? (
+              <span className="text-slate-400">{payment.amountLabel}</span>
+            ) : (
+              payment.amountLabel
+            ),
+          url: payment.invoiceURL ? (
+            <a
+              href={payment.invoiceURL}
+              target="_blank"
+              className="font-semibold text-indigo-500 hover:text-indigo-800 hover:underline"
+            >
+              View
+            </a>
+          ) : null,
+        })
+      );
 
   return (
     <Table
