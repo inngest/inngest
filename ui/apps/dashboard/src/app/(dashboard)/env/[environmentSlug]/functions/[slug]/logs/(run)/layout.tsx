6e964a0da58fba442a76011354a4148c4790d885
Ana <anafilipadealmeida@gmail.com>
2023-12-03 15:04:27 +0000

    Add SlideOver onClose

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/(run)/layout.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/(run)/layout.tsx
@@ -1,9 +1,32 @@
+'use client';
+
+import { type Route } from 'next';
+import { useRouter } from 'next/navigation';
 import { SlideOver } from '@inngest/components/SlideOver';
 
 type RunLayoutProps = {
   children: React.ReactNode;
+  params: {
+    environmentSlug: string;
+    slug: string;
+  };
 };
 
-export default function RunLayout({ children }: RunLayoutProps) {
-  return <SlideOver size="large">{children}</SlideOver>;
+export default function RunLayout({ children, params }: RunLayoutProps) {
+  const router = useRouter();
+
+  return (
+    <SlideOver
+      size="large"
+      onClose={() =>
+        router.push(
+          `/env/${params.environmentSlug}/functions/${encodeURIComponent(
+            params.slug
+          )}/logs` as Route
+        )
+      }
+    >
+      {children}
+    </SlideOver>
+  );
 }
