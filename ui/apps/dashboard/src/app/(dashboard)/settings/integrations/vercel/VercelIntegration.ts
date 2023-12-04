08c8f36f8c95d2f8512d8274d3ab062fc21efc39
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-12-04 15:13:01 -0500

    Display warning when deployment protection is enabled

--- ui/apps/dashboard/src/app/(dashboard)/settings/integrations/vercel/VercelIntegration.ts
+++ ui/apps/dashboard/src/app/(dashboard)/settings/integrations/vercel/VercelIntegration.ts
@@ -1,11 +1,32 @@
-type VercelProject = {
+export enum VercelDeploymentProtection {
+  Disabled = '',
+  ProdDeploymentURLsAndAllPreviews = 'prod_deployment_urls_and_all_previews',
+  Previews = 'preview',
+}
+
+export type VercelProject = {
   id: string;
   name: string;
   servePath?: string;
   isEnabled: boolean;
+  ssoProtection?: {
+    deploymentType: VercelDeploymentProtection;
+  };
 };
 
-type VercelIntegration = {
+export type VercelProjectViaAPI = {
+  id: string;
+  name: string;
+  ssoProtection?: {
+    deploymentType: VercelDeploymentProtection;
+  };
+};
+
+export type VercelProjectAPIResponse = {
+  projects: VercelProjectViaAPI[];
+};
+
+export type VercelIntegration = {
   id: string;
   name: string;
   slug: string;
--- ui/apps/dashboard/src/app/(dashboard)/settings/integrations/vercel/getVercelIntegration.ts
+++ ui/apps/dashboard/src/app/(dashboard)/settings/integrations/vercel/getVercelIntegration.ts
@@ -1,11 +1,7 @@
 import restAPI, { HTTPError } from '@/queries/restAPI';
 import { getEnvironment } from '@/queries/server-only/getEnvironment';
-import type VercelIntegration from './VercelIntegration';
+import { type VercelIntegration, type VercelProjectAPIResponse } from './VercelIntegration';
 import enrichVercelProjects from './enrichVercelProjects';
-
-type VercelProjectsAPIResponse = {
-  projects: { id: string; name: string }[];
-};
 
 export default async function getVercelIntegration(): Promise<VercelIntegration> {
   const environment = await getEnvironment({
@@ -15,7 +11,7 @@ export default async function getVercelIntegration(): Promise<VercelIntegration>
   const url = new URL('/v1/integrations/vercel/projects', process.env.NEXT_PUBLIC_API_URL);
   url.searchParams.set('workspaceID', environment.id);
 
-  let response: VercelProjectsAPIResponse;
+  let response: VercelProjectAPIResponse;
   try {
     response = await restAPI(url).json<{
       projects: { id: string; name: string }[];
