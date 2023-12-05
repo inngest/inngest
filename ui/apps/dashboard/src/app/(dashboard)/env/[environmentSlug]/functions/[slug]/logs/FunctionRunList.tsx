7d1565315b6f22cfac02492b583ef1365466d7cf
Ana Filipa de Almeida <anafilipadealmeida@gmail.com>
2023-12-05 14:36:59 +0000

    Fix loading glitch when runs take longer to load (#910)
    * Add loading condition
    
    * Rename functionRuns

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/FunctionRunList.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/FunctionRunList.tsx
@@ -130,7 +130,7 @@ export default function FunctionRunList({
   }, [environmentSlug, functionSlug]);
 
   const [pageCursors, setPageCursors] = useState<string[]>(['']);
-  const [functionRuns, setFunctionRuns] = useState<RunListItem[]>([]);
+  const [aggregatedFunctionRuns, setAggregatedFunctionRuns] = useState<RunListItem[]>([]);
 
   const tableContainerRef = useRef<HTMLDivElement>(null);
   // We reset the page cursors when the selected statuses or time range change, which resets the list to the first page.
@@ -159,7 +159,8 @@ export default function FunctionRunList({
   const runs = data?.environment?.function?.runs?.edges?.map((edge) => edge?.node) ?? [];
   const endCursor = data?.environment?.function?.runs?.pageInfo.endCursor;
   const hasNextPage = data?.environment?.function?.runs?.pageInfo.hasNextPage;
-  const isLoading = isFetchingEnvironments || fetching;
+  const isLoading =
+    isFetchingEnvironments || fetching || (runs.length > 0 && aggregatedFunctionRuns.length === 0);
 
   useEffect(() => {
     if (
@@ -171,9 +172,9 @@ export default function FunctionRunList({
       setPrevSelectedTimeRange(selectedTimeRange);
       setPrevSelectedTimeField(timeField);
       setPageCursors(['']);
-      setFunctionRuns([]);
+      setAggregatedFunctionRuns([]);
     } else {
-      setFunctionRuns((prevFunctionRuns) => {
+      setAggregatedFunctionRuns((prevFunctionRuns) => {
         const updatedFunctionRuns = prevFunctionRuns.map((prevRun) => {
           const matchingRun = runs.find((run) => run?.id === prevRun.id);
           return matchingRun ?? prevRun;
@@ -190,7 +191,7 @@ export default function FunctionRunList({
     <div className="min-h-0 w-full overflow-y-auto pb-10" ref={tableContainerRef}>
       <Table
         options={{
-          data: functionRuns ?? [],
+          data: aggregatedFunctionRuns ?? [],
           columns,
           getCoreRowModel: getCoreRowModel(),
           enableSorting: false,
@@ -210,7 +211,7 @@ export default function FunctionRunList({
         tableContainerRef={tableContainerRef}
         blankState={isLoading ? <p>Loading...</p> : <p>No function runs</p>}
       />
-      {hasNextPage && (
+      {hasNextPage && aggregatedFunctionRuns.length > 0 && (
         <div className="flex justify-center pt-4">
           <Button
             label="Load More"
