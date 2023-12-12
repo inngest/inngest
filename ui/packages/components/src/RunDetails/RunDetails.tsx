6b9904b1b71f1e19252f95ba3b2beef09a32f97c
Ana Filipa de Almeida <anafilipadealmeida@gmail.com>
2023-12-12 21:00:18 +0000

    Display output on failed runs (#932)
    * Display output on failed runs
    
    * Simplify logic

--- ui/packages/components/src/RunDetails/RunDetails.tsx
+++ ui/packages/components/src/RunDetails/RunDetails.tsx
@@ -89,10 +89,10 @@ export function RunDetails({
       {run && history && getHistoryItemOutput && (
         <>
           <div className="px-5 pt-4">
-            {run.status && run.endedAt && run.output && isSuccess && (
+            {run.status && run.endedAt && run.output && (
               <OutputCard content={run.output} isSuccess={isSuccess} />
             )}
-            <CancellationSummary history={history} />    
+            <CancellationSummary history={history} />
             <WaitingSummary history={history} />
             <SleepingSummary history={history} />
           </div>
