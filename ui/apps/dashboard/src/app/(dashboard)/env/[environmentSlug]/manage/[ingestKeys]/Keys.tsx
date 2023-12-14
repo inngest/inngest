1e333a652cbaa4f4cdcb7e828322e6d2264e7b7e
Ana <anafilipadealmeida@gmail.com>
2023-12-14 13:43:10 +0000

    Add skeleton to keys list

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/manage/[ingestKeys]/Keys.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/manage/[ingestKeys]/Keys.tsx
@@ -1,8 +1,11 @@
+'use client';
+
 import { notFound } from 'next/navigation';
+import { Skeleton } from '@inngest/components/Skeleton';
+import { useQuery } from 'urql';
 
 import { graphql } from '@/gql';
-import graphqlAPI from '@/queries/graphqlAPI';
-import { getEnvironment } from '@/queries/server-only/getEnvironment';
+import { useEnvironment } from '@/queries';
 import KeysListItem from './KeysListItem';
 
 const GetKeysDocument = graphql(`
@@ -22,25 +25,45 @@ type KeysProps = {
   environmentSlug: string;
 };
 
-export default async function Keys({ environmentSlug }: KeysProps) {
-  const environment = await getEnvironment({
+const LoadingSkeleton = () => (
+  <div className="border-b border-slate-100 px-4 py-3">
+    <Skeleton className="mb-1 block h-11 w-full" />
+  </div>
+);
+
+export default function Keys({ environmentSlug }: KeysProps) {
+  const [{ data: environment, fetching: fetchingEnvironment }] = useEnvironment({
     environmentSlug,
   });
-
-  const response = await graphqlAPI.request(GetKeysDocument, {
-    environmentID: environment.id,
+  const [{ data, fetching: fetchingKey }] = useQuery({
+    query: GetKeysDocument,
+    variables: {
+      environmentID: environment?.id!,
+    },
+    pause: !environment?.id,
   });
 
-  const keys = response?.environment?.ingestKeys;
+  const loading = fetchingEnvironment || fetchingKey;
 
-  if (!keys) {
-    notFound();
-  }
+  const keys = data?.environment?.ingestKeys;
 
   function sortFunction(a: { createdAt: string }, b: { createdAt: string }) {
     const dateA = new Date(a.createdAt).getTime();
     const dateB = new Date(b.createdAt).getTime();
     return dateA < dateB ? 1 : -1;
+  }
+
+  if (loading) {
+    return (
+      <>
+        <LoadingSkeleton />
+        <LoadingSkeleton />
+      </>
+    );
+  }
+
+  if (!keys) {
+    notFound();
   }
 
   const orderedKeys = keys.sort(sortFunction);
