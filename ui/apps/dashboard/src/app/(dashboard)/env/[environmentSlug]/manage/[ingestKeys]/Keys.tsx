ab62e7f2d75d378de02cfc018314dec2d96cdff7
Ana <anafilipadealmeida@gmail.com>
2023-12-15 12:28:20 +0000

    Simplify code

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/manage/[ingestKeys]/Keys.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/manage/[ingestKeys]/Keys.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { notFound } from 'next/navigation';
-import { ExclamationCircleIcon } from '@heroicons/react/20/solid';
 import { Skeleton } from '@inngest/components/Skeleton';
 
 import { graphql } from '@/gql';
@@ -65,21 +64,8 @@ export default function Keys({ environmentSlug }: KeysProps) {
     );
   }
 
-  if (!keys) {
+  if (environmentError || error || !keys) {
     notFound();
-  }
-
-  if (environmentError || error) {
-    return (
-      <div className="flex h-full w-full flex-col items-center justify-center gap-5">
-        <div className="inline-flex items-center gap-2 text-red-600">
-          <ExclamationCircleIcon className="h-4 w-4" />
-          <h2 className="text-sm">{`Could not load ${
-            environmentError ? 'environment' : 'list'
-          }`}</h2>
-        </div>
-      </div>
-    );
   }
 
   const orderedKeys = keys.sort(sortFunction);
