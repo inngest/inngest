bba947525f445df7e9bac0d8eedcd42bb063ca47
Igor Gassmann <igor@igassmann.me>
2024-01-03 18:37:35 +0100

    Revert "feat(dashboard): send trace headers to API (#950)"
    This reverts commit f7237bd3a5ca55164511c6724d9896f442012fd9.

--- ui/apps/dashboard/sentry.client.config.ts
+++ ui/apps/dashboard/sentry.client.config.ts
@@ -7,12 +7,6 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 0.2,
-  tracePropagationTargets: [
-    /^\//, // All URLs on current origin.
-    /^https:\/\/api\.inngest\.com\//, // The production API origin.
-    /^https:\/\/api\.inngest\.net\//, // The staging API origin.
-    'localhost', // The local API origin.
-  ],
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1.0,
 });
