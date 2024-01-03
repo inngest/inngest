cb588afc5867b02fff5991c539f68af547e85bbd
Aaron Harper <aaron@inngest.com>
2024-01-03 08:28:27 -0500

    Use h2

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/[externalID]/AppInfoCard/AppInfoCard.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/[externalID]/AppInfoCard/AppInfoCard.tsx
@@ -47,7 +47,7 @@ export function AppInfoCard({ app, className }: Props) {
           className
         )}
       >
-        <div className="border-b border-slate-300 px-4 py-2">App Info</div>
+        <h2 className="border-b border-slate-300 px-4 py-2">App Info</h2>
 
         <dl className="grid grow grid-cols-4 gap-4 p-4">
           {/* Row 1 */}
