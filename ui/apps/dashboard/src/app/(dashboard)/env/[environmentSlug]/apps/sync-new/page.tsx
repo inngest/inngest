9bd9baaf136c6af8773b09d2beaa50c902738348
Ana <anafilipadealmeida@gmail.com>
2024-01-04 23:26:32 +0000

    Add curl command

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/sync-new/page.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/sync-new/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import ArrowPathIcon from '@heroicons/react/20/solid/ArrowPathIcon';
 import Squares2X2Icon from '@heroicons/react/20/solid/Squares2X2Icon';
 import { Button } from '@inngest/components/Button';
+import { CodeLine } from '@inngest/components/CodeLine';
 import { Link as InngestLink, defaultLinkStyles } from '@inngest/components/Link';
 import * as Tabs from '@radix-ui/react-tabs';
 
@@ -35,7 +36,7 @@ export default function Page({ params: { environmentSlug } }: Props) {
             <p>Provide the location of your App.</p>
           </header>
 
-          <Tabs.Root defaultValue="tab1">
+          <Tabs.Root defaultValue="tab1" className="bg-white">
             <Tabs.List className="bg-slate-900 pb-2 pl-6 pt-4 text-slate-400">
               <Tabs.Trigger
                 className="px-4 hover:text-white data-[state=active]:text-white"
@@ -56,7 +57,7 @@ export default function Page({ params: { environmentSlug } }: Props) {
                 Curl Command
               </Tabs.Trigger>
             </Tabs.List>
-            <Tabs.Content className="bg-white" value="tab1">
+            <Tabs.Content value="tab1">
               <div className="border-b border-slate-200 p-8">
                 <p>
                   Inngest allows you to host your app code on any platform then invokes your
@@ -106,7 +107,47 @@ export default function Page({ params: { environmentSlug } }: Props) {
               </footer>
             </Tabs.Content>
             <Tabs.Content value="tab2"></Tabs.Content>
-            <Tabs.Content value="tab3"></Tabs.Content>
+            <Tabs.Content value="tab3">
+              <div className="border-b border-slate-200 p-8">
+                <p>
+                  Inngest allows you to host your app code on any platform then invokes your
+                  functions via HTTP. To deploy functions to Inngest, all you need to do is tell
+                  Inngest where to find them!
+                </p>
+                <br />
+                <p>
+                  Since your code is hosted on another platform, you need to register where your
+                  functions are hosted with Inngest. For example, if you set up the serve handler (
+                  <Link
+                    className={defaultLinkStyles}
+                    href="https://www.inngest.com/docs/reference/serve#how-the-serve-api-handler-works"
+                  >
+                    see docs
+                  </Link>
+                  ) at /api/inngest, and your domain is https://myapp.com, you&apos;ll need to
+                  inform Inngest that your app is hosted at https://myapp.com/api/inngest.
+                </p>
+                <br />
+                <p>
+                  You can deploy from your machine or automate this within a CI/CD pipeline. Send a
+                  simple PUT request to your own application's serve endpoint.
+                </p>
+                <CodeLine code="curl -X PUT https://<your-app>.com/api/inngest" className="mt-6" />
+              </div>
+              <footer className="flex items-center justify-between px-8 py-6">
+                {/* To do:  create apps docs and link them here */}
+                <InngestLink href="https://www.inngest.com/docs/">View Docs</InngestLink>
+                <div className="flex items-center gap-3">
+                  <Button
+                    label="Done"
+                    btnAction={() => {
+                      router.push(`/env/${environmentSlug}/apps` as Route);
+                    }}
+                    kind="primary"
+                  />
+                </div>
+              </footer>
+            </Tabs.Content>
           </Tabs.Root>
         </section>
       </div>
