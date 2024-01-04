4dce4fe256fa73c3bb291e94cd4a7720ca4fe471
Ana <anafilipadealmeida@gmail.com>
2024-01-04 16:45:55 +0000

    Add Apps header

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/page.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/page.tsx
@@ -1,9 +1,15 @@
+import Squares2X2Icon from '@heroicons/react/20/solid/Squares2X2Icon';
+
+import Header from '@/components/Header/Header';
 import { Apps } from './Apps';
 
 export default function Page() {
   return (
-    <div className="overflow-y-auto bg-slate-100">
-      <Apps />
-    </div>
+    <>
+      <Header title="Apps" icon={<Squares2X2Icon className="h-5 w-5 text-white" />} />
+      <div className="h-full overflow-y-auto bg-slate-100">
+        <Apps />
+      </div>
+    </>
   );
 }
