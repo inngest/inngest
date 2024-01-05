dc151797c09b41898e187a3d6a52d80009d4ae44
Igor Gassmann <igor@inngest.com>
2024-01-05 14:52:14 +0100

    feat(dashboard): don't sample replays when not erroring (#960)

--- ui/apps/dashboard/sentry.client.config.ts
+++ ui/apps/dashboard/sentry.client.config.ts
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 0.2,
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.0,
   replaysOnErrorSampleRate: 1.0,
 });
 
