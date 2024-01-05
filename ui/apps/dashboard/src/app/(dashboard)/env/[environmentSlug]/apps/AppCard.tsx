5719fa583ba1a06106577a2dfe2522c211e0e124
Ana <anafilipadealmeida@gmail.com>
2024-01-05 23:39:21 +0000

    Add App Card styles

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/AppCard.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/apps/AppCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import type { Function } from '@inngest/components/types/function';
+import ChevronRightIcon from '@heroicons/react/20/solid/ChevronRightIcon';
 import { classNames } from '@inngest/components/utils/classNames';
 
 import { FrameworkInfo } from '@/components/FrameworkInfo';
@@ -37,40 +37,60 @@ export function AppCard({ app, className, envSlug }: Props) {
   return (
     <div
       className={classNames(
-        'flex w-full min-w-[800px] max-w-[1200px] overflow-hidden rounded-lg border border-slate-300 bg-white',
+        'flex h-56 w-full min-w-[800px] max-w-[1200px] overflow-hidden rounded-lg border border-slate-300 bg-white',
         className
       )}
     >
-      <div className="m-4 mt-8 flex w-64 items-center border-r border-slate-400">
+      <div className="bg-slate-910 flex flex-col justify-center gap-2 px-10">
         <h2>
           <Link
-            className="text-indigo-600 hover:underline"
+            className="transition-color flex cursor-pointer items-center gap-1 text-white underline decoration-transparent decoration-2 underline-offset-4 duration-300 hover:text-indigo-400 hover:decoration-indigo-400"
             href={`/env/${envSlug}/apps/${encodeURIComponent(app.externalID)}`}
           >
             {app.name}
+            <ChevronRightIcon className="h-4 w-4" />
           </Link>
         </h2>
+        {app.latestSync?.url && (
+          <dl>
+            <dt className="hidden">URL</dt>
+            <dd className="text-slate-400">{app.latestSync?.url}</dd>
+          </dl>
+        )}
+        {true && (
+          <dl>
+            <dt className="hidden">URL</dt>
+            <dd className="text-slate-400">
+              {app.latestSync?.url || 'https://growth-546lz4dmn-inngest.vercel.app'}
+            </dd>
+          </dl>
+        )}
       </div>
+      <div className="flex flex-1 items-center px-8">
+        <dl className="grid grow grid-cols-2 gap-4 md:grid-cols-3">
+          {/* Row 1 */}
+          <Description
+            className="col-span-2"
+            detail={
+              app.latestSync && (
+                <div className="flex gap-2">
+                  <SyncStatus status={app.latestSync?.status} />
+                  <Link
+                    className="transition-color flex cursor-pointer items-center gap-1 text-indigo-400 underline decoration-transparent decoration-2 underline-offset-4 duration-300  hover:decoration-indigo-400"
+                    href={`/env/${envSlug}/apps/${encodeURIComponent(app.externalID)}/syncs`}
+                  >
+                    <Time value={app.latestSync?.createdAt} />
+                    <ChevronRightIcon className="h-4 w-4" />
+                  </Link>
+                </div>
+              )
+            }
+            term="App ID"
+          />
+          <Description detail={app.latestSync?.sdkVersion} term="SDK Version" />
 
-      <dl className="m-4 grid grow grid-cols-3 gap-4">
-        {/* Row 1 */}
-        <Description detail={app.externalID} term="App ID" />
-        <Description
-          className="col-span-2"
-          detail={
-            app.latestSync && (
-              <div className="flex gap-2">
-                <Time value={app.latestSync?.createdAt} />
-                <SyncStatus status={app.latestSync?.status} />
-              </div>
-            )
-          }
-          term="App ID"
-        />
-
-        {/* Row 2 */}
-        <Description detail={app.latestSync?.sdkVersion} term="SDK Version" />
-        <Description
+          {/* Row 2 */}
+          {/* <Description
           detail={<LanguageInfo language={app.latestSync?.sdkLanguage} />}
           term="Language"
         />
@@ -78,17 +98,16 @@ export function AppCard({ app, className, envSlug }: Props) {
           detail={<FrameworkInfo framework={app.latestSync?.framework} />}
           term="Framework"
         />
-
-        {/* Row 3 */}
         <Description
           detail={<PlatformInfo platform={app.latestSync?.platform} />}
           term="Platform"
-        />
-        <Description detail={app.functionCount} term="Functions" />
+        /> */}
 
-        {/* Row 4 */}
-        <Description className="col-span-3" detail={app.latestSync?.url} term="URL" />
-      </dl>
+          {/* Row 3 */}
+
+          <Description detail={`${app.functionCount} Functions`} term="Functions" />
+        </dl>
+      </div>
     </div>
   );
 }
@@ -104,8 +123,8 @@ function Description({
 }) {
   return (
     <div className={className}>
-      <dt className="text-xs text-slate-600">{term}</dt>
-      <dd>{detail ?? ''}</dd>
+      <dt className="pb-2 text-sm text-slate-400">{term}</dt>
+      <dd className="text-slate-800">{detail ?? ''}</dd>
     </div>
   );
 }
