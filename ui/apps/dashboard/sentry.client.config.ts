580206c13f37032d721e48b6d9babec0fe46ae12
Igor Gassmann <igor@inngest.com>
2024-01-08 09:46:47 +0100

    feat(dashboard): send trace headers to API (#970)

--- ui/apps/dashboard/sentry.client.config.ts
+++ ui/apps/dashboard/sentry.client.config.ts
@@ -7,6 +7,12 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   tracesSampleRate: 0.2,
+  tracePropagationTargets: [
+    /^\//, // All URLs on current origin.
+    /^https:\/\/api\.inngest\.com\//, // The production API origin.
+    /^https:\/\/api\.inngest\.net\//, // The staging API origin.
+    'localhost', // The local API origin.
+  ],
   replaysSessionSampleRate: 0.0,
   replaysOnErrorSampleRate: 1.0,
 });
