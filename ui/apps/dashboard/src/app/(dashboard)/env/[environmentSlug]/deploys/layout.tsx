50c6f0a336e877a312411b9fa1d6b4b83d9d98ed
Ana <anafilipadealmeida@gmail.com>
2024-01-12 17:00:15 +0000

    Add Deploys banner

--- ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/deploys/layout.tsx
+++ ui/apps/dashboard/src/app/(dashboard)/env/[environmentSlug]/deploys/layout.tsx
@@ -1,5 +1,7 @@
 import { RocketLaunchIcon } from '@heroicons/react/20/solid';
 
+import { Banner } from '@/components/Banner';
+import { getBooleanFlag } from '@/components/FeatureFlags/ServerFeatureFlag';
 import Header from '@/components/Header/Header';
 import DeployButton from './DeployButton';
 import DeployList from './DeployList';
@@ -9,6 +11,7 @@ type DeploysLayoutProps = {
 };
 
 export default async function DeploysLayout({ children }: DeploysLayoutProps) {
+  const isAppsEnabled = await getBooleanFlag('apps-page');
   return (
     <>
       <Header
@@ -16,6 +19,18 @@ export default async function DeploysLayout({ children }: DeploysLayoutProps) {
         icon={<RocketLaunchIcon className="h-3.5 w-3.5 text-white" />}
         action={<DeployButton />}
       />
+      {isAppsEnabled && (
+        <Banner kind="error">
+          <p className="pr-2 text-red-800">
+            The deploys page is getting deprecated. We&apos;ve moved all this information and
+            functionality over to the <b>Apps</b> tab.
+          </p>
+          {/* To do: wire this to the docs */}
+          {/* <Link internalNavigation={false} href="">
+            Learn More
+          </Link> */}
+        </Banner>
+      )}
       <div className="flex flex-grow overflow-hidden bg-slate-50">
         <DeployList />
         {children}
