00dd40bccf48d0de749f080243864baa2a4bc94a
Ana <anafilipadealmeida@gmail.com>
2024-01-17 00:15:43 +0000

    Add media queries

--- ui/apps/dashboard/src/components/Navigation/NavItem.tsx
+++ ui/apps/dashboard/src/components/Navigation/NavItem.tsx
@@ -37,14 +37,15 @@ export default function NavItem<PassedHref extends string>({
       key={href.toString()}
       href={href}
       className={cn(
-        'flex items-center gap-1.5 whitespace-nowrap border-b-2 px-2.5 py-4 text-sm leading-none tracking-wide transition-all',
+        'flex h-12 items-center gap-1.5 whitespace-nowrap border-b-2 px-2.5 text-sm leading-none tracking-wide transition-all',
         isActive
           ? ' border-indigo-400 text-white'
           : 'border-transparent text-slate-400 hover:border-slate-400 hover:text-white'
       )}
     >
-      {icon && icon}
-      {text}
+      <span title={text}>{icon && icon}</span>
+      {/* Hide nav text for smaller resolutions if there's an icon  */}
+      <span className={cn(icon && 'hidden lg:block')}>{text}</span>
       {badge}
     </Link>
   );
