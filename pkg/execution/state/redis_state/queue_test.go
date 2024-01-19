9f121d5b4b65bf70b5587c3eb76552625251470a
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-01-19 08:14:10 -0800

    Fix tests

--- pkg/execution/state/redis_state/queue_test.go
+++ pkg/execution/state/redis_state/queue_test.go
@@ -481,6 +481,13 @@ func TestQueuePeek(t *testing.T) {
 			require.NoError(t, err)
 			require.EqualValues(t, 4, len(items))
 
+			// Ignore items earlies peek time.
+			for _, i := range items {
+				if i.EarliestPeekTime != 0 {
+					i.EarliestPeekTime = 0
+				}
+			}
+
 			require.EqualValues(t, ia.ID, items[0].ID)
 			// NOTE: Scavenging requeues items, and so the time will have changed.
 			require.GreaterOrEqual(t, items[0].AtMS, scavengeAt)
@@ -1006,10 +1013,14 @@ func TestQueuePartitionLease(t *testing.T) {
 	leaseUntil := now.Add(3 * time.Second)
 
 	t.Run("It leases a partition", func(t *testing.T) {
-		// Lease the first item
+		// Lease the first item now.
+		leasedAt := time.Now()
 		leaseID, err := q.PartitionLease(ctx, pA, time.Until(leaseUntil))
 		require.NoError(t, err)
 		require.NotNil(t, leaseID)
+
+		// Pause so that we can assert that the last lease time was set correctly.
+		<-time.After(50 * time.Millisecond)
 
 		t.Run("It updates the partition score", func(t *testing.T) {
 			items, err := q.PartitionPeek(ctx, true, now.Add(time.Hour), PartitionPeekMax)
@@ -1026,11 +1037,13 @@ func TestQueuePartitionLease(t *testing.T) {
 					WorkflowID: idA,
 					Priority:   testPriority,
 					AtS:        ulid.Time(leaseID.Time()).Unix(),
-					Last:       time.Now().Unix(),
+					Last:       items[2].Last, // Use the leased partition time.
 					LeaseID:    leaseID,
 				}, // idA is now last.
 			}, items)
 			requirePartitionScoreEquals(t, r, idA, leaseUntil)
+			// require that the last leased time is within 5ms for tests
+			require.WithinDuration(t, leasedAt, time.UnixMilli(items[2].Last), 5*time.Millisecond)
 		})
 
 		t.Run("It can't lease an existing partition lease", func(t *testing.T) {
