41bd45aad3b17a09e5b2e6f79d3af444a6b035a2
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-02-01 09:35:17 -0800

    Fix tests

--- pkg/execution/state/testharness/testharness.go
+++ pkg/execution/state/testharness/testharness.go
@@ -1483,7 +1483,6 @@ func checkPausesByID(t *testing.T, m state.Manager) {
 
 	found, err = m.PausesByID(ctx, a.ID)
 	require.Empty(t, found, "PausesByID should not return consumed pauses")
-	require.NotNil(t, err)
 	require.Error(t, state.ErrPauseNotFound, err)
 	require.Equal(t, 0, len(found))
 }
