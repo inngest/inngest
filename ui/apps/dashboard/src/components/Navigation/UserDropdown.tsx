446e0385e5ae96dd7fa17daa27bda186adfe6805
Igor Gassmann <igor@inngest.com>
2024-02-08 15:09:54 +0100

    feat(dashboard): UserDropdown

---
+++ ui/apps/dashboard/src/components/Navigation/UserDropdown.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import type { ComponentType } from 'react';
+import type { Route } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useAuth, useUser } from '@clerk/nextjs';
+import {
+  Cog6ToothIcon,
+  LifebuoyIcon,
+  MapPinIcon,
+  NewspaperIcon,
+  PowerIcon,
+} from '@heroicons/react/20/solid';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@inngest/components/DropdownMenu';
+import { Skeleton } from '@inngest/components/Skeleton';
+
+import { useSystemStatus } from '@/app/support/statusPage';
+import SystemStatusIcon from '@/components/Navigation/SystemStatusIcon';
+
+export default function UserDropdown() {
+  const { isLoaded, isSignedIn, user } = useUser();
+  const { signOut } = useAuth();
+  const status = useSystemStatus();
+  const router = useRouter();
+
+  if (!isLoaded) {
+    return (
+      <div className="flex h-full items-center border-l border-slate-800 px-2 py-1.5 md:px-4">
+        <Skeleton className="block size-5 rounded-full" />
+      </div>
+    );
+  }
+
+  if (!isSignedIn) return null;
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="flex h-full items-center gap-2 border-l border-slate-800 px-2 py-1.5 text-sm tracking-wide text-white hover:bg-slate-800 md:px-4">
+        <Image
+          alt="Your profile picture"
+          src={user.imageUrl}
+          width={20}
+          height={20}
+          className="size-5 rounded-full"
+        />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        sideOffset={4}
+        className="bg-slate-940/95 z-50 min-w-[200px] divide-y divide-dashed divide-slate-700 p-0 backdrop-blur"
+      >
+        <DropdownMenuGroup className="p-2">
+          <OrganizationDropdownMenuItem
+            icon={Cog6ToothIcon}
+            href="/settings/user"
+            label="User Settings"
+          />
+        </DropdownMenuGroup>
+        <DropdownMenuGroup className="p-2">
+          <OrganizationDropdownMenuItem
+            icon={MapPinIcon}
+            href="https://roadmap.inngest.com/roadmap"
+            label="Roadmap"
+          />
+          <OrganizationDropdownMenuItem
+            icon={NewspaperIcon}
+            href="https://roadmap.inngest.com/changelog"
+            label="Release Notes"
+          />
+          <OrganizationDropdownMenuItem
+            icon={LifebuoyIcon}
+            href="/support"
+            label="Contact Support"
+          />
+          <OrganizationDropdownMenuItem
+            icon={SystemStatusIcon}
+            href={status.url}
+            label="Status Page"
+          />
+        </DropdownMenuGroup>
+        <DropdownMenuGroup className="p-2">
+          <OrganizationDropdownMenuItem
+            icon={PowerIcon}
+            onSelect={() =>
+              signOut(() =>
+                router.push((process.env.NEXT_PUBLIC_SIGN_IN_PATH || '/sign-in') as Route)
+              )
+            }
+            label="Sign Out"
+          />
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+function OrganizationDropdownMenuItem(props: {
+  icon: ComponentType<{
+    className?: string;
+  }>;
+  label: string;
+  href?: string;
+  onSelect?: () => void;
+}) {
+  return (
+    <DropdownMenuItem
+      onSelect={props.onSelect}
+      asChild
+      className="p-2 font-medium text-slate-400 hover:bg-transparent hover:text-white"
+    >
+      {props.href ? (
+        <Link href={props.href as Route}>
+          <props.icon className="size-4" />
+          {props.label}
+        </Link>
+      ) : (
+        <button>
+          <props.icon className="size-4" />
+          {props.label}
+        </button>
+      )}
+    </DropdownMenuItem>
+  );
+}
