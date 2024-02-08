41ac975df7a19b8971021897ed91d4f8f8b0a9a6
Igor Gassmann <igor@inngest.com>
2024-02-08 12:35:57 +0100

    feat(components): add tailwind-merge

--- ui/packages/components/src/utils/classNames.ts
+++ ui/packages/components/src/utils/classNames.ts
@@ -1,3 +1,34 @@
+import { extendTailwindMerge, twMerge } from 'tailwind-merge';
+
+/**
+ *  @deprecated Use the new {@link cn} function instead. This function is susceptible to
+ *  Tailwind CSS style conflicts.
+ */
 export function classNames(...classes: (string | boolean | undefined)[]): string {
   return classes.filter(Boolean).join(' ');
+}
+
+// Extend the default `tailwind-merge` configuration to include our custom `text-shadow` class group.
+const customTwMerge = extendTailwindMerge({
+  classGroups: {
+    /**
+     * Text Shadow
+     * @see {@link file://../../../../apps/dashboard/src/app/globals.css}
+     */
+    'text-shadow': [{ 'text-shadow': ['', 'md', 'lg', 'none'] }],
+  },
+});
+
+/**
+ * Utility function for conditionally constructing `className` strings without style conflicts when
+ * overriding Tailwind CSS classes.
+ *
+ * @see {@link https://github.com/dcastil/tailwind-merge/blob/main/docs/what-is-it-for.md} for
+ * learning about style conflicts.
+ *
+ * @param inputs - A list of class names to merge.
+ * @returns A string of merged class names.
+ */
+export function cn(...inputs: Parameters<typeof twMerge>): string {
+  return customTwMerge(...inputs);
 }
