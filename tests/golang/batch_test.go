b3bec548f3c7f08adf95d17e6710ccbd74662b4e
Darwin D Wu <darwin67@users.noreply.github.com>
2024-02-07 22:55:55 -0500

    add batch invoke test

--- tests/golang/batch_test.go
+++ tests/golang/batch_test.go
@@ -2,12 +2,14 @@ package golang
 
 import (
 	"context"
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/inngest/inngest/pkg/inngest"
 	"github.com/inngest/inngestgo"
+	"github.com/inngest/inngestgo/step"
 	"github.com/stretchr/testify/require"
 )
 
@@ -57,5 +59,78 @@ func TestBatchEvents(t *testing.T) {
 		<-time.After(5 * time.Second)
 		require.EqualValues(t, 2, atomic.LoadInt32(&counter))
 		require.EqualValues(t, 8, atomic.LoadInt32(&totalEvents))
+	})
+}
+
+func TestBatchInvoke(t *testing.T) {
+	ctx := context.Background()
+	h, server, registerFuncs := NewSDKHandler(t, "batchinvoke")
+	defer server.Close()
+
+	var (
+		counter       int32
+		totalEvents   int32
+		invokeCounter int32
+	)
+
+	batcher := inngestgo.CreateFunction(
+		inngestgo.FunctionOpts{
+			ID:   "batcher",
+			Name: "test batching",
+			BatchEvents: &inngest.EventBatchConfig{
+				MaxSize: 5,
+				Timeout: "5s",
+			},
+		},
+		inngestgo.EventTrigger("batchinvoke/batch", nil),
+		func(ctx context.Context, input inngestgo.Input[BatchEvent]) (any, error) {
+			evts := input.Events
+			atomic.AddInt32(&counter, 1)
+			atomic.AddInt32(&totalEvents, int32(len(evts)))
+			return fmt.Sprintf("batched %d events", len(evts)), nil
+		},
+	)
+	caller := inngestgo.CreateFunction(
+		inngestgo.FunctionOpts{
+			ID:   "caller",
+			Name: "test batching",
+		},
+		inngestgo.EventTrigger("batchinvoke/caller", nil),
+		func(ctx context.Context, input inngestgo.Input[BatchEvent]) (any, error) {
+			_, _ = step.Run(ctx, "print", func(ctx context.Context) (any, error) {
+				fmt.Println("invoking batched fn")
+				return nil, nil
+			})
+			val, err := step.Invoke[any](ctx, "batch", step.InvokeOpts{
+				FunctionId: "batchinvoke-batcher",
+				Data: map[string]any{
+					"name": "invoke",
+				},
+			})
+			fmt.Println("Invoked batch fn:", val)
+			atomic.AddInt32(&invokeCounter, 1)
+			require.NoError(t, err)
+			return true, nil
+		},
+	)
+
+	h.Register(batcher, caller)
+	registerFuncs()
+
+	t.Run("trigger a batch", func(t *testing.T) {
+		// Call invoke twice
+		for i := 0; i < 2; i++ {
+			_, err := inngestgo.Send(ctx, BatchEvent{
+				Name: "batchinvoke/caller",
+				Data: BatchEventData{Time: time.Now()},
+			})
+			require.NoError(t, err)
+		}
+
+		// First trigger should be because of the batch timeout, with two invokes.
+		<-time.After(6 * time.Second)
+		require.EqualValues(t, 1, atomic.LoadInt32(&counter))
+		require.EqualValues(t, 2, atomic.LoadInt32(&totalEvents))
+		require.EqualValues(t, 2, atomic.LoadInt32(&invokeCounter))
 	})
 }
