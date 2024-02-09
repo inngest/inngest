d31e7407c847a7e2c2c9b45c3ad716c7beec1ae2
Igor Gassmann <igor@inngest.com>
2024-02-09 14:15:57 +0100

    fix(dashboard): invite members form style

--- ui/apps/dashboard/src/app/layout.tsx
+++ ui/apps/dashboard/src/app/layout.tsx
@@ -44,11 +44,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             socialButtonsBlockButton__google:
               'inline-flex flex-shrink-0 items-center gap-3 overflow-hidden rounded-[6px] transition-all shadow-outline-secondary-light font-medium px-6 py-2.5 bg-slate-700 text-white shadow-sm hover:bg-slate-500 hover:text-white',
             socialButtonsBlockButtonText: 'text-sm font-regular',
+            form: 'text-left',
             formFieldLabel: 'text-slate-600',
             formFieldInput:
               'border border-slate-300 placeholder-slate-500 shadow transition-all text-sm px-3.5 py-3 rounded-lg',
             formButtonPrimary:
               'inline-flex flex-shrink-0 items-center gap-1 justify-center overflow-hidden text-sm font-regular rounded-[6px] transition-all bg-gradient-to-b from-[#6d7bfe] to-[#6366f1] hover:from-[#7986fd] hover:to-[#7679f9] text-shadow text-white font-medium px-6 py-2.5 capitalize',
+            tagInputContainer:
+              'border border-slate-300 placeholder-slate-500 shadow transition-all rounded-lg focus-within:*:ring-0 *:px-3 *:p-1.5 *:text-sm',
             footerActionText: 'text-sm font-medium text-slate-700',
             footerActionLink:
               'transition-color text-sm font-medium text-indigo-500 underline hover:text-indigo-800',
