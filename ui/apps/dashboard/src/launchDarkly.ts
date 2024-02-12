35aa93582615b4004ae22cd1705fdc5e7a187779
Igor Gassmann <igor@inngest.com>
2024-02-12 15:47:34 +0100

    fix(dashboard): used LD before its initialization completed

--- ui/apps/dashboard/src/launchDarkly.ts
+++ ui/apps/dashboard/src/launchDarkly.ts
@@ -1,6 +1,6 @@
 import { init, type LDClient } from '@launchdarkly/node-server-sdk';
 
-let launchDarklyClient: LDClient | undefined;
+let launchDarklyClient: LDClient;
 
 function initialize() {
   const launchDarklySDKKey = process.env.LAUNCH_DARKLY_SDK_KEY;
@@ -8,12 +8,12 @@ function initialize() {
     throw new Error('LAUNCH_DARKLY_SDK_KEY environment variable is not set.');
   }
   launchDarklyClient = init(launchDarklySDKKey, { stream: false });
-  return launchDarklyClient;
 }
 
 export async function getLaunchDarklyClient(): Promise<LDClient> {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- Since we run our app in a serverless environment, launchDarklyClient can potentially be persisted between invocations.
   if (!launchDarklyClient) {
-    return initialize();
+    initialize();
   }
 
   await launchDarklyClient.waitForInitialization();
