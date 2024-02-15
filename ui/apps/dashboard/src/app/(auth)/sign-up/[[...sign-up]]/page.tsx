fe4e1b7dea88cf430b71ad13adaf8088bcecb511
Igor Gassmann <igor@inngest.com>
2024-02-15 11:54:34 +0100

    hotfix(dashboard): account setup redirect url (#1146)

--- ui/apps/dashboard/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ ui/apps/dashboard/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,28 +2,18 @@ import { cookies } from 'next/headers';
 import { SignUp } from '@clerk/nextjs';
 
 import SplitView from '@/app/(auth)/SplitView';
-import { getBooleanFlag } from '@/components/FeatureFlags/ServerFeatureFlag';
 
 export default async function SignUpPage() {
   const cookieStore = cookies();
   const anonymousIDCookie = cookieStore.get('inngest_anonymous_id');
 
-  const isOrganizationsEnabled = await getBooleanFlag('organizations');
-
   return (
     <SplitView>
       <div className="mx-auto my-8 mt-auto text-center">
         <SignUp
-          afterSignUpUrl={isOrganizationsEnabled ? '/organization-list' : '/sign-up/account-setup'}
+          afterSignUpUrl="/organization-list"
           unsafeMetadata={{
             ...(anonymousIDCookie?.value && { anonymousID: anonymousIDCookie.value }),
-          }}
-          appearance={{
-            elements: {
-              // We need to hide the name fields because we don't want to overwhelm users with too
-              // many fields, but we still want to allow them later to set their name if they want to.
-              formFieldRow__name: 'hidden',
-            },
           }}
         />
       </div>
