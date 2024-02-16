0d742423a1cde7fa2621b902a74e214cebdbf7c2
Igor Gassmann <igor@inngest.com>
2024-02-16 09:25:51 +0100

    chore: remove website app (#1151)

--- ui/apps/website/shared/layout/PageContainer.tsx
+++
@@ -1,3 +0,0 @@
-export default function PageContainer({ children }) {
-  return <div className="relative font-sans">{children}</div>;
-}
--- ui/apps/website/shared/legacy/CLIInstall.tsx
+++
@@ -1,7 +0,0 @@
-import { CommandSnippet } from './CommandSnippet';
-
-const SCRIPT = 'curl -sfL https://cli.inngest.com/install.sh | sh';
-
-const CLIInstall = () => <CommandSnippet command={SCRIPT} copy />;
-
-export default CLIInstall;
