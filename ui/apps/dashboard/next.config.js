dee89b30f5eb8a62522934d7f960c3d6e1031cba
Igor Gassmann <igor@inngest.com>
2024-02-27 18:42:56 +0100

    chore(dashboard): enable source maps in production (#1194)

--- ui/apps/dashboard/next.config.js
+++ ui/apps/dashboard/next.config.js
@@ -3,6 +3,7 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   experimental: {
     typedRoutes: true,
   },
