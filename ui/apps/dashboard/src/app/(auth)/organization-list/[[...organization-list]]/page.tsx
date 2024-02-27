22bc470f76a87fd7c634aeae954b76b92faa12dd
Igor Gassmann <igor@inngest.com>
2024-02-27 10:43:06 +0100

    fix(dashboard): cache revalidate conflicts with clerk

--- ui/apps/dashboard/src/app/(auth)/organization-list/[[...organization-list]]/page.tsx
+++ ui/apps/dashboard/src/app/(auth)/organization-list/[[...organization-list]]/page.tsx
@@ -1,4 +1,3 @@
-import { revalidatePath } from 'next/cache';
 import { OrganizationList } from '@clerk/nextjs';
 
 import SplitView from '@/app/(auth)/SplitView';
@@ -8,10 +7,6 @@ type OrganizationListPageProps = {
 };
 
 export default async function OrganizationListPage({ searchParams }: OrganizationListPageProps) {
-  // We run revalidatePath to clear Next.js cache so that the user doesn't get stale data from a
-  // previous organization if they switch organizations.
-  revalidatePath('/', 'layout');
-
   const redirectURL =
     typeof searchParams.redirect_url === 'string'
       ? searchParams.redirect_url
