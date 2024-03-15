f060a41e81338b6adde050b7bda38d7e8557fb16
Darwin D Wu <darwin67@users.noreply.github.com>
2024-03-15 15:00:23 -0700

    add Metadata field for PubsubMessage to embed trace context

--- pkg/telemetry/propagation.go
+++ pkg/telemetry/propagation.go
@@ -2,10 +2,32 @@ package telemetry
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"go.opentelemetry.io/otel/baggage"
 )
+
+// TraceCarrier stores the data that needs to be carried through systems.
+// e.g. pubsub, queues, etc
+type TraceCarrier struct {
+	Context map[string]string `json:"ctx,omitempty"`
+}
+
+func NewTraceCarrier() *TraceCarrier {
+	return &TraceCarrier{
+		Context: map[string]string{},
+	}
+}
+
+func (tc *TraceCarrier) Unmarshal(data any) error {
+	byt, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(byt, tc)
+}
 
 func AddBaggageMap(ctx context.Context, attrs map[string]string) (context.Context, error) {
 	for key, value := range attrs {
