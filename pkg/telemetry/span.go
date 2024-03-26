683f8afaa4c4985bc9364a02db1cecac91b520fd
Darwin D Wu <darwin67@users.noreply.github.com>
2024-03-26 16:35:36 -0700

    make each span related struct non public for now

--- pkg/telemetry/span.go
+++ pkg/telemetry/span.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-// Span is an attempt to mimic the otel span data structure following the protobuf spec at
+// span is an attempt to mimic the otel span data structure following the protobuf spec at
 // https://github.com/open-telemetry/opentelemetry-proto/blob/v1.1.0/opentelemetry/proto/trace/v1/trace.proto
 //
 // Due to the limitations of the otel lib's API interface, we can't reconstruct spans over boundaries,
@@ -12,13 +12,16 @@ import (
 // we'll need to work around the otel library and have slightly different way of working with the data
 //
 // This file is an attempt to make it as close as possible to official libs so we can minimize deviations.
-type Span struct {
+//
+// NOTE: to make sure it doesn't conflict the the ReadOnlySpan interface functions,
+// certain fields are named in a little weird way.
+type span struct {
 	TraceID      string    `json:"traceID"`
 	SpanID       string    `json:"spanID"`
 	TraceState   string    `json:"traceState"`
 	ParentSpanID *string   `json:"parentSpanID,omitempty"`
 	Flags        [4]byte   `json:"flags"`
-	Name         string    `json:"name"`
+	SpanName     string    `json:"name"`
 	Kind         string    `json:"kind"`
 	StartTime    time.Time `json:"startts"`
 	EndTime      time.Time `json:"endts"`
@@ -27,23 +30,28 @@ type Span struct {
 	ScopeName    string `json:"scopeName"`
 	ScopeVersion string `json:"scopeVersion"`
 
-	ResourceAttr map[string]string `json:"resourceAttr"`
-	SpanAttr     map[string]string `json:"spanAttr"`
+	Attrs map[string]string `json:"attrs"`
 
-	Events []SpanEvent `json:"events"`
-	Links  []SpanLink  `json:"links"`
+	Events []spanEvent `json:"events"`
+	Links  []spanLink  `json:"links"`
 }
 
-type SpanEvent struct {
+type spanEvent struct {
 	Timestamp time.Time         `json:"ts"`
 	Name      string            `json:"name"`
 	Attr      map[string]string `json:"attr"`
 }
 
-type SpanLink struct {
+type spanLink struct {
 	TraceID    string            `json:"traceID"`
 	SpanID     string            `json:"spanID"`
 	TraceState string            `json:"traceState"`
 	Attr       map[string]string `json:"attr"`
 	Flags      [4]byte           `json:"flags"`
+}
+
+// Implement the functions to fulfill trace.ReadOnlySpan
+
+func (s *span) Name() string {
+	return s.SpanName
 }
