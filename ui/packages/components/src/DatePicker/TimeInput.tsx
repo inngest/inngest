7fde76d8dc186ed6fc5f58ef863f2f72c5357ca0
Ana <anafilipadealmeida@gmail.com>
2024-03-30 01:38:02 +0000

    Add time input validation

--- ui/packages/components/src/DatePicker/TimeInput.tsx
+++ ui/packages/components/src/DatePicker/TimeInput.tsx
@@ -1,12 +1,66 @@
-const MAX_HOUR_AM = 12;
-const MAX_HOUR_24 = 24;
+import { useState } from 'react';
+import { ZodError, type ZodSchema } from 'zod';
+
+import {
+  getHourSchema,
+  millisecondsSchema,
+  minutesSchema,
+  periodSchema,
+  secondsSchema,
+} from './timeSchemas';
 
 type TimeInputProps = {
   is24Format?: boolean;
 };
 
 export function TimeInput({ is24Format = false }: TimeInputProps) {
-  const handlePeriodChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  const [hourInput, setHourInput] = useState('');
+  const [minuteInput, setMinuteInput] = useState('');
+  const [secondInput, setSecondInput] = useState('');
+  const [millisecondInput, setMillisecondInput] = useState('');
+  const [periodInput, setPeriodInput] = useState('');
+
+  type HandleTimeChangeProps = {
+    e: React.ChangeEvent<HTMLInputElement>;
+    schema: ZodSchema<any>;
+    setInputValue: React.Dispatch<React.SetStateAction<string>>;
+  };
+
+  const handleTimeChange = ({ e, schema, setInputValue }: HandleTimeChangeProps) => {
+    const value = e.target.value;
+    try {
+      schema.parse(value);
+      setInputValue(value);
+    } catch (error) {
+      if (!(error instanceof ZodError)) {
+        return;
+      }
+      const errorCode = error.errors?.[0]?.code;
+      if (value === '') {
+        setInputValue('');
+      } else if (errorCode === 'too_big' || errorCode === 'too_small') {
+        setInputValue(value);
+      }
+    }
+  };
+
+  const handlePeriodChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputPeriod = e.target.value;
+    const parsedInputPeriod = inputPeriod.toUpperCase();
+    try {
+      periodSchema.parse(parsedInputPeriod);
+      setPeriodInput(parsedInputPeriod);
+    } catch (error) {
+      if (parsedInputPeriod === '') {
+        setPeriodInput('');
+      } else if (
+        parsedInputPeriod.length === 1 &&
+        (parsedInputPeriod.startsWith('A') || parsedInputPeriod.startsWith('P'))
+      ) {
+        setPeriodInput(parsedInputPeriod);
+      }
+    }
+  };
 
   return (
     <div className="flex h-8 items-center rounded-lg border-2 border-transparent bg-white px-3.5 text-sm leading-none placeholder-slate-500 transition-all has-[:focus]:border-indigo-500">
@@ -14,36 +68,44 @@ export function TimeInput({ is24Format = false }: TimeInputProps) {
         className="w-7 px-0.5 text-center focus:outline-none"
         placeholder="HH"
         aria-label="Point in time (Hours)"
-        min={0}
-        max={is24Format ? MAX_HOUR_24 : MAX_HOUR_AM}
         maxLength={2}
+        onChange={(e) =>
+          handleTimeChange({ e, schema: getHourSchema(is24Format), setInputValue: setHourInput })
+        }
+        value={hourInput}
       />
       <span className="px-0.5">:</span>
       <input
         className="w-7 px-0.5 text-center focus:outline-none"
         placeholder="mm"
         aria-label="Point in time (Minutes)"
-        min={0}
-        max={59}
         maxLength={2}
+        onChange={(e) =>
+          handleTimeChange({ e, schema: minutesSchema, setInputValue: setMinuteInput })
+        }
+        value={minuteInput}
       />
       <span className="px-0.5">:</span>
       <input
         className="w-7 px-0.5 text-center focus:outline-none"
         placeholder="ss"
         aria-label="Point in time (Seconds)"
-        min={0}
-        max={59}
         maxLength={2}
+        onChange={(e) =>
+          handleTimeChange({ e, schema: secondsSchema, setInputValue: setSecondInput })
+        }
+        value={secondInput}
       />
       <span className="px-0.5">.</span>
       <input
         className="w-9 px-0.5 text-center focus:outline-none"
         placeholder="sss"
         aria-label="Point in time (Milliseconds)"
-        min={0}
-        max={999}
         maxLength={3}
+        onChange={(e) =>
+          handleTimeChange({ e, schema: millisecondsSchema, setInputValue: setMillisecondInput })
+        }
+        value={millisecondInput}
       />
       {!is24Format && (
         <input
@@ -52,6 +114,7 @@ export function TimeInput({ is24Format = false }: TimeInputProps) {
           aria-label="Point in time (Period)"
           maxLength={2}
           onChange={handlePeriodChange}
+          value={periodInput}
         />
       )}
     </div>
