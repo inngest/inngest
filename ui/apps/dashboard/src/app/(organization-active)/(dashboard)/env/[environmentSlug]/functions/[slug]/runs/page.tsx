b475ea0fe83dd08ef02ff2ac172afd09029b69a0
Ana <anafilipadealmeida@gmail.com>
2024-04-18 15:29:00 +0100

    Render table with fake data

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/[slug]/runs/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/[slug]/runs/page.tsx
@@ -1,5 +1,39 @@
-const Page = () => {
-  return <></>;
-};
+'use client';
 
-export default Page;
+// import { useQuery } from 'urql';
+
+// import { useEnvironment } from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/environment-context';
+import { useSearchParam } from '@/utils/useSearchParam';
+// import { graphql } from '@/gql';
+import RunsTable from './RunsTable';
+import { mockedRuns } from './mockedRuns';
+
+// const GetRunsDocument = graphql(`
+//   query GetRuns($environmentID: ID!) {
+//     workspace(id: $environmentID) {
+//       runs {
+//       }
+//     }
+//   }
+// `);
+
+export default function RunsPage() {
+  //   const environment = useEnvironment();
+  //   const [{ data, fetching: fetchingRuns }] = useQuery({
+  //     query: GetRunsDocument,
+  //     variables: {
+  //       environmentID: environment.id,
+  //       // filter: filtering,
+  //     },
+  //   });
+
+  const runs = mockedRuns || [];
+
+  return (
+    <main className="bg-white">
+      <div className="m-8 flex gap-2">{/* TODO: filters */}</div>
+      {/* @ts-ignore */}
+      <RunsTable data={runs} />
+    </main>
+  );
+}
