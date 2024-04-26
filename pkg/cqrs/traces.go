bce85bcceccfdbe0b9d7a1d1f3426f9bd5d11172
Darwin D Wu <darwin67@users.noreply.github.com>
2024-04-26 15:42:15 -0700

    add writer and reader interfaces

--- pkg/cqrs/traces.go
+++ pkg/cqrs/traces.go
@@ -1,13 +1,16 @@
 package cqrs
 
 import (
+	"context"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/inngest/inngest/pkg/enums"
 	"github.com/oklog/ulid/v2"
 )
 
-type Trace struct {
+// Span represents an distributed span in a function execution flow
+type Span struct {
 	Timestamp          time.Time         `json:"ts"`
 	TraceID            string            `json:"trace_id"`
 	SpanID             string            `json:"span_id"`
@@ -26,6 +29,7 @@ type Trace struct {
 	RunID              *ulid.ULID        `json:"run_id"`
 }
 
+// TraceRun represents a function run backed by a trace
 type TraceRun struct {
 	AccountID   uuid.UUID     `json:"account_id"`
 	WorkspaceID uuid.UUID     `json:"workspace_id"`
@@ -37,7 +41,52 @@ type TraceRun struct {
 	StartedAt   time.Time     `json:"started_at,omitempty"`
 	EndedAt     time.Time     `json:"ended_at,omitempty"`
 	Duration    time.Duration `json:"duration"`
+	SourceID    string        `json:"source_id,omitempty"`
+	TriggerIDs  []ulid.ULID   `json:"trigger_ids"`
+	Triggers    [][]byte      `json:"triggers"`
+	Output      []byte        `json:"output,omitempty"`
 	Status      string        `json:"status"`
 	IsBatch     bool          `json:"is_batch"`
 	IsDebounce  bool          `json:"is_debounce"`
+}
+
+type TraceReadWriter interface {
+	TraceWriter
+	TraceReader
+}
+
+type TraceWriter interface {
+	// InsertSpan writes a trace span into the backing store
+	InsertSpan(ctx context.Context, span Span) error
+	// InsertTraceRun writes a trace based function run into the backing store
+	InsertTraceRun(ctx context.Context, run TraceRun) error
+}
+
+type TraceReader interface {
+	// GetSpansByTraceIDAndRunID retrieves spans based on their traceID and runID
+	GetSpansByTraceIDAndRunID(ctx context.Context, tid string, runID ulid.ULID) ([]*Span, error)
+	// GetTraceRuns retrieves a list of TraceRun based on the options specified
+	GetTraceRuns(ctx context.Context, opt GetTraceRunOpt) ([]*TraceRun, error)
+}
+
+type GetTraceRunOpt struct {
+	Filter GetTraceRunFilter
+	Order  []GetTraceRunOrder
+	Cursor string
+	Items  uint
+}
+
+type GetTraceRunFilter struct {
+	AccountID   uuid.UUID
+	WorkspaceID uuid.UUID
+	AppID       []uuid.UUID
+	FunctionID  []uuid.UUID
+	TimeField   enums.TraceRunTime
+	Status      []enums.RunStatus
+	CEL         string
+}
+
+type GetTraceRunOrder struct {
+	Field     enums.TraceRunTime
+	Direction enums.TraceRunOrder
 }
