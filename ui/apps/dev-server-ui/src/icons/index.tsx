7a8f34a8f8617561f4ef737acc11c64e19d18056
Ana <anafilipadealmeida@gmail.com>
2024-05-09 14:58:56 +0100

    Replace devserver plus icon

--- ui/apps/dev-server-ui/src/icons/index.tsx
+++ ui/apps/dev-server-ui/src/icons/index.tsx
@@ -1,7 +1,6 @@
 // custom icons
 export { IconFeed } from './Feed';
 export { InngestLogo } from './InngestLogo';
-export { IconPlus } from './Plus';
 export { IconSpinner } from './Spinner';
 export { InngestSmallLogo } from './InngestSmallLogo';
 export { IconReplay } from './Replay';
