b3fcdd10583ed14b39a706e7ad528bf4fc7763ea
Ana <anafilipadealmeida@gmail.com>
2024-05-09 14:56:56 +0100

    Replace devserver check and error icons

--- ui/apps/dev-server-ui/src/icons/index.tsx
+++ ui/apps/dev-server-ui/src/icons/index.tsx
@@ -3,7 +3,5 @@ export { IconFeed } from './Feed';
 export { InngestLogo } from './InngestLogo';
 export { IconPlus } from './Plus';
 export { IconSpinner } from './Spinner';
-export { IconCheckCircle } from './CheckCircle';
-export { IconExclamationTriangle } from './ExclamationTriangle';
 export { InngestSmallLogo } from './InngestSmallLogo';
 export { IconReplay } from './Replay';
