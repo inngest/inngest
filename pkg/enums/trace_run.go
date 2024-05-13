2ff735f47d456e6a1d8d8b76fc8cca05d259152e
Darwin D Wu <darwin67@users.noreply.github.com>
2024-05-13 16:50:34 -0700

    change strings to be snake case

--- pkg/enums/trace_run.go
+++ pkg/enums/trace_run.go
@@ -1,4 +1,4 @@
-//go:generate go run github.com/dmarkham/enumer -trimprefix=TraceRunTime -type=TraceRunTime -json -text
+//go:generate go run github.com/dmarkham/enumer -trimprefix=TraceRunTime -type=TraceRunTime -transform=snake -json -text
 package enums
 
 type TraceRunTime int
