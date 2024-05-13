8dd750ae699495dff0c5af75e741887fe1d2725a
Darwin D Wu <darwin67@users.noreply.github.com>
2024-05-12 21:34:57 -0700

    add goleak to queue tests

--- pkg/execution/state/redis_state/queue_test.go
+++ pkg/execution/state/redis_state/queue_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/oklog/ulid/v2"
 	"github.com/redis/rueidis"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 )
 
 func init() {
@@ -24,6 +25,10 @@ func init() {
 }
 
 const testPriority = PriorityDefault
+
+func TestMain(m *testing.M) {
+	goleak.VerifyTestMain(m)
+}
 
 func TestQueueItemScore(t *testing.T) {
 	parse := func(layout, val string) time.Time {
