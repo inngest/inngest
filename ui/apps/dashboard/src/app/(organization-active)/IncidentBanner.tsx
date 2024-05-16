e34d0c262a7c09e7accb4ca60c4520349ef1556d
Ana <anafilipadealmeida@gmail.com>
2024-05-16 19:24:55 +0100

    Add status from status page

--- ui/apps/dashboard/src/app/(organization-active)/IncidentBanner.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/IncidentBanner.tsx
@@ -1,15 +1,34 @@
+'use client';
+
 import { Link } from '@inngest/components/Link';
 
-import { Banner } from '@/components/Banner';
+import { Banner, type Severity } from '@/components/Banner';
 import { useBooleanFlag } from '@/components/FeatureFlags/hooks';
+import { useSystemStatus } from './support/statusPage';
 
 export default function IncidentBanner() {
   const isIncidentBannerEnabled = useBooleanFlag('incident-banner');
-  if (!isIncidentBannerEnabled) return;
+  const status = useSystemStatus();
+
+  if (!isIncidentBannerEnabled.value && status.indicator === 'none') return;
+
+  let message = '';
+  let severity: Severity = 'warning';
+
+  if (isIncidentBannerEnabled.value) {
+    message = ' We are experiencing some issues.';
+  } else {
+    message = `${status.description}`;
+    if (status.indicator === 'minor') {
+      severity = 'info';
+    } else {
+      severity = 'warning';
+    }
+  }
 
   return (
-    <Banner kind="warning">
-      We are experiencing some API issues. Please check the{' '}
+    <Banner kind={severity}>
+      {message} Please check the{' '}
       <span style={{ display: 'inline-flex' }}>
         <Link href="https://status.inngest.com/">status page.</Link>
       </span>
