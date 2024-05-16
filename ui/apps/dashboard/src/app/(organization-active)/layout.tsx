9294b1bda448c6ba9367a709d561f7c7508ccc17
Ana <anafilipadealmeida@gmail.com>
2024-05-16 18:30:17 +0100

    Add banner

--- ui/apps/dashboard/src/app/(organization-active)/layout.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from 'react';
+import { Link } from '@inngest/components/Link';
 
+import { Banner } from '@/components/Banner';
 import { URQLProvider } from '@/queries/URQLProvider';
 
 type OrganizationActiveLayoutProps = {
@@ -7,5 +9,15 @@ type OrganizationActiveLayoutProps = {
 };
 
 export default function OrganizationActiveLayout({ children }: OrganizationActiveLayoutProps) {
-  return <URQLProvider>{children}</URQLProvider>;
+  return (
+    <URQLProvider>
+      <Banner kind="warning">
+        We are experiencing some API issues. Please check the{' '}
+        <span style={{ display: 'inline-flex' }}>
+          <Link href="https://status.inngest.com/">status page.</Link>
+        </span>
+      </Banner>
+      {children}
+    </URQLProvider>
+  );
 }
