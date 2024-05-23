2921578c09a8a38f5a5a907d1020f7e96d772346
Ana <anafilipadealmeida@gmail.com>
2024-05-23 23:06:08 +0100

    Extract RunDetails

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/runs/[runID]/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/runs/[runID]/page.tsx
@@ -1,79 +1,12 @@
-'use client';
-
-import { RunDetails } from '@inngest/components/RunDetailsV2';
-import { StatusCell } from '@inngest/components/Table';
-import { cn } from '@inngest/components/utils/classNames';
-
-import { useEnvironment } from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/environment-context';
-import { useCancelRun } from '@/queries/useCancelRun';
-import { useRerun } from '@/queries/useRerun';
-import { pathCreator } from '@/utils/urls';
-import { toRunStatus } from '../../functions/[slug]/runs/utils';
-import { useRun } from './useRun';
+import { RunDetails } from '@/components/RunDetails/RunDetails';
 
 type Props = {
   params: {
     runID: string;
   };
-  standalone?: boolean;
 };
 
-export default function Page({ params, standalone = true }: Props) {
+export default function Page({ params }: Props) {
   const runID = decodeURIComponent(params.runID);
-  const env = useEnvironment();
-  const cancelRun = useCancelRun({ envID: env.id, runID });
-  const rerun = useRerun({ envID: env.id, envSlug: env.slug, runID });
-
-  const res = useRun({ envID: env.id, runID });
-  if (res.error) {
-    throw res.error;
-  }
-  if (res.isLoading && !res.data) {
-    return <Loading />;
-  }
-  const { run, trace } = res.data;
-
-  async function getOutput() {
-    return null;
-  }
-
-  return (
-    <div className={cn('overflow-y-auto', standalone && 'p-5 pt-8')}>
-      {standalone && (
-        <div className="flex flex-col gap-2 pb-6">
-          {/* @ts-ignore */}
-          {run.trace && <StatusCell status={toRunStatus(run.trace.status)} />}
-          <p className="text-2xl font-medium">{run.function.name}</p>
-          <p className="font-mono text-slate-500">{params.runID}</p>
-        </div>
-      )}
-      <RunDetails
-        standalone={standalone}
-        app={{
-          url: pathCreator.app({ envSlug: env.slug, externalAppID: run.function.app.externalID }),
-          ...run.function.app,
-        }}
-        cancelRun={cancelRun}
-        fn={run.function}
-        getOutput={getOutput}
-        rerun={rerun}
-        run={{
-          id: params.runID,
-          output: null,
-          trace,
-          url: pathCreator.runPopout({ envSlug: env.slug, runID: params.runID }),
-        }}
-      />
-    </div>
-  );
-}
-
-function Loading() {
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-2">
-        <div>Loading</div>
-      </div>
-    </div>
-  );
+  return <RunDetails runID={runID} />;
 }
