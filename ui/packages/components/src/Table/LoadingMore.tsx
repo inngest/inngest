48e18e359f9443ad84a5e4656b62e36fb23cbb0a
Ana <anafilipadealmeida@gmail.com>
2024-05-23 01:46:12 +0100

    Add Loading more component

---
+++ ui/packages/components/src/Table/LoadingMore.tsx
@@ -0,0 +1,13 @@
+import { IconSpinner } from '../icons/Spinner';
+
+export function LoadingMore() {
+  return (
+    <div className="relative mx-auto	mt-4 h-20 w-[510px] overflow-hidden">
+      <div
+        style={{ borderRadius: '50% / 100% 100% 0 0' }}
+        className="absolute top-3 h-24 w-[510px] bg-slate-700/30"
+      ></div>
+      <IconSpinner className="absolute left-0 right-0 top-10 mx-auto h-6 w-6 fill-white" />
+    </div>
+  );
+}
