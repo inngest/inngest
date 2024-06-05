e825c984275ec7c406ef67e1185b0968f764dc66
Aaron Harper <aaron@inngest.com>
2024-06-05 18:49:28 -0400

    Use symbol directly

--- ui/packages/components/src/utils/lazyLoad.ts
+++ ui/packages/components/src/utils/lazyLoad.ts
@@ -3,19 +3,18 @@
 // will be loaded in the future. This is useful for components that need to
 // fetch data asynchronously, but need to render something in the meantime
 
-const loadingSymbol = Symbol();
-const loadingPlaceholder = { [loadingSymbol]: true } as const;
+const loadingSentinel = Symbol();
 
 // This type is used to represent a value that is lazily loaded; think of it
 // like a Promise. It can be either the value itself or a loading symbol. Use
 // the isLazyDone type guard to check if the value is loaded
-export type Lazy<T> = T | typeof loadingPlaceholder;
+export type Lazy<T> = T | typeof loadingSentinel;
 
 /**
  * Type guard for checking if a lazy loaded value is done loading
  */
 export function isLazyDone<T>(data: Lazy<T>): data is T {
-  if (typeof data === 'object' && data !== null && loadingSymbol in data) {
+  if (data === loadingSentinel) {
     return false;
   }
 
@@ -29,7 +28,7 @@ export function isLazyDone<T>(data: Lazy<T>): data is T {
  */
 export function nullishToLazy<T>(data: T | undefined | null): Lazy<T> {
   if (data === undefined || data === null) {
-    return loadingPlaceholder;
+    return loadingSentinel;
   }
   return data;
 }
