c1b7091e61d1df6a63f1e67b244191215c68ab83
Darwin D Wu <darwin67@users.noreply.github.com>
2024-06-12 14:54:11 -0700

    wire up data loader for trace run

--- pkg/coreapi/graph/loaders/trace.go
+++ pkg/coreapi/graph/loaders/trace.go
@@ -2,10 +2,23 @@ package loader
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/graph-gophers/dataloader"
 	"github.com/inngest/inngest/pkg/cqrs"
 )
+
+type TraceRequestKey struct {
+	cqrs.TraceRunIdentifier
+}
+
+func (k *TraceRequestKey) Raw() any {
+	return k
+}
+
+func (k *TraceRequestKey) String() string {
+	return fmt.Sprintf("%s-%s", k.TraceID, k.RunID)
+}
 
 type traceReader struct {
 	loaders *loaders
@@ -13,5 +26,5 @@ type traceReader struct {
 }
 
 func (tr *traceReader) GetRunTrace(ctx context.Context, keys dataloader.Keys) []*dataloader.Result {
-	return nil
+	return []*dataloader.Result{}
 }
