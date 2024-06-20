b06ebd68821b414a58e7b06e05962c4dac11b64b
Ana <anafilipadealmeida@gmail.com>
2024-06-21 00:44:49 +0100

    Add color tokens to tooltips

--- ui/packages/components/tailwind.config.ts
+++ ui/packages/components/tailwind.config.ts
@@ -136,6 +136,10 @@ export default {
       gridTemplateColumns: {
         dashboard: '1fr 1fr 1fr 432px',
       },
+      boxShadowColor: {
+        // temporary token
+        tooltip: 'rgb(var(--color-background-canvas-muted) / <alpha-value>)',
+      },
       boxShadow: {
         'outline-primary-light':
           'inset 0 1px 0 0 rgba(255, 255, 255, 0.1), inset 0 0 0 1px rgba(255, 255, 255, 0.1), 0 1px 3px rgba(0, 0, 0, 0.2)',
