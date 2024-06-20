3b8adc7448e0f3d12ed721b9de55f553cee76d9f
Chris Dzombak <chris@inngest.com>
2024-06-20 11:59:37 -0400

    hide New Replay button when a function is paused

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/[slug]/replay/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/[slug]/replay/page.tsx
@@ -1,10 +1,23 @@
 'use client';
 
 import React from 'react';
+import { useQuery } from 'urql';
 
 import { useEnvironment } from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/environment-context';
 import NewReplayButton from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/[slug]/logs/NewReplayButton';
+import { graphql } from '@/gql';
 import { ReplayList } from './ReplayList';
+
+const GetFunctionPauseStateDocument = graphql(`
+  query GetFunctionPauseState($environmentID: ID!, $functionSlug: String!) {
+    environment: workspace(id: $environmentID) {
+      function: workflowBySlug(slug: $functionSlug) {
+        id
+        isPaused
+      }
+    }
+  }
+`);
 
 type FunctionReplayPageProps = {
   params: {
@@ -12,12 +25,20 @@ type FunctionReplayPageProps = {
   };
 };
 export default function FunctionReplayPage({ params }: FunctionReplayPageProps) {
-  const functionSlug = decodeURIComponent(params.slug);
   const env = useEnvironment();
+  const functionSlug = decodeURIComponent(params.slug);
+  const [{ data }] = useQuery({
+    query: GetFunctionPauseStateDocument,
+    variables: {
+      environmentID: env.id,
+      functionSlug,
+    },
+  });
+  const functionIsPaused = data?.environment.function?.isPaused || false;
 
   return (
     <>
-      {!env.isArchived && (
+      {!env.isArchived && !functionIsPaused && (
         <div className="flex items-center justify-end border-b border-slate-300 px-5 py-2">
           <NewReplayButton functionSlug={functionSlug} />
         </div>
