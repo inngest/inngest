8d7fe06eb11686ff51b3df77d6dc878ddafbcead
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-06-21 12:44:00 +0000

    `consumePause` thoughts

--- pkg/execution/state/redis_state/lua/consumePause.lua
+++ pkg/execution/state/redis_state/lua/consumePause.lua
@@ -16,14 +16,20 @@ local keyMetadata   = KEYS[4]
 local pauseDataKey = ARGV[1] -- used to set data in run state store
 local pauseDataVal = ARGV[2] -- data to set
 
+-- OLD: {estate}:pauses:[PAUSE_ID]
 local pause = redis.call("GET", pauseKey)
 if pause == false or pause == nil then
 	return 1
 end
 
-redis.call("DEL", pauseKey)
+-------------------------------------------------
+-- bang - will be grabbed again
 
 if actionKey ~= nil and pauseDataKey ~= "" then
+	-- [stepId1, stepId43, stepIdsifgs, ...]
+	redis.call("ISITINTHEARRAYMATE", stackKey, pauseDataKey)
+
+	-- do tis or don't
 	redis.call("RPUSH", stackKey, pauseDataKey)
 	redis.call("HSET", actionKey, pauseDataKey, pauseDataVal)
 	redis.call("HINCRBY", keyMetadata, "step_count", 1)
@@ -31,3 +37,8 @@ if actionKey ~= nil and pauseDataKey ~= "" then
 end
 
 return 0
+
+--------------------------------------------------
+-- bang - will be grabbed again
+
+redis.call("DEL", pauseKey)
