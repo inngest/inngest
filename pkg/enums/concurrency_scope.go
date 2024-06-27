2b24cfddf3834614458efafa9e8d00db871ae4d2
Chris Dzombak <chris@inngest.com>
2024-06-27 12:22:04 -0400

    explicitly define values for ConcurrencyScope enum

--- pkg/enums/concurrency_scope.go
+++ pkg/enums/concurrency_scope.go
@@ -6,11 +6,11 @@ type ConcurrencyScope int
 
 const (
 	// ConcurrencyScopeFn represents the default ConcurrencyScope 0, which means limit to the specific function
-	ConcurrencyScopeFn ConcurrencyScope = iota
+	ConcurrencyScopeFn ConcurrencyScope = 0
 	// ConcurrencyScopeEnv limits concurrency to the given environment, forcing environment limits across functions
 	// in the same environment.
-	ConcurrencyScopeEnv
+	ConcurrencyScopeEnv ConcurrencyScope = 1
 	// ConcurrencyScopeAccount limits concurrency to the entire account, foricng global concurrency limits across
 	// all functions within your account.
-	ConcurrencyScopeAccount
+	ConcurrencyScopeAccount ConcurrencyScope = 2
 )
