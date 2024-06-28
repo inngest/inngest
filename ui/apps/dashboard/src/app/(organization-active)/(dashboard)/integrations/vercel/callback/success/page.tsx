0d7e9cd5dbe650463599f6cd172a22ce708b8e57
Jacob Heric <jacob@jacobheric.com>
2024-06-28 16:00:08 -0400

    some more vercel design cleanup

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/integrations/vercel/callback/success/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/integrations/vercel/callback/success/page.tsx
@@ -32,7 +32,7 @@ export default async function SuccessPage({ searchParams }: SuccessProps) {
         <Card className="w-full">
           <Card.Content className="rounded-0 p-0">
             <div className="border-subtle flex h-[72px] flex-row items-start justify-start border-b p-4">
-              <div className="bg-primary-moderate mr-3 mt-1 flex h-4 w-4 items-center justify-center rounded-[50%]">
+              <div className="bg-primary-moderate mr-3 mt-1 flex h-4 w-4 shrink-0 items-center justify-center rounded-[50%] ">
                 <RiCheckLine size={12} className="text-onContrast" />
               </div>
               <div className="text-subtle text-base">
@@ -41,8 +41,8 @@ export default async function SuccessPage({ searchParams }: SuccessProps) {
                 <span className="font-semibold">INNGEST_EVENT_KEY</span> environment variables set.
               </div>
             </div>
-            <div className="flex h-[72px] flex-row  items-start justify-start p-4">
-              <div className="bg-primary-moderate mr-3 mt-1 flex h-4 w-4 items-center justify-center rounded-[50%]">
+            <div className="flex h-[72px] flex-row items-start justify-start p-4">
+              <div className="bg-primary-moderate mr-3 mt-1 flex h-4 w-4 shrink-0 items-center justify-center rounded-[50%]">
                 <RiCheckLine size={12} className="text-white" />
               </div>
               <div className="text-subtle text-base">
