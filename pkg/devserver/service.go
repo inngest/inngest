5447d4d5d1632103c5806338a11d35e8cb34891c
Bruno Scheufler <git@brunoscheufler.com>
2024-07-03 16:58:27 +0200

    INN-3262: Implement state size limit (#1543)
    * INN-3262: Implement state size limit
    
    * Update executor.go
    
    * Update executor.go

--- pkg/devserver/service.go
+++ pkg/devserver/service.go
@@ -40,14 +40,15 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 )
 
-func newService(opts StartOpts, runner runner.Runner, data cqrs.Manager, pb pubsub.Publisher, stepLimitOverrides map[string]int) *devserver {
+func newService(opts StartOpts, runner runner.Runner, data cqrs.Manager, pb pubsub.Publisher, stepLimitOverrides, stateSizeLimitOverrides map[string]int) *devserver {
 	return &devserver{
-		data:               data,
-		runner:             runner,
-		opts:               opts,
-		handlerLock:        &sync.Mutex{},
-		publisher:          pb,
-		stepLimitOverrides: stepLimitOverrides,
+		data:                    data,
+		runner:                  runner,
+		opts:                    opts,
+		handlerLock:             &sync.Mutex{},
+		publisher:               pb,
+		stepLimitOverrides:      stepLimitOverrides,
+		stateSizeLimitOverrides: stateSizeLimitOverrides,
 	}
 }
 
@@ -62,7 +63,8 @@ type devserver struct {
 
 	data cqrs.Manager
 
-	stepLimitOverrides map[string]int
+	stepLimitOverrides      map[string]int
+	stateSizeLimitOverrides map[string]int
 
 	// runner stores the runner
 	runner    runner.Runner
