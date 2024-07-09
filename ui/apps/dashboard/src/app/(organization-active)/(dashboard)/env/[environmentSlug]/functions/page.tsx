f628384b080ef5614ab0ef56d6d8e3ea703ca375
Jacob Heric <jacob@jacobheric.com>
2024-07-09 15:04:50 -0400

    revert function tweaks as they are not meaningful

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/functions/page.tsx
@@ -1,37 +1,27 @@
 'use client';
 
 import { Button } from '@inngest/components/Button';
+import { useBooleanSearchParam } from '@inngest/components/hooks/useSearchParam';
 import { IconFunction } from '@inngest/components/icons/Function';
 
 import { useEnvironment } from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/environment-context';
 import Header, { type HeaderLink } from '@/components/Header/Header';
-import { SkeletonCard } from '../apps/AppCard';
 import { FunctionTable } from './FunctionTable';
 import { useRows } from './useRows';
 
-export default function FunctionListPage({
-  searchParams: { archived: isArchived },
-}: {
-  searchParams: { archived: string };
-}) {
-  const archived = isArchived === 'true';
-  const { error, isLoading, hasMore, loadMore, rows } = useRows({ archived: archived });
+export const runtime = 'nodejs';
+
+export default function FunctionListPage() {
+  const [archived] = useBooleanSearchParam('archived');
+
+  const { error, isLoading, hasMore, loadMore, rows } = useRows({ archived: archived ?? false });
   if (error) {
     throw error;
-  }
-  if (isLoading) {
-    return (
-      <div className="mb-4 mt-16 flex items-center justify-center">
-        <div className="w-full max-w-[1200px]">
-          <SkeletonCard />
-        </div>
-      </div>
-    );
   }
 
   return (
     <>
-      <FunctionsHeader archived={archived} />
+      <FunctionsHeader />
       <div className="flex min-h-0 flex-1 flex-col divide-y divide-slate-100">
         <FunctionTable rows={rows} />
 
@@ -50,8 +40,9 @@ export default function FunctionListPage({
   );
 }
 
-function FunctionsHeader({ archived }: { archived: boolean }) {
+function FunctionsHeader() {
   const env = useEnvironment();
+  const [archived] = useBooleanSearchParam('archived');
 
   const navLinks: HeaderLink[] = [
     {
