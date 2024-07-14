fe6709d625767219913712bd20f462ac4e6adc2a
Ana <anafilipadealmeida@gmail.com>
2024-07-14 20:27:00 +0100

    Add color tokens to Popover

--- ui/packages/components/src/Popover/Popover.tsx
+++ ui/packages/components/src/Popover/Popover.tsx
@@ -11,14 +11,14 @@ export const PopoverContent = forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Portal>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
 >(({ children, className, ...props }, forwardedRef) => {
-  const container = document.getElementById('modals');
+  const container = typeof document !== 'undefined' ? document.getElementById('modals') : undefined;
   return (
     <PopoverPrimitive.Portal container={container}>
       <PopoverPrimitive.Content
         sideOffset={5}
         ref={forwardedRef}
         className={cn(
-          'z-[100] max-h-[var(--radix-popover-content-available-height)] overflow-y-auto overflow-x-hidden rounded bg-white drop-shadow',
+          'bg-canvasBase z-[100] max-h-[var(--radix-popover-content-available-height)] overflow-y-auto overflow-x-hidden rounded drop-shadow',
           className
         )}
         {...props}
