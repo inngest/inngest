b5f1bcc4f50a2e944fdce0749b06130129b28524
Ana <anafilipadealmeida@gmail.com>
2024-07-15 13:59:10 +0100

    Adjust padding

--- ui/packages/components/src/DatePicker/DateInputButton.tsx
+++ ui/packages/components/src/DatePicker/DateInputButton.tsx
@@ -9,7 +9,7 @@ export const DateInputButton = forwardRef<HTMLButtonElement, DateInputButtonProp
       <button
         {...props}
         ref={forwardRef}
-        className={`border-muted bg-canvasBase outline-primary-moderate h-8 rounded-lg border px-3.5 text-sm leading-none outline-2 transition-all focus:outline ${className}`}
+        className={`border-muted bg-canvasBase outline-primary-moderate h-8 rounded-lg border px-2 text-sm leading-none outline-2 transition-all focus:outline ${className}`}
       >
         <span className="flex items-center gap-2">
           <RiCalendarLine className="text-disabled h-5 w-5" />
