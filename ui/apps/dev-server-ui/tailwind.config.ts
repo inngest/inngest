404673f954937f3c2a699ae440b731f812d0779d
Jacob Heric <jacob@jacobheric.com>
2024-07-18 11:30:49 -0400

    switch dev server as well and remove old inline fonts

--- ui/apps/dev-server-ui/tailwind.config.ts
+++ ui/apps/dev-server-ui/tailwind.config.ts
@@ -1,19 +1,9 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 import sharedConfig from '../../packages/components/tailwind.config';
 
 export default {
   ...sharedConfig,
-  theme: {
-    extend: {
-      ...sharedConfig.theme.extend,
-      fontFamily: {
-        sans: ['var(--font-inter-tight)', ...defaultTheme.fontFamily.sans],
-        mono: ['var(--font-roboto-mono)', ...defaultTheme.fontFamily.mono],
-      },
-    },
-  },
   content: ['./src/**/*.{ts,tsx}', '../../packages/components/src/**/*.{ts,tsx}'],
   darkMode: 'class',
   plugins: [require('@headlessui/tailwindcss')],
