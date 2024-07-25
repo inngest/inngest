adb82ef912023610165edd55a4cee5aed311d21d
Aaron Harper <aaron@inngest.com>
2024-07-25 15:19:23 -0400

    Add trust probe

--- pkg/syscode/codes.go
+++ pkg/syscode/codes.go
@@ -5,7 +5,12 @@ const (
 	CodeComboUnsupported          = "combo_unsupported"
 	CodeConcurrencyLimitInvalid   = "concurrency_limit_invalid"
 	CodeConfigInvalid             = "config_invalid"
+	CodeHTTPMissingHeader         = "http_missing_header"
+	CodeHTTPNotOK                 = "http_not_ok"
+	CodeHTTPUnreachable           = "http_unreachable"
+	CodeNotSDK                    = "not_sdk"
 	CodeOutputTooLarge            = "output_too_large"
+	CodeSigVerificationFailed     = "sig_verification_failed"
 	CodeUnknown                   = "unknown"
 	CodeBatchKeyExpressionInvalid = "batch_key_expression_invalid"
 )
