a8407046a06f5a6c92306bb1582fddd0180c7038
Chris Dzombak <chris@inngest.com>
2024-07-29 15:00:22 -0400

    lint: comment

--- pkg/enums/partition_type.go
+++ pkg/enums/partition_type.go
@@ -14,7 +14,7 @@ const (
 	// PartitionTypeDefault indicates a regular partition for job items without
 	// concurrency keys, or with only a fn level concurrency key.
 	PartitionTypeDefault PartitionType = 0
-	// PartitionTypenConcurrency represents a partition for a custom concurrency key
+	// PartitionTypeConcurrencyKey represents a partition for a custom concurrency key
 	PartitionTypeConcurrencyKey PartitionType = 1
 	// PartitionTypeThrottle represents a partition for a custom throttling key.
 	PartitionTypeThrottle PartitionType = 2
