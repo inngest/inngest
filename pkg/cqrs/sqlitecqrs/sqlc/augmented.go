c35aef7b2874525806fd5ea63b2a43bf70d49a41
Darwin D Wu <darwin67@users.noreply.github.com>
2024-07-29 18:14:25 -0700

    impl template

--- pkg/cqrs/sqlitecqrs/sqlc/augmented.go
+++ pkg/cqrs/sqlitecqrs/sqlc/augmented.go
@@ -1,7 +1,11 @@
 package sqlc
 
 import (
+	"fmt"
 	"strings"
+
+	"github.com/google/uuid"
+	"github.com/inngest/inngest/pkg/cqrs"
 )
 
 func (tr *TraceRun) EventIDs() []string {
@@ -10,4 +14,45 @@ func (tr *TraceRun) EventIDs() []string {
 	}
 
 	return strings.Split(string(tr.TriggerIds), ",")
+}
+
+// --- Event
+
+func (e *Event) ToCQRS() (*cqrs.Event, error) {
+	evt := &cqrs.Event{
+		ID:         e.InternalID,
+		ReceivedAt: e.ReceivedAt,
+		EventID:    e.EventID,
+		EventName:  e.EventName,
+		EventTS:    e.EventTs.UnixMilli(),
+	}
+
+	if v, ok := e.AccountID.(string); ok {
+		id, err := uuid.Parse(v)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing account ID: %w", err)
+		}
+		evt.AccountID = id
+	}
+	if v, ok := e.WorkspaceID.(string); ok {
+		id, err := uuid.Parse(v)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing environment ID: %w", err)
+		}
+		evt.WorkspaceID = id
+	}
+
+	// TODO: Event data
+	{
+	}
+
+	// TODO: Event user
+	{
+	}
+
+	if e.EventV.Valid {
+		evt.EventVersion = e.EventV.String
+	}
+
+	return evt, nil
 }
