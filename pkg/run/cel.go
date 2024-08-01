20d193e66bd3c0bab93e733671265545ea845f11
Darwin D Wu <darwin67@users.noreply.github.com>
2024-07-31 18:06:30 -0700

    move sql pre-filter logic into handler

--- pkg/run/cel.go
+++ pkg/run/cel.go
@@ -6,6 +6,9 @@ import (
 	"regexp"
 	"strings"
 
+	sq "github.com/doug-martin/goqu/v9"
+	"github.com/google/cel-go/common/operators"
+	"github.com/inngest/expr"
 	"github.com/inngest/inngest/pkg/event"
 	"github.com/inngest/inngest/pkg/expressions"
 	"golang.org/x/sync/errgroup"
@@ -76,6 +79,37 @@ func (h *ExpressionHandler) HasOutputFilters() bool {
 	return len(h.OutputExprList) > 0
 }
 
+// create pre-filters for database queries
+// - ULID
+// - event id (idempotency key)
+// - event name
+// - version
+// - timestamp
+func (h *ExpressionHandler) ToSQLEventFilters(ctx context.Context) ([]sq.Expression, error) {
+	filters := []sq.Expression{}
+	parser := expressions.ParserSingleton()
+
+	for _, exp := range h.EventExprList {
+		tree, err := parser.Parse(ctx, expr.StringExpression(exp))
+		if err != nil {
+			return nil, fmt.Errorf("error evaluating event expression '%s': %w", exp, err)
+		}
+
+		// check if the expression is targeting the event or not
+		if tree.Root.HasPredicate() && !eventRegex.MatchString(tree.Root.Predicate.Ident) {
+			continue
+		}
+
+		expFilter, err := toSQLEventFilters(ctx, []*expr.Node{&tree.Root})
+		if err != nil {
+			return nil, err
+		}
+		filters = append(filters, expFilter...)
+	}
+
+	return filters, nil
+}
+
 func (h *ExpressionHandler) MatchEventExpressions(ctx context.Context, evt event.Event) (bool, error) {
 	eg := errgroup.Group{}
 	res := make([]bool, len(h.EventExprList))
@@ -114,4 +148,95 @@ func allMatches(res []bool) bool {
 		}
 	}
 	return true
+}
+
+// toSQLEventFilter parses the passed in nodes and converts them into SQL filter expressions
+func toSQLEventFilters(ctx context.Context, nodes []*expr.Node) ([]sq.Expression, error) {
+	filters := []sq.Expression{}
+
+	for _, n := range nodes {
+		if n.HasPredicate() {
+			literal := n.Predicate.Literal
+
+			switch n.Predicate.Ident {
+			case "event.id":
+				id, ok := literal.(string)
+				if !ok {
+					return nil, fmt.Errorf("expects 'event.id' to be a string: %v", literal)
+				}
+				switch n.Predicate.Operator {
+				case operators.Equals:
+					filters = append(filters, sq.C("event_id").Eq(id))
+				case operators.NotEquals:
+					filters = append(filters, sq.C("event_id").Neq(id))
+				}
+			case "event.name":
+				name, ok := literal.(string)
+				if !ok {
+					return nil, fmt.Errorf("expects 'event.name' to be a string: %v", literal)
+				}
+				switch n.Predicate.Operator {
+				case operators.Equals:
+					filters = append(filters, sq.C("event_name").Eq(name))
+				case operators.NotEquals:
+					filters = append(filters, sq.C("event_name").Neq(name))
+				}
+			case "event.ts":
+				ts, ok := literal.(int64)
+				if !ok {
+					return nil, fmt.Errorf("expects 'event.ts' to be an integer: %v", literal)
+				}
+				var f sq.Expression
+				field := "event_ts"
+
+				switch n.Predicate.Operator {
+				case operators.Greater:
+					f = sq.C(field).Gt(ts)
+				case operators.GreaterEquals:
+					f = sq.C(field).Gte(ts)
+				case operators.Equals:
+					f = sq.C(field).Eq(ts)
+				case operators.Less:
+					f = sq.C(field).Lt(ts)
+				case operators.LessEquals:
+					f = sq.C(field).Lte(ts)
+				case operators.NotEquals:
+					f = sq.C(field).Neq(ts)
+				}
+				if f != nil {
+					filters = append(filters, f)
+				}
+			case "event.v":
+				v, ok := literal.(string)
+				if !ok {
+					return nil, fmt.Errorf("expects 'event.v' to be a string: %v", literal)
+				}
+				switch n.Predicate.Operator {
+				case operators.Equals:
+					filters = append(filters, sq.C("event_v").Eq(v))
+				case operators.NotEquals:
+					filters = append(filters, sq.C("event_v").Neq(v))
+				}
+			}
+		}
+
+		// check for further nesting
+		if n.Ands != nil {
+			nested, err := toSQLEventFilters(ctx, n.Ands)
+			if err != nil {
+				return nil, err
+			}
+			filters = append(filters, sq.And(nested...))
+		}
+
+		if n.Ors != nil {
+			nested, err := toSQLEventFilters(ctx, n.Ors)
+			if err != nil {
+				return nil, err
+			}
+			filters = append(filters, sq.Or(nested...))
+		}
+	}
+
+	return filters, nil
 }
