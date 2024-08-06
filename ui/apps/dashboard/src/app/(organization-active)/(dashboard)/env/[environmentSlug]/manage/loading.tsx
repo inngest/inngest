f6fd85d8571a2c07fae345778ab5795d68e87381
Jacob Heric <jacob@jacobheric.com>
2024-08-06 13:32:15 -0400

    fix bugs and add loaders

---
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/manage/loading.tsx
@@ -0,0 +1,7 @@
+import { getBooleanFlag } from '@/components/FeatureFlags/ServerFeatureFlag';
+import PageSkeleton from '@/components/Skeleton/PageSkeleton';
+
+export default async function Loading() {
+  const newIANav = await getBooleanFlag('new-ia-nav');
+  return newIANav ? <PageSkeleton text="Webhooks" /> : '';
+}
