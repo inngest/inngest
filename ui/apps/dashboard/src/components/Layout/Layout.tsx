2e843821f4f0750b5e78e4899788b5b06940fdb1
Jacob Heric <jacob@jacobheric.com>
2024-08-06 14:06:32 -0400

    up z-index on shared modal for new layout sticky nav

--- ui/apps/dashboard/src/components/Layout/Layout.tsx
+++ ui/apps/dashboard/src/components/Layout/Layout.tsx
@@ -23,7 +23,7 @@ export default async function Layout({ activeEnv, children }: LayoutProps) {
     <div className="fixed z-50 flex h-screen w-full flex-row justify-start overflow-y-scroll overscroll-y-none">
       <SideBar collapsed={collapsed} activeEnv={activeEnv} profile={profile} />
 
-      <div className="z-40 flex w-full flex-col">
+      <div className="flex w-full flex-col">
         <IncidentBanner />
         {children}
       </div>
