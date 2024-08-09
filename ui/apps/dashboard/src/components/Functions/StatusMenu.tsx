f0c8291348c2fc01789c44acc2821b30a339a2af
Jacob Heric <jacob@jacobheric.com>
2024-08-09 13:40:30 -0400

    function status zindex (#1664)
    * dump function z-index
    
    * bump z-index back up

--- ui/apps/dashboard/src/components/Functions/StatusMenu.tsx
+++ ui/apps/dashboard/src/components/Functions/StatusMenu.tsx
@@ -17,7 +17,7 @@ export const StatusMenu = ({ envSlug, archived }: { envSlug: string; archived: b
       label="Pause runs"
       multiple={false}
       value={archived ? archivedOption : activeOption}
-      className="z-[49] mr-3 h-[30px]"
+      className="z-20 mr-3 h-[30px]"
     >
       <Select.Button className="h-[28px] w-[142px] py-1 pl-2 pr-3">
         <div className="mr-2 flex flex-row items-center text-sm">
