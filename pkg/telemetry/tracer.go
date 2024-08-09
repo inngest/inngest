8bb6812af5f409dc4a95e926d86a2c15e341c9fb
Darwin D Wu <darwin67@users.noreply.github.com>
2024-08-08 20:02:44 -0700

    update nats connection configuration

--- pkg/telemetry/tracer.go
+++ pkg/telemetry/tracer.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/inngest/inngest/pkg/consts"
 	"github.com/inngest/inngest/pkg/inngest/log"
+	"github.com/inngest/inngest/pkg/logger"
 	"github.com/nats-io/nats.go"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/trace"
@@ -43,6 +45,8 @@ type TracerOpts struct {
 	TraceEndpoint            string
 	TraceURLPath             string
 	TraceMaxPayloadSizeBytes int
+
+	NATS *NatsExporterOpts
 }
 
 func (o TracerOpts) Endpoint() string {
@@ -150,13 +154,36 @@ type natsSpanExporter struct {
 	conn *nats.Conn
 }
 
+type NatsExporterOpts struct {
+	URLs []string
+}
+
 // NewNATSSpanExporter creates an otel compatible exporter that ships the spans to NATS
-func NewNATSSpanExporter(ctx context.Context, urls string) (trace.SpanExporter, error) {
+func NewNATSSpanExporter(ctx context.Context, opts *NatsExporterOpts) (trace.SpanExporter, error) {
+	if opts == nil {
+		return nil, fmt.Errorf("nats exporter setup options unavailable")
+	}
+
+	host, err := os.Hostname()
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving hostname: %w", err)
+	}
+
+	urls := strings.Join(opts.URLs, ",")
+	name := fmt.Sprintf("trace-exporter-%s", host)
+
 	// urls should be comma delimited strings
-	conn, err := nats.Connect(urls)
+	conn, err := nats.Connect(urls,
+		nats.Name(name),
+	)
 	if err != nil {
 		return nil, fmt.Errorf("error connecting to NATS: %w", err)
 	}
+
+	logger.StdlibLogger(ctx).Info("established connection with NATS server",
+		"urls", opts.URLs,
+		"name", name,
+	)
 
 	return &natsSpanExporter{
 		conn: conn,
