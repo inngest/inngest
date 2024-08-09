4fbd0a04b3134727e983cb4249a796101f95f4af
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-08-09 12:12:56 +0000

    Like exporting, only import Dev Server data if in lite

--- pkg/lite/lite.go
+++ pkg/lite/lite.go
@@ -371,7 +371,6 @@ func createInmemoryRedisConnection(ctx context.Context) (rueidis.Client, error) 
 	}
 
 	rc, err := rueidis.NewClient(rueidis.ClientOption{
-		AlwaysPipelining:  false,
 		InitAddress:       []string{redisSingleton.Addr()},
 		DisableCache:      true,
 		BlockingPoolSize:  1,
