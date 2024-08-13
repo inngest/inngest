9f2779f103ac70e2e1addb3be16a77e090f27102
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2024-08-13 15:29:24 +0000

    Add `LiteMaxQueueChunkSize` for limiting chunk size in SQLite

--- pkg/consts/consts.go
+++ pkg/consts/consts.go
@@ -100,6 +100,8 @@ const (
 	// in which priority factors are taken into account.
 	FutureAtLimit = 2 * time.Second
 
+	LiteMaxQueueChunkSize = 1024 * 1024 * 800 // 800MB
+
 	DevServerTempDir     = ".inngest"
 	DevServerDbFile      = "dev_db.db"
 	DevServerRdbFile     = "dev_state.rdb"
