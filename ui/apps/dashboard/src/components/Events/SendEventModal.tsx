ee2c75c6df86ca05af2048167cdb1e092cc9e218
Jacob Heric <jacob@jacobheric.com>
2024-09-03 12:54:01 -0400

    clean up imports

--- ui/apps/dashboard/src/components/Events/SendEventModal.tsx
+++ ui/apps/dashboard/src/components/Events/SendEventModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { Tab } from '@headlessui/react';
