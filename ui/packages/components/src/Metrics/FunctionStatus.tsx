4fd32a734efdaf45d0521197e647b2d79d41b386
Jacob Heric <jacob@jacobheric.com>
2024-09-06 12:54:23 -0400

    hover label background hack to prevent overlap with total label

--- ui/packages/components/src/Metrics/FunctionStatus.tsx
+++ ui/packages/components/src/Metrics/FunctionStatus.tsx
@@ -1,75 +1,90 @@
+import { color } from 'framer-motion';
+
 import { Chart, type ChartProps } from '../Chart/Chart';
 import { FunctionInfo } from './FunctionInfo';
 
 const data = [
-  { value: 1048, name: 'Completed' },
-  { value: 735, name: 'Running' },
-  { value: 580, name: 'Queued' },
-  { value: 484, name: 'Cancelled' },
-  { value: 300, name: 'Failed' },
+  { value: 1048, name: 'Completed', itemStyle: { color: '#2c9b63' } },
+  { value: 735, name: 'Running', itemStyle: { color: '#52b2fd' } },
+  { value: 580, name: 'Queued', itemStyle: { color: '#8b74f9' } },
+  { value: 484, name: 'Cancelled', itemStyle: { color: '#e2e2e2' } },
+  { value: 300, name: 'Failed', itemStyle: { color: '#fa8d86' } },
 ];
 
 const sum = data.reduce((acc, { value }) => acc + value, 0);
 
 const percent = (part: number) => `${((part / sum) * 100).toFixed(0)}%`;
-
-const option: ChartProps['option'] = {
-  legend: {
-    orient: 'vertical',
-    right: '10%',
-    top: 'center',
-    icon: 'circle',
-    formatter: (name: string) =>
-      [name, percent(data.find((d) => d.name === name)?.value || 0)].join(' '),
-  },
-  series: [
-    {
-      name: 'Function Runs',
-      type: 'pie',
-      radius: ['45%', '80%'],
-      center: ['25%', '50%'],
-      avoidLabelOverlap: false,
-      itemStyle: {
-        borderColor: '#fff',
-        borderWidth: 2,
-      },
-      label: {
-        show: false,
-        position: 'center',
-      },
-      emphasis: {
-        label: {
-          show: true,
-          fontSize: 10,
-          formatter: ({ data }: any): string => {
-            return [`{name| ${data?.name}}`, `{value| ${data?.value}}`].join('\n');
-          },
-          rich: {
-            name: {
-              fontSize: 12,
-              lineHeight: 16,
-            },
-            value: {
-              fontSize: 16,
-              lineHeight: 20,
-              fontWeight: 500,
-            },
-          },
-        },
-      },
-      labelLine: {
-        show: false,
-      },
-      data,
+const holeLabel = {
+  rich: {
+    name: {
+      fontSize: 12,
+      lineHeight: 16,
     },
-  ],
+    value: {
+      fontSize: 16,
+      lineHeight: 20,
+      fontWeight: 500,
+    },
+  },
 };
 
-export const FunctionStatus = () => (
-  <div className="bg-canvasBase border-subtle flex h-[300px] w-[448px] flex-col rounded-lg p-5">
-    <div className="text-subtle flex flex-row items-center gap-x-2 text-lg">
-      Function Status <FunctionInfo />
+export const FunctionStatus = () => {
+  const option: ChartProps['option'] = {
+    legend: {
+      orient: 'vertical',
+      right: '10%',
+      top: 'center',
+      icon: 'circle',
+      formatter: (name: string) =>
+        [name, percent(data.find((d) => d.name === name)?.value || 0)].join(' '),
+    },
+
+    series: [
+      {
+        name: 'Function Runs',
+        type: 'pie',
+        radius: ['45%', '80%'],
+        center: ['25%', '50%'],
+        itemStyle: {
+          borderColor: '#fff',
+          borderWidth: 2,
+        },
+        labelLayout: ({ dataIndex }) => {
+          return { hideOverlap: true };
+        },
+        avoidLabelOverlap: true,
+        label: {
+          show: true,
+          position: 'center',
+          formatter: (): string => {
+            return [`{name| Total runs}`, `{value| ${sum}}`].join('\n');
+          },
+          ...holeLabel,
+        },
+        emphasis: {
+          label: {
+            show: true,
+            formatter: ({ data }: any): string => {
+              return [`{name| ${data?.name}}`, `{value| ${data?.value}}`].join('\n');
+            },
+            backgroundColor: '#fff',
+            width: 80,
+            ...holeLabel,
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data,
+      },
+    ],
+  };
+  return (
+    <div className="bg-canvasBase border-subtle flex h-[300px] w-[448px] flex-col rounded-lg p-5">
+      <div className="text-subtle flex flex-row items-center gap-x-2 text-lg">
+        Function Status <FunctionInfo />
+      </div>
+      <Chart option={option} />
     </div>
-    <Chart option={option} />
-  </div>
-);
+  );
+};
