74b3a682b68108eae9b17e55a3dbf81ecd44fce6
Ana <anafilipadealmeida@gmail.com>
2024-09-09 21:52:05 +0100

    Move flag to layout

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/onboarding/layout.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/onboarding/layout.tsx
@@ -1,17 +1,18 @@
 import { Header } from '@inngest/components/Header/Header';
 
+import { ServerFeatureFlag } from '@/components/FeatureFlags/ServerFeatureFlag';
 import Menu from '@/components/Onboarding/Menu';
 import { pathCreator } from '@/utils/urls';
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
-    <div>
+    <ServerFeatureFlag flag="onboarding-flow-cloud">
       <Header breadcrumb={[{ text: 'Getting started', href: pathCreator.onboarding() }]} />
 
       <div className="my-12 grid grid-cols-3">
         <main className="col-span-2 mx-20">{children}</main>
         <Menu />
       </div>
-    </div>
+    </ServerFeatureFlag>
   );
 }
--- ui/apps/dashboard/src/components/Onboarding/content.ts
+++ ui/apps/dashboard/src/components/Onboarding/content.ts
@@ -55,4 +55,4 @@ export const onboardingMenuStepContent: OnboardingMenuContent = {
       icon: RiMagicLine,
     },
   },
-};
+} as const;
