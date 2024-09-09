694bd99db6fbe0568edf1c04155eb62f44bb1345
Ana <anafilipadealmeida@gmail.com>
2024-09-09 19:18:00 +0100

    Add onboarding types

--- ui/apps/dashboard/src/components/Onboarding/content.ts
+++ ui/apps/dashboard/src/components/Onboarding/content.ts
@@ -1,4 +1,7 @@
-import { type OnboardingWidgetContent } from './types';
+import { AppsIcon } from '@inngest/components/icons/sections/Apps';
+import { RiLoopRightLine, RiMagicLine, RiSendPlaneLine } from '@remixicon/react';
+
+import { type OnboardingMenuContent, type OnboardingWidgetContent } from './types';
 
 export const onboardingWidgetContent: OnboardingWidgetContent = {
   step: {
@@ -25,5 +28,31 @@ export const onboardingWidgetContent: OnboardingWidgetContent = {
   },
   tooltip: {
     close: "Close this widget. Reopen from the 'Help & Feedback' menu, if needed.",
+  },
+};
+
+export const onboardingMenuStepContent: OnboardingMenuContent = {
+  title: 'Explore onboarding guide',
+  step: {
+    1: {
+      title: 'Create Inngest app',
+      description: 'Start building in local development',
+      icon: AppsIcon,
+    },
+    2: {
+      title: 'Deploy Inngest app',
+      description: 'Host your app on any platform or infra',
+      icon: RiSendPlaneLine,
+    },
+    3: {
+      title: 'Sync app to Inngest',
+      description: 'Tell Inngest where your app is running',
+      icon: RiLoopRightLine,
+    },
+    4: {
+      title: 'Invoke your function',
+      description: 'Trigger and monitor your first function',
+      icon: RiMagicLine,
+    },
   },
 };
