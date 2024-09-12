c8919835dbc7b7413a2b202e925487113cb374c1
Ana <anafilipadealmeida@gmail.com>
2024-09-12 02:37:05 +0100

    Fix search input

--- ui/packages/components/src/Filter/EntityFilter.tsx
+++ ui/packages/components/src/Filter/EntityFilter.tsx
@@ -43,7 +43,7 @@ export default function EntityFilter({
       isLabelVisible
     >
       <SelectWithSearch.Button isLabelVisible>
-        <div className="w-24 truncate text-nowrap text-left">
+        <div className="min-w-7 max-w-24 truncate text-nowrap text-left">
           {selectedEntities.length === 1 && !areAllEntitiesSelected && (
             <span>{selectedValues[0]?.name}</span>
           )}
