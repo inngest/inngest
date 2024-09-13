5f22c2a40fbb043e224e19df06fdb5afee2dee30
Ana <anafilipadealmeida@gmail.com>
2024-09-13 23:20:01 +0100

    Add Auth UI

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/settings/integrations/neon/(steps)/[step]/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/settings/integrations/neon/(steps)/[step]/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import NeonAuth from '@inngest/components/PostgresIntegrations/Neon/Auth';
 import { IntegrationSteps } from '@inngest/components/PostgresIntegrations/types';
 
+import { pathCreator } from '@/utils/urls';
+
 export default function NeonStep({ params: { step } }: { params: { step: string } }) {
+  const router = useRouter();
+
   if (step === IntegrationSteps.Authorize) {
-    return <div>Page for Auth</div>;
+    return (
+      <NeonAuth
+        next={() => {
+          // TO DO: Save completed step
+          router.push(pathCreator.neonIntegrationStep({ step: IntegrationSteps.FormatWal }));
+        }}
+      />
+    );
   } else if (step === IntegrationSteps.FormatWal) {
     return <div>Page for Format</div>;
   } else if (step === IntegrationSteps.ConnectDb) {
