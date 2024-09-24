348d28a61fc7f597a0d52fda4dbb7c5237c053e5
Ana <anafilipadealmeida@gmail.com>
2024-09-24 14:49:42 +0100

    Update docs links

--- ui/packages/components/src/PostgresIntegrations/Neon/neonContent.tsx
+++ ui/packages/components/src/PostgresIntegrations/Neon/neonContent.tsx
@@ -10,8 +10,7 @@ import {
 export const neonConnectContent: ConnectPostgresIntegrationContent = {
   title: 'Neon',
   logo: <IconNeon className="text-onContrast" size={20} />,
-  // TO DO: Update once we have Neon docs in deploy section
-  url: 'https://www.inngest.com/docs/',
+  url: 'https://www.inngest.com/docs/features/events-triggers/neon?ref=app-connect',
   description:
     'This integration enables you to trigger Inngest functions from your Neon Postgres database updates.',
   step: {
@@ -54,6 +53,5 @@ export const neonMenuStepContent: PostgresIntegrationMenuContent = {
 export const neonIntegrationPageContent: IntegrationPageContent = {
   title: 'Neon',
   logo: <IconNeon className="text-onContrast" size={24} />,
-  // TO DO: Update once we have Neon docs in deploy section
-  url: 'https://www.inngest.com/docs/',
+  url: 'https://www.inngest.com/docs/features/events-triggers/neon?ref=app-integration-page',
 };
