b63050e2100bc4dee07d5fdcb91e4d63abd56418
Jacob Heric <jacob@jacobheric.com>
2024-10-01 09:25:29 -0400

    add webpack configs back for production builds, prod breaks in strange ways if this is missing

--- ui/apps/dashboard/next.config.js
+++ ui/apps/dashboard/next.config.js
@@ -91,6 +91,46 @@ const nextConfig = {
     // Tunnel sentry events to help circumvent ad-blockers.
     tunnelRoute: '/api/sentry',
   },
+  //
+  // Webpack is still here for production builds as next only supports turbo for dev.
+  webpack(config, { isServer }) {
+    // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
+    // into React components. See https://react-svgr.com/docs/next/
+
+    // Grab the existing rule that handles SVG imports
+    // @ts-ignore - this is a private property that is not typed
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    // If client-side, don't polyfill `fs`.
+    // This is needed for `quickjs-emscripten` to work on the client. See https://github.com/justjake/quickjs-emscripten/issues/33#issuecomment-739098440
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+
+    return config;
+  },
 };
 
 /**
