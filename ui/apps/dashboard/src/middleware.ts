77f4cdf8923b263356b42c330aa942de84254345
Jacob Heric <jacob@jacobheric.com>
2024-10-02 12:34:29 -0400

    remove api/trpc match

--- ui/apps/dashboard/src/middleware.ts
+++ ui/apps/dashboard/src/middleware.ts
@@ -71,7 +71,5 @@ export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
   ],
 };
