b70f3a6c4f68750178d7109010badb0d22e0c5c9
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-10-21 16:36:24 -0400

    Shrink loading indicator

--- ui/packages/components/src/RunDetailsV2/RunInfo.tsx
+++ ui/packages/components/src/RunDetailsV2/RunInfo.tsx
@@ -217,9 +217,9 @@ export function RunInfo({
           (initialRunData?.status === 'QUEUED' ? (
             <div className="border-muted bg-canvas border-t">
               <div className="border-l-status-queued flex items-center justify-start border-l-4">
-                <span className="relative ml-4 flex h-3 w-3">
+                <span className="relative ml-4 flex h-2.5 w-2.5">
                   <span className="bg-status-queued absolute inline-flex h-full w-full animate-ping rounded-full opacity-75"></span>
-                  <span className="bg-status-queued relative inline-flex h-3 w-3 rounded-full"></span>
+                  <span className="bg-status-queued relative inline-flex h-2.5 w-2.5 rounded-full"></span>
                 </span>
                 <p className="text-subtle max-h-24 text-ellipsis break-words py-2.5 pl-3 text-sm">
                   Queued run awaiting start...
