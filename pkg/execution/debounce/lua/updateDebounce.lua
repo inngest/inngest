5c3e17a70c8205a09fa626f4082235bed0a6b868
Darwin D Wu <darwin67@users.noreply.github.com>
2024-11-05 12:27:35 -0800

    don't return an error and create a new debounce instead

--- pkg/execution/debounce/lua/updateDebounce.lua
+++ pkg/execution/debounce/lua/updateDebounce.lua
@@ -16,7 +16,7 @@ local keyDbc = KEYS[2] -- debounce info key
 -- and create a new debounce job.
 local keyQueueHash = KEYS[3]
 
-local debounceID  = ARGV[1] 
+local debounceID  = ARGV[1]
 local debounce    = ARGV[2]
 local ttl         = tonumber(ARGV[3])
 local queueJobID  = ARGV[4]
@@ -56,12 +56,16 @@ end
 -- Check that the queue item is not leased (ie. this debounce is not in progress)
 local item = get_queue_item(keyQueueHash, queueJobID)
 if item == nil then
-	-- The queue item was not found.  Return a new debounce.
-	return -3
+	-- The queue item was not found.  Create a new debounce.
+
+  redis.call("SETEX", keyPtr, ttl, debounceID)
+  redis.call("HSET", keyDbc, debounceID, debounce)
+
+  return ttl
 end
 
 if item.leaseID ~= nil and item.leaseID ~= cjson.null and decode_ulid_time(item.leaseID) > currentTime then
-	-- The debounce queue item is leased. 
+	-- The debounce queue item is leased.
 	return -1
 end
 
