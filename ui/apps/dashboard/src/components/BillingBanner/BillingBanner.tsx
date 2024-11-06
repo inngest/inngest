ad30a7045c264cb47a46c5efc6a8fee1d1746037
Aaron Harper <aaron@inngest.com>
2024-11-06 12:31:36 -0500

    Use suspense

--- ui/apps/dashboard/src/components/BillingBanner/BillingBanner.tsx
+++ ui/apps/dashboard/src/components/BillingBanner/BillingBanner.tsx
@@ -1,73 +1,29 @@
-'use client';
+import { graphql } from '@/gql';
+import graphqlAPI from '@/queries/graphqlAPI';
+import { BillingBannerView } from './BillingBannerView';
 
-import { useMemo } from 'react';
-import { NewButton } from '@inngest/components/Button';
-
-import { pathCreator } from '@/utils/urls';
-import { Banner } from '../Banner';
-import { useBooleanLocalStorage } from './localStorage';
-import { parseEntitlementUsage } from './parse';
-
-type Props = {
-  entitlementUsage: {
-    runCount: {
-      current: number;
-      limit: number | null;
-    };
-  };
-};
-
-export function BillingBanner({ entitlementUsage }: Props) {
-  const { bannerMessage, bannerSeverity, items } = parseEntitlementUsage(entitlementUsage);
-
-  const isVisible = useBooleanLocalStorage('BillingBanner:visible', true);
-
-  const onDismiss = useMemo(() => {
-    // Error banners can't be dismissed.
-    if (bannerSeverity === 'error') {
-      return;
-    }
-
-    return () => {
-      isVisible.set(false);
-    };
-  }, [bannerSeverity, isVisible]);
-
-  // Error banners are always visible.
-  if (!isVisible.value && bannerSeverity !== 'error') {
+export async function BillingBanner() {
+  let entitlementUsage;
+  try {
+    entitlementUsage = (await graphqlAPI.request(entitlementUsageQuery)).account.entitlementUsage;
+  } catch (e) {
+    console.error(e);
     return null;
   }
 
-  if (items.length === 0) {
-    return null;
-  }
-
-  // Wait for localStorage to be hydrated before rendering the banner.
-  if (!isVisible.isReady) {
-    return null;
-  }
-
-  return (
-    <Banner className="flex" kind={bannerSeverity} onDismiss={onDismiss}>
-      <div className="flex grow">
-        <div className="grow">
-          {bannerMessage}
-          <ul className="list-none">
-            {items.map(([k, v]) => (
-              <li key={k}>{v}</li>
-            ))}
-          </ul>
-        </div>
-
-        <div className="flex items-center">
-          <NewButton
-            appearance="outlined"
-            href={pathCreator.billing()}
-            kind="secondary"
-            label="Upgrade plan"
-          />
-        </div>
-      </div>
-    </Banner>
-  );
+  return <BillingBannerView entitlementUsage={entitlementUsage} />;
 }
+
+const entitlementUsageQuery = graphql(`
+  query EntitlementUsage {
+    account {
+      id
+      entitlementUsage {
+        runCount {
+          current
+          limit
+        }
+      }
+    }
+  }
+`);
--- ui/apps/dashboard/src/components/Layout/Layout.tsx
+++ ui/apps/dashboard/src/components/Layout/Layout.tsx
@@ -1,12 +1,10 @@
 'use server';
 
-import { type ReactNode } from 'react';
+import { Suspense, type ReactNode } from 'react';
 
 import IncidentBanner from '@/app/(organization-active)/IncidentBanner';
 import { getNavCollapsed } from '@/app/actions';
 import { BillingBanner } from '@/components/BillingBanner';
-import { graphql } from '@/gql';
-import graphqlAPI from '@/queries/graphqlAPI';
 import { getProfileDisplay } from '@/queries/server-only/profile';
 import type { Environment } from '@/utils/environments';
 import SideBar from './SideBar';
@@ -21,37 +19,19 @@ export default async function Layout({ activeEnv, children }: LayoutProps) {
   const collapsed = await getNavCollapsed();
   const profile = await getProfileDisplay();
 
-  let entitlementUsage;
-  try {
-    entitlementUsage = (await graphqlAPI.request(entitlementUsageQuery)).account.entitlementUsage;
-  } catch (e) {
-    console.error(e);
-  }
-
   return (
     <div className="fixed z-50 flex h-screen w-full flex-row justify-start overflow-y-scroll overscroll-y-none">
       <SideBar collapsed={collapsed} activeEnv={activeEnv} profile={profile} />
 
       <div className="flex w-full flex-col overflow-x-scroll">
         <IncidentBanner />
-        {entitlementUsage && <BillingBanner entitlementUsage={entitlementUsage} />}
+
+        <Suspense>
+          <BillingBanner />
+        </Suspense>
 
         {children}
       </div>
     </div>
   );
 }
-
-const entitlementUsageQuery = graphql(`
-  query EntitlementUsage {
-    account {
-      id
-      entitlementUsage {
-        runCount {
-          current
-          limit
-        }
-      }
-    }
-  }
-`);
