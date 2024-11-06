604906732dc39c46645dc5eaeed0c88f4ec12744
Jacob Heric <jacob@jacobheric.com>
2024-11-05 19:30:09 -0500

    fix type error

--- ui/apps/dev-server-ui/src/app/(dashboard)/run/page.tsx
+++ ui/apps/dev-server-ui/src/app/(dashboard)/run/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
         pollInterval={2500}
         rerun={rerun}
         runID={runID}
-        stepAIEnabled={featureFlags.includes('step.ai')}
+        stepAIEnabled={true}
       />
     </div>
   );
