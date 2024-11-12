ad5ba8d52cb66dc48eec85951209bf3f7b5ad0a9
Jacob Heric <jacob@jacobheric.com>
2024-11-12 13:44:48 -0500

    fix imports

--- ui/apps/dashboard/src/components/Navigation/Monitor.tsx
+++ ui/apps/dashboard/src/components/Navigation/Monitor.tsx
@@ -3,7 +3,6 @@ import { MetricsIcon } from '@inngest/components/icons/sections/Metrics';
 import { RunsIcon } from '@inngest/components/icons/sections/Runs';
 
 import type { Environment as EnvType } from '@/utils/environments';
-import { useBooleanFlag } from '../FeatureFlags/hooks';
 import { getNavRoute } from './Navigation';
 
 export default function Monitor({
