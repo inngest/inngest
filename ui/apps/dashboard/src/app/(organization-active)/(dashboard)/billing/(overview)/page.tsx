ed8828708326e751c60f3c98bb562bae30524299
Ana <anafilipadealmeida@gmail.com>
2024-11-13 19:18:40 +0000

    Fetch entitlements

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/billing/(overview)/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/billing/(overview)/page.tsx
@@ -2,31 +2,36 @@ import { NewButton } from '@inngest/components/Button';
 import { Card } from '@inngest/components/Card/Card';
 
 import { LimitBar } from '@/components/Billing/LimitBars/LimitBar';
+import { getEntitlementUsage } from '@/components/Billing/actions';
 import { pathCreator } from '@/utils/urls';
 
 const plan = 'Free';
-const runs = {
-  title: 'Runs',
-  description: 'A single durable function execution. Additional runs are $5 per 200k.',
-  current: 8000000,
-  limit: 5000000,
-};
 
-const steps = {
-  title: 'Steps',
-  description: 'An individual step in durable functions. Additional steps are $5 per 200k.',
-  current: 15000000,
-  limit: 25000000,
-};
-export default function Page() {
+export default async function Page() {
+  const entitlementUsage = await getEntitlementUsage();
+
+  const runs = {
+    title: 'Runs',
+    description: 'A single durable function execution. Additional runs are available for purchase.',
+    current: entitlementUsage?.runCount?.current || 0,
+    limit: entitlementUsage?.runCount?.limit || 0,
+  };
+
+  // const steps = {
+  //   title: 'Steps',
+  //   description: 'An individual step in durable functions. Additional steps are available for purchase.',
+  //   current: entitlementUsage?.stepCount?.current || 0,
+  //   limit: entitlementUsage?.stepCount?.limit || 0,
+  // };
+
   return (
     <div className="grid grid-cols-3 gap-4">
       <Card className="col-span-2">
         <Card.Content>
           <p className="text-muted mb-1">Your plan</p>
           <p className="text-basis text-xl">{plan}</p>
-          <LimitBar data={runs} className="my-4" />
-          <LimitBar data={steps} className="mb-6" />
+          {entitlementUsage?.runCount && <LimitBar data={runs} className="my-4" />}
+          {/* {entitlementUsage?.stepCount && <LimitBar data={steps} className="mb-6" />} */}
           <div className="flex flex-col items-center gap-2">
             <p>Custom needs?</p>
             <NewButton
