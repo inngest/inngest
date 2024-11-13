79cad9200d8292c15083f63b67f853c2573af10a
Darwin D Wu <darwin67@users.noreply.github.com>
2024-11-12 23:21:56 -0800

    add redisscript loader

--- pkg/connect/state/redis.go
+++ pkg/connect/state/redis.go
@@ -2,13 +2,71 @@ package state
 
 import (
 	"context"
+	"embed"
 	"fmt"
+	"io/fs"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	connpb "github.com/inngest/inngest/proto/gen/connect/v1"
 	"github.com/redis/rueidis"
 )
+
+//go:embed lua/*
+var embedded embed.FS
+
+var (
+	scripts = map[string]*rueidis.Lua{}
+	include = regexp.MustCompile(`-- \$include\(([\w.]+)\)`)
+)
+
+func init() {
+	entries, err := embedded.ReadDir("lua")
+	if err != nil {
+		panic(fmt.Errorf("error reading redis lua dir: %w", err))
+	}
+	readRedisScripts("lua", entries)
+}
+
+func readRedisScripts(path string, entries []fs.DirEntry) {
+	for _, e := range entries {
+		// NOTE: When using embed go always uses forward slashes as a path
+		// prefix. filepath.Join uses OS-specific prefixes which fails on
+		// windows, so we construct the path using Sprintf for all platforms
+		if e.IsDir() {
+			entries, _ := embedded.ReadDir(fmt.Sprintf("%s/%s", path, e.Name()))
+			readRedisScripts(path+"/"+e.Name(), entries)
+			continue
+		}
+
+		byt, err := embedded.ReadFile(fmt.Sprintf("%s/%s", path, e.Name()))
+		if err != nil {
+			panic(fmt.Errorf("error reading redis lua script: %w", err))
+		}
+
+		name := path + "/" + e.Name()
+		name = strings.TrimPrefix(name, "lua/")
+		name = strings.TrimSuffix(name, ".lua")
+		val := string(byt)
+
+		// Add any includes.
+		items := include.FindAllStringSubmatch(val, -1)
+		if len(items) > 0 {
+			// Replace each include
+			for _, include := range items {
+				byt, err = embedded.ReadFile(fmt.Sprintf("lua/includes/%s", include[1]))
+				if err != nil {
+					panic(fmt.Errorf("error reading redis lua include: %w", err))
+				}
+				val = strings.ReplaceAll(val, include[0], string(byt))
+			}
+		}
+
+		scripts[name] = rueidis.NewLuaScript(val)
+	}
+}
 
 type redisConnectionStateManager struct {
 	client rueidis.Client
@@ -58,6 +116,10 @@ func (r *redisConnectionStateManager) DeleteConnection(ctx context.Context, conn
 //
 
 func (r *redisConnectionStateManager) OnConnected(ctx context.Context, data *connpb.SDKConnectRequestData) {
+	// meta := &connpb.ConnMetadata{
+	// 	Language: data.SdkLanguage,
+	// }
+
 }
 
 func (r *redisConnectionStateManager) OnAuthenticated(ctx context.Context, auth *AuthContext) {
