dec6e6309b6dd0d9bb1d31a157aeeba4a3eb08ee
Dan Farrelly <daniel.j.farrelly@gmail.com>
2024-11-18 17:41:06 -0500

    Update toaster for cloud

--- ui/apps/dashboard/src/components/Toaster.tsx
+++ ui/apps/dashboard/src/components/Toaster.tsx
@@ -2,4 +2,20 @@
 
 import { Toaster } from 'sonner';
 
-export default Toaster;
+export default function ToasterWrapper() {
+  return (
+    <Toaster
+      duration={20_000}
+      toastOptions={{
+        // Ensure that the toast is clickable when there are overlays/modals
+        className: 'pointer-events-auto drop-shadow-lg',
+        style: {
+          background: `rgb(var(--color-background-canvas-base))`,
+          borderRadius: 0,
+          borderWidth: '0px 0px 2px',
+          color: `rgb(var(--color-foreground-base))`,
+        },
+      }}
+    />
+  );
+}
