f04cf50173155921bf8441c82d37c89b3041a258
Ana <anafilipadealmeida@gmail.com>
2024-11-19 17:20:22 +0000

    Update billing banner

--- ui/apps/dashboard/src/utils/urls.ts
+++ ui/apps/dashboard/src/utils/urls.ts
@@ -45,8 +45,8 @@ export const pathCreator = {
   appSyncs({ envSlug, externalAppID }: { envSlug: string; externalAppID: string }): Route {
     return `/env/${envSlug}/apps/${encodeURIComponent(externalAppID)}/syncs` as Route;
   },
-  billing(): Route {
-    return '/billing' as Route;
+  billing({ ref }: { ref?: string } = {}): Route {
+    return `/billing${ref ? `?ref=${ref}` : ''}` as Route;
   },
   createApp({ envSlug }: { envSlug: string }): Route {
     return `/env/${envSlug}/apps/sync-new` as Route;
