256fcfbd311e70c9374b91261e797589691b99db
Jacob Heric <jacob@jacobheric.com>
2024-11-20 14:16:25 -0500

    eslint ignore unimplemented dashboard side rerun mutation

--- ui/apps/dashboard/src/queries/useRerunFromStep.tsx
+++ ui/apps/dashboard/src/queries/useRerunFromStep.tsx
@@ -3,9 +3,14 @@ type RerunFromStep = {
   fromStep: { stepID: string; input: string };
 };
 
-export function useRerunFromStep({ runID, fromStep }: RerunFromStep) {
+export function useRerunFromStep(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  { runID, fromStep }: RerunFromStep
+) {
   const rerunFromStep = async ({
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     runID,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     fromStep,
   }: {
     runID: string;
