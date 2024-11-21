b8eecd6f1a04272c287f416739733a30b6a39a54
Darwin D Wu <darwin67@users.noreply.github.com>
2024-11-21 12:44:10 -0800

    consolidate conns query into one

--- pkg/connect/rest/v0/conns.go
+++ pkg/connect/rest/v0/conns.go
@@ -2,8 +2,9 @@ package v0
 
 import (
 	"encoding/json"
-	connpb "github.com/inngest/inngest/proto/gen/connect/v1"
 	"net/http"
+
+	connpb "github.com/inngest/inngest/proto/gen/connect/v1"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
@@ -13,10 +14,17 @@ import (
 )
 
 // showConnections retrieves the list of connections from the gateway state
-func (c *router) showConnectionsByEnv(w http.ResponseWriter, r *http.Request) {
+//
+// Provides query params to further filter the returned data
+//   - group_id
+//   - app_id
+func (c *router) showConnections(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	var envID uuid.UUID
+	var (
+		envID uuid.UUID
+		appID *uuid.UUID
+	)
 	switch c.Dev {
 	case true:
 		envID = consts.DevServerEnvId
@@ -39,85 +47,48 @@ func (c *router) showConnectionsByEnv(w http.ResponseWriter, r *http.Request) {
 		envID = id
 	}
 
-	conns, err := c.ConnectManager.GetConnectionsByEnvID(ctx, envID)
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, publicerr.Error{
-			Err:     err,
-			Message: err.Error(),
-			Status:  http.StatusInternalServerError,
-		})
-		return
-	}
-
-	reply := &rest.ShowConnsReply{
-		Data: conns,
-	}
-
-	resp, err := json.Marshal(reply)
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, publicerr.Error{
-			Err:     err,
-			Message: err.Error(),
-			Status:  http.StatusInternalServerError,
-		})
-		return
-	}
-
-	_, _ = w.Write(resp)
-}
-
-func (c *router) showConnectionsByApp(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	var envID uuid.UUID
-	switch c.Dev {
-	case true:
-		envID = consts.DevServerEnvId
-
-	case false:
-		// Expect UUID
-		param := chi.URLParam(r, "envID")
-		id, err := uuid.Parse(param)
-		if err != nil {
-			_ = publicerr.WriteHTTP(w, publicerr.Error{
-				Err:     err,
-				Message: "invalid environment ID",
-				Data: map[string]any{
-					"envID": param,
-				},
-				Status: http.StatusBadRequest,
-			})
-			return
-		}
-		envID = id
-	}
-
-	var appID uuid.UUID
+	// Check appID query param
 	{
-		param := chi.URLParam(r, "appID")
+		param := r.URL.Query().Get("app_id")
 		id, err := uuid.Parse(param)
 		if err != nil {
 			_ = publicerr.WriteHTTP(w, publicerr.Error{
 				Err:     err,
-				Message: "invalid app ID",
-				Data:    map[string]any{"appID": param},
+				Message: "app_id is invalid UUID",
+				Data:    map[string]any{"app_id": param},
 				Status:  http.StatusBadRequest,
 			})
 			return
 		}
-		appID = id
+		appID = &id
 	}
 
-	conns, err := c.ConnectManager.GetConnectionsByAppID(ctx, envID, appID)
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, publicerr.Error{
-			Err:     err,
-			Message: err.Error(),
-			Status:  http.StatusInternalServerError,
-		})
-		return
+	var (
+		conns []*connpb.ConnMetadata
+		err   error
+	)
+	switch {
+	case appID != nil:
+		if conns, err = c.ConnectManager.GetConnectionsByAppID(ctx, envID, *appID); err != nil {
+			_ = publicerr.WriteHTTP(w, publicerr.Error{
+				Err:     err,
+				Message: err.Error(),
+				Status:  http.StatusInternalServerError,
+			})
+			return
+		}
+	default:
+		if conns, err = c.ConnectManager.GetConnectionsByEnvID(ctx, envID); err != nil {
+			_ = publicerr.WriteHTTP(w, publicerr.Error{
+				Err:     err,
+				Message: err.Error(),
+				Status:  http.StatusInternalServerError,
+			})
+			return
+		}
 	}
 
+	// Respond
 	if len(conns) == 0 {
 		conns = []*connpb.ConnMetadata{}
 	}
@@ -128,7 +99,11 @@ func (c *router) showConnectionsByApp(w http.ResponseWriter, r *http.Request) {
 
 	resp, err := json.Marshal(reply)
 	if err != nil {
-		_ = publicerr.WriteHTTP(w, publicerr.Errorf(http.StatusInternalServerError, "error serializing response"))
+		_ = publicerr.WriteHTTP(w, publicerr.Error{
+			Err:     err,
+			Message: err.Error(),
+			Status:  http.StatusInternalServerError,
+		})
 		return
 	}
 
