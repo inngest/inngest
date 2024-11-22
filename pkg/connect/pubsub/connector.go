1c29c2ca3424163eb3fab085cd08c7853cdcf2fb
Darwin D Wu <darwin67@users.noreply.github.com>
2024-11-21 16:26:40 -0800

    update connector generator

--- pkg/connect/pubsub/connector.go
+++ pkg/connect/pubsub/connector.go
@@ -1,8 +1,10 @@
 package pubsub
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/inngest/inngest/pkg/logger"
 	"github.com/redis/rueidis"
 )
 
@@ -11,25 +13,33 @@ type Connector interface {
 	RequestForwarder
 }
 
-type ConnectorOpt func() (Connector, error)
+type ConnectorOpt func(ctx context.Context) (Connector, error)
 
-func NewConnector(initialize ConnectorOpt) (Connector, error) {
-	return initialize()
+func NewConnector(ctx context.Context, initialize ConnectorOpt) (Connector, error) {
+	return initialize(ctx)
 }
 
-func WithRedis(opt rueidis.ClientOption) ConnectorOpt {
-	return func() (Connector, error) {
+func WithRedis(opt rueidis.ClientOption, listen bool) ConnectorOpt {
+	return func(ctx context.Context) (Connector, error) {
 		rc, err := rueidis.NewClient(opt)
 		if err != nil {
 			return nil, fmt.Errorf("error initializing redis client for connector: %w", err)
 		}
 
-		return NewRedisPubSubConnector(rc), nil
+		connector, err := NewRedisPubSubConnector(rc), nil
+		if listen {
+			go func() {
+				if err := connector.Wait(ctx); err != nil {
+					logger.StdlibLogger(ctx).Error("error waiting for pubsub messages", "error", err)
+				}
+			}()
+		}
+		return connector, err
 	}
 }
 
 func WithNoop() ConnectorOpt {
-	return func() (Connector, error) {
+	return func(ctx context.Context) (Connector, error) {
 		return noopConnector{}, nil
 	}
 }
