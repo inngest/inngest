3916f928931fb7990e8e8bfc75edc2e2b3d23e5e
Darwin D Wu <darwin67@users.noreply.github.com>
2024-11-25 15:15:24 -0800

    add span testing

--- tests/golang/parallel_test.go
+++ tests/golang/parallel_test.go
@@ -2,15 +2,19 @@ package golang
 
 import (
 	"context"
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
 
+	"github.com/inngest/inngest/pkg/coreapi/graph/models"
 	"github.com/inngest/inngest/pkg/enums"
 	"github.com/inngest/inngest/pkg/inngest"
+	"github.com/inngest/inngest/tests/client"
 	"github.com/inngest/inngestgo"
 	"github.com/inngest/inngestgo/experimental/group"
 	"github.com/inngest/inngestgo/step"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,11 +23,13 @@ type parallelTestEvt inngestgo.GenericEvent[any, any]
 func TestParallelSteps(t *testing.T) {
 	ctx := context.Background()
 
+	c := client.New(t)
 	h, server, registerFuncs := NewSDKHandler(t, "parallel")
 	defer server.Close()
 
 	var (
 		counter int32
+		runID   string
 	)
 
 	a := inngestgo.CreateFunction(
@@ -32,6 +38,10 @@ func TestParallelSteps(t *testing.T) {
 		}},
 		inngestgo.EventTrigger("test/parallel", nil),
 		func(ctx context.Context, input inngestgo.Input[parallelTestEvt]) (any, error) {
+			if runID == "" {
+				runID = input.InputCtx.RunID
+			}
+
 			res := group.Parallel(ctx,
 				func(ctx context.Context) (any, error) {
 					return step.Run(ctx, "p1", func(ctx context.Context) (any, error) {
@@ -79,10 +89,33 @@ func TestParallelSteps(t *testing.T) {
 	t.Run("verify in-progress", func(t *testing.T) {
 		<-time.After(2 * time.Second)
 		require.Equal(t, int32(2), atomic.LoadInt32(&counter))
+
+		_ = c.WaitForRunTraces(ctx, t, &runID, client.WaitForRunTracesOptions{
+			Status:         models.FunctionStatusRunning,
+			ChildSpanCount: 2,
+			Timeout:        2 * time.Second,
+			Interval:       200 * time.Millisecond,
+		})
 	})
 
 	t.Run("verify completion", func(t *testing.T) {
 		<-time.After(10 * time.Second)
 		require.Equal(t, int32(4), atomic.LoadInt32(&counter))
+
+		run := c.WaitForRunTraces(ctx, t, &runID, client.WaitForRunTracesOptions{
+			Status:         models.FunctionStatusCompleted,
+			ChildSpanCount: 4,
+			Timeout:        5 * time.Second,
+			Interval:       250 * time.Millisecond,
+		})
+
+		// check on spans
+		for _, cspan := range run.Trace.ChildSpans {
+			t.Run(fmt.Sprintf("child: %s", cspan.Name), func(t *testing.T) {
+				assert.Equal(t, 0, cspan.Attempts)
+				assert.Equal(t, models.StepOpRun.String(), cspan.StepOp)
+				assert.Equal(t, models.RunTraceSpanStatusCompleted.String(), cspan.Status)
+			})
+		}
 	})
 }
