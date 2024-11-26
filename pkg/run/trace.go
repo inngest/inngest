3d9f3422b56e07cf56bfead6ab9f2b481106da03
Darwin <5746693+darwin67@users.noreply.github.com>
2024-11-25 16:44:38 -0800

    Update API ignoring `StepPlanned` opcodes (#1994)
    In progress runs are stored as `StepPlanned` opcode data, and the API currently ignores them, so you can't see anything that are in progress.
    
    Removing these cases will show the in progress executions again, but we need to make sure `StepPlanned` spans are still ignored for visualization.
    
    This will annotate pure discovery spans to make it clear they're only used for planning, so they can be ignored when data is retrieved for the UI.
    
    Also update the name of the span with the generator name from plans.
    
    ---------
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

--- pkg/run/trace.go
+++ pkg/run/trace.go
@@ -73,9 +73,11 @@ func NewRunTree(opts RunTreeOpts) (*runTree, error) {
 	}
 
 	for _, s := range opts.Spans {
-		// don't even bother
+		// ignore parallelism planning spans
 		if s.StepOpCode() == enums.OpcodeStepPlanned {
-			continue
+			if _, ok := s.SpanAttributes[consts.OtelSysStepPlan]; ok {
+				continue
+			}
 		}
 
 		if s.ScopeName == consts.OtelScopeFunction {
@@ -99,9 +101,11 @@ func NewRunTree(opts RunTreeOpts) (*runTree, error) {
 
 	// loop through again to construct parent/child relationship
 	for _, s := range opts.Spans {
-		// don't even bother
+		// ignore parallelism planning spans
 		if s.StepOpCode() == enums.OpcodeStepPlanned {
-			continue
+			if _, ok := s.SpanAttributes[consts.OtelSysStepPlan]; ok {
+				continue
+			}
 		}
 
 		if s.ParentSpanID != nil {
@@ -195,7 +199,7 @@ func (tb *runTree) ToRunSpan(ctx context.Context) (*rpbv2.RunSpan, error) {
 	return root, nil
 }
 
-func (tb *runTree) toRunSpan(ctx context.Context, s *cqrs.Span) (*rpbv2.RunSpan, bool, error) {
+func (tb *runTree) toRunSpan(ctx context.Context, s *cqrs.Span) (span *rpbv2.RunSpan, skipped bool, err error) {
 	res, skipped := tb.constructSpan(ctx, s)
 	if skipped {
 		return nil, skipped, nil
@@ -277,8 +281,6 @@ func (tb *runTree) toRunSpan(ctx context.Context, s *cqrs.Span) (*rpbv2.RunSpan,
 
 				return nil, false, fmt.Errorf("error grouping invoke: %w", err)
 			}
-		case enums.OpcodeStepPlanned: // don't bother
-			return nil, true, nil
 		default:
 			// execution spans
 			if s.ScopeName == consts.OtelScopeExecution {
