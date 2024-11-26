44a76525babf1aeb4a9168c1d81d26349c3667be
Tony Holdstock-Brown <tonyhb@gmail.com>
2024-11-25 17:05:11 -0800

    Add generateText parsing

--- pkg/util/aigateway/aigateway.go
+++ pkg/util/aigateway/aigateway.go
@@ -7,6 +7,16 @@ import (
 	"net/url"
 )
 
+var (
+	errInvalidFormat = fmt.Errorf("invalid api format")
+
+	inputParser = []func(json.RawMessage) (ParsedInferenceRequest, error){
+		parseOpenAIRequest,
+		parseAnthropicRequest,
+		parseVercelGenerateTextArgs,
+	}
+)
+
 // ParsedRequest represents the parsed request data for a given inference request.
 //
 // Note that this is not stored, and instead is computed just in time for each input
@@ -17,6 +27,7 @@ type ParsedInferenceRequest struct {
 	Seed                *int     `json:"seed,omitempty"`
 	Temprature          float32  `json:"temperature,omitempty"`
 	TopP                float32  `json:"top_p,omitempty"`
+	TopK                int      `json:"top_k,omitempty"`
 	MaxTokens           int      `json:"max_tokens,omitempty"`
 	MaxCompletionTokens int      `json:"max_completion_tokens,omitempty"`
 	StopSequences       []string `json:"stop,omitempty"`
@@ -33,8 +44,19 @@ type ParsedInferenceResponse struct {
 	// TODO: Tool use selections, parsed.
 }
 
+// ParseUnknownInput attempts to parse the given request without prior knowledge
+// of the request format.  This is used when wrapping AI calls.
+//
+// Note that the input here may be a request body, or it may be the arguments passed
+// to a JS function.  Because of this, there are multiple formats per model API that
+// we need to hit.
 func ParseUnknownInput(ctx context.Context, req json.RawMessage) (ParsedInferenceRequest, error) {
-	return ParsedInferenceRequest{}, fmt.Errorf("todo")
+	for _, parser := range inputParser {
+		if parsed, err := parser(req); err == nil {
+			return parsed, nil
+		}
+	}
+	return ParsedInferenceRequest{}, fmt.Errorf("unable to parse input")
 }
 
 // Parse validates an inference request.  This checks which model, format, and URLs we should
@@ -52,24 +74,102 @@ func ParseUnknownInput(ctx context.Context, req json.RawMessage) (ParsedInferenc
 func ParseInput(ctx context.Context, req Request) (ParsedInferenceRequest, error) {
 	switch req.Format {
 	case FormatOpenAIChat:
-		// OpenAI Chat is the default format, so fall through to the default.
-		fallthrough
+		return parseOpenAIRequest(req.Body)
 	default:
-		// Parse everything as an OpenAI Chat request.
-		rf := RFOpenAIChatCompletion{}
-		if err := json.Unmarshal(req.Body, &rf); err != nil {
-			return ParsedInferenceRequest{}, err
-		}
-
-		return ParsedInferenceRequest{
-			Model:               rf.Model,
-			Seed:                rf.Seed,
-			Temprature:          rf.Temperature,
-			TopP:                rf.TopP,
-			MaxTokens:           rf.MaxTokens,
-			MaxCompletionTokens: rf.MaxCompletionTokens,
-			StopSequences:       rf.Stop,
-		}, nil
+		// OpenAI Chat is the default format
+		return parseOpenAIRequest(req.Body)
 	}
 
+}
+
+// parseOpenAIRequest assumes that the body is a fully-defined OpenAI request.
+func parseOpenAIRequest(body json.RawMessage) (ParsedInferenceRequest, error) {
+	// Parse everything as an OpenAI Chat request.
+	rf := RFOpenAIChatCompletion{}
+	if err := json.Unmarshal(body, &rf); err != nil {
+		return ParsedInferenceRequest{}, err
+	}
+	if rf.Model == "" {
+		return ParsedInferenceRequest{}, errInvalidFormat
+	}
+
+	return ParsedInferenceRequest{
+		Model:               rf.Model,
+		Seed:                rf.Seed,
+		Temprature:          rf.Temperature,
+		TopP:                rf.TopP,
+		MaxTokens:           rf.MaxTokens,
+		MaxCompletionTokens: rf.MaxCompletionTokens,
+		StopSequences:       rf.Stop,
+	}, nil
+}
+
+func parseAnthropicRequest(body json.RawMessage) (ParsedInferenceRequest, error) {
+	return ParsedInferenceRequest{}, errInvalidFormat
+}
+
+//
+// step.ai.wrap parsing.
+//
+
+type vercelGenerateText struct {
+	Model  vercelGenerateTextModel
+	System string
+	Prompt string
+
+	Seed          *int     `json:"seed,omitempty"`
+	Temprature    float32  `json:"temperature,omitempty"`
+	TopP          float32  `json:"topP,omitempty"`
+	TopK          int      `json:"topK,omitempty"`
+	MaxTokens     int      `json:"maxTokens,omitempty"`
+	StopSequences []string `json:"stopSequences,omitempty"`
+}
+
+type vercelGenerateTextModel struct {
+	Config               vercelGenerateTextModelConfig
+	ModelID              string `json:"modelId"`
+	Settings             struct{}
+	SpecificationVersion string
+}
+
+type vercelGenerateTextModelConfig struct {
+	Compatibility string
+	Provider      string
+}
+
+// parseVercelGenerateTextArgs attempts to parse the `generateText` arguments from
+// step.ai.wrap.
+func parseVercelGenerateTextArgs(body json.RawMessage) (ParsedInferenceRequest, error) {
+	if len(body) <= 2 {
+		return ParsedInferenceRequest{}, errInvalidFormat
+	}
+
+	var req vercelGenerateText
+	if body[0] == '[' {
+		// Unwrap this into an array of structs, as the JS SDK sends every argument as an array.
+		reqs := []vercelGenerateText{}
+		if err := json.Unmarshal(body, &reqs); err != nil || len(reqs) == 0 {
+			return ParsedInferenceRequest{}, err
+		}
+		req = reqs[0]
+	} else {
+		if err := json.Unmarshal(body, &req); err != nil {
+			return ParsedInferenceRequest{}, err
+		}
+	}
+
+	if req.Model.ModelID == "" {
+		return ParsedInferenceRequest{}, errInvalidFormat
+	}
+
+	parsed := ParsedInferenceRequest{
+		Model:         req.Model.ModelID,
+		Seed:          req.Seed,
+		Temprature:    req.Temprature,
+		TopP:          req.TopP,
+		TopK:          req.TopK,
+		MaxTokens:     req.MaxTokens,
+		StopSequences: req.StopSequences,
+	}
+	return parsed, nil
 }
--- pkg/util/aigateway/aigateway_test.go
+++ pkg/util/aigateway/aigateway_test.go
@@ -1,6 +1,54 @@
 package aigateway
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
 
-func TestParse(t *testing.T) {
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseVercelGenerateText(t *testing.T) {
+	inputs := []struct {
+		input    json.RawMessage
+		expected ParsedInferenceRequest
+		err      error
+	}{
+		{
+			// most basic demo
+			input: json.RawMessage(`[{"model":{"config":{"compatibility":"strict","provider":"openai.chat"},"modelId":"gpt-4-turbo","settings":{},"specificationVersion":"v1"},"prompt":"What is love?"}]`),
+			expected: ParsedInferenceRequest{
+				Model: "gpt-4-turbo",
+			},
+		},
+		{
+			// system prompt
+			input: json.RawMessage(`[{"model":{"config":{"compatibility":"strict","provider":"openai.chat"},"modelId":"gpt-4-turbo","settings":{},"specificationVersion":"v1"},"prompt":"What is love?"}]`),
+			expected: ParsedInferenceRequest{
+				Model: "gpt-4-turbo",
+			},
+		},
+		{
+			// settings
+			input: json.RawMessage(`[{"maxTokens":512,"model":{"config":{"compatibility":"strict","provider":"openai.chat"},"modelId":"gpt-4-turbo","settings":{},"specificationVersion":"v1"},"prompt":"What is love?","seed":123,"stopSequences":["\u003cstop /\u003e"],"system":"you are helpful and terse.","temperature":0.3,"topK":50,"topP":0.75}]`),
+			expected: ParsedInferenceRequest{
+				Model:         "gpt-4-turbo",
+				Seed:          ptr(123),
+				Temprature:    0.3,
+				TopP:          0.75,
+				TopK:          50,
+				MaxTokens:     512,
+				StopSequences: []string{"<stop />"},
+			},
+		},
+	}
+
+	for _, test := range inputs {
+		actual, err := parseVercelGenerateTextArgs(test.input)
+		require.EqualValues(t, test.err, err)
+		require.EqualValues(t, test.expected, actual)
+	}
+}
+
+func ptr[T any](in T) *T {
+	return &in
 }
