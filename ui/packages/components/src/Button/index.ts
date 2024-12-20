ca2f23e888a21e7b1b817ea68d0528ce21687460
Ana <anafilipadealmeida@gmail.com>
2024-12-20 12:19:23 +0000

    Tweak Alert Modal

--- ui/packages/components/src/Button/index.ts
+++ ui/packages/components/src/Button/index.ts
@@ -1,4 +1,4 @@
 export { OldButton as Button } from './OldButton';
-export { Button as NewButton } from './Button';
+export { Button as NewButton, type ButtonKind } from './Button';
 export { getButtonColors, getButtonSizeStyles, getDisabledStyles } from './oldButtonStyles';
 export { SplitButton } from './SplitButton';
