8a21954e6383e40fc8ca26c45cc25dec7e32bd3e
Ana <anafilipadealmeida@gmail.com>
2024-12-31 03:57:55 +0000

    Tweak info tooltips

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/page.tsx
@@ -24,12 +24,14 @@ const AppInfo = () => (
     <TooltipContent
       side="right"
       sideOffset={2}
-      className="border-muted text-muted text-md mt-6 flex flex-col rounded-lg border p-0 text-sm"
+      className="border-muted text-muted mt-6 flex flex-col rounded-lg border p-0 text-sm"
     >
-      <div className="border-b px-4 py-2 ">Apps map directly to your products or services.</div>
+      <div className="border-subtle border-b px-4 py-2 ">
+        Apps map directly to your products or services.
+      </div>
 
       <div className="px-4 py-2">
-        <NewLink href="https://www.inngest.com/docs/apps" size="small">
+        <NewLink href="https://www.inngest.com/docs/apps" target="_blank">
           Learn how apps work
         </NewLink>
       </div>
