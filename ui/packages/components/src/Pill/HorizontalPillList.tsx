407957cfec78a223f67b3aa5805681c73df6afe4
Ana <anafilipadealmeida@gmail.com>
2025-01-03 01:28:13 +0000

    Align pills in table

--- ui/packages/components/src/Pill/HorizontalPillList.tsx
+++ ui/packages/components/src/Pill/HorizontalPillList.tsx
@@ -31,5 +31,5 @@ export function HorizontalPillList({ pills, alwaysVisibleCount }: FunctionsCellC
     );
   }
 
-  return <>{pills}</>;
+  return <div className="flex items-center">{pills}</div>;
 }
