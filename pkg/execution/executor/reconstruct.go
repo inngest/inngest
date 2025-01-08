381c76888d8d0530ba09e1a59a69738c0636978d
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-01-08 10:57:10 +0000

    Reset `reconstruct` changes

--- pkg/execution/executor/reconstruct.go
+++ pkg/execution/executor/reconstruct.go
@@ -17,11 +17,7 @@ func reconstruct(ctx context.Context, tr cqrs.TraceReader, req execution.Schedul
 	// Load the original run state and copy the state from the original
 	// run to the new run.
 	origTraceRun, err := tr.GetTraceRun(ctx, cqrs.TraceRunIdentifier{
-		RunID:       *req.OriginalRunID,
-		WorkspaceID: req.WorkspaceID,
-		AppID:       req.AppID,
-		FunctionID:  req.Function.ID,
-		AccountID:   req.AccountID,
+		RunID: *req.OriginalRunID,
 	})
 	if err != nil {
 		return fmt.Errorf("error loading original trace run: %w", err)
