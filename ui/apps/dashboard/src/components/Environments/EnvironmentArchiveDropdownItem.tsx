d58259a2e40723fc0f44fcaac3233a1cb099ecd8
Ana <anafilipadealmeida@gmail.com>
2025-01-09 17:49:37 +0000

    Fix open/close

--- ui/apps/dashboard/src/components/Environments/EnvironmentArchiveDropdownItem.tsx
+++ ui/apps/dashboard/src/components/Environments/EnvironmentArchiveDropdownItem.tsx
@@ -9,9 +9,10 @@ import { EnvironmentArchiveModal } from './EnvironmentArchiveModal';
 
 type Props = {
   env: { id: string; isArchived: boolean; name: string; type: EnvironmentType };
+  onClose: () => void;
 };
 
-export function EnvironmentArchiveDropdownItem({ env }: Props) {
+export function EnvironmentArchiveDropdownItem({ env, onClose }: Props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   // Need to store local state since the mutations don't invalidate the cache,
@@ -44,10 +45,14 @@ export function EnvironmentArchiveDropdownItem({ env }: Props) {
         isArchived={isArchived}
         isBranchEnv={env.type === EnvironmentType.BranchChild}
         isOpen={isModalOpen}
-        onCancel={() => setIsModalOpen(false)}
+        onCancel={() => {
+          setIsModalOpen(false);
+          onClose();
+        }}
         onSuccess={() => {
           setIsModalOpen(false);
           setIsArchived(!isArchived);
+          onClose();
         }}
       />
     </>
