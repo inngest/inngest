ac6ad84fb4efa09312c537c877f340f389f72d42
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-01-20 16:20:08 -0500

    INN-3465: Disable auto-discovery after first app is discovered
    Auto-discover's main purpose to to help developers get started when spinning up their app for the first time. Scanning all ports and URL paths gets noisy during most development. We can cover most use cases to help devs with this new pattern of disabling auto-discover after the first app has been added.

--- cmd/commands/dev.go
+++ cmd/commands/dev.go
@@ -31,7 +31,7 @@ func NewCmdDev() *cobra.Command {
 	cmd.Flags().String("host", "", "host to run the API on")
 	cmd.Flags().StringP("port", "p", "8288", "port to run the API on")
 	cmd.Flags().StringSliceP("sdk-url", "u", []string{}, "SDK URLs to load functions from")
-	cmd.Flags().Bool("no-discovery", false, "Disable autodiscovery")
+	cmd.Flags().Bool("no-discovery", false, "Disable app auto-discovery")
 	cmd.Flags().Bool("no-poll", false, "Disable polling of apps for updates")
 	cmd.Flags().Int("poll-interval", devserver.DefaultPollInterval, "Interval in seconds between polling for updates to apps")
 	cmd.Flags().Int("retry-interval", 0, "Retry interval in seconds for linear backoff when retrying functions - must be 1 or above")
