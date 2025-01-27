251e2fa1986460219446f2c6329f81dadefe1828
Darwin D Wu <darwin67@users.noreply.github.com>
2025-01-27 15:30:59 -0800

    add simple kafka exporter

--- pkg/telemetry/exporters/kafka.go
+++ pkg/telemetry/exporters/kafka.go
@@ -5,27 +5,93 @@ import (
 	"fmt"
 
 	"go.opentelemetry.io/otel/sdk/trace"
+	"gocloud.dev/pubsub"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
-	notImplementedErr = fmt.Errorf("not implemented")
+	defaultMsgKey = "fn_id"
 )
 
 type kafkaSpanExporter struct {
+	topic *pubsub.Topic
+	key   string
 }
 
-type kafkaSpansExporterOpts struct{}
+type kafkaSpansExporterOpts struct {
+	topic string
+	key   string
+}
 
-func NewKafkaSpanExporter(ctx context.Context, opts ...kafkaSpansExporterOpts) (trace.SpanExporter, error) {
-	exp := &kafkaSpanExporter{}
+type KafkaSpansExporterOpts func(k *kafkaSpansExporterOpts)
 
-	return exp, nil
+func NewKafkaSpanExporter(ctx context.Context, opts ...KafkaSpansExporterOpts) (trace.SpanExporter, error) {
+	conf := &kafkaSpansExporterOpts{}
+
+	for _, apply := range opts {
+		apply(conf)
+	}
+
+	if conf.topic == "" {
+		return nil, fmt.Errorf("no topic provided for span exporter")
+	}
+
+	if conf.key == "" {
+		conf.key = defaultMsgKey
+	}
+
+	// construct topic URL
+	topicURL := fmt.Sprintf("kafka://%s?key_name=%s", conf.topic, conf.key)
+
+	// Open kafka topic with URL
+	//
+	// NOTE: the set of kafka brokers must be set in an environment variable KAFKA_BROKERS
+	topic, err := pubsub.OpenTopic(ctx, topicURL)
+	if err != nil {
+		return nil, fmt.Errorf("error opening topic with kafka for span exporter: %w", err)
+	}
+
+	return &kafkaSpanExporter{
+		topic: topic,
+		key:   conf.key,
+	}, nil
 }
 
 func (e *kafkaSpanExporter) ExportSpans(ctx context.Context, spans []trace.ReadOnlySpan) error {
-	return notImplementedErr
+	ctx = context.WithoutCancel(ctx)
+
+	for _, sp := range spans {
+		span, err := SpanToProto(ctx, sp)
+		if err != nil {
+			return err
+		}
+
+		byt, err := proto.Marshal(span)
+		if err != nil {
+			// TODO: log error
+			return fmt.Errorf("error serializing span into binary: %w", err)
+		}
+
+		msg := &pubsub.Message{
+			Metadata: map[string]string{},
+			Body:     byt,
+		}
+		if e.key == defaultMsgKey {
+			msg.Metadata[e.key] = span.GetId().GetFunctionId()
+		}
+
+		if err := e.topic.Send(ctx, &pubsub.Message{}); err != nil {
+			// TODO: log error
+
+			return fmt.Errorf("error publishing span to kafka: %w", err)
+		}
+
+		// TODO: return metrics for exported count
+	}
+
+	return nil
 }
 
 func (e *kafkaSpanExporter) Shutdown(ctx context.Context) error {
-	return notImplementedErr
+	return e.topic.Shutdown(ctx)
 }
