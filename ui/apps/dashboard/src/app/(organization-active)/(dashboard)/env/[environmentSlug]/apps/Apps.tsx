48e94e226408c20995e4a6fe3c847ead02c7ac09
Ana <anafilipadealmeida@gmail.com>
2025-01-30 23:17:23 +0000

    Render cards and flatten data

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/Apps.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/Apps.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { AppCard } from '@inngest/components/Apps/AppCard';
-
+import AppCards from '@/components/Apps/AppCards';
 import { EmptyActiveCard, EmptyArchivedCard } from '@/components/Apps/EmptyAppsCard';
 import { UnattachedSyncsCard } from '@/components/Apps/UnattachedSyncsCard';
 import { useEnvironment } from '@/components/Environments/environment-context';
@@ -16,7 +15,6 @@ export function Apps({ isArchived = false }: Props) {
   const env = useEnvironment();
 
   const res = useApps({ envID: env.id, isArchived });
-  console.log(res);
   if (res.error) {
     throw res.error;
   }
@@ -32,37 +30,13 @@ export function Apps({ isArchived = false }: Props) {
 
   const { apps, latestUnattachedSyncTime } = res.data;
   const hasApps = apps.length > 0;
-  // Sort apps by latest sync time
-  const sortedApps = apps.sort((a, b) => {
-    return (
-      (b.lastSyncedAt ? new Date(b.lastSyncedAt).getTime() : 0) -
-      (a.lastSyncedAt ? new Date(a.lastSyncedAt).getTime() : 0)
-    );
-  });
 
   return (
     <div className="flex items-center justify-center">
       <div className="w-full max-w-[1200px]">
         {!hasApps && !isArchived && <EmptyActiveCard envSlug={env.slug} />}
         {!hasApps && isArchived && <EmptyArchivedCard />}
-        {sortedApps.map((app) => {
-          return (
-            <div className="mb-6" key={app.id}>
-              <AppCard
-                // TO DO: make the error and warning status
-                kind={isArchived ? 'default' : 'primary'}
-              >
-                <AppCard.Content
-                  app={app}
-                  // TO DO: build pills and actions
-                  pill={<></>}
-                  actions={<></>}
-                ></AppCard.Content>
-              </AppCard>
-            </div>
-          );
-        })}
-
+        {hasApps && <AppCards apps={apps} envSlug={env.slug} />}
         {latestUnattachedSyncTime && !isArchived && (
           <UnattachedSyncsCard envSlug={env.slug} latestSyncTime={latestUnattachedSyncTime} />
         )}
---
+++ ui/apps/dashboard/src/components/Apps/AppCardContent.tsx
@@ -0,0 +1,63 @@
+import { Link } from '@inngest/components/Link/Link';
+import { type AppKind } from '@inngest/components/types/app';
+import { RiExternalLinkLine } from '@remixicon/react';
+
+import { type FlattenedApp } from '@/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/useApps';
+import { syncKind, syncStatusText } from '@/components/SyncStatusPill';
+import { pathCreator } from '@/utils/urls';
+
+const getAppCardContent = ({ app, envSlug }: { app: FlattenedApp; envSlug: string }) => {
+  const statusKey = app.status ?? 'default';
+  const appKind: AppKind = app.isArchived ? 'default' : syncKind[statusKey] ?? 'default';
+
+  const status = app.isArchived ? 'Archived' : syncStatusText[statusKey] ?? null;
+
+  const footerHeader = app.error ? (
+    `Error: ${app.error}`
+  ) : app.functionCount === 0 ? (
+    'There are currently no functions registered at this URL.'
+  ) : (
+    <div className="flex w-full items-center justify-between">
+      <span>
+        {app.functionCount} {app.functionCount === 1 ? 'function' : 'functions'} found
+      </span>
+      <Link size="small" href={pathCreator.functions({ envSlug: envSlug })} arrowOnHover>
+        View functions
+      </Link>
+    </div>
+  );
+
+  const footerContent =
+    app.functionCount === 0 ? (
+      <>
+        <p className="text-subtle pb-4">
+          Ensure you have created a function and are exporting it correctly from your serve()
+          command.
+        </p>
+        <Link
+          size="small"
+          target="_blank"
+          href="https://www.inngest.com/docs/learn/serving-inngest-functions?ref=cloud-app"
+          iconAfter={<RiExternalLinkLine className="h-4 w-4" />}
+        >
+          How to serve functions
+        </Link>
+      </>
+    ) : (
+      <ul className="divide-subtle divide-y">
+        {[...app.functions]
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .map((func) => {
+            return (
+              <li key={func.id} className="text-subtle py-2">
+                {func.name}
+              </li>
+            );
+          })}
+      </ul>
+    );
+
+  return { appKind, status, footerHeader, footerContent };
+};
+
+export default getAppCardContent;
