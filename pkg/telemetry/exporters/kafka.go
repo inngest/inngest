94af7f6215bf8d8bf73023abd3f22e8db20b488f
Darwin D Wu <darwin67@users.noreply.github.com>
2025-01-31 12:42:17 -0800

    change producer to use franz-go client

--- pkg/telemetry/exporters/kafka.go
+++ pkg/telemetry/exporters/kafka.go
@@ -5,13 +5,10 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/IBM/sarama"
 	"github.com/inngest/inngest/pkg/logger"
 	"github.com/inngest/inngest/pkg/telemetry/metrics"
+	"github.com/twmb/franz-go/pkg/kgo"
 	"go.opentelemetry.io/otel/sdk/trace"
-	"gocloud.dev/pubsub"
-	"gocloud.dev/pubsub/batcher"
-	"gocloud.dev/pubsub/kafkapubsub"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -23,9 +20,8 @@ var (
 )
 
 type kafkaSpanExporter struct {
-	topic    *pubsub.Topic
-	key      string
-	poolSize int
+	client *kgo.Client
+	key    string
 }
 
 type kafkaSpansExporterOpts struct {
@@ -84,31 +80,25 @@ func NewKafkaSpanExporter(ctx context.Context, opts ...KafkaSpansExporterOpts) (
 		return nil, fmt.Errorf("no topic provided for span exporter")
 	}
 
-	// Configure kafka connection options
-	kconf := kafkapubsub.MinimalConfig()
-	kconf.Producer.MaxMessageBytes = defaultMaxMsgBytes
-	kconf.Producer.RequiredAcks = sarama.WaitForAll // Most durable
-
-	kopts := kafkapubsub.TopicOptions{
-		BatcherOptions: batcher.Options{
-			MaxHandlers:  conf.handlerNum,
-			MaxBatchSize: conf.handlerBatchSize,
-		},
-	}
-
 	if conf.key != "" {
 		conf.key = defaultMsgKey
-		kopts.KeyName = defaultMsgKey
 	}
 
-	topic, err := kafkapubsub.OpenTopic(conf.addrs, kconf, conf.topic, &kopts)
+	cl, err := kgo.NewClient(
+		kgo.SeedBrokers(conf.addrs...),
+		kgo.DefaultProduceTopic(conf.topic),
+		kgo.RequiredAcks(kgo.AllISRAcks()), // Most durable with some perf hits
+
+		// Increment metrics on data loss detection
+		kgo.ProducerOnDataLossDetected(func(topic string, partition int32) {}),
+	)
 	if err != nil {
-		return nil, fmt.Errorf("error opening topic with kafka for span exporter: %w", err)
+		return nil, fmt.Errorf("error initializing kafka client: %w", err)
 	}
 
 	return &kafkaSpanExporter{
-		topic: topic,
-		key:   conf.key,
+		client: cl,
+		key:    conf.key,
 	}, nil
 }
 
@@ -118,57 +108,48 @@ func (e *kafkaSpanExporter) ExportSpans(ctx context.Context, spans []trace.ReadO
 	l := logger.StdlibLogger(ctx)
 
 	wg := sync.WaitGroup{}
-
 	for _, sp := range spans {
 		wg.Add(1)
 
-		go func(ctx context.Context, sp trace.ReadOnlySpan) {
+		span, err := SpanToProto(ctx, sp)
+		if err != nil {
+			l.Error("error converting span to proto", "err", err)
+			continue
+		}
+
+		id := span.GetId()
+		byt, err := proto.Marshal(span)
+		if err != nil {
+			l.Error("error serializing span into binary",
+				"err", err,
+				"acctID", id.AccountId,
+				"wsID", id.EnvId,
+				"fnID", id.FunctionId,
+				"runID", id.RunId,
+			)
+		}
+
+		rec := &kgo.Record{Value: byt}
+		switch e.key {
+		case "account_id", "acct_id":
+			rec.Key = []byte(id.GetAccountId())
+		case "workspace_id", "ws_id", "env_id":
+			rec.Key = []byte(id.GetEnvId())
+		case "workflow_id", "wf_id", "function_id", "fn_id":
+			rec.Key = []byte(id.GetFunctionId())
+		case "run_id":
+			rec.Key = []byte(id.GetRunId())
+		}
+
+		e.client.Produce(ctx, rec, func(r *kgo.Record, err error) {
 			defer wg.Done()
 
-			span, err := SpanToProto(ctx, sp)
-			if err != nil {
-				l.Error("error converting span to proto", "err", err)
-			}
-
-			id := span.GetId()
-			byt, err := proto.Marshal(span)
-			if err != nil {
-				l.Error("error serializing span into binary",
-					"err", err,
-					"acctID", id.AccountId,
-					"wsID", id.EnvId,
-					"fnID", id.FunctionId,
-					"runID", id.RunId,
-				)
-			}
-
-			msg := &pubsub.Message{
-				Metadata: map[string]string{},
-				Body:     byt,
-			}
-			switch e.key {
-			case "account_id", "acct_id":
-				msg.Metadata[e.key] = id.GetAccountId()
-			case "workspace_id", "ws_id", "env_id":
-				msg.Metadata[e.key] = id.GetEnvId()
-			case "workflow_id", "wf_id", "function_id", "fn_id":
-				msg.Metadata[e.key] = id.GetFunctionId()
-			case "run_id":
-				msg.Metadata[e.key] = id.GetRunId()
-			}
-
+			// TODO: do something here?
 			status := "success"
-			if err := e.topic.Send(ctx, msg); err != nil {
-				l.Error("error publishing span to kafka",
-					"err", err,
-					"acctID", id.AccountId,
-					"wsID", id.EnvId,
-					"fnID", id.FunctionId,
-					"runID", id.RunId,
-				)
 
+			if err != nil {
+				l.Error("error on producing span", "error", err)
 				status = "error"
-				// TODO: should attempt error handling or resending it
 			}
 
 			metrics.IncrSpanExportedCounter(ctx, metrics.CounterOpt{
@@ -178,9 +159,8 @@ func (e *kafkaSpanExporter) ExportSpans(ctx context.Context, spans []trace.ReadO
 					"status":   status,
 				},
 			})
+		})
 
-			return
-		}(ctx, sp)
 	}
 
 	wg.Wait()
@@ -188,5 +168,6 @@ func (e *kafkaSpanExporter) ExportSpans(ctx context.Context, spans []trace.ReadO
 }
 
 func (e *kafkaSpanExporter) Shutdown(ctx context.Context) error {
-	return e.topic.Shutdown(ctx)
+	e.client.Close()
+	return nil
 }
