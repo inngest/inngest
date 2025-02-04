9179edb5a7cba79bf30b612184d900b9ed201072
Ana <anafilipadealmeida@gmail.com>
2025-02-04 20:54:47 +0000

    Add tooltip to workers count

--- ui/packages/components/src/Workers/WorkersCounter.tsx
+++ ui/packages/components/src/Workers/WorkersCounter.tsx
@@ -1,3 +1,5 @@
+import { Tooltip, TooltipContent, TooltipTrigger } from '@inngest/components/Tooltip/Tooltip';
+
 import { getStatusBackgroundClass, getStatusBorderClass } from '../Status/statusClasses';
 import { groupedWorkerStatuses, type GroupedWorkerStatus } from '../types/workers';
 import { cn } from '../utils/classNames';
@@ -9,28 +11,50 @@ type Props = {
 
 export default function WorkersCounter({ counts, className }: Props) {
   return (
-    <div className="flex gap-2">
-      {groupedWorkerStatuses.map((status) => {
-        const backgroundClass = getStatusBackgroundClass(status);
-        const borderClass = getStatusBorderClass(status);
-        return (
-          <div
-            key={status}
-            className={cn('flex items-center gap-0.5', className)}
-            title={`${status}: ${counts[status] || '0'}`}
-          >
+    <Tooltip>
+      <TooltipTrigger>
+        <div className="flex gap-2">
+          {groupedWorkerStatuses.map((status) => {
+            const backgroundClass = getStatusBackgroundClass(status);
+            const borderClass = getStatusBorderClass(status);
+            return (
+              <div key={status} className={cn('flex items-center gap-0.5', className)}>
+                <div
+                  className={cn(
+                    'h-[10px] w-[10px] rounded-full border',
+                    backgroundClass,
+                    borderClass,
+                    className
+                  )}
+                />
+                <span className="text-subtle text-sm">{counts[status] || '0'}</span>
+              </div>
+            );
+          })}
+        </div>
+      </TooltipTrigger>
+      <TooltipContent
+        side="bottom"
+        sideOffset={0}
+        align="start"
+        className="border-subtle flex flex-col gap-1 rounded-md border p-2 pr-3 text-xs"
+      >
+        {groupedWorkerStatuses.map((status) => (
+          <div key={status} className="flex items-center gap-1">
             <div
               className={cn(
                 'h-[10px] w-[10px] rounded-full border',
-                backgroundClass,
-                borderClass,
-                className
+                getStatusBackgroundClass(status),
+                getStatusBorderClass(status)
               )}
             />
-            <span className="text-subtle text-sm">{counts[status] || '0'}</span>
+            <div className="flex w-full items-center justify-between gap-3">
+              <div className="text-muted lowercase first-letter:capitalize">{status} workers</div>
+              {counts[status] || 0}
+            </div>
           </div>
-        );
-      })}
-    </div>
+        ))}
+      </TooltipContent>
+    </Tooltip>
   );
 }
