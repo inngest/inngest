0f2b407333aebc7821aaf05c61f4851e61a49d84
Ana <anafilipadealmeida@gmail.com>
2025-02-10 18:39:52 +0000

    Add worker table borders

--- ui/packages/components/src/Table/CompactPaginatedTable.tsx
+++ ui/packages/components/src/Table/CompactPaginatedTable.tsx
@@ -78,111 +78,115 @@ export default function CompactPaginatedTable<T>({
     },
   });
 
-  const tableStyles =
-    'w-full border-subtle rounded-md border border-separate border-spacing-0 overflow-hidden';
+  const tableStyles = 'w-full';
   const tableHeadStyles = 'bg-canvasSubtle';
-  const tableBodyStyles = 'divide-y divide-subtle';
   const tableColumnStyles = 'px-4';
+  const expandedRowSideBorder =
+    'before:bg-surfaceMuted relative before:absolute before:bottom-0 before:left-0 before:top-0 before:w-0.5';
 
   const isEmpty = data.length < 1 && !isLoading;
 
   return (
-    <table className={cn(tableStyles)}>
-      <thead className={tableHeadStyles}>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id} className="h-12">
-            {headerGroup.headers.map((header) => (
-              <th
-                key={header.id}
-                style={{
-                  width: header.getSize(),
-                }}
-                className={cn(tableColumnStyles, 'text-muted text-left text-sm font-normal')}
-              >
-                {header.isPlaceholder ? null : (
-                  <div
-                    className={cn(
-                      header.column.getCanSort() &&
-                        'flex cursor-pointer select-none items-center gap-1'
-                    )}
-                    onClick={header.column.getToggleSortingHandler()}
-                  >
-                    {flexRender(header.column.columnDef.header, header.getContext())}
-                    {{
-                      asc: <RiSortDesc className="h-4 w-4" />,
-                      desc: <RiSortAsc className="h-4 w-4" />,
-                    }[header.column.getIsSorted() as string] ?? null}
-                  </div>
-                )}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody className={tableBodyStyles}>
-        {isEmpty && (
-          <tr>
-            <td
-              className="text-muted h-12 text-center text-sm font-normal"
-              colSpan={table.getVisibleFlatColumns().length}
-            >
-              No results were found.
-            </td>
-          </tr>
-        )}
-        {!isEmpty &&
-          table.getRowModel().rows.map((row) => (
-            <Fragment key={row.id}>
-              <tr className="h-12">
-                {row.getVisibleCells().map((cell) => {
-                  // TO DO: Add left border without layout shift
-                  return (
-                    <td
-                      key={cell.id}
-                      style={{
-                        width: cell.column.getSize(),
-                      }}
-                      className={tableColumnStyles}
+    <div className="border-subtle overflow-hidden rounded-md border">
+      <table className={cn(tableStyles)}>
+        <thead className={tableHeadStyles}>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id} className="border-subtle h-12 border-b">
+              {headerGroup.headers.map((header) => (
+                <th
+                  key={header.id}
+                  style={{
+                    width: header.getSize(),
+                  }}
+                  className={cn(tableColumnStyles, 'text-muted text-left text-sm font-normal')}
+                >
+                  {header.isPlaceholder ? null : (
+                    <div
+                      className={cn(
+                        header.column.getCanSort() &&
+                          'flex cursor-pointer select-none items-center gap-1'
+                      )}
+                      onClick={header.column.getToggleSortingHandler()}
                     >
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </td>
-                  );
-                })}
-              </tr>
-              {row.getIsExpanded() && !isLoading && (
-                <tr>
-                  <td
-                    colSpan={row.getVisibleCells().length}
-                    className={cn(
-                      row.getIsExpanded()
-                        ? 'before:bg-surfaceMuted relative before:absolute before:bottom-0 before:left-0 before:top-0 before:w-0.5'
-                        : ''
-                    )}
-                  >
-                    {renderSubComponent({ row })}
-                  </td>
-                </tr>
-              )}
-            </Fragment>
-          ))}
-      </tbody>
-      {/* TO DO: Add pagination footer */}
-      {!isEmpty && (
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(header.column.columnDef.footer, header.getContext())}
+                      {flexRender(header.column.columnDef.header, header.getContext())}
+                      {{
+                        asc: <RiSortDesc className="h-4 w-4" />,
+                        desc: <RiSortAsc className="h-4 w-4" />,
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  )}
                 </th>
               ))}
             </tr>
           ))}
-        </tfoot>
-      )}
-    </table>
+        </thead>
+        <tbody>
+          {isEmpty && (
+            <tr>
+              <td
+                className="text-muted h-12 text-center text-sm font-normal"
+                colSpan={table.getVisibleFlatColumns().length}
+              >
+                No results were found.
+              </td>
+            </tr>
+          )}
+          {!isEmpty &&
+            table.getRowModel().rows.map((row) => (
+              <Fragment key={row.id}>
+                <tr
+                  className={
+                    row.getIsExpanded() ? 'h-12' : 'border-subtle h-12 border-b last:border-b-0'
+                  }
+                >
+                  {row.getVisibleCells().map((cell, i) => {
+                    return (
+                      <td
+                        key={cell.id}
+                        style={{
+                          width: cell.column.getSize(),
+                        }}
+                        className={cn(
+                          i === 0 && row.getIsExpanded() ? expandedRowSideBorder : '',
+                          tableColumnStyles
+                        )}
+                      >
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </td>
+                    );
+                  })}
+                </tr>
+                {row.getIsExpanded() && !isLoading && (
+                  <tr className="border-subtle border-b last:border-b-0">
+                    <td
+                      colSpan={row.getVisibleCells().length}
+                      className={cn(row.getIsExpanded() ? expandedRowSideBorder : '')}
+                    >
+                      {renderSubComponent({ row })}
+                    </td>
+                  </tr>
+                )}
+              </Fragment>
+            ))}
+        </tbody>
+        {/* TO DO: Add pagination footer */}
+        {!isEmpty && (
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(header.column.columnDef.footer, header.getContext())}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        )}
+      </table>
+    </div>
   );
 }
 
