98628741f7fb53b028824a7d5f33dbc4b294dc7b
Aaron Harper <aaron@inngest.com>
2025-02-14 15:57:35 -0500

    checkpoint

---
+++ ui/apps/dashboard2/app/routes/__root.tsx
@@ -0,0 +1,43 @@
+import type { ReactNode } from 'react';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { Meta, Scripts } from '@tanstack/start';
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <Meta />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ ui/apps/dashboard2/app/ssr.tsx
@@ -0,0 +1,9 @@
+import { getRouterManifest } from '@tanstack/start/router-manifest';
+import { createStartHandler, defaultStreamHandler } from '@tanstack/start/server';
+
+import { createRouter } from './router';
+
+export default createStartHandler({
+  createRouter,
+  getRouterManifest,
+})(defaultStreamHandler);
