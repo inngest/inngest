0242c40f6e1406903191e77e37814500bc97075a
Bruno Scheufler <git@brunoscheufler.com>
2025-02-14 16:26:26 +0100

    notify if noop connector is used

--- pkg/connect/pubsub/noop.go
+++ pkg/connect/pubsub/noop.go
@@ -2,6 +2,7 @@ package pubsub
 
 import (
 	"context"
+	"github.com/inngest/inngest/pkg/logger"
 	"github.com/oklog/ulid/v2"
 
 	"github.com/google/uuid"
@@ -12,26 +13,38 @@ import (
 type noopConnector struct{}
 
 func (noopConnector) Proxy(ctx context.Context, appID uuid.UUID, data *connpb.GatewayExecutorRequestData) (*connpb.SDKResponse, error) {
+	logger.StdlibLogger(ctx).Error("using no-op connector to proxy message", "data", data)
+
 	return &connpb.SDKResponse{}, nil
 }
 
 func (noopConnector) ReceiveExecutorMessages(ctx context.Context, onMessage func(byt []byte, data *connpb.GatewayExecutorRequestData)) error {
+	logger.StdlibLogger(ctx).Error("using no-op connector to receive executor messages")
+
 	return nil
 }
 
 func (noopConnector) RouteExecutorRequest(ctx context.Context, gatewayId ulid.ULID, appId uuid.UUID, connId ulid.ULID, data *connpb.GatewayExecutorRequestData) error {
+	logger.StdlibLogger(ctx).Error("using no-op connector to forward executor request to gateway", "gateway_id", gatewayId, "app_id", appId, "conn_id", connId)
+
 	return nil
 }
 
 func (noopConnector) ReceiveRoutedRequest(ctx context.Context, gatewayId ulid.ULID, appId uuid.UUID, connId ulid.ULID, onMessage func(rawBytes []byte, data *connpb.GatewayExecutorRequestData)) error {
+	logger.StdlibLogger(ctx).Error("using no-op connector receive routed request", "gateway_id", gatewayId, "app_id", appId, "conn_id", connId)
+
 	return nil
 }
 
 func (noopConnector) AckMessage(ctx context.Context, appId uuid.UUID, requestId string, source AckSource) error {
+	logger.StdlibLogger(ctx).Error("using no-op connector to ack message", "request_id", requestId, "source", source)
+
 	return nil
 }
 
 func (noopConnector) NotifyExecutor(ctx context.Context, resp *connpb.SDKResponse) error {
+	logger.StdlibLogger(ctx).Error("using no-op connector to notify executor", "resp", resp)
+
 	return nil
 }
 
