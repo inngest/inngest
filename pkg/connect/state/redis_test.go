4fb59302610c6253abcf78824469bf8da3cdb725
Bruno Scheufler <git@brunoscheufler.com>
2025-02-20 12:58:46 +0100

    Update redis_test.go

--- pkg/connect/state/redis_test.go
+++ pkg/connect/state/redis_test.go
@@ -212,6 +212,18 @@ func TestUpsertConnection(t *testing.T) {
 			membersByGroup1, err := r.SMembers(connectionsByGroup1Key)
 			require.NoError(t, err)
 			require.Equal(t, []string{connId.String()}, membersByGroup1)
+
+			retrievedConn, err := connManager.GetConnection(ctx, envId, connId)
+			require.NoError(t, err)
+			require.Equal(t, expectedConn, retrievedConn)
+
+			connsByEnv, err := connManager.GetConnectionsByEnvID(ctx, envId)
+			require.NoError(t, err)
+			require.Equal(t, []*connect.ConnMetadata{expectedConn}, connsByEnv)
+
+			connsByGroup1, err := connManager.GetConnectionsByGroupID(ctx, envId, group1Id)
+			require.NoError(t, err)
+			require.Equal(t, []*connect.ConnMetadata{expectedConn}, connsByGroup1)
 		})
 
 		t.Run("subsequent upsert should update", func(t *testing.T) {
@@ -238,154 +250,18 @@ func TestUpsertConnection(t *testing.T) {
 			membersByGroup1, err := r.SMembers(connectionsByGroup1Key)
 			require.NoError(t, err)
 			require.Equal(t, []string{connId.String()}, membersByGroup1)
-		})
-	})
 
-	t.Run("single unsynced app", func(t *testing.T) {
-		r := miniredis.RunT(t)
-
-		rc, err := rueidis.NewClient(rueidis.ClientOption{
-			InitAddress:  []string{r.Addr()},
-			DisableCache: true,
-		})
-		require.NoError(t, err)
-		defer rc.Close()
-
-		connManager := NewRedisConnectionStateManager(rc)
-
-		ctx := context.Background()
-
-		lastHeartbeat := time.Now().Truncate(time.Minute)
-
-		accountId, envId := uuid.New(), uuid.New()
-		connId, gatewayId := ulid.MustNew(ulid.Now(), rand.Reader), ulid.MustNew(ulid.Now(), rand.Reader)
-
-		app1Version := "app-1-v.test"
-
-		connectionsByEnvKey := connManager.connectionHash(envId)
-		group1Id := "app-1-hash"
-
-		connectionsByGroup1Key := connManager.connIndexByGroup(envId, group1Id)
-
-		groupsByEnvKey := connManager.workerGroupHash(envId)
-
-		// No groups created
-		require.False(t, r.Exists(groupsByEnvKey))
-
-		// No connections upserted
-		require.False(t, r.Exists(connectionsByEnvKey))
-
-		// No indexes created
-		require.False(t, r.Exists(connectionsByGroup1Key))
-
-		group1 := &WorkerGroup{
-			AccountID:     accountId,
-			EnvID:         envId,
-			AppName:       "app-1",
-			AppVersion:    &app1Version,
-			SDKLang:       "go",
-			SDKVersion:    "v-test",
-			FunctionSlugs: []string{"fn-1", "fn-2"},
-			Hash:          group1Id,
-		}
-		group1Byt, err := json.Marshal(group1)
-		require.NoError(t, err)
-
-		groupIds := []string{group1Id}
-
-		attrs := &connect.SystemAttributes{
-			CpuCores: 10,
-			MemBytes: 1024 * 1024,
-			Os:       "testOS",
-		}
-
-		expectedConn := &connect.ConnMetadata{
-			Id:       connId.String(),
-			GroupIds: groupIds,
-
-			InstanceId:      "my-worker",
-			Status:          connect.ConnectionStatus_READY,
-			SdkLanguage:     "go",
-			SdkVersion:      "v-test",
-			Attributes:      attrs,
-			GatewayId:       gatewayId.String(),
-			LastHeartbeatAt: timestamppb.New(lastHeartbeat),
-		}
-		connByt, err := json.Marshal(expectedConn)
-		require.NoError(t, err)
-
-		conn := &Connection{
-			AccountID:    accountId,
-			EnvID:        envId,
-			ConnectionId: connId,
-			WorkerIP:     "127.0.0.1",
-			Data: &connect.WorkerConnectRequestData{
-				ConnectionId: connId.String(),
-				InstanceId:   "my-worker",
-				Apps: []*connect.AppConfiguration{
-					{
-						AppName:    "app-1",
-						AppVersion: &app1Version,
-						Functions:  nil,
-					},
-				},
-				WorkerManualReadinessAck: false,
-				SystemAttributes:         attrs,
-				SdkVersion:               "v-test",
-				SdkLanguage:              "go",
-			},
-			Groups: map[string]*WorkerGroup{
-				group1Id: group1,
-			},
-			GatewayId: gatewayId,
-		}
-
-		t.Run("initial upsert should create", func(t *testing.T) {
-			err = connManager.UpsertConnection(ctx, conn, connect.ConnectionStatus_READY, lastHeartbeat)
+			retrievedConn, err := connManager.GetConnection(ctx, envId, connId)
 			require.NoError(t, err)
+			require.Equal(t, expectedConn, retrievedConn)
 
-			// Groups created
-			require.True(t, r.Exists(groupsByEnvKey))
-			require.Equal(t, string(group1Byt), r.HGet(groupsByEnvKey, group1Id))
-
-			// Connections upserted
-			require.True(t, r.Exists(connectionsByEnvKey))
-			hkeysByEnv, err := r.HKeys(connectionsByEnvKey)
+			connsByEnv, err := connManager.GetConnectionsByEnvID(ctx, envId)
 			require.NoError(t, err)
-			require.Equal(t, []string{connId.String()}, hkeysByEnv)
-			require.Equal(t, string(connByt), r.HGet(connectionsByEnvKey, connId.String()))
+			require.Equal(t, []*connect.ConnMetadata{expectedConn}, connsByEnv)
 
-			// Indexes created
-			require.True(t, r.Exists(connectionsByGroup1Key))
-			membersByGroup1, err := r.SMembers(connectionsByGroup1Key)
+			connsByGroup1, err := connManager.GetConnectionsByGroupID(ctx, envId, group1Id)
 			require.NoError(t, err)
-			require.Equal(t, []string{connId.String()}, membersByGroup1)
-		})
-
-		t.Run("subsequent upsert should update", func(t *testing.T) {
-			err = connManager.UpsertConnection(ctx, conn, connect.ConnectionStatus_DISCONNECTED, lastHeartbeat)
-			require.NoError(t, err)
-
-			expectedConn.Status = connect.ConnectionStatus_DISCONNECTED
-			connByt, err := json.Marshal(expectedConn)
-			require.NoError(t, err)
-
-			// Groups created
-			require.True(t, r.Exists(groupsByEnvKey))
-			require.Equal(t, string(group1Byt), r.HGet(groupsByEnvKey, group1Id))
-
-			// Connections upserted
-			require.True(t, r.Exists(connectionsByEnvKey))
-			hkeysByEnv, err := r.HKeys(connectionsByEnvKey)
-			require.NoError(t, err)
-			require.Equal(t, []string{connId.String()}, hkeysByEnv)
-			require.Equal(t, string(connByt), r.HGet(connectionsByEnvKey, connId.String()))
-
-			// Indexes created
-			require.True(t, r.Exists(connectionsByGroup1Key))
-			membersByGroup1, err := r.SMembers(connectionsByGroup1Key)
-			require.NoError(t, err)
-			require.Equal(t, []string{connId.String()}, membersByGroup1)
+			require.Equal(t, []*connect.ConnMetadata{expectedConn}, connsByGroup1)
 		})
 	})
 
@@ -629,6 +505,26 @@ func TestUpsertConnection(t *testing.T) {
 		require.False(t, r.Exists(connectionsByApp1Key))
 		require.False(t, r.Exists(connectionsByGroup1Key))
 		require.False(t, r.Exists(connectionsByGroup2Key))
+
+		retrievedConn, err := connManager.GetConnection(ctx, envId, connId)
+		require.NoError(t, err)
+		require.Nil(t, retrievedConn)
+
+		connsByEnv, err := connManager.GetConnectionsByEnvID(ctx, envId)
+		require.NoError(t, err)
+		require.Nil(t, connsByEnv)
+
+		connsByApp1, err := connManager.GetConnectionsByAppID(ctx, envId, appId1)
+		require.NoError(t, err)
+		require.Nil(t, connsByApp1)
+
+		connsByGroup1, err := connManager.GetConnectionsByGroupID(ctx, envId, group1Id)
+		require.NoError(t, err)
+		require.Nil(t, connsByGroup1)
+
+		connsByGroup2, err := connManager.GetConnectionsByGroupID(ctx, envId, group2Id)
+		require.NoError(t, err)
+		require.Nil(t, connsByGroup2)
 
 		group1 := &WorkerGroup{
 			AccountID:     accountId,
