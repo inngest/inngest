e03042b49cfc7cbb374188cf1b3a9acbb772d244
Bruno Scheufler <git@brunoscheufler.com>
2025-02-24 13:49:04 +0100

    Update auth.go

--- pkg/connect/auth/auth.go
+++ pkg/connect/auth/auth.go
@@ -13,13 +13,16 @@ type Entitlements struct {
 }
 
 type Response struct {
-	AccountID    uuid.UUID
-	EnvID        uuid.UUID
+	AccountID uuid.UUID
+	EnvID     uuid.UUID
+
+	// Entitlements are included in short-lived token
 	Entitlements Entitlements
 }
 
 type Handler func(context.Context, *connect.WorkerConnectRequestData) (*Response, error)
 
 type SessionTokenSigner interface {
+	// SignSessionToken issues a short-lived session token including entitlements
 	SignSessionToken(accountId uuid.UUID, envId uuid.UUID, expireAfter time.Duration, entitlements Entitlements) (string, error)
 }
