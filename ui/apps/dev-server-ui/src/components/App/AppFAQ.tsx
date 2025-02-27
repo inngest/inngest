43384b42a54ac1162ba050be6a63f805365b7bf9
Ana <anafilipadealmeida@gmail.com>
2025-02-27 17:42:10 +0000

    Change carbon-1000

--- ui/apps/dev-server-ui/src/components/App/AppFAQ.tsx
+++ ui/apps/dev-server-ui/src/components/App/AppFAQ.tsx
@@ -6,6 +6,8 @@ import { RiAddLine, RiFunctionLine, RiPlayFill } from '@remixicon/react';
 
 import HelperCard from './HelperCard';
 
+const enableOnboarding = false;
+
 export default function AppFAQ({ openByDefault = false }) {
   return (
     <AccordionList
@@ -18,38 +20,40 @@ export default function AppFAQ({ openByDefault = false }) {
           NEED HELP SETTING UP YOUR APP?
         </AccordionList.Trigger>
         <AccordionList.Content className="px-0">
-          <div className="mb-8 grid grid-cols-1 gap-3 md:grid-cols-3">
-            <HelperCard
-              href="/apps/choose-framework"
-              icon={
-                <div className="bg-primary-3xSubtle w-fit rounded-sm p-[10px]">
-                  <RiAddLine className="h-5 w-5" />
-                </div>
-              }
-              title="Add Inngest to existing project"
-              description="Choose your preferred framework and build your app using inngest."
-            />
-            <HelperCard
-              href="/apps/choose-template"
-              icon={
-                <div className="bg-tertiary-3xSubtle w-fit rounded-sm p-[10px]">
-                  <RiFunctionLine className="h-5 w-5" />
-                </div>
-              }
-              title="Grab a starter template"
-              description="Choose from our pre-built templates for a faster start."
-            />
-            <HelperCard
-              href=""
-              icon={
-                <div className="bg-quaternary-cool3xSubtle w-fit rounded-sm p-[10px]">
-                  <RiPlayFill className="h-5 w-5" />
-                </div>
-              }
-              title="Run a demo"
-              description="Explore our demo project to see Inngest in action locally."
-            />
-          </div>
+          {enableOnboarding && (
+            <div className="mb-8 grid grid-cols-1 gap-3 md:grid-cols-3">
+              <HelperCard
+                href="/apps/choose-framework"
+                icon={
+                  <div className="bg-primary-3xSubtle w-fit rounded-sm p-[10px]">
+                    <RiAddLine className="h-5 w-5" />
+                  </div>
+                }
+                title="Add Inngest to existing project"
+                description="Choose your preferred framework and build your app using inngest."
+              />
+              <HelperCard
+                href="/apps/choose-template"
+                icon={
+                  <div className="bg-tertiary-3xSubtle w-fit rounded-sm p-[10px]">
+                    <RiFunctionLine className="h-5 w-5" />
+                  </div>
+                }
+                title="Grab a starter template"
+                description="Choose from our pre-built templates for a faster start."
+              />
+              <HelperCard
+                href=""
+                icon={
+                  <div className="bg-quaternary-cool3xSubtle w-fit rounded-sm p-[10px]">
+                    <RiPlayFill className="h-5 w-5" />
+                  </div>
+                }
+                title="Run a demo"
+                description="Explore our demo project to see Inngest in action locally."
+              />
+            </div>
+          )}
           <AccordionList type="multiple" defaultValue={[]}>
             <AccordionList.Item value="app_definition">
               <AccordionList.Trigger>What is an app?</AccordionList.Trigger>
