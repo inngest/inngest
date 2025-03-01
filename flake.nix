f629f4d37ff7a3d3c836c27ca6c3e5f98c0a3071
Darwin D Wu <darwin67@users.noreply.github.com>
2025-02-28 23:48:54 -0800

    Update node version due to pnpm issue
    https://github.com/pnpm/pnpm/issues/9029

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Inngest Dev Server";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,7 +12,7 @@
         pkgs = import nixpkgs { inherit system; };
         corepack = pkgs.stdenv.mkDerivation {
           name = "corepack";
-          buildInputs = [ pkgs.nodejs_20 ];
+          buildInputs = [ pkgs.nodejs_22 ];
           phases = [ "installPhase" ];
           installPhase = ''
             mkdir -p $out/bin
