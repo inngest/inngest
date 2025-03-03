245772ff2cfeda52cc62b9d520c17adf79c3e1ef
Chris Dzombak <chris@dzombak.com>
2025-03-03 16:38:09 -0700

    Fix server-side error on billing overview page (#2221)
    * Fix server-side error on billing overview page
    
    * minor: remove unnecessary whitespace
    
    * Simplify code
    
    ---------
    
    Co-authored-by: Ana Filipa de Almeida <anafilipadealmeida@gmail.com>

--- ui/apps/dashboard/src/components/Billing/Addons/MetricsExportEntitlementValue.tsx
+++ ui/apps/dashboard/src/components/Billing/Addons/MetricsExportEntitlementValue.tsx
@@ -15,35 +15,23 @@ export default function MetricsExportEntitlementValue({
     return 'Not enabled';
   }
 
-  if (!granularitySeconds || !freshnessSeconds) {
-    throw new Error(
-      'granularitySeconds and freshnessSeconds must be given when metrics export is enabled'
-    );
-  }
-
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          gap: '0.5rem',
-        }}
-      >
-        <span className="font-medium">Enabled</span>
-        <span
-          className="border-subtle"
-          style={{ borderLeftWidth: '1px', borderRightWidth: '1px' }}
-        ></span>
-        <span className="text-muted">Granularity</span>
-        <span className="font-medium">{entitlementSecondsToStr(granularitySeconds)}</span>
-        <span
-          className="border-subtle"
-          style={{ borderLeftWidth: '1px', borderRightWidth: '1px' }}
-        ></span>
-        <span className="text-muted">Delay</span>
-        <span className="font-medium">{entitlementSecondsToStr(freshnessSeconds)}</span>
-      </div>
-    </>
+    <div className="flex items-center gap-2">
+      <span className="font-medium">Enabled</span>
+      {granularitySeconds && (
+        <>
+          <span className="border-subtle border-l border-r" />
+          <span className="text-muted">Granularity</span>
+          <span className="font-medium">{entitlementSecondsToStr(granularitySeconds)}</span>
+        </>
+      )}
+      {freshnessSeconds && (
+        <>
+          <span className="border-subtle border-l border-r" />
+          <span className="text-muted">Delay</span>
+          <span className="font-medium">{entitlementSecondsToStr(freshnessSeconds)}</span>
+        </>
+      )}
+    </div>
   );
 }
