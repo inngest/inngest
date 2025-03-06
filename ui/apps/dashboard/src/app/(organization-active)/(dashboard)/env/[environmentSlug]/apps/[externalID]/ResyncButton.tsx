489f43c6664fef7b639b5601097a4fcea892097b
Ana <anafilipadealmeida@gmail.com>
2025-03-06 17:56:16 +0000

    Change case

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/ResyncButton.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/ResyncButton.tsx
@@ -28,7 +28,7 @@ export function ResyncButton({
         onClick={() => setIsModalVisible(true)}
         disabled={disabled}
         kind="primary"
-        label={appMethod === methodTypes.Connect ? 'Migrate to Serve' : 'Resync'}
+        label={appMethod === methodTypes.Connect ? 'Migrate to serve' : 'Resync'}
         icon={<RiRefreshLine />}
         iconSide="left"
       />
