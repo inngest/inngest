70b2558bb90be5a587c72d8c7b24b177c53f1644
Ana Filipa de Almeida <anafilipadealmeida@gmail.com>
2025-03-06 19:12:00 +0000

    Add case for connect apps (#2229)
    * Add case for connect apps
    
    * Change case
    
    * Apply suggestions from code review
    
    Co-authored-by: Dan Farrelly <djfarrelly@users.noreply.github.com>
    
    * Change appearance
    
    ---------
    
    Co-authored-by: Dan Farrelly <djfarrelly@users.noreply.github.com>

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/ResyncModal.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/ResyncModal.tsx
@@ -4,6 +4,7 @@ import { Button } from '@inngest/components/Button';
 import { Input } from '@inngest/components/Forms/Input';
 import { Modal } from '@inngest/components/Modal';
 import { Switch, SwitchLabel, SwitchWrapper } from '@inngest/components/Switch';
+import { methodTypes } from '@inngest/components/types/app';
 import { cn } from '@inngest/components/utils/classNames';
 import { RiLoopLeftLine } from '@remixicon/react';
 import { toast } from 'sonner';
@@ -35,9 +36,17 @@ type Props = {
   onClose: () => void;
   url: string;
   platform: string | null;
+  appMethod?: string;
 };
 
-export default function ResyncModal({ appExternalID, isOpen, onClose, url, platform }: Props) {
+export default function ResyncModal({
+  appExternalID,
+  isOpen,
+  onClose,
+  url,
+  platform,
+  appMethod,
+}: Props) {
   const [overrideValue, setOverrideValue] = useState(url);
   const [isURLOverridden, setURLOverridden] = useState(false);
   const [failure, setFailure] = useState<CodedError>();
@@ -91,87 +100,93 @@ export default function ResyncModal({ appExternalID, isOpen, onClose, url, platf
     }
   }
 
+  const isConnect = appMethod === methodTypes.Connect;
+
   return (
-    <Modal
-      className="w-[800px]"
-      description="Send a new sync request to your app"
-      isOpen={isOpen}
-      onClose={onClose}
-      title={
-        <div className="mb-4 flex flex-row items-center gap-3">
+    <Modal className="w-[800px]" isOpen={isOpen} onClose={onClose}>
+      <Modal.Header>
+        <div className="flex flex-row items-center gap-3">
           <RiLoopLeftLine className="h-6 w-6" />
-          <h2 className="text-lg font-medium">Resync App</h2>
+          <h2 className="text-lg font-medium">{isConnect ? 'Migrate to serve' : 'Resync app'}</h2>
         </div>
-      }
-    >
-      <div className="border-subtle border-b px-6">
-        {platform === 'vercel' && !failure && (
-          <Alert className="my-6" severity="info" showIcon={false}>
-            Vercel generates a unique URL for each deployment (
-            <Alert.Link
-              severity="info"
-              className="inline"
-              href="https://vercel.com/docs/deployments/generated-urls"
-              target="_blank"
-            >
-              see docs
-            </Alert.Link>
-            ). Please confirm that you are using the correct URL if you choose a deployment&apos;s
-            generated URL instead of a static domain for your app.
-          </Alert>
-        )}
-        <p className="my-6">
-          This initiates the sync request to your app which pushes the updated function
-          configuration to Inngest.
-        </p>
-
-        <p className="mb-2">The URL where you serve Inngest functions:</p>
-
-        <div className="mb-6 flex-1">
-          <Input
-            placeholder="https://example.com/api/inngest"
-            name="url"
-            value={url}
-            onChange={(e) => {
-              setOverrideValue(e.target.value);
-            }}
-            readOnly={!isURLOverridden}
-            className={cn(!isURLOverridden && 'bg-disabled')}
-          />
-        </div>
-        <div className="mb-6">
-          <SwitchWrapper>
-            <Switch
-              checked={isURLOverridden}
-              disabled={isSyncing}
-              onCheckedChange={() => {
-                setURLOverridden((prev) => !prev);
-              }}
-              id="override"
-            />
-            <SwitchLabel htmlFor="override">Override Input</SwitchLabel>
-          </SwitchWrapper>
-          {isURLOverridden && !failure && (
-            <p className="text-warning pl-[50px] pt-1">
-              Please ensure that your app ID (
+      </Modal.Header>
+      <Modal.Body>
+        <>
+          {platform === 'vercel' && !failure && (
+            <Alert className="my-6" severity="info" showIcon={false}>
+              Vercel generates a unique URL for each deployment (
               <Alert.Link
-                severity="warning"
-                size="medium"
+                severity="info"
                 className="inline"
-                href="https://www.inngest.com/docs/apps#apps-in-sdk"
+                href="https://vercel.com/docs/deployments/generated-urls"
+                target="_blank"
               >
-                docs
+                see docs
               </Alert.Link>
-              ) is not changed before resyncing. Changing the app ID will result in the creation of
-              a new app in this environment.
+              ). Please confirm that you are using the correct URL if you choose a deployment&apos;s
+              generated URL instead of a static domain for your app.
+            </Alert>
+          )}
+          {isConnect ? (
+            <p className="my-6">
+              Apps using connect automatically sync every time they connect to Inngest - a manual
+              resync is not necessary. Migrate the app to use HTTP via &quot;serve&quot; by setting
+              your URL.
+            </p>
+          ) : (
+            <p className="my-6">
+              This initiates the sync request to your app which pushes the updated function
+              configuration to Inngest.
             </p>
           )}
-        </div>
 
-        {failure && !isSyncing && <SyncFailure error={failure} />}
-      </div>
+          <p className="mb-2">The URL where you serve Inngest functions:</p>
 
-      <div className="flex justify-end gap-2 p-6">
+          <div className="mb-6 flex-1">
+            <Input
+              placeholder="https://example.com/api/inngest"
+              name="url"
+              value={url}
+              onChange={(e) => {
+                setOverrideValue(e.target.value);
+              }}
+              readOnly={!isURLOverridden}
+              className={cn(!isURLOverridden && 'bg-disabled')}
+            />
+          </div>
+          <div className="mb-6">
+            <SwitchWrapper>
+              <Switch
+                checked={isURLOverridden}
+                disabled={isSyncing}
+                onCheckedChange={() => {
+                  setURLOverridden((prev) => !prev);
+                }}
+                id="override"
+              />
+              <SwitchLabel htmlFor="override">Override Input</SwitchLabel>
+            </SwitchWrapper>
+            {isURLOverridden && !failure && (
+              <p className="text-warning pl-[50px] pt-1">
+                Please ensure that your app ID (
+                <Alert.Link
+                  severity="warning"
+                  size="medium"
+                  className="inline"
+                  href="https://www.inngest.com/docs/apps#apps-in-sdk"
+                >
+                  docs
+                </Alert.Link>
+                ) is not changed before resyncing. Changing the app ID will result in the creation
+                of a new app in this environment.
+              </p>
+            )}
+          </div>
+
+          {failure && !isSyncing && <SyncFailure error={failure} />}
+        </>
+      </Modal.Body>
+      <Modal.Footer className="flex justify-end gap-2">
         <Button
           appearance="outlined"
           kind="secondary"
@@ -180,8 +195,13 @@ export default function ResyncModal({ appExternalID, isOpen, onClose, url, platf
           label="Cancel"
         />
 
-        <Button onClick={onSync} disabled={isSyncing} kind="primary" label="Resync App" />
-      </div>
+        <Button
+          onClick={onSync}
+          disabled={isSyncing || (!isURLOverridden && isConnect)}
+          kind="primary"
+          label={isConnect ? 'Migrate app' : 'Resync app'}
+        />
+      </Modal.Footer>
     </Modal>
   );
 }
