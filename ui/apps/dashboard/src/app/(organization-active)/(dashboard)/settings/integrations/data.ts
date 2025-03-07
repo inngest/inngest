ed25954ba91427f7403ae55826331d9e7a18cab7
Aaron Harper <aaron@inngest.com>
2025-03-06 20:20:54 -0500

    Surface Vercel error on integrations page

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/settings/integrations/data.ts
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/settings/integrations/data.ts
@@ -1,3 +1,5 @@
+import { ClientError } from 'graphql-request';
+
 import { graphql } from '@/gql/gql';
 import type { VercelIntegration } from '@/gql/graphql';
 import graphqlAPI from '@/queries/graphqlAPI';
@@ -22,13 +24,17 @@ const vercelIntegrationQuery = graphql(`
   }
 `);
 
-export async function getVercelIntegration(): Promise<VercelIntegration | null> {
+export async function getVercelIntegration(): Promise<VercelIntegration | null | Error> {
   try {
     const res = await graphqlAPI.request(vercelIntegrationQuery);
     return res.account.vercelIntegration ?? null;
   } catch (err) {
-    // TODO: Handle this in the backend instead of swallowing here.
-    console.error(err);
-    return null;
+    if (err instanceof ClientError) {
+      return new Error(err.response.errors?.[0]?.message ?? 'Unknown error');
+    }
+    if (err instanceof Error) {
+      return err;
+    }
+    return new Error('Unknown error');
   }
 }
