33b4b740df1908855eeb095151d73d1713bbab41
Tony Holdstock-Brown <tonyhb@gmail.com>
2025-03-21 07:39:01 -0700

    RFC

--- pkg/execution/driver/httpdriver/dialer.go
+++ pkg/execution/driver/httpdriver/dialer.go
@@ -18,7 +18,7 @@ func init() {
 		"169.254.0.0/16", // RFC3927 link-local
 		"::1/128",        // IPv6 loopback
 		"fe80::/10",      // IPv6 link-local
-		"fc00::/7",       // IPv6 unique local addr
+		"fc00::/7",       // IPv6 unique local addr (RFC4193)
 		"ff00::/8",       // multicast
 		"fec0::/10",      // deprecated
 	} {
