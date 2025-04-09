70ad062166e01d5359fd53c35515e9ce6582b13f
Tony Holdstock-Brown <tonyhb@gmail.com>
2025-04-08 23:12:28 -0700

    Add stub code

---
+++ pkg/execution/pauses/block.go
@@ -0,0 +1,145 @@
+package pauses
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/inngest/inngest/pkg/execution/state"
+	"github.com/oklog/ulid/v2"
+	"gocloud.dev/blob"
+)
+
+const (
+	// DefaultPausesPerBlock is the number of pauses to store in a single block.
+	// A pause equates to roughly ~0.75-1KB of data, so this is a good default
+	// of roughly 40mb blocks.
+	DefaultPausesPerBlock = 40_000
+)
+
+// Block represents a block of pauses.
+type Block struct {
+	// ID is the block ID.
+	ID ulid.ULID
+	// Index is the index for this block, eg. the workspac and event name.
+	Index Index
+	// Pauses is the slice of pauses in this block.
+	Pauses []*state.Pause
+}
+
+type blockstore struct {
+	// size is the size of blocks when writing
+	size int
+
+	// buf is the backing buffer that we process blocks from when flushing.
+	//
+	// We call `PausesSince` on this buffer to get all pauses from zeroTime to
+	// now.  Note that this is not optimal;  this may load more pauses (in batches,
+	// not in entirety) than we need.  In the future we may want to add a method
+	// to fetch the first N pauses from the buffer.
+	//
+	// Right now, the backing implementation
+	buf Bufferer
+
+	bucket *blob.Bucket
+}
+
+func (b blockstore) BlockSize() int {
+	return b.size
+}
+
+// FlushIndexBlock processes a given index, fetching pauses from the backing buffer
+// and writing to a block.
+func (b blockstore) FlushIndexBlock(ctx context.Context, index Index) error {
+	if b.buf == nil || b.bucket == nil || b.size == 0 {
+		return nil
+	}
+
+	// TODO: CLAIM A LEASE.
+
+	iter, err := b.buf.PausesSince(ctx, index, time.Time{})
+	if err != nil {
+		return fmt.Errorf("failed to load pauses from buffer: %w", err)
+	}
+
+	block := &Block{
+		Index:  index,
+		Pauses: make([]*state.Pause, b.size),
+	}
+
+	n := 0
+	for iter.Next(ctx) {
+		item := iter.Val(ctx)
+		if item == nil {
+			continue
+		}
+
+		block.Pauses[n] = item
+
+		n++
+		if n >= b.size {
+			// We've hit our block size. Quit iterating
+			break
+		}
+	}
+
+	if iter.Error() != nil {
+		return fmt.Errorf("error iterating over buffered pauses: %w", iter.Error())
+	}
+
+	if n < b.size {
+		// We didn't find enough non-nil pauses to fill the block.  Log a warning
+		// and return.  This shouldn't happen, as we shouldn't return nil pauses
+		// from iterators often;  this only happens in a race where the iterator
+		// has pauses in-memory which are then deleted while iterating, leading to
+		// a race condition.
+		return nil
+	}
+
+	// Trim any pauses that are nil.
+	block.Pauses = block.Pauses[:n]
+
+	// TODO: Use the last pause ID as the block ID;  this ensures the block ID
+	// encodes the last pause's timestamp, which is useful for ordering.
+	//
+	// NOTE: Pause IDs are UUIDs, and they are NOT v7 UUIDs...  they're random.
+	//       We generate deterministic ULIDs from pauses here based off of the
+	//       pause timestamp and the UUID.
+
+	// TODO: Marshal the block.  Parquet/Protobuf/etc.
+
+	// Now that we have our block, write it to the backing store.
+	key := b.BlockKey(index, block.ID)
+	if err := b.bucket.WriteAll(ctx, key, []byte{}, nil); err != nil {
+		return fmt.Errorf("failed to write block: %w", err)
+	}
+
+	// TODO: Write block metadata
+
+	// TODO: Remove len(block.Pauses) from the buffer, as they've been flushed.
+
+	return fmt.Errorf("not implemented")
+}
+
+func (b blockstore) BlocksSince(ctx context.Context, index Index, since time.Time) ([]ulid.ULID, error) {
+	// TODO: Read from backing KV (redis/valkey/memorydb/fdb) indexes.
+	return nil, fmt.Errorf("not implemented")
+}
+
+func (b blockstore) ReadBlock(ctx context.Context, index Index, blockID ulid.ULID) (*Block, error) {
+	key := b.BlockKey(index, blockID)
+	byt, err := b.bucket.ReadAll(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: Unmarshal the block
+	_ = byt
+
+	return nil, fmt.Errorf("not implemented")
+}
+
+// GenerateKey generates a key for a given block ID.
+func (b blockstore) BlockKey(idx Index, blockID ulid.ULID) string {
+	return fmt.Sprintf("pauses/%s/%s/blk_%s", idx.WorkspaceID, idx.EventName, blockID)
+}
---
+++ pkg/execution/pauses/pauses.go
@@ -0,0 +1,72 @@
+package pauses
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/inngest/inngest/pkg/execution/state"
+	"github.com/oklog/ulid/v2"
+)
+
+// Index represents the index for a specific pause.  A pause is a signal
+// for a given workspace/event combination and expression;  its always bound
+// by this index.
+type Index struct {
+	WorkspaceID uuid.UUID `json:"workspaceID"`
+	EventName   string    `json:"eventName"`
+}
+
+// Manager implements a buffer and a block reader/flusher, writing pauses to a buffer
+// then flushing them to blocks when the buffer fills.
+type Manager interface {
+	// Bufferer is the core interface used to interact with pauses;  as an end user
+	// of this package you need to only write and read pauses since a given date.
+	Bufferer
+
+	// TODO: Delete and consume
+}
+
+// Bufferer represents a datastore which accepts all writes for pauses.
+// The buffer writes them to a datastore before being periodically flushed
+// to blocks on disk.
+type Bufferer interface {
+	// Write writes one or more pauses to the backing store.  Note that the index
+	// for each pause must be the same.
+	//
+	// This returns the total number of pauses in the buffer.
+	Write(ctx context.Context, index Index, pauses ...*state.Pause) (int, error)
+
+	// PausesSince loads pauses in the buffer for a given index, since a given time.
+	// If the time is ZeroTime, this must return all indexes in the buffer.
+	//
+	// Note that this does not return blocks, as this only reads from the BufferIndexer.
+	PausesSince(ctx context.Context, index Index, since time.Time) (state.PauseIterator, error)
+}
+
+// BlockFlusher is an interface which writes blocks to a backing store
+type BlockFlusher interface {
+	// FlushIndexBlock processes a given index, fetching pauses from the backing buffer
+	// and writing to a block.
+	FlushIndexBlock(ctx context.Context, index Index) error
+
+	// BlockSize returns the number of pauses saved in each block.
+	BlockSize() int
+}
+
+// BlockReader reads blocks for a given index.
+type BlockReader interface {
+	// BlocksSince returns all block IDs that have been written for a given index,
+	// since a given time.
+	//
+	// If the time is ZeroTime, all blocks for the index must be returned.
+	BlocksSince(ctx context.Context, index Index, since time.Time) ([]ulid.ULID, error)
+
+	// ReadBlock reads a single block given an index and block ID.
+	ReadBlock(ctx context.Context, index Index, blockID ulid.ULID) (*Block, error)
+}
+
+type BlockKeyGenerator interface {
+	// GenerateKey generates a key for a given block ID.
+	BlockKey(idx Index, blockID ulid.ULID) string
+}
