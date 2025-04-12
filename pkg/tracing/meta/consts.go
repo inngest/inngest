791281d566af72a6ada5f73769132dde7893f10d
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-04-12 23:40:52 +0000

    Potential const shortenings

--- pkg/tracing/meta/consts.go
+++ pkg/tracing/meta/consts.go
@@ -1,60 +1,61 @@
 package meta
 
+// TODO Comments are for potential shortenings later
 const (
 	// Implementation
-	PropagationKey = "user-otel-ctx"
+	PropagationKey = "user-otel-ctx" // u-ctx
 
 	// Top-level span names
-	SpanNameRun              = "executor.run"
-	SpanNameStepDiscovery    = "executor.step.discovery"
-	SpanNameStep             = "executor.step"
-	SpanNameExecution        = "executor.execution"
-	SpanNameDynamicExtension = "EXTEND"
+	SpanNameRun              = "executor.run"            // run
+	SpanNameStepDiscovery    = "executor.step.discovery" // step.disc
+	SpanNameStep             = "executor.step"           // step
+	SpanNameExecution        = "executor.execution"      // exec
+	SpanNameDynamicExtension = "EXTEND"                  // EXT
 
 	// Run attributes
-	AttributeAccountID        = "_inngest.account.id"
-	AttributeAppID            = "_inngest.app.id"
-	AttributeBatchID          = "_inngest.batch.id"
-	AttributeBatchTimestamp   = "_inngest.batch.ts"
-	AttributeCronSchedule     = "_inngest.cron.schedule"
-	AttributeDropSpan         = "_inngest.executor.drop"
-	AttributeEnvID            = "_inngest.env.id"
-	AttributeEventIDs         = "_inngest.event.ids"
-	AttributeFunctionID       = "_inngest.function.id"
-	AttributeFunctionVersion  = "_inngest.function.version"
-	AttributeInternalLocation = "_inngest.internal.location"
-	AttributeRunID            = "_inngest.run.id"
+	AttributeAccountID        = "_inngest.account.id"        // _acct
+	AttributeAppID            = "_inngest.app.id"            // _app
+	AttributeBatchID          = "_inngest.batch.id"          // _b.id
+	AttributeBatchTimestamp   = "_inngest.batch.ts"          // _b.ts
+	AttributeCronSchedule     = "_inngest.cron.schedule"     // _cron
+	AttributeDropSpan         = "_inngest.executor.drop"     // _drop
+	AttributeEnvID            = "_inngest.env.id"            // _env
+	AttributeEventIDs         = "_inngest.event.ids"         // _e.ids
+	AttributeFunctionID       = "_inngest.function.id"       // _fn
+	AttributeFunctionVersion  = "_inngest.function.version"  // _fn.v
+	AttributeInternalLocation = "_inngest.internal.location" // _loc
+	AttributeRunID            = "_inngest.run.id"            // _run
 
 	// Dynamic span controls
-	AttributeDynamicSpanID = "_inngest.dynamic.span.id"
-	AttributeDynamicStatus = "_inngest.dynamic.status"
+	AttributeDynamicSpanID = "_inngest.dynamic.span.id" // _d.id
+	AttributeDynamicStatus = "_inngest.dynamic.status"  // _d.st
 
 	// Link attributes
-	LinkAttributeType            = "_inngest.link.type"
-	LinkAttributeTypeFollowsFrom = "follows_from"
+	LinkAttributeType            = "_inngest.link.type" // _l.type
+	LinkAttributeTypeFollowsFrom = "follows_from"       // follows
 
 	// Generic step attributes
-	AttributeStepID          = "_inngest.step.id"
-	AttributeStepName        = "_inngest.step.name"
-	AttributeStepOp          = "_inngest.step.op"
-	AttributeStepAttempt     = "_inngest.step.attempt"
-	AttributeStepMaxAttempts = "_inngest.step.max_attempts"
+	AttributeStepID          = "_inngest.step.id"           // _s.id
+	AttributeStepName        = "_inngest.step.name"         // _s.n
+	AttributeStepOp          = "_inngest.step.op"           // _s.op
+	AttributeStepAttempt     = "_inngest.step.attempt"      // _s.a
+	AttributeStepMaxAttempts = "_inngest.step.max_attempts" // _s.m
 
 	// Invoke attributes
-	AttributeStepInvokeExpiry         = "_inngest.step.invoke_function.expiry"
-	AttributeStepInvokeFunctionID     = "_inngest.step.invoke_function.id"
-	AttributeStepInvokeTriggerEventID = "_inngest.step.invoke_function.trigger_event_id"
+	AttributeStepInvokeExpiry         = "_inngest.step.invoke_function.expiry"           // _s.if.exp
+	AttributeStepInvokeFunctionID     = "_inngest.step.invoke_function.id"               // _s.if.id
+	AttributeStepInvokeTriggerEventID = "_inngest.step.invoke_function.trigger_event_id" // _s.if.eid
 
 	// Sleep attributes
-	AttributeStepSleepDuration = "_inngest.step.sleep.duration"
+	AttributeStepSleepDuration = "_inngest.step.sleep.duration" // _s.sleep
 
 	// WaitForEvent attributes
-	AttributeStepWaitForEventExpiry = "_inngest.step.wait_for_event.expiry"
-	AttributeStepWaitForEventIf     = "_inngest.step.wait_for_event.if"
-	AttributeStepWaitForEventName   = "_inngest.step.wait_for_event.name"
+	AttributeStepWaitForEventExpiry = "_inngest.step.wait_for_event.expiry" // _s.w.exp
+	AttributeStepWaitForEventIf     = "_inngest.step.wait_for_event.if"     // _s.w.if
+	AttributeStepWaitForEventName   = "_inngest.step.wait_for_event.name"   // _s.w.name
 
 	// HTTP (serve) attributes
-	AttributeResponseHeaders    = "_inngest.response.headers"
-	AttributeResponseStatusCode = "_inngest.response.status_code"
-	AttributeResponseOutputSize = "_inngest.response.output_size"
+	AttributeResponseHeaders    = "_inngest.response.headers"     // _res.h
+	AttributeResponseStatusCode = "_inngest.response.status_code" // _res.st
+	AttributeResponseOutputSize = "_inngest.response.output_size" // _res.sz
 )
