190c7348a78e5a16969688b1c40eaefedbb9e73f
Darwin D Wu <darwin67@users.noreply.github.com>
2025-04-21 10:55:26 -0700

    replace dialer

--- pkg/execution/driver/httpdriver/dialer.go
+++ pkg/execution/driver/httpdriver/dialer.go
@@ -3,12 +3,13 @@ package httpdriver
 import (
 	"context"
 	"fmt"
-	"math/rand/v2"
+	// "math/rand/v2"
 	"net"
 	"time"
 
 	"github.com/inngest/inngest/pkg/execution/driver/httpdriver/dnscache"
 	"github.com/inngest/inngest/pkg/logger"
+	mdnscache "go.mercari.io/go-dnscache"
 )
 
 const (
@@ -87,112 +88,121 @@ type SecureDialerOpts struct {
 type DialFunc = func(ctx context.Context, network, addr string) (net.Conn, error)
 
 func SecureDialer(o SecureDialerOpts) DialFunc {
-	if o.dial == nil {
-		// Always use the default dialer.  Only allow overrides in testing.
-		o.dial = Dialer.DialContext
+	l := logger.StdlibLogger(context.TODO())
+
+	resolver, err := mdnscache.New(3*time.Second, 5*time.Second, mdnscache.WithLogger(l))
+	if err != nil {
+		return nil
 	}
 
-	return func(ctx context.Context, network, addr string) (net.Conn, error) {
-		// network will be one of the well defined networks as per
-		// https://pkg.go.dev/net#Dial, eg "tcp", "tcp4", "tcp6", etc.
-		//
-		// addr may be a domain or ip and port: "example.com:443", "192.0.2.1:http",
-		// "[fe80::1%lo0]:53".
-		//
-		// We always want to ensure we translate the domains to IP addresses.
-		host, port, err := net.SplitHostPort(addr)
-		if err != nil {
-			return nil, err
-		}
+	return mdnscache.DialFunc(resolver, nil)
 
-		if !o.AllowHostDocker && isDockerHost(host) {
-			return nil, fmt.Errorf("Unable to make request to %s at IP %s: accessing docker host", addr, host)
-		}
+	// if o.dial == nil {
+	// 	// Always use the default dialer.  Only allow overrides in testing.
+	// 	o.dial = Dialer.DialContext
+	// }
 
-		// Ensure that the current hostname is not a domain name.
-		ips, err := cachedResolver.LookupHost(ctx, host)
-		if err != nil {
-			return nil, err
-		}
+	// return func(ctx context.Context, network, addr string) (net.Conn, error) {
+	// 	// network will be one of the well defined networks as per
+	// 	// https://pkg.go.dev/net#Dial, eg "tcp", "tcp4", "tcp6", etc.
+	// 	//
+	// 	// addr may be a domain or ip and port: "example.com:443", "192.0.2.1:http",
+	// 	// "[fe80::1%lo0]:53".
+	// 	//
+	// 	// We always want to ensure we translate the domains to IP addresses.
+	// 	host, port, err := net.SplitHostPort(addr)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-		if o.log {
-			logger.StdlibLogger(ctx).Info("domain resolved",
-				"address", addr,
-				"hosts", ips,
-			)
-		}
+	// 	if !o.AllowHostDocker && isDockerHost(host) {
+	// 		return nil, fmt.Errorf("Unable to make request to %s at IP %s: accessing docker host", addr, host)
+	// 	}
 
-		for _, ip := range ips {
-			if !o.AllowPrivate && isPrivateHost(ip) {
-				return nil, fmt.Errorf("Unable to make request to %s at IP %s: private IP range", addr, ip)
-			}
-			if !o.AllowNAT64 && isNat64(ip) {
-				return nil, fmt.Errorf("Unable to make request to %s at IP %s: NAT64 address", addr, ip)
-			}
-		}
+	// 	// Ensure that the current hostname is not a domain name.
+	// 	ips, err := cachedResolver.LookupHost(ctx, host)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-		// Randomize the order of the IPs. The purpose is to evenly distribute
-		// load across the IPs.
-		rand.Shuffle(len(ips), func(i, j int) {
-			ips[i], ips[j] = ips[j], ips[i]
-		})
+	// 	if o.log {
+	// 		logger.StdlibLogger(ctx).Info("domain resolved",
+	// 			"address", addr,
+	// 			"hosts", ips,
+	// 		)
+	// 	}
 
-		// Try each IP until we get a connection.
-		var conn net.Conn
-		for _, ip := range ips {
-			// We need to give the dialer an IP address. Otherwise, it will do
-			// DNS lookup that doesn't use the cached resolver.
-			addr := net.JoinHostPort(ip, port)
+	// 	for _, ip := range ips {
+	// 		if !o.AllowPrivate && isPrivateHost(ip) {
+	// 			return nil, fmt.Errorf("Unable to make request to %s at IP %s: private IP range", addr, ip)
+	// 		}
+	// 		if !o.AllowNAT64 && isNat64(ip) {
+	// 			return nil, fmt.Errorf("Unable to make request to %s at IP %s: NAT64 address", addr, ip)
+	// 		}
+	// 	}
 
-			conn, err = o.dial(ctx, network, addr)
-			if err == nil {
-				break
-			}
-		}
-		return conn, err
-	}
+	// 	// Randomize the order of the IPs. The purpose is to evenly distribute
+	// 	// load across the IPs.
+	// 	rand.Shuffle(len(ips), func(i, j int) {
+	// 		ips[i], ips[j] = ips[j], ips[i]
+	// 	})
+
+	// 	// Try each IP until we get a connection.
+	// 	var conn net.Conn
+	// 	for _, ip := range ips {
+	// 		// We need to give the dialer an IP address. Otherwise, it will do
+	// 		// DNS lookup that doesn't use the cached resolver.
+	// 		addr := net.JoinHostPort(ip, port)
+
+	// 		conn, err = o.dial(ctx, network, addr)
+	// 		if err == nil {
+	// 			break
+	// 		}
+	// 	}
+	// 	return conn, err
+	// }
 }
 
-func isDockerHost(host string) bool {
-	return host == "host.docker.internal"
-}
+// func isDockerHost(host string) bool {
+// 	return host == "host.docker.internal"
+// }
 
-func isPrivateHost(host string) bool {
-	// fast path;  non-exhaustive for fast lookups.  Basic string matching.
-	if host == "localhost" || host == "0.0.0.0" || host == "localhost.localdomain" {
-		return true
-	}
-	ip := net.ParseIP(host)
-	if ip != nil {
-		return isPrivateIP(ip)
-	}
-	return false
-}
+// func isPrivateHost(host string) bool {
+// 	// fast path;  non-exhaustive for fast lookups.  Basic string matching.
+// 	if host == "localhost" || host == "0.0.0.0" || host == "localhost.localdomain" {
+// 		return true
+// 	}
+// 	ip := net.ParseIP(host)
+// 	if ip != nil {
+// 		return isPrivateIP(ip)
+// 	}
+// 	return false
+// }
 
-func isPrivateIP(ip net.IP) bool {
-	if ip.IsLoopback() ||
-		ip.IsUnspecified() ||
-		ip.IsLinkLocalUnicast() ||
-		ip.IsLinkLocalMulticast() ||
-		ip.IsMulticast() {
-		return true
-	}
+// func isPrivateIP(ip net.IP) bool {
+// 	if ip.IsLoopback() ||
+// 		ip.IsUnspecified() ||
+// 		ip.IsLinkLocalUnicast() ||
+// 		ip.IsLinkLocalMulticast() ||
+// 		ip.IsMulticast() {
+// 		return true
+// 	}
 
-	for _, block := range privateIPBlocks {
-		if block.Contains(ip) {
-			return true
-		}
-	}
+// 	for _, block := range privateIPBlocks {
+// 		if block.Contains(ip) {
+// 			return true
+// 		}
+// 	}
 
-	return false
-}
+// 	return false
+// }
 
-func isNat64(host string) bool {
-	ip := net.ParseIP(host)
-	for _, block := range nat64blocks {
-		if block.Contains(ip) {
-			return true
-		}
-	}
-	return false
-}
+// func isNat64(host string) bool {
+// 	ip := net.ParseIP(host)
+// 	for _, block := range nat64blocks {
+// 		if block.Contains(ip) {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
