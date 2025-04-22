0ac045ef9279a710b61bffb7354045b0cabefb8b
Ana <anafilipadealmeida@gmail.com>
2025-04-22 12:47:35 +0100

    Tweak table padding

--- ui/packages/components/src/Table/NewTable.tsx
+++ ui/packages/components/src/Table/NewTable.tsx
@@ -89,7 +89,7 @@ export default function Table<T>({
 
   const tableStyles = 'w-full';
   const tableHeadStyles = 'bg-canvasSubtle sticky top-0 z-[2]';
-  const tableColumnStyles = 'px-6';
+  const tableColumnStyles = 'px-4';
   const expandedRowSideBorder =
     'before:bg-surfaceMuted relative before:absolute before:bottom-0 before:left-0 before:top-0 before:w-0.5';
 
