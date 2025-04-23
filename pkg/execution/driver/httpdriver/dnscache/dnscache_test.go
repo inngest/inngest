875686a603641bcc57c9e3d2474422d02a19c4ed
Darwin <5746693+darwin67@users.noreply.github.com>
2025-04-23 07:51:18 -0700

    Simplify dnscache (#2359)
    Simplifying to what we need for DNS caching.
    Also added timeouts in multiple areas so it doesn't get stucked on Dial.
    
    ---------
    Co-authored-by: Darwin D Wu <darwin67@users.noreply.github.com>

--- pkg/execution/driver/httpdriver/dnscache/dnscache_test.go
+++ pkg/execution/driver/httpdriver/dnscache/dnscache_test.go
@@ -2,76 +2,56 @@ package dnscache
 
 import (
 	"context"
-	"net"
+	"fmt"
 	"net/http"
-	"strings"
 	"testing"
 	"time"
 
+	"github.com/inngest/inngest/pkg/logger"
 	"github.com/stretchr/testify/require"
 )
 
 func TestDNSCache(t *testing.T) {
-	cachedResolver := &Resolver{}
+	ctx := context.Background()
+	l := logger.StdlibLogger(ctx)
 
-	dialer := &net.Dialer{KeepAlive: 15 * time.Second}
+	ttl := 2 * time.Second
+
+	cachedResolver := New(
+		WithCacheTTL(ttl),
+		WithLogger(l),
+	)
 
 	c := http.Client{
 		Transport: &http.Transport{
-			DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				// network will be one of the well defined networks as per
-				// https://pkg.go.dev/net#Dial, eg "tcp", "tcp4", "tcp6", etc.
-				//
-				// addr may be a domain or ip and port: "example.com:443", "192.0.2.1:http",
-				// "[fe80::1%lo0]:53".
-				//
-				// We always want to ensure we translate the domains to IP addresses.
-				host, port, err := net.SplitHostPort(addr)
-				if err != nil {
-					return nil, err
-				}
-
-				addrs, err := cachedResolver.LookupHost(t.Context(), host)
-				require.NoError(t, err)
-
-				// Try each IP until we get a connection.
-				var conn net.Conn
-				for _, ip := range addrs {
-					// We need to give the dialer an IP address. Otherwise, it will do
-					// DNS lookup that doesn't use the cached resolver.
-					addr := net.JoinHostPort(ip, port)
-
-					conn, err = dialer.DialContext(ctx, network, addr)
-					if err == nil {
-						break
-					}
-				}
-				return conn, err
-			},
+			DialContext: cachedResolver.Dialer(),
 		},
 	}
 
 	// inngest and vercel use SNI
-	addrs := []string{"https://www.example.com", "https://www.inngest.com", "https://vercel.com"}
+	hosts := []string{"www.example.com", "www.inngest.com", "vercel.com"}
 
-	for _, host := range addrs {
-		resp, err := c.Get(host)
-		require.NoError(t, err)
-		require.EqualValues(t, 200, resp.StatusCode)
+	for _, host := range hosts {
+		t.Run(host, func(t *testing.T) {
+			t.Run("non cached", func(t *testing.T) {
+				require.False(t, cachedResolver.isCached(host))
+			})
 
-		entry, ok := cachedResolver.cache.Get("h" + strings.ReplaceAll(host, "https://", ""))
-		require.True(t, ok)
-		require.NotNil(t, entry)
-		require.True(t, entry.used)
+			t.Run("request", func(t *testing.T) {
+				resp, err := c.Get(fmt.Sprintf("https://%s", host))
+				require.NoError(t, err)
+				require.EqualValues(t, 200, resp.StatusCode)
+			})
+
+			t.Run("cached", func(t *testing.T) {
+				require.True(t, cachedResolver.isCached(host))
+			})
+
+			<-time.After(ttl)
+
+			t.Run("cache expired", func(t *testing.T) {
+				require.False(t, cachedResolver.isCached(host))
+			})
+		})
 	}
-
-	require.EqualValues(t, len(addrs), cachedResolver.lookups)
-
-	// These shouldn't incur lookups, as we just looked them up.
-	for _, host := range addrs {
-		resp, err := c.Get(host)
-		require.NoError(t, err)
-		require.EqualValues(t, 200, resp.StatusCode)
-	}
-	require.EqualValues(t, len(addrs), cachedResolver.lookups)
 }
