11c87e918a8982baa48aa420c621cb3bb824c86f
Darwin D Wu <darwin67@users.noreply.github.com>
2025-04-26 11:23:39 -0700

    add debug command

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,7 @@
                              ;; command-args ("dap" "--listen" "127.0.0.1:55878" "--log")
                              command-cwd default-directory
                              host "127.0.0.1"
-                             port 55878
+                             port 40000
                              :type "go"
                              :mode "remote"
                              :request "attach"
