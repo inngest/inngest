c479d381a860bf62784cc2894bfa171f14b67863
Tony Holdstock-Brown <tony@inngest.com>
2025-05-01 20:19:29 -0700

    Update pkg/execution/state/redis_state/queue.go
    Co-authored-by: Bruno Scheufler <git@brunoscheufler.com>

--- pkg/execution/state/redis_state/queue.go
+++ pkg/execution/state/redis_state/queue.go
@@ -795,7 +795,7 @@ type QueueRunMode struct {
 	// Continuations enables continuations
 	Continuations bool
 
-	// PauseFluster determines whether pauses are flushed from hot storage
+	// PauseFlusher determines whether pauses are flushed from hot storage
 	// to blob store
 	PauseFlusher bool
 
