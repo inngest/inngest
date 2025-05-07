8d0b96de6c55501f6aed4adc38bea70fa2f247b4
Darwin D Wu <darwin67@users.noreply.github.com>
2025-05-07 14:08:33 -0700

    update crit to allow different configurations

--- pkg/util/critctx.go
+++ pkg/util/critctx.go
@@ -8,22 +8,49 @@ import (
 	"github.com/inngest/inngest/pkg/logger"
 )
 
-func Crit(ctx context.Context, name string, f func(ctx context.Context) error, withinBounds ...time.Duration) error {
-	_, err := CritT(ctx, name, func(ctx context.Context) (any, error) { return nil, f(ctx) }, withinBounds...)
+type crit struct {
+	boundaries []time.Duration
+	maxDur     time.Duration
+}
+
+type CritOpt func(c *crit)
+
+func WithBoundaries(b ...time.Duration) CritOpt {
+	return func(c *crit) {
+		c.boundaries = b
+	}
+}
+
+func WithMaxDuration(dur time.Duration) CritOpt {
+	return func(c *crit) {
+		c.maxDur = dur
+	}
+}
+
+func Crit(ctx context.Context, name string, f func(ctx context.Context) error, opts ...CritOpt) error {
+	_, err := CritT(ctx, name, func(ctx context.Context) (any, error) { return nil, f(ctx) }, opts...)
 	return err
 }
 
 // Crit is a util to wrap a lambda with a non-cancellable context.  It allows an optional time boundary
 // for checking context deadlines;  if the parent ctx has a deadline shorter than the boundary we exit
 // immediately with an error.
-func CritT[T any](ctx context.Context, name string, f func(ctx context.Context) (T, error), withinBounds ...time.Duration) (resp T, err error) {
+func CritT[T any](ctx context.Context, name string, f func(ctx context.Context) (T, error), opts ...CritOpt) (resp T, err error) {
+	cr := crit{
+		boundaries: []time.Duration{},
+	}
+
+	for _, apply := range opts {
+		apply(&cr)
+	}
+
 	// If withinBounds is set, we have some time period in which we must complete the Crit
 	// section.
 	//
 	// Check the parent context to see if there's a deadline, and if the deadline < withinBounds
 	// don't even bother.  The crit section must exist within some retryable process.
-	if len(withinBounds) == 1 {
-		if dl, ok := ctx.Deadline(); ok && time.Until(dl) < withinBounds[0] {
+	if len(cr.boundaries) == 1 {
+		if dl, ok := ctx.Deadline(); ok && time.Until(dl) < cr.boundaries[0] {
 			return resp, fmt.Errorf("context deadline shorter than critical bounds: %s", name)
 		}
 	}
@@ -37,9 +64,9 @@ func CritT[T any](ctx context.Context, name string, f func(ctx context.Context) 
 
 	// XXX: Instrument critical section durations and error responses via the names.
 
-	if len(withinBounds) == 1 {
+	if len(cr.boundaries) == 1 {
 		actual := time.Since(pre)
-		if actual > withinBounds[0] {
+		if actual > cr.boundaries[0] {
 			// This took longer than the predefined boundaries, so log a fat warning.
 			logger.StdlibLogger(ctx).Warn("critical section took longer than boundaries", "name", name, "duration_ms", actual.Milliseconds())
 		}
