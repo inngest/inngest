8e4433a037cc91eafcf839b6cb566c7580139c68
Darwin D Wu <darwin67@users.noreply.github.com>
2025-05-21 12:55:29 -0700

    update idempotency test

--- tests/golang/event_idempotency_test.go
+++ tests/golang/event_idempotency_test.go
@@ -6,22 +6,33 @@ import (
 	"testing"
 	"time"
 
+	"github.com/inngest/inngest/pkg/coreapi/graph/models"
+	"github.com/inngest/inngest/tests/client"
 	"github.com/inngest/inngestgo"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestEventIdempotency(t *testing.T) {
 	ctx := context.Background()
 	r := require.New(t)
+	c := client.New(t)
 	inngestClient, server, registerFuncs := NewSDKHandler(t, "test")
 	defer server.Close()
 
-	var counter int32
+	var (
+		counter int32
+		runID   string
+	)
 	_, err := inngestgo.CreateFunction(
 		inngestClient,
 		inngestgo.FunctionOpts{ID: "test"},
 		inngestgo.EventTrigger("test", nil),
 		func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+			if runID == "" {
+				runID = input.InputCtx.RunID
+			}
+
 			atomic.AddInt32(&counter, 1)
 			return nil, nil
 		},
@@ -39,9 +50,9 @@ func TestEventIdempotency(t *testing.T) {
 
 	t.Run("same ID", func(t *testing.T) {
 		// Only 1 run if multiple events have the same ID
-
-		sendEvent("abc")
-		sendEvent("abc")
+		for range 5 {
+			sendEvent("abc")
+		}
 
 		r.Eventually(func() bool {
 			return atomic.LoadInt32(&counter) == 1
@@ -51,6 +62,22 @@ func TestEventIdempotency(t *testing.T) {
 		<-time.After(100 * time.Millisecond)
 
 		r.Equal(int32(1), atomic.LoadInt32(&counter))
+
+		t.Run("trace run should have appropriate data", func(t *testing.T) {
+			run := c.WaitForRunTraces(ctx, t, &runID, client.WaitForRunTracesOptions{
+				Status:         models.FunctionStatusCompleted,
+				ChildSpanCount: 1,
+			})
+			require.False(t, run.IsBatch)
+			require.Nil(t, run.BatchCreatedAt)
+
+			t.Run("exec", func(t *testing.T) {
+				exec := run.Trace.ChildSpans[0]
+
+				assert.Equal(t, "function success", exec.Name)
+				assert.False(t, exec.IsRoot)
+			})
+		})
 	})
 
 	t.Run("different IDs", func(t *testing.T) {
