6771307c00c009543c20cd31e2342b8fb32a565a
Albert Chae <albertchae@users.noreply.github.com>
2025-05-27 09:18:16 -0700

    implement FunctionBySlug in resolver so make gql succeeds to the end now

--- pkg/coreapi/graph/resolvers/function.go
+++ pkg/coreapi/graph/resolvers/function.go
@@ -12,3 +12,13 @@ func (r *functionResolver) App(ctx context.Context, obj *models.Function) (*cqrs
 	appID := uuid.MustParse(obj.AppID)
 	return r.Data.GetAppByID(ctx, appID)
 }
+
+func (qr *queryResolver) FunctionBySlug(ctx context.Context, query models.FunctionQuery) (*models.Function, error) {
+	dummyLocalWorkspaceID := uuid.MustParse("6c6f6361-6c00-0000-0000-000000000000")
+
+	fn, err := qr.Data.GetFunctionByExternalID(ctx, dummyLocalWorkspaceID, "local", query.FunctionSlug)
+	if err != nil {
+		return nil, err
+	}
+	return models.MakeFunction(fn)
+}
