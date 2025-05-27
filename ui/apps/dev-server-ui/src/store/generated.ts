92e6963787a2463a51be561d72632098331b25e9
Albert Chae <albertchae@users.noreply.github.com>
2025-05-27 09:34:11 -0700

    pnpm dev:codegen

--- ui/apps/dev-server-ui/src/store/generated.ts
+++ ui/apps/dev-server-ui/src/store/generated.ts
@@ -231,6 +231,7 @@ export type FunctionConfiguration = {
   priority: Maybe<Scalars['String']>;
   rateLimit: Maybe<RateLimitConfiguration>;
   retries: RetryConfiguration;
+  singleton: Maybe<SingletonConfiguration>;
   throttle: Maybe<ThrottleConfiguration>;
 };
 
@@ -726,6 +727,17 @@ export enum RunsV2OrderByField {
   StartedAt = 'STARTED_AT'
 }
 
+export type SingletonConfiguration = {
+  __typename?: 'SingletonConfiguration';
+  key: Maybe<Scalars['String']>;
+  mode: SingletonMode;
+};
+
+export enum SingletonMode {
+  Cancel = 'CANCEL',
+  Skip = 'SKIP'
+}
+
 export type SleepStepInfo = {
   __typename?: 'SleepStepInfo';
   sleepUntil: Scalars['Time'];
@@ -862,6 +874,13 @@ export type GetFunctionsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type GetFunctionsQuery = { __typename?: 'Query', functions: Array<{ __typename?: 'Function', id: string, slug: string, name: string, url: string, triggers: Array<{ __typename?: 'FunctionTrigger', type: FunctionTriggerTypes, value: string }> | null, app: { __typename?: 'App', name: string } }> | null };
+
+export type GetFunctionQueryVariables = Exact<{
+  functionSlug: Scalars['String'];
+}>;
+
+
+export type GetFunctionQuery = { __typename?: 'Query', functionBySlug: { __typename?: 'Function', name: string, id: string, concurrency: number, config: string, slug: string, url: string, configuration: { __typename?: 'FunctionConfiguration', priority: string | null, cancellations: Array<{ __typename?: 'CancellationConfiguration', event: string, timeout: string | null, condition: string | null }>, retries: { __typename?: 'RetryConfiguration', value: number, isDefault: boolean | null }, eventsBatch: { __typename?: 'EventsBatchConfiguration', maxSize: number, timeout: string, key: string | null } | null, concurrency: Array<{ __typename?: 'ConcurrencyConfiguration', scope: ConcurrencyScope, key: string | null, limit: { __typename?: 'ConcurrencyLimitConfiguration', value: number, isPlanLimit: boolean | null } }>, rateLimit: { __typename?: 'RateLimitConfiguration', limit: number, period: string, key: string | null } | null, debounce: { __typename?: 'DebounceConfiguration', period: string, key: string | null } | null, throttle: { __typename?: 'ThrottleConfiguration', burst: number, key: string | null, limit: number, period: string } | null } } | null };
 
 export type GetAppsQueryVariables = Exact<{ [key: string]: never; }>;
 
@@ -1197,6 +1216,58 @@ export const GetFunctionsDocument = `
   }
 }
     `;
+export const GetFunctionDocument = `
+    query GetFunction($functionSlug: String!) {
+  functionBySlug(query: {functionSlug: $functionSlug}) {
+    name
+    id
+    concurrency
+    config
+    configuration {
+      cancellations {
+        event
+        timeout
+        condition
+      }
+      retries {
+        value
+        isDefault
+      }
+      priority
+      eventsBatch {
+        maxSize
+        timeout
+        key
+      }
+      concurrency {
+        scope
+        limit {
+          value
+          isPlanLimit
+        }
+        key
+      }
+      rateLimit {
+        limit
+        period
+        key
+      }
+      debounce {
+        period
+        key
+      }
+      throttle {
+        burst
+        key
+        limit
+        period
+      }
+    }
+    slug
+    url
+  }
+}
+    `;
 export const GetAppsDocument = `
     query GetApps {
   apps {
@@ -1514,6 +1585,9 @@ const injectedRtkApi = api.injectEndpoints({
     GetFunctions: build.query<GetFunctionsQuery, GetFunctionsQueryVariables | void>({
       query: (variables) => ({ document: GetFunctionsDocument, variables })
     }),
+    GetFunction: build.query<GetFunctionQuery, GetFunctionQueryVariables>({
+      query: (variables) => ({ document: GetFunctionDocument, variables })
+    }),
     GetApps: build.query<GetAppsQuery, GetAppsQueryVariables | void>({
       query: (variables) => ({ document: GetAppsDocument, variables })
     }),
@@ -1578,5 +1652,5 @@ const injectedRtkApi = api.injectEndpoints({
 });
 
 export { injectedRtkApi as api };
-export const { useGetEventQuery, useLazyGetEventQuery, useGetFunctionRunQuery, useLazyGetFunctionRunQuery, useGetFunctionsQuery, useLazyGetFunctionsQuery, useGetAppsQuery, useLazyGetAppsQuery, useGetAppQuery, useLazyGetAppQuery, useCreateAppMutation, useUpdateAppMutation, useDeleteAppMutation, useGetTriggersStreamQuery, useLazyGetTriggersStreamQuery, useGetFunctionRunStatusQuery, useLazyGetFunctionRunStatusQuery, useGetFunctionRunOutputQuery, useLazyGetFunctionRunOutputQuery, useGetHistoryItemOutputQuery, useLazyGetHistoryItemOutputQuery, useInvokeFunctionMutation, useCancelRunMutation, useRerunMutation, useRerunFromStepMutation, useGetRunsQuery, useLazyGetRunsQuery, useCountRunsQuery, useLazyCountRunsQuery, useGetRunQuery, useLazyGetRunQuery, useGetTraceResultQuery, useLazyGetTraceResultQuery, useGetTriggerQuery, useLazyGetTriggerQuery, useGetWorkerConnectionsQuery, useLazyGetWorkerConnectionsQuery, useCountWorkerConnectionsQuery, useLazyCountWorkerConnectionsQuery } = injectedRtkApi;
+export const { useGetEventQuery, useLazyGetEventQuery, useGetFunctionRunQuery, useLazyGetFunctionRunQuery, useGetFunctionsQuery, useLazyGetFunctionsQuery, useGetFunctionQuery, useLazyGetFunctionQuery, useGetAppsQuery, useLazyGetAppsQuery, useGetAppQuery, useLazyGetAppQuery, useCreateAppMutation, useUpdateAppMutation, useDeleteAppMutation, useGetTriggersStreamQuery, useLazyGetTriggersStreamQuery, useGetFunctionRunStatusQuery, useLazyGetFunctionRunStatusQuery, useGetFunctionRunOutputQuery, useLazyGetFunctionRunOutputQuery, useGetHistoryItemOutputQuery, useLazyGetHistoryItemOutputQuery, useInvokeFunctionMutation, useCancelRunMutation, useRerunMutation, useRerunFromStepMutation, useGetRunsQuery, useLazyGetRunsQuery, useCountRunsQuery, useLazyCountRunsQuery, useGetRunQuery, useLazyGetRunQuery, useGetTraceResultQuery, useLazyGetTraceResultQuery, useGetTriggerQuery, useLazyGetTriggerQuery, useGetWorkerConnectionsQuery, useLazyGetWorkerConnectionsQuery, useCountWorkerConnectionsQuery, useLazyCountWorkerConnectionsQuery } = injectedRtkApi;
 
