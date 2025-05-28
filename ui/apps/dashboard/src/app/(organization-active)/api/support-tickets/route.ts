ace94d0197879e4b7a84e07b49447cfb85cbb4b7
Jacob Heric <jacob@jacobheric.com>
2025-05-28 14:13:07 -0400

    make next happy

--- ui/apps/dashboard/src/app/(organization-active)/api/support-tickets/route.ts
+++ ui/apps/dashboard/src/app/(organization-active)/api/support-tickets/route.ts
@@ -55,7 +55,7 @@ function createComponents(
 // brute force find or create a customer in plain to work around
 // upsert issues in the API when users change or re-use email addresses
 // across orgs
-export const getCustomerId = async (email: string, name?: string) => {
+const getCustomerId = async (email: string, name?: string) => {
   const existing = await client.getCustomerByEmail({
     email,
   });
