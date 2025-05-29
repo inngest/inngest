88550cd680391fac6c7c06157706dae1a19dc35a
Darwin D Wu <darwin67@users.noreply.github.com>
2025-05-28 17:42:49 -0700

    add bulk run to kind

--- pkg/enums/cancellation_kind.go
+++ pkg/enums/cancellation_kind.go
@@ -5,6 +5,10 @@ package enums
 type CancellationKind int
 
 const (
+	// CancellationKindRun represents a single run to be cancelled
 	CancellationKindRun CancellationKind = iota
+	// CancellationKindBacklog represents a bulk cancellation of runs
+	CancellationKindBulkRun
+	// CancellationKindBacklog represents a backlog that needs to be cancelled
 	CancellationKindBacklog
 )
