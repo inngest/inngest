627790eef25d908792e52e84e847dec87bdb4319
albertchae <217050+albertchae@users.noreply.github.com>
2025-05-30 09:07:34 -0700

    PR feedback
    - tighter GQL types, will need to backport to monorepo
    - use DevServerEnvID constant to future proof against multiple
      environments in dev server

--- pkg/coreapi/graph/resolvers/function.go
+++ pkg/coreapi/graph/resolvers/function.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	"github.com/inngest/inngest/pkg/consts"
 	"github.com/inngest/inngest/pkg/coreapi/graph/models"
 	"github.com/inngest/inngest/pkg/cqrs"
 )
@@ -14,9 +15,7 @@ func (r *functionResolver) App(ctx context.Context, obj *models.Function) (*cqrs
 }
 
 func (qr *queryResolver) FunctionBySlug(ctx context.Context, query models.FunctionQuery) (*models.Function, error) {
-	dummyLocalWorkspaceID := uuid.MustParse("6c6f6361-6c00-0000-0000-000000000000")
-
-	fn, err := qr.Data.GetFunctionByExternalID(ctx, dummyLocalWorkspaceID, "local", query.FunctionSlug)
+	fn, err := qr.Data.GetFunctionByExternalID(ctx, consts.DevServerEnvID, "local", query.FunctionSlug)
 	if err != nil {
 		return nil, err
 	}
