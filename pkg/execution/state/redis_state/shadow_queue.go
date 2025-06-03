b7900304eb0ac32476516379148c7a51153abfc7
Darwin D Wu <darwin67@users.noreply.github.com>
2025-06-03 13:44:29 -0700

    move it to different file

--- pkg/execution/state/redis_state/shadow_queue.go
+++ pkg/execution/state/redis_state/shadow_queue.go
@@ -5,14 +5,12 @@ import (
 	"crypto/rand"
 	"errors"
 	"fmt"
-	"iter"
 	"math"
 	mrand "math/rand"
 	"sync"
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/inngest/inngest/pkg/consts"
 	"github.com/inngest/inngest/pkg/enums"
 	"github.com/inngest/inngest/pkg/telemetry/metrics"
 	"github.com/inngest/inngest/pkg/telemetry/redis_telemetry"
@@ -801,102 +799,4 @@ func (q *queue) peekGlobalShadowPartitionAccounts(ctx context.Context, sequentia
 	}
 
 	return p.peekUUIDPointer(ctx, rc.kg.GlobalAccountShadowPartitions(), sequential, until, limit)
-}
-
-func newShadowContinuation(shardName string) *shadowCont {
-	return &shadowCont{
-		continues: map[string]shadowContinuation{},
-		cooldown:  map[string]time.Time{},
-		limit:     0,
-		shardName: shardName,
-	}
-}
-
-// shadowCont represents the continuation for shadow partitions
-type shadowCont struct {
-	sync.Mutex
-
-	continues map[string]shadowContinuation
-	cooldown  map[string]time.Time
-	limit     uint
-
-	shardName string
-}
-
-func (sc *shadowCont) Add(ctx context.Context, p *QueueShadowPartition, ctr uint) {
-	sc.Lock()
-	defer sc.Unlock()
-
-	if ctr == 1 {
-		if len(sc.continues) > consts.QueueShadowContinuationMaxPartitions {
-			metrics.IncrQueueShadowContinuationOpCounter(ctx, metrics.CounterOpt{PkgName: pkgName, Tags: map[string]any{"queue_shard": sc.shardName, "op": "max_capacity"}})
-			return
-		}
-
-		if t, ok := sc.cooldown[p.PartitionID]; ok && t.After(time.Now()) {
-			metrics.IncrQueueShadowContinuationOpCounter(ctx, metrics.CounterOpt{PkgName: pkgName, Tags: map[string]any{"queue_shard": sc.shardName, "op": "cooldown"}})
-			return
-		}
-
-		delete(sc.cooldown, p.PartitionID)
-	}
-
-	c, ok := sc.continues[p.PartitionID]
-	if !ok || c.count < ctr {
-		sc.continues[p.PartitionID] = shadowContinuation{shadowPart: p, count: ctr}
-		metrics.IncrQueueShadowContinuationOpCounter(ctx, metrics.CounterOpt{PkgName: pkgName, Tags: map[string]any{"queue_shard": sc.shardName, "op": "added"}})
-	}
-}
-
-func (sc *shadowCont) Remove(ctx context.Context, p *QueueShadowPartition, cooldown bool) {
-	sc.Lock()
-	defer sc.Unlock()
-
-	delete(sc.continues, p.PartitionID)
-
-	if cooldown {
-		// Add a cooldown, preventing this partition from being added as a continuation
-		// for a given period of time.
-		//
-		// Note that this isn't shared across replicas;  cooldowns
-		// only exist in the current replica.
-		sc.cooldown[p.PartitionID] = time.Now().Add(consts.QueueShadowContinuationCooldownPeriod)
-		metrics.IncrQueueShadowContinuationOpCounter(ctx, metrics.CounterOpt{PkgName: pkgName, Tags: map[string]any{"queue_shard": sc.shardName, "op": "removed"}})
-	}
-}
-
-func (sc *shadowCont) Continuations() iter.Seq[shadowContinuation] {
-	cont := []shadowContinuation{}
-
-	sc.Lock()
-	for _, c := range sc.continues {
-		cont = append(cont, c)
-	}
-	sc.Unlock()
-
-	return func(yield func(shadowContinuation) bool) {
-		for _, c := range cont {
-			if !yield(c) {
-				return
-			}
-		}
-	}
-}
-
-func (sc *shadowCont) Has(p *QueueShadowPartition) bool {
-	sc.Lock()
-	defer sc.Unlock()
-
-	_, ok := sc.continues[p.PartitionID]
-	return ok
-}
-
-func (sc *shadowCont) IsWithinLimit(ctr uint) bool {
-	return ctr >= sc.limit
-}
-
-// shadowContinuation is the equivalent of continuation for shadow partitions
-type shadowContinuation struct {
-	shadowPart *QueueShadowPartition
-	count      uint
 }
