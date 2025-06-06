d59bcda958e6418a4f3c7f2aacfe735018b2efab
Darwin D Wu <darwin67@users.noreply.github.com>
2025-06-06 13:16:09 -0700

    add tests and fixing some logic

--- pkg/execution/state/redis_state/iterator.go
+++ pkg/execution/state/redis_state/iterator.go
@@ -3,11 +3,20 @@ package redis_state
 type QueueIteratorOpt func(o *queueIterOpt)
 
 type queueIterOpt struct {
+	batchSize      int64
 	allowKeyQueues func() bool
 }
 
 func WithQueueItemIteratorAllowKeyQueues(kq func() bool) QueueIteratorOpt {
 	return func(o *queueIterOpt) {
 		o.allowKeyQueues = kq
+	}
+}
+
+func WithQueueItemIterBatchSize(size int64) QueueIteratorOpt {
+	return func(o *queueIterOpt) {
+		if size > 0 {
+			o.batchSize = size
+		}
 	}
 }
