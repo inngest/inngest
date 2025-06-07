df02cef7bfc0aac337f189e7f04e994f51e1b4cc
Jacob Heric <jacob@jacobheric.com>
2025-06-07 07:26:55 -0400

    remove hard coded feature flags in dev server

--- ui/apps/dev-server-ui/src/utils/featureFlags.ts
+++ ui/apps/dev-server-ui/src/utils/featureFlags.ts
@@ -2,7 +2,7 @@ import type { BooleanFlag } from '@inngest/components/SharedContext/useBooleanFl
 
 export const useBooleanFlag = (flag: string, defaultValue: boolean = false): BooleanFlag => {
   if (flag === 'step-over-debugger') {
-    return { isReady: true, value: true };
+    return { isReady: true, value: false };
   }
 
   return { isReady: true, value: defaultValue };
