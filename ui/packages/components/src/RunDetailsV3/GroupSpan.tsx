fc93d931fb8430592207cc9b997260a47a258564
Jacob Heric <jacob@jacobheric.com>
2025-06-09 09:21:26 -0400

    move group height to tailwind

--- ui/packages/components/src/RunDetailsV3/GroupSpan.tsx
+++ ui/packages/components/src/RunDetailsV3/GroupSpan.tsx
@@ -11,13 +11,12 @@ export const GroupSpan = ({ width, status, depth = 1 }: GroupSpanProps) => {
   return (
     <div
       className={cn(
-        `absolute rounded-sm`,
+        `absolute h-[calc(100%-8px)] rounded-sm`,
         getStatusBackgroundClass(status),
         depth === 1 ? 'bg-opacity-20' : 'bg-opacity-10'
       )}
       style={{
         width: width,
-        height: 'calc(100% - 8px)',
       }}
     />
   );
