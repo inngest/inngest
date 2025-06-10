4e9bf09b3b83964ac57b417e9de4d637cecb1091
Albert Chae <albertchae@users.noreply.github.com>
2025-06-10 06:23:52 -0700

    fixup table styling

--- ui/packages/components/src/FunctionConfiguration/ConfigurationTable.tsx
+++ ui/packages/components/src/FunctionConfiguration/ConfigurationTable.tsx
@@ -16,21 +16,21 @@ export default function ConfigurationTable({ header, entries }: ConfigurationTab
   }
 
   return (
-    <div className="overflow-hidden rounded border border-gray-300 ">
+    <div className="border-subtle overflow-hidden rounded border">
       <table className="w-full border-collapse">
         <thead>
-          <tr className="h-8 border-b bg-gray-100 dark:bg-transparent">
+          <tr className="bg-disabled h-8 border-b">
             <td className="text-basis px-2 text-sm font-medium" colSpan={2}>
               <div className="flex items-center gap-2">
                 {header}
-                <RiInformationLine className="h-5 w-5" />
+                <RiInformationLine className="bg-canvasSubtle h-5 w-5" />
               </div>
             </td>
           </tr>
         </thead>
         <tbody>
           {entries.map((entry) => (
-            <tr className="h-8 border-b border-gray-200">
+            <tr className="h-8 border-b">
               <td className="text-muted px-2 text-sm">{entry.label}</td>
               <td className="text-basis px-2 text-right text-sm">{entry.value}</td>
             </tr>
