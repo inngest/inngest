defdb20cafd355e3fba3aa8fb2e5adafe20a84b5
Darwin D Wu <darwin67@users.noreply.github.com>
2025-06-12 17:52:22 -0700

    add some interval to provide some space so it doesn't overwhelm the queue

--- pkg/execution/state/redis_state/iterator.go
+++ pkg/execution/state/redis_state/iterator.go
@@ -1,10 +1,13 @@
 package redis_state
 
+import "time"
+
 type QueueIterOpt func(o *queueIterOpt)
 
 type queueIterOpt struct {
 	batchSize      int64
 	allowKeyQueues func() bool
+	interval       time.Duration
 }
 
 func WithQueueItemIterAllowKeyQueues(kq func() bool) QueueIterOpt {
@@ -18,5 +21,11 @@ func WithQueueItemIterBatchSize(size int64) QueueIterOpt {
 		if size > 0 {
 			o.batchSize = size
 		}
+	}
+}
+
+func WithQueueItemIterInterval(itv time.Duration) QueueIterOpt {
+	return func(o *queueIterOpt) {
+		o.interval = itv
 	}
 }
