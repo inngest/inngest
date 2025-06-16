c082ba4f93f5f5e9263571a6fec20ba722e0421c
Jack Williams <1736957+jpwilliams@users.noreply.github.com>
2025-06-16 13:55:42 +0000

    Remove dev APIs

--- pkg/devserver/api.go
+++ pkg/devserver/api.go
@@ -64,7 +64,6 @@ func (a *devapi) addRoutes() {
 	a.Use(headers.StaticHeadersMiddleware(a.devserver.Opts.Config.GetServerKind()))
 
 	a.Get("/dev", a.Info)
-	a.Get("/jack/{runID}", a.Jack)
 	a.Post("/dev/traces", a.OTLPTrace)
 	a.Post("/fn/register", a.Register)
 	// This allows tests to remove apps by URL
@@ -115,31 +114,6 @@ func (a devapi) UI(w http.ResponseWriter, r *http.Request) {
 	tel.SendMetadata(r.Context(), m)
 
 	byt := parsedRoutes.serve(r.Context(), r.URL.Path)
-	_, _ = w.Write(byt)
-}
-
-func (a devapi) Jack(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	runID := chi.URLParam(r, "runID")
-	parsed, err := ulid.Parse(runID)
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, publicerr.Wrapf(err, 400, "Invalid event ID: %s", runID))
-		return
-	}
-
-	spans, err := a.devserver.Data.GetSpansByRunID(ctx, parsed)
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, err)
-		return
-	}
-
-	byt, err := json.MarshalIndent(spans, "", "  ")
-	if err != nil {
-		_ = publicerr.WriteHTTP(w, err)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	_, _ = w.Write(byt)
 }
 
