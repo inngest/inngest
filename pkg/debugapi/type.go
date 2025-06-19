00d598589cbcfa4fb4b319bdc2bf49d29e41edd9
Darwin D Wu <darwin67@users.noreply.github.com>
2025-06-19 13:47:00 -0700

    swap to use publicerr

--- pkg/debugapi/type.go
+++ pkg/debugapi/type.go
@@ -2,6 +2,5 @@ package debugapi
 
 // DebugResponse shows the response structure for debug API calls
 type DebugResponse struct {
-	Data  any   `json:"data,omitempty"`
-	Error error `json:"error,omitempty"`
+	Data any `json:"data,omitempty"`
 }
