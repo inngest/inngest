f46ebedf19dd607318bf0f90f085f450d814adf5
Darwin D Wu <darwin67@users.noreply.github.com>
2025-06-19 13:59:42 -0700

    add partition retrieval

--- pkg/execution/state/redis_state/debug.go
+++ pkg/execution/state/redis_state/debug.go
@@ -2,6 +2,7 @@ package redis_state
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 )
 
@@ -11,5 +12,41 @@ type PartitionInspectionResult struct {
 }
 
 func (q *queue) PartitionByID(ctx context.Context, shard QueueShard, partitionID string) (*PartitionInspectionResult, error) {
-	return nil, fmt.Errorf("not implemented")
+	var (
+		qp  QueuePartition
+		sqp QueueShadowPartition
+	)
+
+	rc := shard.RedisClient.Client()
+	kg := shard.RedisClient.kg
+	// load queue partition
+	{
+		cmd := rc.B().Hget().Key(kg.PartitionItem()).Field(partitionID).Build()
+		byt, err := rc.Do(ctx, cmd).AsBytes()
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving queue partition: %w", err)
+		}
+
+		if err := json.Unmarshal(byt, &qp); err != nil {
+			return nil, fmt.Errorf("error unmarshalling queue partition: %w", err)
+		}
+	}
+
+	// load shadow partition
+	{
+		cmd := rc.B().Hget().Key(kg.ShadowPartitionMeta()).Field(partitionID).Build()
+		byt, err := rc.Do(ctx, cmd).AsBytes()
+		if err != nil {
+			return nil, fmt.Errorf("error retrieving shadow partition: %w", err)
+		}
+
+		if err := json.Unmarshal(byt, &sqp); err != nil {
+			return nil, fmt.Errorf("error unmarshalling shadow partition: %w", err)
+		}
+	}
+
+	return &PartitionInspectionResult{
+		QueuePartition:       &qp,
+		QueueShadowPartition: &sqp,
+	}, nil
 }
