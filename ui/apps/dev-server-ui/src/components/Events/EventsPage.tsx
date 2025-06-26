ebbfeb52bd4e1f718fa02b62ec07e0972614eaed
Ana <anafilipadealmeida@gmail.com>
2025-06-26 16:51:42 +0200

    Add pages

---
+++ ui/apps/dev-server-ui/src/components/Events/EventsPage.tsx
@@ -0,0 +1,104 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '@inngest/components/Button/Button';
+import { EventsTable } from '@inngest/components/Events/EventsTable';
+import { InternalEventsToggle } from '@inngest/components/Events/InternalEventsToggle';
+import { Header } from '@inngest/components/Header/Header';
+import { RefreshButton } from '@inngest/components/Refresh/RefreshButton';
+import { RiExternalLinkLine, RiRefreshLine } from '@remixicon/react';
+
+// import { ExpandedRowActions } from '@/components/Events/ExpandedRowActions';
+import SendEventButton from '@/components/Event/SendEventButton';
+// import { useAllEventTypes } from '@/components/EventTypes/useEventTypes';
+import { EventInfo } from '@/components/Events/EventInfo';
+
+// import { SendEventModal } from '@/components/Events/SendEventModal';
+// import { useEventDetails, useEventPayload, useEvents } from '@/components/Events/useEvents';
+// import { useReplayModal } from '@/components/Events/useReplayModal';
+
+export default function EventsPage({
+  eventTypeNames,
+  showHeader = true,
+}: {
+  eventTypeNames?: string[];
+  showHeader?: boolean;
+}) {
+  const router = useRouter();
+  //   const { isModalVisible, selectedEvent, openModal, closeModal } = useReplayModal();
+
+  //   const getEvents = useEvents();
+  //   const getEventDetails = useEventDetails();
+  //   const getEventPayload = useEventPayload();
+  //   const getEventTypes = useAllEventTypes();
+
+  return (
+    <>
+      {showHeader && (
+        <Header
+          breadcrumb={[{ text: 'Events' }]}
+          infoIcon={<EventInfo />}
+          action={
+            <div className="flex items-center gap-1.5">
+              <RefreshButton />
+              <SendEventButton
+                label="Send event"
+                data={JSON.stringify({
+                  name: '',
+                  data: {},
+                  user: {},
+                })}
+              />
+              <InternalEventsToggle />
+            </div>
+          }
+        />
+      )}
+      {/* <EventsTable
+        getEvents={getEvents}
+        getEventDetails={getEventDetails}
+        getEventPayload={getEventPayload}
+        getEventTypes={getEventTypes}
+        eventNames={eventTypeNames}
+        singleEventTypePage={false}
+        features={{
+          history: Number.MAX_SAFE_INTEGER,
+        }}
+        emptyActions={
+          <>
+            <Button
+              appearance="outlined"
+              label="Refresh"
+              onClick={() => router.refresh()}
+              icon={<RiRefreshLine />}
+              iconSide="left"
+            />
+            <Button
+              label="Go to docs"
+              href="https://www.inngest.com/docs/events"
+              target="_blank"
+              icon={<RiExternalLinkLine />}
+              iconSide="left"
+            />
+          </>
+        }
+        expandedRowActions={({ eventName, payload }) => (
+          <ExpandedRowActions
+            eventName={eventName}
+            payload={payload}
+            onReplay={openModal}
+            envSlug={envSlug}
+          />
+        )}
+      />
+      {selectedEvent && (
+        <SendEventModal
+          isOpen={isModalVisible}
+          eventName={selectedEvent.name}
+          onClose={closeModal}
+          initialData={selectedEvent.data}
+        />
+      )} */}
+    </>
+  );
+}
