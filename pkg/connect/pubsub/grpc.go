af9ceca27e7f255bb27e411f016bf0d2d107ca78
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-06-30 11:45:18 +0100

    Dynamically connect to connect gateways

--- pkg/connect/pubsub/grpc.go
+++ pkg/connect/pubsub/grpc.go
@@ -3,7 +3,6 @@ package pubsub
 import (
 	"context"
 	"fmt"
-
 	connectConfig "github.com/inngest/inngest/pkg/config/connect"
 	"github.com/inngest/inngest/pkg/connect/state"
 	"github.com/inngest/inngest/pkg/logger"
@@ -11,6 +10,7 @@ import (
 	"github.com/oklog/ulid/v2"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"sync"
 )
 
 type GatewayGrpcForwarder interface {
@@ -20,9 +20,10 @@ type GatewayGrpcForwarder interface {
 
 type gatewayGrpcForwarder struct {
 	gatewayManager state.GatewayManager
+	mu             sync.RWMutex
 
-	// TODO: Synchronization
-	grpcClients map[string]connectpb.ConnectGatewayClient
+	// TODO: Cleanup old gateways
+	grpcClients    map[string]connectpb.ConnectGatewayClient
 }
 
 func NewGatewayGrpcForwarder(stateManager state.GatewayManager) GatewayGrpcForwarder {
@@ -38,13 +39,18 @@ func (i *gatewayGrpcForwarder) ConnectToGateways(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
+
 	logger.StdlibLogger(ctx).Debug("got connect gateways to connect to", "len", len(gateways))
+
+	i.mu.Lock()
+	defer i.mu.Unlock()
 
 	for _, g := range gateways {
 		url := fmt.Sprintf("%s:%d", g.IPAddress, connectConfig.Gateway(ctx).GRPCPort)
 		conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
 		if err != nil {
 			logger.StdlibLogger(ctx).Error("could not create grpc client", err)
+			continue
 		}
 
 		rpcClient := connectpb.NewConnectGatewayClient(conn)
@@ -57,24 +63,63 @@ func (i *gatewayGrpcForwarder) ConnectToGateways(ctx context.Context) error {
 			logger.StdlibLogger(ctx).Error("could not ping connect gateway at startup", "url", url, "message", message, "err", err)
 		} else {
 			i.grpcClients[g.Id.String()] = rpcClient
-			logger.StdlibLogger(ctx).Info("connect gateway successful", "message", message, "url", url)
+			logger.StdlibLogger(ctx).Info("connected to connect gateway", "message", message, "url", url)
 		}
 	}
+
 	return nil
 }
-func (i *gatewayGrpcForwarder) Forward(ctx context.Context, gatewayID ulid.ULID, connectionID ulid.ULID, data *connectpb.GatewayExecutorRequestData) error {
-	grpcClient := i.grpcClients[gatewayID.String()]
-	if grpcClient == nil {
-		// TODO: Switch to a warning or info and try to create a new grpc client dynamically
-		logger.StdlibLogger(ctx).Error("could not find grpc client for connect gateway")
-		return fmt.Errorf("could not find grpc client for connect gateway")
+
+// connectToGateway attempts to create a new gRPC client for a gateway that wasn't doesn't have a grpc client yet.
+func (i *gatewayGrpcForwarder) connectToGateway(ctx context.Context, gatewayID ulid.ULID) (connectpb.ConnectGatewayClient, error) {
+	gateway, err := i.gatewayManager.GetGateway(ctx, gatewayID)
+	if err != nil {
+		return nil, fmt.Errorf("could not find gateway %s: %w", gatewayID.String(), err)
 	}
-	// TODO: Call forward
+
+	url := fmt.Sprintf("%s:%d", gateway.IPAddress, connectConfig.Gateway(ctx).GRPCPort)
+	conn, err := grpc.NewClient(url, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("could not create grpc client for %s: %w", url, err)
+	}
+
+	rpcClient := connectpb.NewConnectGatewayClient(conn)
+
+	result, err := rpcClient.Ping(ctx, &connectpb.PingRequest{})
+	if err != nil || result.GetMessage() != "ok" {
+		return nil, fmt.Errorf("could not ping gateway %s: %w", url, err)
+	}
+
+	i.mu.Lock()
+	i.grpcClients[gatewayID.String()] = rpcClient
+	i.mu.Unlock()
+
+	logger.StdlibLogger(ctx).Info("dynamically connected to connect gateway", "message", result, "url", url)
+
+	return rpcClient, nil
+}
+
+func (i *gatewayGrpcForwarder) Forward(ctx context.Context, gatewayID ulid.ULID, connectionID ulid.ULID, data *connectpb.GatewayExecutorRequestData) error {
+	i.mu.RLock()
+	grpcClient := i.grpcClients[gatewayID.String()]
+	i.mu.RUnlock()
+
+	if grpcClient == nil {
+		logger.StdlibLogger(ctx).Warn("grpc client not found for gateway, attempting to create dynamically", "gatewayID", gatewayID.String())
+
+		var err error
+		grpcClient, err = i.connectToGateway(ctx, gatewayID)
+		if err != nil {
+			logger.StdlibLogger(ctx).Error("could not create grpc client dynamically", "gatewayID", gatewayID.String(), "err", err)
+			return fmt.Errorf("could not find or create grpc client for gateway %s: %w", gatewayID.String(), err)
+		}
+	}
+
 	reply, err := grpcClient.Forward(ctx, &connectpb.ForwardRequest{
 		ConnectionID: connectionID.String(),
 		Data:         data,
 	})
-	logger.StdlibLogger(ctx).Debug("grpc message forwarded to connect gateway", "reply", reply, "err", err)
 
-	return nil
+	logger.StdlibLogger(ctx).Debug("grpc message forwarded to connect gateway", "reply", reply, "err", err)
+	return err
 }
