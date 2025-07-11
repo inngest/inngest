0c9f7ca28ac1b2814e3ac9c9b5f9ab52b54854df
Albert Chae <albertchae@users.noreply.github.com>
2025-07-10 22:38:40 -0700

    Add a hidden in-memory flag for the dev server
    Sometimes it is useful for inngest devs to have a persisted sqlite while
    working on the dev server. It is error prone to remember to stash and/or
    not commit the change in code.
    
    It is hidden since most end users should rely on the default in memory setting

--- cmd/commands/internal/localconfig/devconfig.go
+++ cmd/commands/internal/localconfig/devconfig.go
@@ -98,6 +98,7 @@ func mapDevFlags(cmd *cobra.Command) error {
 	err = errors.Join(err, viper.BindPFlag("tick", cmd.Flags().Lookup("tick")))
 	err = errors.Join(err, viper.BindPFlag("sdk-url", cmd.Flags().Lookup("sdk-url")))
 	err = errors.Join(err, viper.BindPFlag("connect-gateway-port", cmd.Flags().Lookup("connect-gateway-port")))
+	err = errors.Join(err, viper.BindPFlag("in-memory", cmd.Flags().Lookup("in-memory")))
 
 	return err
 }
