6cdac489eb3879d43513a57be6e56b2e76aae13b
Albert Chae <albertchae@users.noreply.github.com>
2025-07-12 10:09:17 -0700

    Add some missing defer rows.Close()
    sqlc adds these in their codegen but we have to do it manually with goqu

--- pkg/cqrs/base_cqrs/cqrs.go
+++ pkg/cqrs/base_cqrs/cqrs.go
@@ -794,6 +794,7 @@ func (w wrapper) GetEventsByExpressions(ctx context.Context, cel []string) ([]*c
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	res := []*cqrs.Event{}
 	for rows.Next() {
@@ -1667,6 +1668,7 @@ func (w wrapper) GetTraceRuns(ctx context.Context, opt cqrs.GetTraceRunOpt) ([]*
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	res := []*cqrs.TraceRun{}
 	var count uint
@@ -2161,6 +2163,7 @@ func (w wrapper) GetWorkerConnections(ctx context.Context, opt cqrs.GetWorkerCon
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
 	res := []*cqrs.WorkerConnection{}
 	var count uint
