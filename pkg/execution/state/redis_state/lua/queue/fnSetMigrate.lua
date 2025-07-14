90ef9c637f6fba551012be3b6823256b80d67ec6
Bruno Scheufler <git@brunoscheufler.com>
2025-07-14 21:07:25 +0200

    Set `norefill` when pausing or starting migration

--- pkg/execution/state/redis_state/lua/queue/fnSetMigrate.lua
+++ pkg/execution/state/redis_state/lua/queue/fnSetMigrate.lua
@@ -7,12 +7,27 @@
 
 ]]
 
-local keyFnMeta = KEYS[1]
+local keyFnMeta               = KEYS[1]
+local keyShadowPartitionMeta  = KEYS[2]
 
-local migrate     = tonumber(ARGV[1])
-local defaultMeta  = ARGV[2]
+local migrate       = tonumber(ARGV[1])
+local defaultMeta   = ARGV[2]
+local partitionID   = ARGV[3]
 
 -- $include(get_fn_meta.lua)
+-- $include(get_partition_item.lua)
+
+-- update shadow partition
+local existing = get_shadow_partition_item(keyShadowPartitionMeta, partitionID)
+if existing ~= nil and existing ~= false then
+  if migrate == 1 then
+    existing.norefill = true
+  else
+    existing.norefill = false
+  end
+  redis.call("HSET", keyShadowPartitionMeta, partitionID, cjson.encode(existing))
+end
+
 local existing = get_fn_meta(keyFnMeta)
 if existing == nil then
 	redis.call("SET", keyFnMeta, defaultMeta)
