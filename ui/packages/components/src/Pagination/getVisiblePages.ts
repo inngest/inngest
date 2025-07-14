f6e15328830269edb92461c1a630071cdbcb7a40
Dylan Dover <dylan@inngest.com>
2025-07-14 15:20:58 -0500

    improve readability of getVisiblePages.ts

--- ui/packages/components/src/Pagination/getVisiblePages.ts
+++ ui/packages/components/src/Pagination/getVisiblePages.ts
@@ -1,3 +1,11 @@
+const FIRST_PAGE_SLOT_SIZE_1 = 1;
+const CURRENT_PAGE_SLOT_SIZE_1 = 1;
+const LAST_PAGE_SLOT_SIZE_1 = 1;
+const SINGLE_ELLIPSIS_SLOT_SIZE_1 = 1;
+
+const EARLY_TRANSITION_OFFSET = 1;
+const ONE_BASED_INDEX_OFFSET = 1;
+
 type GetVisiblePagesConfig = {
   current: number;
   total: number;
@@ -6,12 +14,12 @@ type GetVisiblePagesConfig = {
 
 type PaginationConfig = {
   current: number;
-  total: number;
   beginningConsecutivePages: number;
-  endConsecutivePages: number;
-  middleConsecutivePages: number;
   beginningThreshold: number;
+  endConsecutivePages: number;
   endThreshold: number;
+  middleConsecutivePages: number;
+  total: number;
 };
 
 export function getVisiblePages({
@@ -21,33 +29,36 @@ export function getVisiblePages({
 }: GetVisiblePagesConfig): Array<number | '...'> {
   const slots = variant === 'narrow' ? 5 : 7;
 
-  // How many consecutive pages each pattern shows (calculated to fill exactly slots)
-  const beginningConsecutivePages = slots - 1 - 1; // slots - ellipsis - last_page
-  const endConsecutivePages = slots - 1 - 1; // slots - first_page - ellipsis
-  const middleConsecutivePages = slots - 1 - 2 - 1; // slots - first_page - 2_ellipses - last_page
+  // How many consecutive pages each pattern shows (calculated to fill exactly <slots> slots)
+  const beginningConsecutivePages = slots - SINGLE_ELLIPSIS_SLOT_SIZE_1 - LAST_PAGE_SLOT_SIZE_1;
+  const endConsecutivePages = slots - FIRST_PAGE_SLOT_SIZE_1 - SINGLE_ELLIPSIS_SLOT_SIZE_1;
+  const middleConsecutivePages =
+    slots - FIRST_PAGE_SLOT_SIZE_1 - 2 * SINGLE_ELLIPSIS_SLOT_SIZE_1 - LAST_PAGE_SLOT_SIZE_1;
 
   if (slots >= total) return makeInclusiveRange(1, total);
 
-  // Transition early on both ends to keep current page position stable within slot array.
-  const beginningThreshold = beginningConsecutivePages - 1;
-  const endThreshold = total - (endConsecutivePages - 1) + 1;
+  // Determine when to switch between pagination patterns to keep current page position stable.
+  // The easiest way to understand EARLY_TRANSITION_OFFSET is to toggle it and observe the behavior difference on transitioning between patterns.
+  const lastPageForBeginningPattern = beginningConsecutivePages - EARLY_TRANSITION_OFFSET;
+  const firstPageForEndPattern =
+    total - endConsecutivePages + ONE_BASED_INDEX_OFFSET + EARLY_TRANSITION_OFFSET;
 
   const config: PaginationConfig = {
     current,
-    total,
     beginningConsecutivePages,
+    beginningThreshold: lastPageForBeginningPattern,
     endConsecutivePages,
+    endThreshold: firstPageForEndPattern,
     middleConsecutivePages,
-    beginningThreshold,
-    endThreshold,
+    total,
   };
 
-  if (current <= beginningThreshold) return handleBeginningCase(config);
-  else if (current >= endThreshold) return handleEndCase(config);
+  if (current <= lastPageForBeginningPattern) return handleBeginningCase(config);
+  else if (current >= firstPageForEndPattern) return handleEndCase(config);
   else return handleMiddleCase(config);
 }
 
-// Pattern: [first beginningConsecutivePages, '...', total]
+// Shows: [1, 2, 3, ..., N] where 3 is beginningConsecutivePages and N is total
 function handleBeginningCase({
   beginningConsecutivePages,
   total,
@@ -55,25 +66,27 @@ function handleBeginningCase({
   return [...makeInclusiveRange(1, beginningConsecutivePages), '...', total];
 }
 
-// Pattern: [1, '...', last endConsecutivePages]
+// Shows: [1, ..., 8, 9, N] where 8 is startOfEndPages and N is total
 function handleEndCase({ endConsecutivePages, total }: PaginationConfig): Array<number | '...'> {
-  const startOfConsecutive = total - (endConsecutivePages - 1);
-  return [1, '...', ...makeInclusiveRange(startOfConsecutive, total)];
+  const startOfEndPages = total - endConsecutivePages + ONE_BASED_INDEX_OFFSET;
+  return [1, '...', ...makeInclusiveRange(startOfEndPages, total)];
 }
 
-// Pattern: [1, '...', current-X, current, current+X, '...', total]
+// Shows: [1, ..., 4, 5, 6, ..., N] where 5 is current and N is total
 function handleMiddleCase({
   current,
   middleConsecutivePages,
   total,
 }: PaginationConfig): Array<number | '...'> {
-  const pagesBeforeCurrent = Math.floor((middleConsecutivePages - 1) / 2);
-  const pagesAfterCurrent = middleConsecutivePages - 1 - pagesBeforeCurrent;
+  // NOTE: Watch out for unequal divisions if adding more variants.
+  const pagesBeforeCurrent = (middleConsecutivePages - CURRENT_PAGE_SLOT_SIZE_1) / 2;
+  const pagesAfterCurrent = middleConsecutivePages - CURRENT_PAGE_SLOT_SIZE_1 - pagesBeforeCurrent;
 
-  const startOfMid = current - pagesBeforeCurrent;
-  const endOfMid = current + pagesAfterCurrent;
+  const startOfMiddleRange = current - pagesBeforeCurrent;
+  const endOfMiddleRange = current + pagesAfterCurrent;
+  const middlePages = makeInclusiveRange(startOfMiddleRange, endOfMiddleRange);
 
-  return [1, '...', ...makeInclusiveRange(startOfMid, endOfMid), '...', total];
+  return [1, '...', ...middlePages, '...', total];
 }
 
 export function makeInclusiveRange(start: number, end: number): Array<number> {
