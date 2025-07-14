462cfbde0911d63ae903ed136d705f2f2331e05c
Bruno Scheufler <git@brunoscheufler.com>
2025-07-14 21:11:10 +0200

    If function was already paused, keep `norefill` at true

--- pkg/execution/state/redis_state/lua/queue/fnSetMigrate.lua
+++ pkg/execution/state/redis_state/lua/queue/fnSetMigrate.lua
@@ -17,28 +17,30 @@ local partitionID   = ARGV[3]
 -- $include(get_fn_meta.lua)
 -- $include(get_partition_item.lua)
 
+local existingMeta = get_fn_meta(keyFnMeta)
+
+
 -- update shadow partition
-local existing = get_shadow_partition_item(keyShadowPartitionMeta, partitionID)
-if existing ~= nil and existing ~= false then
-  if migrate == 1 then
-    existing.norefill = true
+local existingShadow = get_shadow_partition_item(keyShadowPartitionMeta, partitionID)
+if existingShadow ~= nil and existingShadow ~= false then
+  if migrate == 1 or existingMeta.off == true then
+    existingShadow.norefill = true
   else
-    existing.norefill = false
+    existingShadow.norefill = false
   end
-  redis.call("HSET", keyShadowPartitionMeta, partitionID, cjson.encode(existing))
+  redis.call("HSET", keyShadowPartitionMeta, partitionID, cjson.encode(existingShadow))
 end
 
-local existing = get_fn_meta(keyFnMeta)
-if existing == nil then
+if existingMeta == nil then
 	redis.call("SET", keyFnMeta, defaultMeta)
 	return 0
 end
 
 if migrate >= 1 then
-  existing.migrate = true
+  existingMeta.migrate = true
 else
-  existing.migrate = false
+  existingMeta.migrate = false
 end
-redis.call("SET", keyFnMeta, cjson.encode(existing))
+redis.call("SET", keyFnMeta, cjson.encode(existingMeta))
 
 return 0
--- pkg/execution/state/redis_state/lua/queue/fnSetPaused.lua
+++ pkg/execution/state/redis_state/lua/queue/fnSetPaused.lua
@@ -17,28 +17,29 @@ local partitionID  = ARGV[3]
 -- $include(get_fn_meta.lua)
 -- $include(get_partition_item.lua)
 
+local existingMeta = get_fn_meta(keyFnMeta)
+
 -- update shadow partition
-local existing = get_shadow_partition_item(keyShadowPartitionMeta, partitionID)
-if existing ~= nil and existing ~= false then
-  if isPaused == 1 then
-    existing.norefill = true
+local existingShadow = get_shadow_partition_item(keyShadowPartitionMeta, partitionID)
+if existingShadow ~= nil and existingShadow ~= false then
+  if isPaused == 1 or existingMeta.migrate == true then
+    existingShadow.norefill = true
   else
-    existing.norefill = false
+    existingShadow.norefill = false
   end
-  redis.call("HSET", keyShadowPartitionMeta, partitionID, cjson.encode(existing))
+  redis.call("HSET", keyShadowPartitionMeta, partitionID, cjson.encode(existingShadow))
 end
 
-local existing = get_fn_meta(keyFnMeta)
-if existing == nil then
+if existingMeta == nil then
 	redis.call("SET", keyFnMeta, defaultMeta)
 	return 0
 end
 
 if isPaused == 1 then
-    existing.off = true
+    existingMeta.off = true
 else
-    existing.off = false
+    existingMeta.off = false
 end
-redis.call("SET", keyFnMeta, cjson.encode(existing))
+redis.call("SET", keyFnMeta, cjson.encode(existingMeta))
 
 return 0
