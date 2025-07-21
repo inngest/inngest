e51b1db2dc58c8d541af0eddf92c27add555e41d
Dylan Dover <dylan@inngest.com>
2025-07-21 11:52:48 -0500

    capitalization change

--- ui/apps/dashboard/src/components/Environments/FilterResultDetails.tsx
+++ ui/apps/dashboard/src/components/Environments/FilterResultDetails.tsx
@@ -8,7 +8,7 @@ export function FilterResultDetails({ size }: FilterResultDetailsProps) {
   return (
     <div className="flex items-center px-3 py-2 max-[625px]:hidden">
       <span className="text-light whitespace-nowrap text-sm">
-        {size} {size === 1 ? 'environment' : 'environments'}
+        {size} {size === 1 ? 'Environment' : 'Environments'}
       </span>
     </div>
   );
