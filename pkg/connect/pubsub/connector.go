56acb060747c4072e9b41fbfd1af0080decf9ab0
Riadh <22998716+KiKoS0@users.noreply.github.com>
2025-07-24 21:19:21 +0000

    Rename pubsub connector

--- pkg/connect/pubsub/connector.go
+++ pkg/connect/pubsub/connector.go
@@ -1,23 +1,9 @@
 package pubsub
 
-import (
-	"context"
-	"github.com/redis/rueidis"
-)
-
 type Connector interface {
 	RequestForwarder
 }
 
-type ConnectorOpt func(ctx context.Context) (Connector, error)
-
-func NewConnector(ctx context.Context, initialize ConnectorOpt) (Connector, error) {
-	return initialize(ctx)
-}
-
-func WithRedis(clientConfig rueidis.ClientOption, listen bool, opts RedisPubSubConnectorOpts) ConnectorOpt {
-	return func(ctx context.Context) (Connector, error) {
-		connector := newRedisPubSubConnector(opts)
-		return connector, nil
-	}
+func NewConnector(opts GRPCConnectorOpts) Connector {
+	return newGRPCConnector(opts)
 }
