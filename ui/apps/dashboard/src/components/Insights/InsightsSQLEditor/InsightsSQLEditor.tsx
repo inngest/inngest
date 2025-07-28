d0d80e78897804780aa4d1756759d440920cf948
Dylan Dover <dylan@inngest.com>
2025-07-28 16:47:07 -0500

    query: add (mocked) ability to execute a query

--- ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditor.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditor.tsx
@@ -1,22 +1,12 @@
 'use client';
 
-import { useState } from 'react';
 import { SQLEditor } from '@inngest/components/SQLEditor/SQLEditor';
 
-const DEFAULT_QUERY = `SELECT
-  HOUR(ts) as hour,
-  COUNT(*) as count
-WHERE
-  name = 'cli/dev_ui.loaded'
-  AND data.os != 'linux'
-  AND ts > 1752845983000
-GROUP BY
-  hour
-ORDER BY
-  hour desc`;
+interface InsightsSQLEditorProps {
+  content: string;
+  onChange: (value: string) => void;
+}
 
-export function InsightsSQLEditor() {
-  const [content, setContent] = useState<string>(DEFAULT_QUERY);
-
-  return <SQLEditor content={content} onChange={setContent} />;
+export function InsightsSQLEditor({ content, onChange }: InsightsSQLEditorProps) {
+  return <SQLEditor content={content} onChange={onChange} />;
 }
