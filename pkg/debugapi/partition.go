579b88bd17ab05eb38d1f0768473f75fcb0b2928
Darwin D Wu <darwin67@users.noreply.github.com>
2025-07-28 17:16:59 -0700

    copy partition retrieval logic to grpc function

--- pkg/debugapi/partition.go
+++ pkg/debugapi/partition.go
@@ -2,7 +2,10 @@ package debugapi
 
 import (
 	"context"
+	"fmt"
 
+	"cuelang.org/go/pkg/uuid"
+	"github.com/inngest/inngest/pkg/consts"
 	pb "github.com/inngest/inngest/proto/gen/debug/v1"
 )
 
@@ -11,5 +14,32 @@ func (d *debugAPI) GetPartition(ctx context.Context, req *pb.PartitionRequest) (
 }
 
 func (d *debugAPI) GetPartitionStatus(ctx context.Context, req *pb.PartitionRequest) (*pb.PartitionStatusResponse, error) {
-	return nil, errNotImplemented
+	var queueName *string
+	if _, err := uuid.Parse(req.GetId()); err != nil {
+		queueName = &req.Id
+	}
+
+	shard, err := d.ShardSelector(ctx, consts.DevServerAccountID, queueName)
+	if err != nil {
+		return nil, fmt.Errorf("error finding shard for GetPartition: %w", err)
+	}
+
+	pt, err := d.Queue.PartitionByID(ctx, shard, req.GetId())
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving partition: %w", err)
+	}
+
+	return &pb.PartitionStatusResponse{
+		Id:      req.GetId(),
+		Paused:  pt.Paused,
+		Migrate: pt.Migrate,
+
+		AccountActive:     int64(pt.AccountActive),
+		AccountInProgress: int64(pt.AccountInProgress),
+		Ready:             int64(pt.Ready),
+		InProgress:        int64(pt.InProgress),
+		Active:            int64(pt.Active),
+		Future:            int64(pt.Future),
+		Backlogs:          int64(pt.Backlogs),
+	}, nil
 }
