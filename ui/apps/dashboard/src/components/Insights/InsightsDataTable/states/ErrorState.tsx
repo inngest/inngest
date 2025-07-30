6bfc2056ccb318b8f3b72e79d0d7a722658f3bef
Dylan Dover <dylan@inngest.com>
2025-07-30 15:49:17 -0500

    allow retrying initial query error

--- ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ErrorState.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ErrorState.tsx
@@ -1,16 +1,32 @@
 'use client';
 
 import { Alert } from '@inngest/components/Alert';
+import { Button } from '@inngest/components/Button/Button';
 
 import { useInsightsQueryContext } from '../../context';
 
 const FALLBACK_ERROR = 'Something went wrong. Please try again.';
 
 export function ErrorState() {
-  const { error } = useInsightsQueryContext();
+  const { error, runQuery } = useInsightsQueryContext();
 
   return (
-    <Alert className="text-sm" severity="error">
+    <Alert
+      className="rounded-none text-sm"
+      inlineButton={
+        <Button
+          appearance="solid"
+          className="ml-auto h-auto p-0 text-sm font-medium underline"
+          kind="secondary"
+          label="Retry"
+          size="medium"
+          onClick={() => {
+            runQuery();
+          }}
+        />
+      }
+      severity="error"
+    >
       {error ?? FALLBACK_ERROR}
     </Alert>
   );
