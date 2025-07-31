7d4a71f3b9bdd42d9aaba404c938ea3d57fa8249
Dylan Dover <dylan@inngest.com>
2025-07-31 13:22:08 -0500

    fix: prevent freeze up when editing query while in loading state

--- ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/LoadingState.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/LoadingState.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { memo } from 'react';
 import Table from '@inngest/components/Table/NewTable';
 import { type ColumnDef } from '@tanstack/react-table';
 
@@ -9,6 +10,9 @@ const LOADING_COLUMNS: ColumnDef<any, any>[] = [
   { id: 'col3', header: undefined, accessorKey: 'col3' },
 ];
 
-export function LoadingState() {
+// This component will freeze the UI if it's not memoized and
+// the user causes a re-render by typing in the SQL editor.
+// TODO: Debug this issue with the underlying Table that causes this.
+export const LoadingState = memo(function LoadingState() {
   return <Table columns={LOADING_COLUMNS} data={undefined} isLoading />;
-}
+});
