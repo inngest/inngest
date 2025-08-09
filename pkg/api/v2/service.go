611ebe1e642515e2027b2025c3a5e36827a36e0d
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-08 22:14:42 -0700

    create v2 api service

--- pkg/api/v2/service.go
+++ pkg/api/v2/service.go
@@ -3,22 +3,30 @@ package v2
 import (
 	"context"
 	"fmt"
+	"net"
 
+	"github.com/inngest/inngest/pkg/logger"
 	"github.com/inngest/inngest/pkg/service"
 	pb "github.com/inngest/inngest/proto/gen/api/v2"
 	"google.golang.org/grpc"
 )
 
-func NewAPIV2() service.Service {
+func NewAPIV2(o Opts) service.Service {
 	return &api{
 		rpc: grpc.NewServer(),
+		log: o.Log.With("caller", "api-v2"),
 	}
+}
+
+type Opts struct {
+	Log logger.Logger
 }
 
 type api struct {
 	pb.V2Server
 
 	rpc *grpc.Server
+	log logger.Logger
 }
 
 func (a *api) Name() string {
@@ -26,13 +34,33 @@ func (a *api) Name() string {
 }
 
 func (a *api) Pre(ctx context.Context) error {
-	return fmt.Errorf("not implemented")
+	pb.RegisterV2Server(a.rpc, a)
+
+	return nil
 }
 
 func (a *api) Run(ctx context.Context) error {
-	return fmt.Errorf("not implemented")
+	addr := fmt.Sprintf(":%d", 10551)
+
+	log := a.log.With("addr", addr)
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Error("could not listen on port for v2 api", "error", err)
+		return err
+	}
+
+	log.Info("start api v2", "addr", addr)
+	err = a.rpc.Serve(l)
+	if err != nil {
+		log.Error("error serving api v2", "error", err)
+		return err
+	}
+
+	return nil
 }
 
 func (a *api) Stop(ctx context.Context) error {
-	return fmt.Errorf("not implemented")
+	a.rpc.GracefulStop()
+	return nil
 }
