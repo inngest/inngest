4d92955f6756a0bb22c882da228fe5d3ee017bf1
Ana <anafilipadealmeida@gmail.com>
2025-08-14 00:42:03 +0100

    Add table blank state

---
+++ ui/packages/components/src/Table/TableBlankState.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+type TableBlankStateProps = {
+  icon?: React.ReactNode;
+  actions: React.ReactNode;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+};
+
+export default function TableBlankState({
+  actions,
+  title,
+  description,
+  icon,
+}: TableBlankStateProps) {
+  const iconElement = React.isValidElement(icon)
+    ? React.cloneElement(icon as React.ReactElement, {
+        className: 'h-7 w-7',
+      })
+    : null;
+
+  return (
+    <div className="text-basis mt-36 flex flex-col items-center justify-center gap-5">
+      {iconElement && (
+        <div className="bg-canvasSubtle text-light rounded-md p-3 ">{iconElement}</div>
+      )}
+      <div className="text-center">
+        <p className="mb-1.5 text-xl">{title}</p>
+        <p className="text-subtle max-w-md text-sm">{description}</p>
+      </div>
+      <div className="flex items-center gap-3">{actions}</div>
+    </div>
+  );
+}
