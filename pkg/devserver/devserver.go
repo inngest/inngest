18af3fc71a09ac5069d73d004968073a0da4c0bc
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-17 23:22:09 -0700

    setup cron manager

--- pkg/devserver/devserver.go
+++ pkg/devserver/devserver.go
@@ -40,6 +40,7 @@ import (
 	"github.com/inngest/inngest/pkg/event"
 	"github.com/inngest/inngest/pkg/execution"
 	"github.com/inngest/inngest/pkg/execution/batch"
+	"github.com/inngest/inngest/pkg/execution/cron"
 	"github.com/inngest/inngest/pkg/execution/debounce"
 	"github.com/inngest/inngest/pkg/execution/driver"
 	"github.com/inngest/inngest/pkg/execution/exechttp"
@@ -339,6 +340,7 @@ func start(ctx context.Context, opts StartOpts) error {
 
 	batcher := batch.NewRedisBatchManager(shardedClient.Batch(), rq, batch.WithLogger(l))
 	debouncer := debounce.NewRedisDebouncer(unshardedClient.Debounce(), queueShard, rq)
+	croner := cron.NewRedisCronManager()
 
 	sn := singleton.New(ctx, queueShards, shardSelector)
 
@@ -474,6 +476,7 @@ func start(ctx context.Context, opts StartOpts) error {
 		executor.WithServiceExecutor(exec),
 		executor.WithServiceBatcher(batcher),
 		executor.WithServiceDebouncer(debouncer),
+		executor.WithServiceCroner(croner),
 		executor.WithServiceLogger(l),
 		executor.WithServiceShardSelector(shardSelector),
 		executor.WithServiceEnableKeyQueues(func(ctx context.Context, acctID uuid.UUID) bool {
