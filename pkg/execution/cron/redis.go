d2158b952c96e67cb60de86ee7cb9b75c12bcce8
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-18 23:04:28 -0700

    setup blanket implementation for UpdateSchedule

--- pkg/execution/cron/redis.go
+++ pkg/execution/cron/redis.go
@@ -25,5 +25,25 @@ func (c *redisCronManager) ScheduleNext(ctx context.Context) error {
 }
 
 func (c *redisCronManager) UpdateSchedule(ctx context.Context, ci CronItem) error {
+	switch ci.Op {
+	case CronOpNew, CronOpUnpause:
+		// TODO
+		// - enqueue new schedule
+		// - update mapping
+
+	case CronOpUpdate:
+		// TODO
+		// - delete and dequeue existing queue item
+		// - enqueue new schedule
+		// - update mapping
+
+	case CronOpPause:
+		// TODO
+		// - delete and dequeue existing queue item
+
+	default:
+		return fmt.Errorf("unknow cron operation: %d", ci.Op)
+	}
+
 	return fmt.Errorf("not implemented")
 }
