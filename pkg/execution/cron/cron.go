718a301311b6b5fe721e748ab74256c138d4bb6e
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-18 22:58:36 -0700

    update cron item with op and implement dequeue by jobID

--- pkg/execution/cron/cron.go
+++ pkg/execution/cron/cron.go
@@ -7,10 +7,37 @@ import (
 	"github.com/oklog/ulid/v2"
 )
 
+type CronOp int
+
+const (
+	CronOpNew CronOp = iota
+	CronOpUpdate
+	CronOpPause
+	CronOpUnpause
+)
+
 // CronManager represents the handling of cron
 type CronManager interface {
 	ScheduleNext(ctx context.Context) error
-	UpsertSchedule(ctx context.Context, fnID uuid.UUID) error
+	// UpdateSchedule handles the updating of the next scheduled item.
+	//
+	// Scenarios:
+	//
+	// ## New schedule
+	// Creates a new schedule
+	//
+	// ## Update schedule
+	// Updates the schedule when the following conditions are met
+	// - function version is larger
+	// - queue item ID is not identical (this should be an no-op when a retry happens for the system queue)
+	//
+	// ## Function pause
+	// Deletes the existing schedule
+	//
+	// ## Function unpause
+	// Creates a schedule, pretty much similar to new
+	//
+	UpdateSchedule(ctx context.Context, ci CronItem) error
 }
 
 // CronItem represent an item that can be scheduled via the cron expression
@@ -22,4 +49,5 @@ type CronItem struct {
 	FunctionID      uuid.UUID `json:"fnID"`
 	FunctionVersion int       `json:"fnV"`
 	Expression      string    `jaon:"expr"`
+	Op              CronOp    `json:"op"`
 }
