e593a9b4191915022862aa003f16ac957cd473c8
Dylan Dover <dylan@inngest.com>
2025-08-19 17:48:23 -0500

    undo broken commit

--- ui/packages/components/src/Tabs/Tab.tsx
+++ ui/packages/components/src/Tabs/Tab.tsx
@@ -48,7 +48,7 @@ export const Tab = forwardRef<React.ElementRef<typeof TabsPrimitive.Trigger>, Ta
         value={value}
         {...props}
       >
-        {iconBefore && <span className="flex-shrink-0">{iconBefore}</span>}
+        {finalIconBefore && <span className="flex-shrink-0">{finalIconBefore}</span>}
         <span ref={textRef} className="flex-1 truncate text-left">
           {children}
         </span>
