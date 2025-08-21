8a110af766c3fd83fc46b7e1b2ad6b771d641475
Darwin D Wu <darwin67@users.noreply.github.com>
2025-08-21 00:26:45 -0700

    add comments to CronItem

--- pkg/execution/cron/cron.go
+++ pkg/execution/cron/cron.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/inngest/inngest/pkg/enums"
-	"github.com/inngest/inngest/pkg/execution/queue"
 	"github.com/oklog/ulid/v2"
 	cron "github.com/robfig/cron/v3"
 )
@@ -80,14 +79,26 @@ type CronManager interface {
 
 // CronItem represent an item that can be scheduled via the cron expression
 type CronItem struct {
-	ID              ulid.ULID    `json:"id"`
-	AccountID       uuid.UUID    `json:"acctID"`
-	WorkspaceID     uuid.UUID    `json:"wsID"`
-	AppID           uuid.UUID    `json:"appID"`
-	FunctionID      uuid.UUID    `json:"fnID"`
-	FunctionVersion int          `json:"fnV"`
-	Expression      string       `jaon:"expr"`
-	Op              enums.CronOp `json:"op"`
+	// ID embeds the time this job needs to run if it's a process type
+	// ULID was chosen as a convinent way to provide a unique identifier with a timestamp
+	// embedded in it
+	ID ulid.ULID `json:"id"`
+	// Tenant
+	AccountID       uuid.UUID `json:"acctID"`
+	WorkspaceID     uuid.UUID `json:"wsID"`
+	AppID           uuid.UUID `json:"appID"`
+	FunctionID      uuid.UUID `json:"fnID"`
+	FunctionVersion int       `json:"fnV"`
+	// Expression is the actual cron expression being used
+	Expression string `jaon:"expr"`
+	// JobID stores queue item ID that's supposed to be handling this cron item.
+	// This is only available if it's a process type.
+	//
+	// NOTE
+	// This is based on the assumption that the ID field is always used for JobID assignments when enqueueing for idempotency handling reasons.
+	JobID string `json:"prevJobID,omitempty"`
+	// Op indicates what type of cron operation this item is for.
+	Op enums.CronOp `json:"op"`
 }
 
 // Equal checks if the cron item is identical
@@ -100,22 +111,16 @@ func (i CronItem) Equal(ci CronItem) bool {
 		i.FunctionID == ci.FunctionID &&
 		i.FunctionVersion == ci.FunctionVersion &&
 		i.Expression == ci.Expression &&
+		i.JobID == ci.JobID &&
 		i.Op == ci.Op
 }
 
+// SyncID is used for the jobID when enqueueing non processing types
 func (i CronItem) SyncID() string {
-	return fmt.Sprintf("%s-sync", i.ID)
+	return fmt.Sprintf("%s:sync", i.ID)
 }
 
+// ProcessID is used for the jobID when enqueueing processing types
 func (i CronItem) ProcessID() string {
-	return fmt.Sprintf("%s-process", i.ID)
-}
-
-// QueueItemID returns the hash of the queue item ID that's supposed to be handling this cron item
-//
-// NOTE
-// This is based on the assumption that the ID field is always used for JobID assignments when enqueueing.
-// So if that changes, some stuff will break.
-func (i CronItem) JobID() string {
-	return queue.HashID(context.TODO(), i.ProcessID())
+	return fmt.Sprintf("%s:process", i.ID)
 }
