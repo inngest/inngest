531b410f3db5d0180ab298e58d494968b28edd5d
Dylan Dover <dylan@inngest.com>
2025-08-28 14:22:59 -0500

    update run query button to include keyboard shortcut

--- ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditorQueryButton.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditorQueryButton.tsx
@@ -1,9 +1,23 @@
 'use client';
 
 import { Button } from '@inngest/components/Button/Button';
-import { RiPlayFill } from '@remixicon/react';
+import { RiCommandLine, RiCornerDownLeftFill } from '@remixicon/react';
 
 import { useInsightsStateMachineContext } from '../InsightsStateMachineContext/InsightsStateMachineContext';
+
+function QueryButtonLabel({ isRunning }: { isRunning: boolean }) {
+  if (isRunning) return null;
+
+  return (
+    <div className="flex items-center gap-2">
+      <span>Run query</span>
+      <div className="bg-primary-moderate flex shrink-0 gap-0.5 rounded-[4px] px-1 py-0.5">
+        <RiCommandLine className="h-4 w-4" />
+        <RiCornerDownLeftFill className="h-4 w-4" />
+      </div>
+    </div>
+  );
+}
 
 export function InsightsSQLEditorQueryButton() {
   const { query, runQuery, status } = useInsightsStateMachineContext();
@@ -11,11 +25,9 @@ export function InsightsSQLEditorQueryButton() {
 
   return (
     <Button
-      className="w-[110px]"
+      className="w-[135px] font-medium"
       disabled={query.trim() === '' || isRunning}
-      icon={<RiPlayFill className="h-4 w-4" />}
-      iconSide="left"
-      label={isRunning ? undefined : 'Run query'}
+      label={<QueryButtonLabel isRunning={isRunning} />}
       loading={isRunning}
       onClick={(e) => {
         runQuery();
