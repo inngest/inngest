a20c30389c3a7456019977c5cd0a8898be059b16
Dylan Dover <dylan@inngest.com>
2025-08-28 15:33:33 -0500

    support cmd+enter to fetch results while focused on monaco editor

--- ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditor.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/InsightsSQLEditor.tsx
@@ -1,11 +1,43 @@
 'use client';
 
-import { SQLEditor } from '@inngest/components/SQLEditor/SQLEditor';
+import { useCallback, useLayoutEffect, useRef } from 'react';
+import { SQLEditor, type SQLEditorMountCallback } from '@inngest/components/SQLEditor/SQLEditor';
 
 import { useInsightsStateMachineContext } from '../InsightsStateMachineContext/InsightsStateMachineContext';
+import { getCanRunQuery } from './utils';
 
 export function InsightsSQLEditor() {
-  const { onChange, query } = useInsightsStateMachineContext();
+  const { onChange, query, runQuery, status } = useInsightsStateMachineContext();
+  const isRunning = status === 'loading';
 
-  return <SQLEditor content={query} onChange={onChange} />;
+  // useLatestCallback ensures Monaco's onMount gets a stable reference while the callback
+  // nevertheless executes with fresh state (query, isRunning, runQuery).
+  const handleEditorMount: SQLEditorMountCallback = useLatestCallback((editor, monaco) => {
+    const disposable = editor.onKeyDown((e) => {
+      if (e.keyCode === monaco.KeyCode.Enter && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (getCanRunQuery(query, isRunning)) runQuery();
+      }
+    });
+
+    return () => {
+      disposable.dispose();
+    };
+  });
+
+  return <SQLEditor content={query} onChange={onChange} onMount={handleEditorMount} />;
+}
+
+function useLatestCallback<A extends any[], R>(callback: (...args: A) => R) {
+  const ref = useRef(callback);
+
+  useLayoutEffect(() => {
+    ref.current = callback;
+  }, [callback]);
+
+  return useCallback((...args: A) => {
+    return ref.current(...args);
+  }, []);
 }
