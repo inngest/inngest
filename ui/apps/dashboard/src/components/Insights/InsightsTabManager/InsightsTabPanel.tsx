4034efae1c9e6c0de0381335688a271923288d12
Dylan Dover <dylan@inngest.com>
2025-09-04 11:08:34 -0500

    add results limit pill

--- ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsTabPanel.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsTabPanel.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { Pill } from '@inngest/components/Pill';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@inngest/components/Tooltip/Tooltip';
+
 import { InsightsDataTable } from '@/components/Insights/InsightsDataTable/InsightsDataTable';
 import { InsightsSQLEditor } from '@/components/Insights/InsightsSQLEditor/InsightsSQLEditor';
 import { InsightsSQLEditorDownloadCSVButton } from '@/components/Insights/InsightsSQLEditor/InsightsSQLEditorDownloadCSVButton';
@@ -47,7 +50,23 @@ export function InsightsTabPanel({ isHomeTab, isTemplatesTab, tab }: InsightsTab
             <InsightsSQLEditorDownloadCSVButton />
           </>
         }
-        title="Results"
+        title={
+          <div className="mr-1 flex items-center gap-2">
+            <span>Results</span>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span>
+                  <Pill appearance="outlined" kind="info">
+                    LIMIT 1000
+                  </Pill>
+                </span>
+              </TooltipTrigger>
+              <TooltipContent className="p-2 text-xs" side="right" sideOffset={3}>
+                Results are currently limited to at most 1000 rows.
+              </TooltipContent>
+            </Tooltip>
+          </div>
+        }
       >
         <InsightsDataTable />
       </Section>
