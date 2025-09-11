6ea1db961e59e164a4352e7051b6924d143a2e83
doverdbng <dylan@inngest.com>
2025-09-11 14:36:41 -0500

    [Insights]: Prevent query editor perf issue via memoization (#2973)
    * memoize Table usage in ResultsTable

--- ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/ResultsTable.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/ResultsTable.tsx
@@ -1,10 +1,27 @@
 'use client';
 
+import { memo } from 'react';
 import { Table } from '@inngest/components/Table';
+import type { ColumnDef } from '@tanstack/react-table';
 
 import { useInsightsStateMachineContext } from '@/components/Insights/InsightsStateMachineContext/InsightsStateMachineContext';
+import type { InsightsFetchResult } from '@/components/Insights/InsightsStateMachineContext/types';
 import { ResultsTableFooter, assertData } from './ResultsTableFooter';
 import { useColumns } from './useColumns';
+
+type InsightsEntry = InsightsFetchResult['rows'][number];
+type InsightsColumnValue = InsightsEntry['values'][string];
+type InsightsTableProps = {
+  cellClassName: string;
+  columns: ColumnDef<InsightsEntry, InsightsColumnValue>[];
+  data: InsightsEntry[];
+};
+
+function InsightsTable({ columns, data, cellClassName }: InsightsTableProps) {
+  return <Table<InsightsEntry> cellClassName={cellClassName} columns={columns} data={data} />;
+}
+
+const MemoizedInsightsTable = memo(InsightsTable);
 
 export function ResultsTable() {
   const { data } = useInsightsStateMachineContext();
@@ -16,7 +33,7 @@ export function ResultsTable() {
   return (
     <div className="flex h-full flex-col overflow-hidden">
       <div className="flex-1 overflow-auto" id="insights-table-container">
-        <Table
+        <MemoizedInsightsTable
           columns={columns}
           data={data.rows}
           cellClassName="[&:not(:first-child)]:border-l [&:not(:first-child)]:border-light box-border"
