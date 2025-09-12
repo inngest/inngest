b4a537b5d4f9965cc1e75084ba98f536ee2af15e
Dylan Dover <dylan@inngest.com>
2025-09-12 13:42:53 -0500

    add expand button (no behavior)

--- ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/JSONAwareTextCell.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/JSONAwareTextCell.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@inngest/components/Button/Button';
 import { TextCell } from '@inngest/components/Table';
+import { RiSidebarFoldLine } from '@remixicon/react';
 
 import { getFormattedJSONObjectOrArrayString } from './json';
 
@@ -19,6 +21,25 @@ export function JSONAwareTextCell({ children }: JSONAwareTextCellProps) {
       >
         {formattedJSON}
       </pre>
+      <ExpandSidebarButton />
     </div>
+  );
+}
+
+function ExpandSidebarButton() {
+  return (
+    <Button
+      appearance="outlined"
+      className="bg-surface absolute bottom-2 right-2 z-10 rounded-md border px-2 py-1 text-xs opacity-0 shadow transition-opacity group-focus-within:opacity-100 group-hover:opacity-100"
+      icon={<RiSidebarFoldLine />}
+      iconSide="left"
+      kind="secondary"
+      label="Expand"
+      onClick={(e) => {
+        e.preventDefault();
+        alert('test');
+      }}
+      size="small"
+    />
   );
 }
