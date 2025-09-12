a51f8c2b8325071e8d30d4264d29aa87748f453c
Dylan Dover <dylan@inngest.com>
2025-09-12 10:51:07 -0500

    align data to the top of table cells

--- ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/ResultsTable.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsDataTable/states/ResultsState/ResultsTable.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import { Table } from '@inngest/components/Table';
 import type { ColumnDef } from '@tanstack/react-table';
 
 import { useInsightsStateMachineContext } from '@/components/Insights/InsightsStateMachineContext/InsightsStateMachineContext';
 import type { InsightsFetchResult } from '@/components/Insights/InsightsStateMachineContext/types';
 import { ResultsTableFooter, assertData } from './ResultsTableFooter';
+import { mockData } from './mockData';
 import { useColumns } from './useColumns';
 
 type InsightsEntry = InsightsFetchResult['rows'][number];
@@ -25,18 +26,20 @@ const MemoizedInsightsTable = memo(InsightsTable);
 
 export function ResultsTable() {
   const { data } = useInsightsStateMachineContext();
+  const [useMockData, setUseMockData] = useState(true);
 
-  const { columns } = useColumns(data);
+  const dataToUse = useMockData ? mockData : data;
+  const { columns } = useColumns(dataToUse);
 
-  if (!assertData(data)) return null;
+  if (!assertData(dataToUse)) return null;
 
   return (
     <div className="flex h-full flex-col overflow-hidden">
       <div className="flex-1 overflow-auto" id="insights-table-container">
         <MemoizedInsightsTable
           columns={columns}
-          data={data.rows}
-          cellClassName="[&:not(:first-child)]:border-l [&:not(:first-child)]:border-light box-border"
+          data={dataToUse.rows}
+          cellClassName="[&:not(:first-child)]:border-l [&:not(:first-child)]:border-light box-border align-top p-2.5"
         />
       </div>
       <ResultsTableFooter />
