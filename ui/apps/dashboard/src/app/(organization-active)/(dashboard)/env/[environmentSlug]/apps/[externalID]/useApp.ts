1a38412b37574b378fb17145c78b826637cc52d4
Andy Lawrence <andy@inngest.com>
2025-09-19 14:36:33 -0400

    Refactor GraphQL queries to use latestSyncedConfig

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/useApp.ts
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/apps/[externalID]/useApp.ts
@@ -15,7 +15,7 @@ const query = graphql(`
         externalID
         functions {
           id
-          latestVersion {
+          latestSyncedConfig {
             triggers {
               type
               value
@@ -80,7 +80,7 @@ export function useApp({ envID, externalAppID }: { envID: string; externalAppID:
         functions: app.functions.map((fn) => {
           return {
             ...fn,
-            triggers: fn.latestVersion.triggers,
+            triggers: fn.latestSyncedConfig?.triggers || [],
           };
         }),
         latestSync,
