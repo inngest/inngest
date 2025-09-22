eed7c7f51c8b7dcadbbbfa3d041c44186c7d3720
Ted Werbel <werbel.ted@gmail.com>
2025-09-22 18:43:41 -0400

    add placeholder for insights chat

---
+++ ui/apps/dashboard/src/components/Insights/InsightsTabManager/ChatPlaceholder.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export function ChatPlaceholder() {
+  return (
+    <div className="flex h-full w-[312px] flex-col items-center justify-center border-l border-gray-200 bg-gray-50">
+      <div className="text-center">Chat Placeholder</div>
+    </div>
+  );
+}
