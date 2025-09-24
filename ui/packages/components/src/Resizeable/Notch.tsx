57c93737ffb65b0f73bf79a7964f57b238767ae4
Dylan Dover <dylan@inngest.com>
2025-09-24 17:37:22 -0500

    improve notch styling

--- ui/packages/components/src/Resizeable/Notch.tsx
+++ ui/packages/components/src/Resizeable/Notch.tsx
@@ -35,10 +35,13 @@ export function Notch({ containerRef, minSplitPercentage, maxSplitPercentage }: 
 
   return (
     <div
-      className="absolute left-0 right-0 top-[var(--split)] -translate-y-1/2 cursor-row-resize"
+      className="absolute left-0 right-0 top-[var(--split)] z-10 flex h-6 -translate-y-1/2 cursor-row-resize items-center justify-center"
       onMouseDown={handleMouseDown}
     >
-      <div className="bg-border mx-auto h-1 w-12 rounded-full" />
+      <div className="bg-canvasMuted pointer-events-none absolute left-0 right-0 h-0.5" />
+      <div className="border-muted bg-canvasSubtle pointer-events-none relative h-2 w-8 rounded-full border">
+        <div className="bg-surfaceSubtle absolute left-1/2 top-1/2 h-px w-3 -translate-x-1/2 -translate-y-1/2 rounded-full" />
+      </div>
     </div>
   );
 }
