7a60b676feb8f0bbc50bddd5a2510de4c82a1ff6
Linell Bonnette <tlbonnette@gmail.com>
2025-09-26 12:55:22 -0500

    feat: add OpcodeStepFailed support (#2992)
    * feat: add OpcodeStepFailed support
    
    Also adds `MaxAttempts`
    
    [EXE-252: Add StepFailed Opcode](https://linear.app/inngest/issue/EXE-252/add-stepfailed-opcode)
    [EXE-253: Add `MaxAttempts` to SDKRequestContext](https://linear.app/inngest/issue/EXE-253/add-maxattempts-to-sdkrequestcontext)
    
    * Support `OpcodeStepFailed` in checkpointing
    
    Really does look this simple!
    
    * Don't track maxattempts in test dsl
    
    ---------
    
    Co-authored-by: Jack Williams <1736957+jpwilliams@users.noreply.github.com>

--- pkg/tracing/util.go
+++ pkg/tracing/util.go
@@ -206,7 +206,7 @@ func generatorAttrs(op *state.GeneratorOpcode) *meta.SerializableAttrs {
 			}
 		}
 
-	case enums.OpcodeStep, enums.OpcodeStepRun, enums.OpcodeStepError:
+	case enums.OpcodeStep, enums.OpcodeStepRun, enums.OpcodeStepError, enums.OpcodeStepFailed:
 		{
 			// Output (success or error)
 			if output, err := op.Output(); err == nil {
