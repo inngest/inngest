c84c5ed6f536a6f392763e0d893be73ce1721990
ジョン <hello@johnbuchta.com>
2025-09-26 10:00:08 -0400

    changing sample rate for all sessions from 0 to .5 while we use the trial (#3065)

--- ui/apps/dashboard/sentry.client.config.ts
+++ ui/apps/dashboard/sentry.client.config.ts
@@ -13,7 +13,7 @@ Sentry.init({
     /^https:\/\/api\.inngest\.net\//, // The staging API origin.
     'localhost', // The local API origin.
   ],
-  replaysSessionSampleRate: 0.0,
+  replaysSessionSampleRate: 0.5,
   replaysOnErrorSampleRate: 1.0,
 });
 
