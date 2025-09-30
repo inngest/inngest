9b9f138ff1faab252dc4e27031c57538fc44d974
Dylan Dover <dylan@inngest.com>
2025-09-30 11:09:24 -0500

    adds ARITHMETIC_FUNCTIONS

--- ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/functions.ts
+++ ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/functions.ts
@@ -17,6 +17,35 @@ const AGGREGATE_FUNCTIONS: FunctionDescriptor[] = [
   { name: 'var_samp', signature: 'var_samp(${1:expr})' },
 ];
 
+const ARITHMETIC_FUNCTIONS: FunctionDescriptor[] = [
+  { name: 'abs', signature: 'abs(${1:val})' },
+  { name: 'byteswap', signature: 'byteswap(${1:val})' },
+  { name: 'divide', signature: 'divide(${1:a}, ${2:b})' },
+  // { name: 'divideDecimal', signature: 'divideDecimal(${1:a}, ${2:b})' }, error
+  //{ name: 'divideOrNull', signature: 'divideOrNull(${1:a}, ${2:b})' }, base 16
+  { name: 'gcd', signature: 'gcd(${1:a}, ${2:b})' },
+  { name: 'ifNotFinite', signature: 'ifNotFinite(${1:a}, ${2:b})' },
+  { name: 'intDiv', signature: 'intDiv(${1:a}, ${2:b})' },
+  // { name: 'intDivOrNull', signature: 'intDivOrNull(${1:a}, ${2:b})' }, base 16
+  { name: 'intDivOrZero', signature: 'intDivOrZero(${1:a}, ${2:b})' },
+  { name: 'isFinite', signature: 'isFinite(${1:val})' },
+  { name: 'isInfinite', signature: 'isInfinite(${1:val})' },
+  { name: 'isNaN', signature: 'isNaN(${1:val})' },
+  { name: 'lcm', signature: 'lcm(${1:a}, ${2:b})' },
+  { name: 'max2', signature: 'max2(${1:a}, ${2:b})' },
+  { name: 'min2', signature: 'min2(${1:a}, ${2:b})' },
+  { name: 'minus', signature: 'minus(${1:a}, ${2:b})' },
+  { name: 'modulo', signature: 'modulo(${1:a}, ${2:b})' },
+  // { name: 'moduloOrNull', signature: 'moduloOrNull(${1:a}, ${2:b})' }, base 16
+  { name: 'moduloOrZero', signature: 'moduloOrZero(${1:a}, ${2:b})' },
+  { name: 'multiply', signature: 'multiply(${1:a}, ${2:b})' },
+  // { name: 'multiplyDecimal', signature: 'multiplyDecimal(${1:a}, ${2:b})' }, error
+  { name: 'negate', signature: 'negate(${1:val})' },
+  { name: 'plus', signature: 'plus(${1:a}, ${2:b})' },
+  { name: 'positivemodulo', signature: 'positivemodulo(${1:a}, ${2:b})' },
+  // { name: 'positivemoduloornull', signature: 'positivemoduloornull(${1:a}, ${2:b})' }, base 16
+];
+
 const COMPARISON_FUNCTIONS: FunctionDescriptor[] = [
   { name: 'equals', signature: 'equals(${1:a}, ${2:b})' },
   { name: 'greater', signature: 'greater(${1:a}, ${2:b})' },
@@ -26,7 +55,11 @@ const COMPARISON_FUNCTIONS: FunctionDescriptor[] = [
   { name: 'notEquals', signature: 'notEquals(${1:a}, ${2:b})' },
 ];
 
-export const SUPPORTED_FUNCTIONS = sortByName([...AGGREGATE_FUNCTIONS, ...COMPARISON_FUNCTIONS]);
+export const SUPPORTED_FUNCTIONS = sortByName([
+  ...AGGREGATE_FUNCTIONS,
+  ...ARITHMETIC_FUNCTIONS,
+  ...COMPARISON_FUNCTIONS,
+]);
 
 function sortByName(fns: FunctionDescriptor[]): FunctionDescriptor[] {
   return fns.sort((a, b) => a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
