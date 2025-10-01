e6106c023cfb9090ecb8626460a89cfbc6e8f7d2
Jacob Heric <jacob@jacobheric.com>
2025-10-01 11:49:32 -0400

    * don't sync project zero apps * protect against panics on duration calc, make that work with project zero

--- pkg/devserver/service.go
+++ pkg/devserver/service.go
@@ -319,7 +319,8 @@ func (d *devserver) pollSDKs(ctx context.Context) {
 		urls := map[string]struct{}{}
 		if apps, err := d.Data.GetApps(ctx, consts.DevServerEnvID, nil); err == nil {
 			for _, app := range apps {
-				if app.Method == enums.AppMethodConnect.String() {
+				fmt.Println("shit app.Method", app.Method)
+				if app.Method == enums.AppMethodConnect.String() || app.Method == enums.AppMethodAPI.String() {
 					continue
 				}
 
