9bbd9992700d69dfad0e8dc7a2aeb6107362a113
Dylan Dover <dylan@inngest.com>
2025-10-07 16:13:11 -0500

    add default-src fallback

--- ui/apps/dashboard/next.config.js
+++ ui/apps/dashboard/next.config.js
@@ -3,6 +3,7 @@ const { withSentryConfig } = require('@sentry/nextjs');
 
 const CSP_HEADER = `
   base-uri 'none';
+  default-src 'self';
   font-src 'self' https://fonts-cdn.inngest.com;
   form-action 'self';
   frame-ancestors 'none';
