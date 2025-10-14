98fad2b52f25a903b96d959e08353242bf85cf6c
Jacob Heric <jacob@jacobheric.com>
2025-10-14 14:42:14 -0400

    linting

--- ui/apps/dashboard/src/components/RunDetails/RunDetails.tsx
+++ ui/apps/dashboard/src/components/RunDetails/RunDetails.tsx
@@ -3,7 +3,6 @@
 import { RunDetailsV3 } from '@inngest/components/RunDetailsV3/RunDetailsV3';
 import { cn } from '@inngest/components/utils/classNames';
 
-import { useBooleanFlag } from '@/components/FeatureFlags/hooks';
 import { useGetTrigger } from './useGetTrigger';
 
 type Props = {
