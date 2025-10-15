9f4d2a4bc29defe6dbc45cfc10cabbed2b38a45b
Lakshmi Kasinathan <lakshmi@inngest.com>
2025-10-15 13:50:25 -0700

    add more e2e tests

--- tests/golang/cron_test.go
+++ tests/golang/cron_test.go
@@ -2,6 +2,7 @@ package golang
 
 import (
 	"context"
+	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -14,6 +15,7 @@ import (
 )
 
 func TestCron(t *testing.T) {
+	t.Parallel()
 	ctx := context.Background()
 
 	c := client.New(t)
@@ -72,5 +74,177 @@ func TestCron(t *testing.T) {
 			assert.NotNil(t, trigger.Cron)
 			assert.Nil(t, trigger.BatchID)
 		})
+	})
+}
+
+func TestCronRemoveCronTrigger(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+
+	inngestClient, server, registerFuncs := NewSDKHandler(t, "remove-cron")
+	defer server.Close()
+
+	var (
+		counter int32
+		runID   string
+	)
+
+	_, err := inngestgo.CreateFunction(
+		inngestClient,
+		inngestgo.FunctionOpts{ID: "cron-test"},
+		inngestgo.CronTrigger("* * * * *"),
+		func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+			if runID == "" {
+				runID = input.InputCtx.RunID
+			}
+			atomic.AddInt32(&counter, 1)
+
+			return "schedule done", nil
+		},
+	)
+	r.NoError(err)
+	registerFuncs()
+
+	t.Run("cron should run", func(t *testing.T) {
+		r.Eventually(func() bool {
+			return atomic.LoadInt32(&counter) == 1
+		}, time.Minute, 5*time.Second)
+	})
+
+	t.Run("re-register function to remove cron", func(t *testing.T) {
+		_, err := inngestgo.CreateFunction(
+			inngestClient,
+			inngestgo.FunctionOpts{ID: "cron-test"},
+			inngestgo.EventTrigger("test/ehh", nil),
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				if runID == "" {
+					runID = input.InputCtx.RunID
+				}
+				atomic.AddInt32(&counter, 1)
+
+				return "schedule done", nil
+			},
+		)
+		r.NoError(err)
+		registerFuncs()
+
+		time.Sleep(time.Minute)
+		r.Equal(int32(1), atomic.LoadInt32(&counter))
+	})
+}
+
+func TestCronUpdateCronTrigger(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+
+	inngestClient, server, registerFuncs := NewSDKHandler(t, randomSuffix("reduce-cron-frequency"))
+	defer server.Close()
+
+	var (
+		counter int32
+		runID   string
+	)
+
+	_, err := inngestgo.CreateFunction(
+		inngestClient,
+		inngestgo.FunctionOpts{ID: "cron-test"},
+		inngestgo.CronTrigger("*/2 * * * *"),
+		func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+			if runID == "" {
+				runID = input.InputCtx.RunID
+			}
+			atomic.AddInt32(&counter, 1)
+
+			return "schedule done", nil
+		},
+	)
+	r.NoError(err)
+	registerFuncs()
+
+	t.Run("cron should run", func(t *testing.T) {
+		r.Eventually(func() bool {
+			return atomic.LoadInt32(&counter) == 1
+		}, 2*time.Minute, 5*time.Second)
+	})
+
+	t.Run("re-register function to reduce cron frequency", func(t *testing.T) {
+		_, err := inngestgo.CreateFunction(
+			inngestClient,
+			inngestgo.FunctionOpts{ID: "cron-test"},
+			inngestgo.CronTrigger("* * * * *"),
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				if runID == "" {
+					runID = input.InputCtx.RunID
+				}
+				atomic.AddInt32(&counter, 1)
+				fmt.Println("scheduled function ran at", time.Now())
+
+				return "schedule done", nil
+			},
+		)
+		r.NoError(err)
+		registerFuncs()
+
+		r.Eventually(func() bool {
+			return atomic.LoadInt32(&counter) == 2
+		}, 1*time.Minute, 5*time.Second)
+	})
+}
+
+func TestCronAddCronTrigger(t *testing.T) {
+	t.Parallel()
+
+	r := require.New(t)
+
+	inngestClient, server, registerFuncs := NewSDKHandler(t, randomSuffix("add-cron-trigger"))
+	defer server.Close()
+
+	var (
+		counter int32
+		runID   string
+	)
+
+	_, err := inngestgo.CreateFunction(
+		inngestClient,
+		inngestgo.FunctionOpts{ID: "cron-test"},
+		inngestgo.CronTrigger("*/2 * * * *"),
+		func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+			if runID == "" {
+				runID = input.InputCtx.RunID
+			}
+			atomic.AddInt32(&counter, 1)
+
+			return "schedule done", nil
+		},
+	)
+	r.NoError(err)
+	registerFuncs()
+
+	t.Run("re-register function to add another cron trigger", func(t *testing.T) {
+		_, err := inngestgo.CreateFunction(
+			inngestClient,
+			inngestgo.FunctionOpts{ID: "cron-test"},
+			inngestgo.MultipleTriggers{
+				inngestgo.CronTrigger("*/2 * * * *"),
+				inngestgo.CronTrigger("* * * * *"),
+			},
+			func(ctx context.Context, input inngestgo.Input[any]) (any, error) {
+				if runID == "" {
+					runID = input.InputCtx.RunID
+				}
+				atomic.AddInt32(&counter, 1)
+				fmt.Println("scheduled function ran at", time.Now())
+
+				return "schedule done", nil
+			},
+		)
+		r.NoError(err)
+		registerFuncs()
+
+		r.Eventually(func() bool {
+			return atomic.LoadInt32(&counter) == 2
+		}, 2*time.Minute, 5*time.Second)
 	})
 }
