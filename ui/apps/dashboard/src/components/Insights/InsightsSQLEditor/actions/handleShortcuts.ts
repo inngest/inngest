acd93aefbd3b887034d05c4c6d07a9143378c8b0
Dylan Dover <dylan@inngest.com>
2025-10-16 16:55:33 -0500

    add shortcuts (without impl)

--- ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/actions/handleShortcuts.ts
+++ ui/apps/dashboard/src/components/Insights/InsightsSQLEditor/actions/handleShortcuts.ts
@@ -13,11 +13,46 @@ export function handleShortcuts(
   runQuery: () => void
 ) {
   return editor.onKeyDown((e) => {
-    if (e.keyCode === monaco.KeyCode.Enter && (e.metaKey || e.ctrlKey)) {
+    if (hasKeys(e, ['metaOrCtrl', monaco.KeyCode.Enter])) {
       e.preventDefault();
       e.stopPropagation();
-
       if (getCanRunQuery(latestQueryRef.current, isRunningRef.current)) runQuery();
     }
+
+    if (hasKeys(e, ['metaOrCtrl', monaco.KeyCode.KeyS])) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('Insights: Save query (Cmd/Ctrl+Alt+S)');
+      return;
+    }
+
+    if (hasKeys(e, ['metaOrCtrl', 'alt', monaco.KeyCode.KeyT])) {
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('Insights: New tab (Cmd/Ctrl+Shift+N)');
+      return;
+    }
   });
+}
+
+type EditorInstance = Parameters<SQLEditorMountCallback>[0];
+type KeyEvent = Parameters<Parameters<EditorInstance['onKeyDown']>[0]>[0];
+type KeySpec = 'alt' | 'metaOrCtrl' | number;
+
+function hasKeys(ev: KeyEvent, keys: ReadonlyArray<KeySpec>): boolean {
+  for (const k of keys) {
+    switch (k) {
+      case 'alt':
+        if (!ev.altKey) return false;
+        break;
+      case 'metaOrCtrl':
+        if (!(ev.metaKey || ev.ctrlKey)) return false;
+        break;
+      default:
+        if (ev.keyCode !== k) return false;
+        break;
+    }
+  }
+
+  return true;
 }
