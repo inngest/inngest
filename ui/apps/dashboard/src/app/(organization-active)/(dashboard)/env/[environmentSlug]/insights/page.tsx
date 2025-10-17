c7412d6de9f9c3b52f2f418738ed96c09179f45f
Dylan Dover <dylan@inngest.com>
2025-10-17 13:50:57 -0500

    make sure triggering save query by shortcut updates UI

--- ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/insights/page.tsx
+++ ui/apps/dashboard/src/app/(organization-active)/(dashboard)/env/[environmentSlug]/insights/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import { useQuery } from 'urql';
 
+import { SaveTabProvider } from '@/components/Insights/InsightsSQLEditor/SaveTabContext';
 import { useDocumentShortcuts } from '@/components/Insights/InsightsSQLEditor/actions/handleShortcuts';
 import { useInsightsTabManager } from '@/components/Insights/InsightsTabManager/InsightsTabManager';
 import { TabManagerProvider } from '@/components/Insights/InsightsTabManager/TabManagerContext';
@@ -34,16 +35,18 @@ export default function InsightsPage() {
 
   return (
     <StoredQueriesProvider tabManagerActions={actions}>
-      <TabManagerProvider actions={actions} activeTab={activeTab}>
-        <div className="flex h-full w-full flex-1 overflow-hidden">
-          {isQueryHelperPanelVisible && (
-            <div className="w-[240px] flex-shrink-0">
-              <QueryHelperPanel activeSavedQueryId={activeSavedQueryId} />
-            </div>
-          )}
-          <div className="flex h-full w-full flex-1 flex-col overflow-hidden">{tabManager}</div>
-        </div>
-      </TabManagerProvider>
+      <SaveTabProvider>
+        <TabManagerProvider actions={actions} activeTab={activeTab}>
+          <div className="flex h-full w-full flex-1 overflow-hidden">
+            {isQueryHelperPanelVisible && (
+              <div className="w-[240px] flex-shrink-0">
+                <QueryHelperPanel activeSavedQueryId={activeSavedQueryId} />
+              </div>
+            )}
+            <div className="flex h-full w-full flex-1 flex-col overflow-hidden">{tabManager}</div>
+          </div>
+        </TabManagerProvider>
+      </SaveTabProvider>
     </StoredQueriesProvider>
   );
 }
