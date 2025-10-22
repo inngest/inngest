94c606b321e76e3664eb8cd770c0f4871ffe9c47
Jacob Heric <jacob@jacobheric.com>
2025-10-22 18:09:58 -0400

    one more tailwind typescript error

--- ui/apps/dev-server-ui/tailwind.config.ts
+++ ui/apps/dev-server-ui/tailwind.config.ts
@@ -1,8 +1,10 @@
+import baseConfig from '../../packages/components/tailwind.config';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     '../../packages/components/src/**/*.{ts,tsx}',
   ],
-  presets: [require('../../packages/components/tailwind.config.ts')],
+  presets: [baseConfig],
 };
