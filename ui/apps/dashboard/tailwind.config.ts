c632e7e6e5860e0199809d2bc3ada3581c2328b8
Jacob Heric <jacob@jacobheric.com>
2025-10-23 08:22:30 -0400

    leave dashboard tailwind config alone for now

--- ui/apps/dashboard/tailwind.config.ts
+++ ui/apps/dashboard/tailwind.config.ts
@@ -5,4 +5,5 @@ import sharedConfig from '../../packages/components/tailwind.config';
 export default {
   ...sharedConfig,
   content: ['./src/**/*.{ts,tsx}', '../../packages/components/src/**/*.{ts,tsx}'],
+  plugins: [require('@tailwindcss/forms'), require('@headlessui/tailwindcss')],
 } satisfies Config;
