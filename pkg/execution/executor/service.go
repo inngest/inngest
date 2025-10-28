abcd2160f34bc5bcb0a04964c3e3a8f587b9477d
Lakshmi Kasinathan <lakshmi@inngest.com>
2025-10-28 16:57:38 -0700

    Make logs less noisy (#3215)

--- pkg/execution/executor/service.go
+++ pkg/execution/executor/service.go
@@ -888,7 +888,7 @@ func (s *svc) handleCronHealthCheck(ctx context.Context, item queue.Item) error 
 	}
 
 	hcTime := ci.ID.Timestamp()
-	l.Info("starting cron health check", "scheduled_health_check_time", hcTime)
+	l.Trace("starting cron health check", "scheduled_health_check_time", hcTime)
 
 	cqrsFns, err := s.data.GetFunctions(ctx)
 	if err != nil {
@@ -950,13 +950,11 @@ func (s *svc) handleCronHealthCheck(ctx context.Context, item queue.Item) error 
 	}
 
 	err = eg.Wait()
-	l.Info("health checks finished", "success", success, "failed", failed, "errors", errored)
+	l.Trace("health checks finished", "success", success, "failed", failed, "errors", errored)
 
 	if err != nil {
 		return fmt.Errorf("some cron health checks errored %w", err)
 	}
-
-	l.Info("finished cron health check")
 
 	// enqueue next health check.
 	return s.croner.EnqueueNextHealthCheck(ctx)
