61a3bb6697433b73b8807b34abe1f60a435b94ad
Dylan Dover <dylan@inngest.com>
2025-10-29 10:02:29 -0500

    remove broken usage of useMemo in a map

--- ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsHelperPanel/InsightsHelperPanelControl.tsx
+++ ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsHelperPanel/InsightsHelperPanelControl.tsx
@@ -23,20 +23,16 @@ export function InsightsHelperPanelControl({
   return (
     <div className="border-subtle flex h-full w-[56px] flex-col items-center gap-2 border-l px-3 py-2">
       {items.map((item) => {
-        const sharedProps = useMemo(
-          () =>
-            ({
-              'aria-label': item.title,
-              className: cn(
-                'flex h-8 items-center justify-center rounded-md text-subtle transition-colors w-8',
-                activeTitle !== item.title && 'hover:bg-canvasSubtle',
-                activeTitle === item.title &&
-                  'bg-secondary-4xSubtle hover:bg-secondary-3xSubtle text-info'
-              ),
-              title: item.title,
-            } as const),
-          [activeTitle, item.title]
-        );
+        const sharedProps = {
+          'aria-label': item.title,
+          className: cn(
+            'flex h-8 items-center justify-center rounded-md text-subtle transition-colors w-8',
+            activeTitle !== item.title && 'hover:bg-canvasSubtle',
+            activeTitle === item.title &&
+              'bg-secondary-4xSubtle hover:bg-secondary-3xSubtle text-info'
+          ),
+          title: item.title,
+        } as const;
 
         if (item.href) {
           return (
