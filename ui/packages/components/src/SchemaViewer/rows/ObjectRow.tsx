8eb3f71f61723df7778cb3d2865bf160e211a93a
Dylan Dover <dylan@inngest.com>
2025-10-30 13:56:04 -0500

    add collapsibility and tracing line to ObjectRow

--- ui/packages/components/src/SchemaViewer/rows/ObjectRow.tsx
+++ ui/packages/components/src/SchemaViewer/rows/ObjectRow.tsx
@@ -1,42 +1,38 @@
 'use client';
 
-import * as React from 'react';
+import { useState } from 'react';
 
 import type { ObjectNode, SchemaNode, ValueNode } from '../types';
+import { CollapsibleRowWidget } from './CollapsibleRowWidget';
+import { Row } from './Row';
+import { ValueRow } from './ValueRow';
 
 export type ObjectRowProps = { node: ObjectNode };
 
 export function ObjectRow({ node }: ObjectRowProps): React.ReactElement {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      {} {node.name} object
-      <div style={{ paddingLeft: 12 }}>
-        {node.children.map((child) => (
-          <React.Fragment key={child.path}>{renderChild(child)}</React.Fragment>
-        ))}
+    <div className="flex flex-col gap-1">
+      <div className="flex cursor-pointer items-center" onClick={() => setOpen((v) => !v)}>
+        <CollapsibleRowWidget open={open} />
+        <div className="-ml-0.5">
+          <ValueRow boldName={open} node={makeFauxValueNode(node)} typeLabelOverride={'{}'} />
+        </div>
       </div>
+      {open && (
+        <div className="border-subtle ml-2 border-l pl-3">
+          <div className="flex flex-col gap-1">
+            {node.children.map((child) => (
+              <Row key={child.path} node={child} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
-function renderChild(child: SchemaNode): React.ReactElement | null {
-  if (child.kind === 'value') {
-    const valueNode = child as ValueNode;
-    const typeLabel = Array.isArray(valueNode.type) ? valueNode.type.join(' | ') : valueNode.type;
-    return (
-      <div>
-        {valueNode.name} {typeLabel}
-      </div>
-    );
-  }
-  if (child.kind === 'array') {
-    // Ignoring arrays for now
-    return null;
-  }
-  // object
-  return (
-    <div>
-      {} {child.name} object
-    </div>
-  );
+function makeFauxValueNode(node: SchemaNode): ValueNode {
+  return { kind: 'value', name: node.name, path: node.path, type: 'object' };
 }
