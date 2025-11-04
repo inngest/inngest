db923978f18d3db814b13908d589d8fc2e00abe0
Bruno Scheufler <git@brunoscheufler.com>
2025-11-04 21:51:27 +0100

    wip key layout

--- pkg/constraintapi/lua/acquire.lua
+++ pkg/constraintapi/lua/acquire.lua
@@ -1,1 +1,29 @@
+--
+-- Acquire
+-- - checks constraint capacity given constraints and the current configuration
+-- -
+--
+
+local keyLeasesHash = KEYS[1]
+
+local requestDetails = ARGV[1]
+
+-- TODO: Handle operation idempotency
+
+-- TODO: Verify no far newer config was seen (reduce driftt)
+
+-- TODO: Handle constraint idempotency (do we need to skip GCRA?)
+
+-- TODO: Compute constraint capacity
+
+-- TODO: If missing capacity, exit early (return limiting constraint and details)
+
+-- TODO: Update constraint state with granted capacity
+
+-- TODO: Store request details, granted capacity
+
+-- TODO: Add lease to active leases for account (and account pointer to set to scavenger shard)
+
+-- TODO: Set operation idempotency key
+
 return 0
