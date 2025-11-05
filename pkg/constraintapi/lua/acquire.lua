920c495a86d9074d4a36852accbf3beb13733be2
Bruno Scheufler <git@brunoscheufler.com>
2025-11-05 11:03:40 +0100

    wip

--- pkg/constraintapi/lua/acquire.lua
+++ pkg/constraintapi/lua/acquire.lua
@@ -13,6 +13,13 @@ local accountScopedKeyPrefix = ARGV[1]
 
 local requestDetails = cjson.decode(ARGV[2])
 
+local requested = requestDetails.r
+local config = requestDetails.c
+local constraints = requestDetails.s
+
+local envID = requestDetails.e
+local functionID = requestDetails.f
+
 -- TODO: Handle operation idempotency (was this request seen before?)
 local opIdempotency = redis.call("GET", keyOperationIdempotency)
 if opIdempotency ~= nil and opIdempotency ~= false then
@@ -27,12 +34,23 @@ end
 -- TODO: Handle constraint idempotency (do we need to skip GCRA? only for single leases with valid idempotency)
 
 -- TODO: Compute constraint capacity
+local granted = requested
 
 -- TODO: If missing capacity, exit early (return limiting constraint and details)
+if granted == 0 then
+	return { 2 }
+end
 
 -- TODO: Update constraint state with granted capacity
 
--- TODO: Store request details, granted capacity
+-- Populate request details
+requestDetails.g = granted
+requestDetails.a = granted
+
+-- Store request details
+-- TODO: Should this have a TTL just in case? e.g. 24h?
+-- If we did this, we could not properly clean up some state, so maybe we should just trust the scavenger
+redis.call("SET", keyRequestState, cjson.encode(requestDetails))
 
 -- TODO: Bulk-Set lease idempotency keys: foreach lease: lease idempotency key -> leaseID without idempotency period (that is set on Release)
 
