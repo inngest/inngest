7919b4a3307cf67a6c3e0ef70372d21e4fec43e2
Dylan Dover <dylan@inngest.com>
2025-11-10 12:31:32 -0600

    add missing use client directive

--- ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsHelperPanel/features/SchemaExplorer/useSchemasInUse.ts
+++ ui/apps/dashboard/src/components/Insights/InsightsTabManager/InsightsHelperPanel/features/SchemaExplorer/useSchemasInUse.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { useEffect, useMemo, useState } from 'react';
 import useDebounce from '@inngest/components/hooks/useDebounce';
 import { useQueries, type UseQueryResult } from '@tanstack/react-query';
