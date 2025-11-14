cded81f6c5ceec2925d27c194f79c7af11b2ca92
Riley Wilburn <riley@inngest.com>
2025-11-14 13:13:31 -0500

    Expand trace view to fit min/max descendent span timestamps

--- ui/packages/components/src/RunDetailsV3/Timeline.tsx
+++ ui/packages/components/src/RunDetailsV3/Timeline.tsx
@@ -1,6 +1,9 @@
+import { max, min } from 'date-fns';
+
 import { toMaybeDate } from '../utils/date';
 import { isLazyDone, type Lazy } from '../utils/lazyLoad';
 import { Trace } from './Trace';
+import { traceWalk } from './utils';
 
 type Props = {
   runID: string;
@@ -13,8 +16,17 @@ export const Timeline = ({ runID, trace }: Props) => {
     return null;
   }
 
-  const minTime = new Date(trace.queuedAt);
-  const maxTime = toMaybeDate(trace.endedAt) ?? new Date();
+  let minTime = new Date(trace.queuedAt);
+  let maxTime = toMaybeDate(trace.endedAt) ?? new Date();
+
+  traceWalk(trace, (t) => {
+    minTime = min([minTime, new Date(t.queuedAt)]);
+
+    const endedAt = toMaybeDate(t.endedAt);
+    if (endedAt) {
+      maxTime = max([endedAt, maxTime]);
+    }
+  });
 
   return (
     <div className={`w-full pb-4 pr-8`}>
