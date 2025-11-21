dc7b8f504c8a6a53f49d458d3afe6f1f41e8999a
Riley Wilburn <riley@inngest.com>
2025-11-21 15:40:11 -0500

    better type param order

--- pkg/util/map.go
+++ pkg/util/map.go
@@ -1,6 +1,6 @@
 package util
 
-func ConvertStringishMap[M2 ~map[K2]V2, K1, K2 ~string, V1, V2 ~string | ~[]byte, M1 ~map[K1]V1](m1 M1) M2 {
+func ConvertStringishMap[M2 ~map[K2]V2, M1 ~map[K1]V1, K1, K2 ~string, V1, V2 ~string | ~[]byte](m1 M1) M2 {
 	m2 := make(M2, len(m1))
 	for k, v := range m1 {
 		m2[K2(k)] = V2(v)
