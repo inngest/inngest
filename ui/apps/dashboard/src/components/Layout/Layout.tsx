65d5ef58dbdb8f3ba851bb0a7b54be09da547fc9
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-11-22 13:49:29 -0500

    Disable banner hotfix

--- ui/apps/dashboard/src/components/Layout/Layout.tsx
+++ ui/apps/dashboard/src/components/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Suspense, type ReactNode } from 'react';
 import IncidentBanner from '@/app/(organization-active)/IncidentBanner';
 import { getNavCollapsed } from '@/app/actions';
 import { BillingBanner } from '@/components/BillingBanner';
-import { ExecutionOverageBanner } from '@/components/ExecutionOverage';
+// import { ExecutionOverageBanner } from '@/components/ExecutionOverage';
 import { getProfileDisplay } from '@/queries/server-only/profile';
 import type { Environment } from '@/utils/environments';
 import SideBar from './SideBar';
@@ -33,9 +33,7 @@ export default async function Layout({ activeEnv, children }: LayoutProps) {
           <BillingBanner />
         </Suspense>
 
-        <Suspense>
-          <ExecutionOverageBanner />
-        </Suspense>
+        <Suspense>{/* <ExecutionOverageBanner /> */}</Suspense>
 
         {children}
       </div>
