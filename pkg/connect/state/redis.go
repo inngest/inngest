490ef719e5a7485e4a8ee931743d50b1899e8055
mar-inngest <muzammil@inngest.com>
2025-11-25 13:07:05 -0800

    [SYS-370] - Total Capacity For Connect Workers Concurrency (#3304)
    * [SYS-370] - Metrics For Connect Workers Concurrency
    
    * Add a Max concurrency column in the UI
    
    * Add Int64 data type for graphql
    
    * Store Max Concurrency in the database
    
    * Finally the UI show correct workers
    
    * Adding Connection Metrics Lifecycle
    
    * Generate Volume Metrics
    
    * Only display UI if there is data
    
    * adding instance id
    
    * Remove UI Changes And Move Then To A Separate Pull
    
    * Moved More UI changes

--- pkg/connect/state/redis.go
+++ pkg/connect/state/redis.go
@@ -918,8 +918,8 @@ func (r *redisConnectionStateManager) GetWorkerCapacities(ctx context.Context, e
 	isWorkerCapacityUnlimited := totalCapacity == 0
 	if isWorkerCapacityUnlimited {
 		return &WorkerCapacity{
-			Total:     totalCapacity,
-			Available: consts.ConnectWorkerNoConcurrencyLimitForRequests,
+			Total:     consts.ConnectWorkerCapacityForNoConcurrencyLimit,
+			Available: consts.ConnectWorkerCapacityForNoConcurrencyLimit,
 		}, nil
 	}
 
