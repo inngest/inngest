235f290ef1e9517a6edb86268b450bc75303d0a3
Riley Wilburn <riley@inngest.com>
2025-11-26 11:29:34 -0500

    missing arg

--- pkg/execution/executor/executor.go
+++ pkg/execution/executor/executor.go
@@ -1515,8 +1515,8 @@ func (e *executor) Execute(ctx context.Context, id state.Identifier, item queue.
 		if e.allowStepMetadata.Enabled(ctx, instance.Metadata().ID.Tenant.AccountID) {
 			l := l.With("step_metadata", true)
 			for _, opcode := range resp.Generator {
-				for _, metadata := range opcode.Metadata {
-					if err := metadata.Validate(); err != nil {
+				for _, md := range opcode.Metadata {
+					if err := md.Validate(); err != nil {
 						l.Warn("invalid metadata in driver response", "error", err)
 						continue
 					}
@@ -1527,7 +1527,8 @@ func (e *executor) Execute(ctx context.Context, id state.Identifier, item queue.
 						ctx,
 						&instance,
 						"executor.ExecutePostMetadata",
-						metadata,
+						md,
+						md.Scope,
 					)
 					if err != nil {
 						l.Warn("error creating metadata span", "error", err)
