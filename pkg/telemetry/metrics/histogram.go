25685cc0461dc578a24c6bf605462e482fdf1cb5
Tony HB <tonyhb@gmail.com>
2025-12-04 08:17:11 -0800

    Update metrics internally for tracking

--- pkg/telemetry/metrics/histogram.go
+++ pkg/telemetry/metrics/histogram.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// in milliseconds
-	DefaultBoundaries          = []float64{10, 50, 100, 200, 500, 1000, 2000, 5000, 10000}
+	DefaultBoundaries          = []float64{10, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 30000, 60000}
 	QueueItemLatencyBoundaries = []float64{
 		5, 10, 50, 100, 200, 500, // < 1s
 		1000, 2000, 5000, 30_000, // < 1m
@@ -350,6 +350,17 @@ func HistogramConstraintAPIScavengerLeaseAge(ctx context.Context, age time.Durat
 		PkgName:     opts.PkgName,
 		MetricName:  "constraintapi_scavenger_shard_lease_age",
 		Description: "Distribution of scavenger expired lease age",
+		Tags:        opts.Tags,
+		Unit:        "ms",
+		Boundaries:  DefaultBoundaries,
+	})
+}
+
+func HistogramAsyncCheckpointStartLatency(ctx context.Context, age time.Duration, opts HistogramOpt) {
+	RecordIntHistogramMetric(ctx, age.Milliseconds(), HistogramOpt{
+		PkgName:     opts.PkgName,
+		MetricName:  "checkpoint_start_latency",
+		Description: "Distribution of time it took to receive the API request and start processing",
 		Tags:        opts.Tags,
 		Unit:        "ms",
 		Boundaries:  DefaultBoundaries,
