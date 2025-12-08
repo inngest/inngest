f98ae1685fa8693448d17350dcb8b97167cf4bf2
Bruno Scheufler <bruno.scheufler@gmail.com>
2025-12-08 15:00:03 +0100

    Revert "Use emission interval to compute variance instead of original period_…"
    This reverts commit 26a0eed2be7c580c5610903083560aac3091e61f.

--- pkg/execution/ratelimit/ratelimit_lua_test.go
+++ pkg/execution/ratelimit/ratelimit_lua_test.go
@@ -16,7 +16,7 @@ import (
 
 const prefix = "{rl}:"
 
-// initRedis creates miniredis/rueidis for Lua and fake clock
+// initRedis creates both miniredis/rueidis for Lua, throttled store, and fake clock
 func initRedis(t *testing.T) (*miniredis.Miniredis, rueidis.Client, clockwork.FakeClock) {
 	r := miniredis.RunT(t)
 
