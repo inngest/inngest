a8325f8813fd815feb256cefc4187b8ddb412e61
Bruno Scheufler <git@brunoscheufler.com>
2025-12-12 15:30:24 +0100

    fix gcra

--- pkg/execution/state/redis_state/gcra_ratelimit_test.go
+++ pkg/execution/state/redis_state/gcra_ratelimit_test.go
@@ -102,3 +102,69 @@ func TestBurstUsage(t *testing.T) {
 		})
 	}
 }
+
+func TestGCRA(t *testing.T) {
+	getThrottleState := func(t *testing.T, r *miniredis.Miniredis, key string) time.Time {
+		value, err := r.Get(key)
+		require.NoError(t, err)
+
+		parsed, err := strconv.Atoi(value)
+		require.NoError(t, err)
+
+		return time.UnixMilli(int64(parsed))
+	}
+
+	ctx := context.Background()
+
+	t.Run("limiting should work", func(t *testing.T) {
+		clock := clockwork.NewFakeClock()
+		r, rc := initRedis(t)
+		defer rc.Close()
+		key := "test:throttle:1"
+		period := 1 * time.Hour
+		limit := 1
+		burst := 0
+
+		// First request should succeed (not be limited)
+		limited, usedBurst, err := oldLuaGCRARateLimit(ctx, rc, key, clock.Now(), period, limit, burst)
+		require.NoError(t, err)
+		require.False(t, limited, "first request should not be limited")
+		require.False(t, usedBurst)
+
+		require.WithinDuration(t, clock.Now().Add(1*time.Hour), getThrottleState(t, r, key), time.Second)
+
+		// Second request should be limited
+		limited, usedBurst, err = oldLuaGCRARateLimit(ctx, rc, key, clock.Now(), period, limit, burst)
+		require.NoError(t, err)
+		require.True(t, limited)
+		require.False(t, usedBurst)
+	})
+
+	// NOTE: This asserts we retain high throughput in case the arrival rate of requests is non-uniform
+	// This essentially validates that we disable request rate smoothing by default
+	t.Run("should allow using up entire limit at once", func(t *testing.T) {
+		clock := clockwork.NewFakeClock()
+		r, rc := initRedis(t)
+		defer rc.Close()
+		key := "test:throttle:1"
+		period := 1 * time.Hour
+		limit := 5
+		burst := 0
+
+		// Should be able to use up limit AT ONCE
+		for i := range limit {
+			limited, usedBurst, err := oldLuaGCRARateLimit(ctx, rc, key, clock.Now(), period, limit, burst)
+			require.NoError(t, err)
+			require.False(t, limited, "%d should not be limited", i)
+			require.True(t, (i == 0 && !usedBurst) || (i > 0 && usedBurst))
+		}
+
+		require.WithinDuration(t, clock.Now().Add(1*time.Hour), getThrottleState(t, r, key), time.Second)
+
+		// Follow-up request should be limited
+		limited, usedBurst, err := oldLuaGCRARateLimit(ctx, rc, key, clock.Now(), period, limit, burst)
+		require.NoError(t, err)
+		require.True(t, limited)
+		require.False(t, usedBurst)
+	})
+}
