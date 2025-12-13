b78e77a381bbd40db66f802b366e1bcc390866c0
Jacob Heric <jacob@jacobheric.com>
2025-12-13 16:38:52 -0500

    fix broken sign out button

--- ui/apps/dashboard/src/routes/(auth)/sign-out.tsx
+++ ui/apps/dashboard/src/routes/(auth)/sign-out.tsx
@@ -1,24 +1,17 @@
+import { SignOutButton } from '@/components/Auth/SignOutButton';
 import SplitView from '@/components/SignIn/SplitView';
-import { SignOutButton } from '@clerk/tanstack-react-start';
-import { createFileRoute, useNavigate } from '@tanstack/react-router';
+
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/(auth)/sign-out')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const navigate = useNavigate();
-
   return (
     <SplitView>
       <div className="flex flex-row items-center justify-center">
-        <SignOutButton
-          signOutCallback={() => {
-            //
-            // Ensure proper cleanup by navigating to sign-in after sign-out completes
-            navigate({ to: '/sign-in', replace: true });
-          }}
-        />
+        <SignOutButton />
       </div>
     </SplitView>
   );
