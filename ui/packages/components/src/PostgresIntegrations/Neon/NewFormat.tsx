7c68b90783956dc9999593f46e12ce462123d5a6
Jacob Heric <jacob@jacobheric.com>
2025-12-15 13:44:27 -0500

    use tanstack route button/link to not href to prevent total reload

--- ui/packages/components/src/PostgresIntegrations/Neon/NewFormat.tsx
+++ ui/packages/components/src/PostgresIntegrations/Neon/NewFormat.tsx
@@ -97,7 +97,7 @@ export default function NeonFormat({
       {isVerified ? (
         <Button
           label="Next"
-          href={`/settings/integrations/${integration}/${IntegrationSteps.ConnectDb}`}
+          to={`/settings/integrations/${integration}/${IntegrationSteps.ConnectDb}`}
         />
       ) : (
         <Button
