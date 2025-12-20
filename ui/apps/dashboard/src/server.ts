922202d2f5f8a602da2df1d98de595ffec4f4d63
Jacob Heric <jacob@jacobheric.com>
2025-12-20 08:36:03 -0500

    server side env vars

--- ui/apps/dashboard/src/server.ts
+++ ui/apps/dashboard/src/server.ts
@@ -5,8 +5,8 @@ const dsn = process.env.VITE_SENTRY_DSN;
 console.log('server side sentry dsn', dsn);
 
 if (dsn) {
-  const release = process.env.VITE_VERCEL_GIT_COMMIT_SHA;
-  const environment = process.env.VITE_VERCEL_ENV;
+  const release = process.env.VERCEL_GIT_COMMIT_SHA;
+  const environment = process.env.VERCEL_ENV;
 
   console.log('server side sentry release', release);
   console.log('server side sentry environment', environment);
