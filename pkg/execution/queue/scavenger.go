8a4e21157281af349bae1ac14ec35dda8aeaa809
Bruno Scheufler <git@brunoscheufler.com>
2025-12-24 16:57:34 +0100

    wip

--- pkg/execution/queue/scavenger.go
+++ pkg/execution/queue/scavenger.go
@@ -2,6 +2,7 @@ package queue
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	"github.com/inngest/inngest/pkg/telemetry/metrics"
@@ -68,4 +69,18 @@ func (q *queueProcessor) runScavenger(ctx context.Context) {
 			q.scavengerLeaseLock.Unlock()
 		}
 	}
+}
+
+func RandomScavengeOffset(seed int64, count int64, limit int) int64 {
+	// only apply random offset if there are more total items to scavenge than the limit
+	if count > int64(limit) {
+		r := rand.New(rand.NewSource(seed))
+
+		// the result of count-limit must be greater than 0 as we have already checked count > limit
+		// we increase the argument by 1 to make the highest possible index accessible
+		// example: for count = 9, limit = 3, we want to access indices 0 through 6, not 0 through 5
+		return r.Int63n(count - int64(limit) + 1)
+	}
+
+	return 0
 }
