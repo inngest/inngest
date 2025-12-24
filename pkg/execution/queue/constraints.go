62325048db6c4e02333cdca6d089f4be62e19b29
Bruno Scheufler <git@brunoscheufler.com>
2025-12-24 03:16:38 +0100

    wip

--- pkg/execution/queue/constraints.go
+++ pkg/execution/queue/constraints.go
@@ -1,6 +1,11 @@
 package queue
 
-import "github.com/inngest/inngest/pkg/enums"
+import (
+	"time"
+
+	"github.com/inngest/inngest/pkg/constraintapi"
+	"github.com/inngest/inngest/pkg/enums"
+)
 
 type PartitionConstraintConfig struct {
 	FunctionVersion int `json:"fv,omitempty,omitzero"`
@@ -49,4 +54,85 @@ type PartitionConcurrency struct {
 	// Up to two custom concurrency keys on user-defined scopes, optionally specifying a key. The key is required
 	// on env or account level scopes.
 	CustomConcurrencyKeys []CustomConcurrencyLimit `json:"cck,omitempty"`
+}
+
+type BacklogRefillConstraintCheckResult struct {
+	ItemsToRefill        []string
+	ItemCapacityLeases   []CapacityLease
+	LimitingConstraint   enums.QueueConstraint
+	SkipConstraintChecks bool
+
+	RetryAfter time.Time
+}
+
+func ConvertLimitingConstraint(
+	constraints PartitionConstraintConfig,
+	limitingConstraints []constraintapi.ConstraintItem,
+) enums.QueueConstraint {
+	constraint := enums.QueueConstraintNotLimited
+
+	for _, c := range limitingConstraints {
+		switch {
+		// Account concurrency
+		case
+			c.Kind == constraintapi.ConstraintKindConcurrency &&
+				c.Concurrency.Scope == enums.ConcurrencyScopeAccount &&
+				c.Concurrency.KeyExpressionHash == "":
+			constraint = enums.QueueConstraintAccountConcurrency
+
+		// Function concurrency
+		case
+			c.Kind == constraintapi.ConstraintKindConcurrency &&
+				c.Concurrency.Scope == enums.ConcurrencyScopeFn &&
+				c.Concurrency.KeyExpressionHash == "":
+			constraint = enums.QueueConstraintFunctionConcurrency
+
+		// Custom concurrency key 1
+		case
+			len(constraints.Concurrency.CustomConcurrencyKeys) > 0 &&
+				c.Kind == constraintapi.ConstraintKindConcurrency &&
+				c.Concurrency.Mode == constraints.Concurrency.CustomConcurrencyKeys[0].Mode &&
+				c.Concurrency.Scope == constraints.Concurrency.CustomConcurrencyKeys[0].Scope &&
+				c.Concurrency.KeyExpressionHash == constraints.Concurrency.CustomConcurrencyKeys[0].HashedKeyExpression:
+			constraint = enums.QueueConstraintCustomConcurrencyKey1
+
+		// Custom concurrency key 2
+		case
+			len(constraints.Concurrency.CustomConcurrencyKeys) > 1 &&
+				c.Kind == constraintapi.ConstraintKindConcurrency &&
+				c.Concurrency.Mode == constraints.Concurrency.CustomConcurrencyKeys[1].Mode &&
+				c.Concurrency.Scope == constraints.Concurrency.CustomConcurrencyKeys[1].Scope &&
+				c.Concurrency.KeyExpressionHash == constraints.Concurrency.CustomConcurrencyKeys[1].HashedKeyExpression:
+			constraint = enums.QueueConstraintCustomConcurrencyKey2
+
+		// Throttle
+		case
+			c.Kind == constraintapi.ConstraintKindThrottle:
+			constraint = enums.QueueConstraintThrottle
+		}
+	}
+
+	return constraint
+}
+
+type ItemLeaseConstraintCheckResult struct {
+	// capacityLease optionally returns a capacity lease ID which
+	// must be passed to the processing function to be extended
+	// while processing the item.
+	CapacityLease *CapacityLease
+
+	// limitingConstraint returns the most limiting constraint in case
+	// no capacity was available.
+	LimitingConstraint enums.QueueConstraint
+
+	// skipConstraintChecks determines whether subsequent operations
+	// should check and enforce constraints, and whether constraint state
+	// should be updated while processing a queue item.
+	//
+	// When enrolled to the Constraint API and holding a valid capacity lease,
+	// constraint checks _and_ updates may be skipped, as state is maintained within
+	// the Constraint API.
+	SkipConstraintChecks bool
+
+	RetryAfter time.Time
 }
