74f8f20357af91b2229bec082e94c5ca20bbb0ea
Riley Wilburn <riley@inngest.com>
2026-01-07 11:27:34 -0500

    css tweaks

--- ui/packages/components/src/RunDetailsV3/MetadataAttrs.tsx
+++ ui/packages/components/src/RunDetailsV3/MetadataAttrs.tsx
@@ -55,13 +55,13 @@ const MetadataAttrRow = ({
         } mt-2 flex max-h-full flex-col gap-2`}
       >
         <div className="text-muted bg-canvasSubtle sticky top-0 flex flex-row px-4 py-2 text-sm font-medium leading-tight">
-          <div className="w-72">Key</div>
+          <div className="w-48">Key</div>
           <div className="">Value</div>
         </div>
         {Object.entries(values).map(([key, value]) => {
           return (
-            <div key={`metadata-attr-${key}`} className={`pb-2'} flex flex-row items-center px-4`}>
-              <div className="text-muted w-72 text-sm font-normal leading-tight">{key}</div>
+            <div key={`metadata-attr-${key}`} className="flex flex-row items-center px-4 pb-2">
+              <div className="text-muted w-48 text-sm font-normal leading-tight">{key}</div>
               <div className="text-basis truncate text-sm font-normal leading-tight">
                 {String(value) || '--'}
               </div>
