236633db08d65f8a8155a64b7f1b0552f1c0a86a
Riadh <22998716+KiKoS0@users.noreply.github.com>
2026-01-20 19:38:50 +0100

    smaller pause payload as we don't need the whole struct for now

--- proto/gen/state/v2/state.pb.go
+++ proto/gen/state/v2/state.pb.go
@@ -1524,31 +1524,16 @@ func (x *PauseIdentifier) GetAccountId() string {
 	return ""
 }
 
+// Pause contains only the fields needed to consume a pause.
+// Other metadata is ignored.
 type Pause struct {
-	state               protoimpl.MessageState `protogen:"open.v1"`
-	Id                  string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-	WorkspaceId         string                 `protobuf:"bytes,2,opt,name=workspace_id,json=workspaceId,proto3" json:"workspace_id,omitempty"`
-	Identifier          *PauseIdentifier       `protobuf:"bytes,3,opt,name=identifier,proto3" json:"identifier,omitempty"`
-	Outgoing            string                 `protobuf:"bytes,4,opt,name=outgoing,proto3" json:"outgoing,omitempty"`
-	Incoming            string                 `protobuf:"bytes,5,opt,name=incoming,proto3" json:"incoming,omitempty"`
-	StepName            string                 `protobuf:"bytes,6,opt,name=step_name,json=stepName,proto3" json:"step_name,omitempty"`
-	Opcode              *string                `protobuf:"bytes,7,opt,name=opcode,proto3,oneof" json:"opcode,omitempty"`
-	Expires             *timestamppb.Timestamp `protobuf:"bytes,8,opt,name=expires,proto3" json:"expires,omitempty"`
-	Event               *string                `protobuf:"bytes,9,opt,name=event,proto3,oneof" json:"event,omitempty"`
-	Expression          *string                `protobuf:"bytes,10,opt,name=expression,proto3,oneof" json:"expression,omitempty"`
-	InvokeCorrelationId *string                `protobuf:"bytes,11,opt,name=invoke_correlation_id,json=invokeCorrelationId,proto3,oneof" json:"invoke_correlation_id,omitempty"`
-	InvokeTargetFnId    *string                `protobuf:"bytes,12,opt,name=invoke_target_fn_id,json=invokeTargetFnId,proto3,oneof" json:"invoke_target_fn_id,omitempty"`
-	SignalId            *string                `protobuf:"bytes,13,opt,name=signal_id,json=signalId,proto3,oneof" json:"signal_id,omitempty"`
-	OnTimeout           bool                   `protobuf:"varint,14,opt,name=on_timeout,json=onTimeout,proto3" json:"on_timeout,omitempty"`
-	DataKey             string                 `protobuf:"bytes,15,opt,name=data_key,json=dataKey,proto3" json:"data_key,omitempty"`
-	Cancel              bool                   `protobuf:"varint,16,opt,name=cancel,proto3" json:"cancel,omitempty"`
-	MaxAttempts         *int32                 `protobuf:"varint,17,opt,name=max_attempts,json=maxAttempts,proto3,oneof" json:"max_attempts,omitempty"`
-	GroupId             string                 `protobuf:"bytes,18,opt,name=group_id,json=groupId,proto3" json:"group_id,omitempty"`
-	TriggeringEventId   *string                `protobuf:"bytes,19,opt,name=triggering_event_id,json=triggeringEventId,proto3,oneof" json:"triggering_event_id,omitempty"`
-	ParallelMode        int32                  `protobuf:"varint,20,opt,name=parallel_mode,json=parallelMode,proto3" json:"parallel_mode,omitempty"`
-	CreatedAt           *timestamppb.Timestamp `protobuf:"bytes,21,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
-	unknownFields       protoimpl.UnknownFields
-	sizeCache           protoimpl.SizeCache
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Id            string                 `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	WorkspaceId   string                 `protobuf:"bytes,2,opt,name=workspace_id,json=workspaceId,proto3" json:"workspace_id,omitempty"`
+	Identifier    *PauseIdentifier       `protobuf:"bytes,3,opt,name=identifier,proto3" json:"identifier,omitempty"`
+	DataKey       string                 `protobuf:"bytes,4,opt,name=data_key,json=dataKey,proto3" json:"data_key,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
 }
 
 func (x *Pause) Reset() {
@@ -1602,130 +1587,11 @@ func (x *Pause) GetIdentifier() *PauseIdentifier {
 	return nil
 }
 
-func (x *Pause) GetOutgoing() string {
-	if x != nil {
-		return x.Outgoing
-	}
-	return ""
-}
-
-func (x *Pause) GetIncoming() string {
-	if x != nil {
-		return x.Incoming
-	}
-	return ""
-}
-
-func (x *Pause) GetStepName() string {
-	if x != nil {
-		return x.StepName
-	}
-	return ""
-}
-
-func (x *Pause) GetOpcode() string {
-	if x != nil && x.Opcode != nil {
-		return *x.Opcode
-	}
-	return ""
-}
-
-func (x *Pause) GetExpires() *timestamppb.Timestamp {
-	if x != nil {
-		return x.Expires
-	}
-	return nil
-}
-
-func (x *Pause) GetEvent() string {
-	if x != nil && x.Event != nil {
-		return *x.Event
-	}
-	return ""
-}
-
-func (x *Pause) GetExpression() string {
-	if x != nil && x.Expression != nil {
-		return *x.Expression
-	}
-	return ""
-}
-
-func (x *Pause) GetInvokeCorrelationId() string {
-	if x != nil && x.InvokeCorrelationId != nil {
-		return *x.InvokeCorrelationId
-	}
-	return ""
-}
-
-func (x *Pause) GetInvokeTargetFnId() string {
-	if x != nil && x.InvokeTargetFnId != nil {
-		return *x.InvokeTargetFnId
-	}
-	return ""
-}
-
-func (x *Pause) GetSignalId() string {
-	if x != nil && x.SignalId != nil {
-		return *x.SignalId
-	}
-	return ""
-}
-
-func (x *Pause) GetOnTimeout() bool {
-	if x != nil {
-		return x.OnTimeout
-	}
-	return false
-}
-
 func (x *Pause) GetDataKey() string {
 	if x != nil {
 		return x.DataKey
 	}
 	return ""
-}
-
-func (x *Pause) GetCancel() bool {
-	if x != nil {
-		return x.Cancel
-	}
-	return false
-}
-
-func (x *Pause) GetMaxAttempts() int32 {
-	if x != nil && x.MaxAttempts != nil {
-		return *x.MaxAttempts
-	}
-	return 0
-}
-
-func (x *Pause) GetGroupId() string {
-	if x != nil {
-		return x.GroupId
-	}
-	return ""
-}
-
-func (x *Pause) GetTriggeringEventId() string {
-	if x != nil && x.TriggeringEventId != nil {
-		return *x.TriggeringEventId
-	}
-	return ""
-}
-
-func (x *Pause) GetParallelMode() int32 {
-	if x != nil {
-		return x.ParallelMode
-	}
-	return 0
-}
-
-func (x *Pause) GetCreatedAt() *timestamppb.Timestamp {
-	if x != nil {
-		return x.CreatedAt
-	}
-	return nil
 }
 
 type ConsumePauseOpts struct {
@@ -2052,45 +1918,14 @@ const file_state_v2_state_proto_rawDesc = "" +
 	"\vfunction_id\x18\x02 \x01(\tR\n" +
 	"functionId\x12\x1d\n" +
 	"\n" +
-	"account_id\x18\x03 \x01(\tR\taccountId\"\xa3\a\n" +
+	"account_id\x18\x03 \x01(\tR\taccountId\"\x90\x01\n" +
 	"\x05Pause\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\tR\x02id\x12!\n" +
 	"\fworkspace_id\x18\x02 \x01(\tR\vworkspaceId\x129\n" +
 	"\n" +
 	"identifier\x18\x03 \x01(\v2\x19.state.v2.PauseIdentifierR\n" +
-	"identifier\x12\x1a\n" +
-	"\boutgoing\x18\x04 \x01(\tR\boutgoing\x12\x1a\n" +
-	"\bincoming\x18\x05 \x01(\tR\bincoming\x12\x1b\n" +
-	"\tstep_name\x18\x06 \x01(\tR\bstepName\x12\x1b\n" +
-	"\x06opcode\x18\a \x01(\tH\x00R\x06opcode\x88\x01\x01\x124\n" +
-	"\aexpires\x18\b \x01(\v2\x1a.google.protobuf.TimestampR\aexpires\x12\x19\n" +
-	"\x05event\x18\t \x01(\tH\x01R\x05event\x88\x01\x01\x12#\n" +
-	"\n" +
-	"expression\x18\n" +
-	" \x01(\tH\x02R\n" +
-	"expression\x88\x01\x01\x127\n" +
-	"\x15invoke_correlation_id\x18\v \x01(\tH\x03R\x13invokeCorrelationId\x88\x01\x01\x122\n" +
-	"\x13invoke_target_fn_id\x18\f \x01(\tH\x04R\x10invokeTargetFnId\x88\x01\x01\x12 \n" +
-	"\tsignal_id\x18\r \x01(\tH\x05R\bsignalId\x88\x01\x01\x12\x1d\n" +
-	"\n" +
-	"on_timeout\x18\x0e \x01(\bR\tonTimeout\x12\x19\n" +
-	"\bdata_key\x18\x0f \x01(\tR\adataKey\x12\x16\n" +
-	"\x06cancel\x18\x10 \x01(\bR\x06cancel\x12&\n" +
-	"\fmax_attempts\x18\x11 \x01(\x05H\x06R\vmaxAttempts\x88\x01\x01\x12\x19\n" +
-	"\bgroup_id\x18\x12 \x01(\tR\agroupId\x123\n" +
-	"\x13triggering_event_id\x18\x13 \x01(\tH\aR\x11triggeringEventId\x88\x01\x01\x12#\n" +
-	"\rparallel_mode\x18\x14 \x01(\x05R\fparallelMode\x129\n" +
-	"\n" +
-	"created_at\x18\x15 \x01(\v2\x1a.google.protobuf.TimestampR\tcreatedAtB\t\n" +
-	"\a_opcodeB\b\n" +
-	"\x06_eventB\r\n" +
-	"\v_expressionB\x18\n" +
-	"\x16_invoke_correlation_idB\x16\n" +
-	"\x14_invoke_target_fn_idB\f\n" +
-	"\n" +
-	"_signal_idB\x0f\n" +
-	"\r_max_attemptsB\x16\n" +
-	"\x14_triggering_event_id\"h\n" +
+	"identifier\x12\x19\n" +
+	"\bdata_key\x18\x04 \x01(\tR\adataKey\"O\n" +
 	"\x10ConsumePauseOpts\x12'\n" +
 	"\x0fidempotency_key\x18\x01 \x01(\tR\x0eidempotencyKey\x12\x12\n" +
 	"\x04data\x18\x02 \x01(\fR\x04data\"a\n" +
@@ -2196,39 +2031,36 @@ var file_state_v2_state_proto_depIdxs = []int32{
 	0,  // 22: state.v2.LoadStateResponse.metadata:type_name -> state.v2.Metadata
 	34, // 23: state.v2.LoadStateResponse.steps:type_name -> state.v2.LoadStateResponse.StepsEntry
 	26, // 24: state.v2.Pause.identifier:type_name -> state.v2.PauseIdentifier
-	35, // 25: state.v2.Pause.expires:type_name -> google.protobuf.Timestamp
-	35, // 26: state.v2.Pause.created_at:type_name -> google.protobuf.Timestamp
-	36, // 27: state.v2.ConsumePauseOpts.data:type_name -> google.protobuf.Struct
-	27, // 28: state.v2.ConsumePauseRequest.pause:type_name -> state.v2.Pause
-	28, // 29: state.v2.ConsumePauseRequest.opts:type_name -> state.v2.ConsumePauseOpts
-	29, // 30: state.v2.ConsumePauseResponse.result:type_name -> state.v2.ConsumePauseResult
-	6,  // 31: state.v2.RunService.Create:input_type -> state.v2.CreateStateRequest
-	8,  // 32: state.v2.RunService.Delete:input_type -> state.v2.DeleteStateRequest
-	18, // 33: state.v2.RunService.Exists:input_type -> state.v2.ExistsRequest
-	12, // 34: state.v2.RunService.UpdateMetadata:input_type -> state.v2.UpdateMetadataRequest
-	14, // 35: state.v2.RunService.SaveStep:input_type -> state.v2.SaveStepRequest
-	16, // 36: state.v2.RunService.SavePending:input_type -> state.v2.SavePendingRequest
-	30, // 37: state.v2.RunService.ConsumePause:input_type -> state.v2.ConsumePauseRequest
-	10, // 38: state.v2.RunService.LoadMetadata:input_type -> state.v2.LoadMetadataRequest
-	20, // 39: state.v2.RunService.LoadEvents:input_type -> state.v2.LoadEventsRequest
-	22, // 40: state.v2.RunService.LoadSteps:input_type -> state.v2.LoadStepsRequest
-	24, // 41: state.v2.RunService.LoadState:input_type -> state.v2.LoadStateRequest
-	7,  // 42: state.v2.RunService.Create:output_type -> state.v2.CreateStateResponse
-	9,  // 43: state.v2.RunService.Delete:output_type -> state.v2.DeleteStateResponse
-	19, // 44: state.v2.RunService.Exists:output_type -> state.v2.ExistsResponse
-	13, // 45: state.v2.RunService.UpdateMetadata:output_type -> state.v2.UpdateMetadataResponse
-	15, // 46: state.v2.RunService.SaveStep:output_type -> state.v2.SaveStepResponse
-	17, // 47: state.v2.RunService.SavePending:output_type -> state.v2.SavePendingResponse
-	31, // 48: state.v2.RunService.ConsumePause:output_type -> state.v2.ConsumePauseResponse
-	11, // 49: state.v2.RunService.LoadMetadata:output_type -> state.v2.LoadMetadataResponse
-	21, // 50: state.v2.RunService.LoadEvents:output_type -> state.v2.LoadEventsResponse
-	23, // 51: state.v2.RunService.LoadSteps:output_type -> state.v2.LoadStepsResponse
-	25, // 52: state.v2.RunService.LoadState:output_type -> state.v2.LoadStateResponse
-	42, // [42:53] is the sub-list for method output_type
-	31, // [31:42] is the sub-list for method input_type
-	31, // [31:31] is the sub-list for extension type_name
-	31, // [31:31] is the sub-list for extension extendee
-	0,  // [0:31] is the sub-list for field type_name
+	27, // 25: state.v2.ConsumePauseRequest.pause:type_name -> state.v2.Pause
+	28, // 26: state.v2.ConsumePauseRequest.opts:type_name -> state.v2.ConsumePauseOpts
+	29, // 27: state.v2.ConsumePauseResponse.result:type_name -> state.v2.ConsumePauseResult
+	6,  // 28: state.v2.RunService.Create:input_type -> state.v2.CreateStateRequest
+	8,  // 29: state.v2.RunService.Delete:input_type -> state.v2.DeleteStateRequest
+	18, // 30: state.v2.RunService.Exists:input_type -> state.v2.ExistsRequest
+	12, // 31: state.v2.RunService.UpdateMetadata:input_type -> state.v2.UpdateMetadataRequest
+	14, // 32: state.v2.RunService.SaveStep:input_type -> state.v2.SaveStepRequest
+	16, // 33: state.v2.RunService.SavePending:input_type -> state.v2.SavePendingRequest
+	30, // 34: state.v2.RunService.ConsumePause:input_type -> state.v2.ConsumePauseRequest
+	10, // 35: state.v2.RunService.LoadMetadata:input_type -> state.v2.LoadMetadataRequest
+	20, // 36: state.v2.RunService.LoadEvents:input_type -> state.v2.LoadEventsRequest
+	22, // 37: state.v2.RunService.LoadSteps:input_type -> state.v2.LoadStepsRequest
+	24, // 38: state.v2.RunService.LoadState:input_type -> state.v2.LoadStateRequest
+	7,  // 39: state.v2.RunService.Create:output_type -> state.v2.CreateStateResponse
+	9,  // 40: state.v2.RunService.Delete:output_type -> state.v2.DeleteStateResponse
+	19, // 41: state.v2.RunService.Exists:output_type -> state.v2.ExistsResponse
+	13, // 42: state.v2.RunService.UpdateMetadata:output_type -> state.v2.UpdateMetadataResponse
+	15, // 43: state.v2.RunService.SaveStep:output_type -> state.v2.SaveStepResponse
+	17, // 44: state.v2.RunService.SavePending:output_type -> state.v2.SavePendingResponse
+	31, // 45: state.v2.RunService.ConsumePause:output_type -> state.v2.ConsumePauseResponse
+	11, // 46: state.v2.RunService.LoadMetadata:output_type -> state.v2.LoadMetadataResponse
+	21, // 47: state.v2.RunService.LoadEvents:output_type -> state.v2.LoadEventsResponse
+	23, // 48: state.v2.RunService.LoadSteps:output_type -> state.v2.LoadStepsResponse
+	25, // 49: state.v2.RunService.LoadState:output_type -> state.v2.LoadStateResponse
+	39, // [39:50] is the sub-list for method output_type
+	28, // [28:39] is the sub-list for method input_type
+	28, // [28:28] is the sub-list for extension type_name
+	28, // [28:28] is the sub-list for extension extendee
+	0,  // [0:28] is the sub-list for field type_name
 }
 
 func init() { file_state_v2_state_proto_init() }
@@ -2237,7 +2069,6 @@ func file_state_v2_state_proto_init() {
 		return
 	}
 	file_state_v2_state_proto_msgTypes[2].OneofWrappers = []any{}
-	file_state_v2_state_proto_msgTypes[27].OneofWrappers = []any{}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
