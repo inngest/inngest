4bdb5ae1b03e6ea15dcfedab894129996e722c58
Jacob Heric <jacob@jacobheric.com>
2026-01-27 14:56:09 -0800

    remake protos

--- proto/gen/api/v2/service.pb.go
+++ proto/gen/api/v2/service.pb.go
@@ -7,15 +7,14 @@
 package apiv2
 
 import (
-	reflect "reflect"
-	sync "sync"
-	unsafe "unsafe"
-
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2/options"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
