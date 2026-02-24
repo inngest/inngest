8912a49a183453f963feae8852457bc692711e34
Tony HB <tonyhb@gmail.com>
2026-02-24 13:15:41 -0800

    comments n ting

--- pkg/logger/optlog.go
+++ pkg/logger/optlog.go
@@ -4,10 +4,12 @@ import (
 	"github.com/google/uuid"
 )
 
+// LogEnabler is a function type that will only allow logs for the given account/log name
+// on a truthy return value.
 type LogEnabler func(acctID uuid.UUID, logname string) bool
 
+// DefaultLogEnabler always disables all optional logging.  Update this package variable
+// with your own [LogEnabler] to allow optional logging.
 var DefaultLogEnabler = func(acctID uuid.UUID, logname string) bool {
 	return false
 }
-
-func ShouldLog(acctID uuid.UUID, logname string)
