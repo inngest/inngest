1bb4e3b9de376e432f9e6e375b0306bda35a8d78
Bruno Scheufler <git@brunoscheufler.com>
2026-03-16 13:39:15 +0100

    wip

--- pkg/execution/state/redis_state/queue_scavenge_test.go
+++ pkg/execution/state/redis_state/queue_scavenge_test.go
@@ -1216,7 +1216,7 @@ func TestQueueScavenge(t *testing.T) {
 		require.Equal(t, 0, item.ScavengeCount)
 
 		// Lease the item and let it expire
-		_, err = shard.Lease(ctx, item, 5*time.Second, clock.Now(), nil, osqueue.LeaseOptionDisableConstraintChecks(false))
+		_, err = shard.Lease(ctx, item, 5*time.Second, clock.Now(), nil)
 		require.NoError(t, err)
 
 		clock.Advance(6 * time.Second)
@@ -1233,7 +1233,7 @@ func TestQueueScavenge(t *testing.T) {
 		require.Equal(t, 1, requeued.ScavengeCount)
 
 		// Lease and expire again to verify accumulation
-		_, err = shard.Lease(ctx, *requeued, 5*time.Second, clock.Now(), nil, osqueue.LeaseOptionDisableConstraintChecks(false))
+		_, err = shard.Lease(ctx, *requeued, 5*time.Second, clock.Now(), nil)
 		require.NoError(t, err)
 
 		clock.Advance(6 * time.Second)
