8197e84472aa8f213b4a07caadcadea057569ddb
James Riley Wilburn <riley@inngest.com>
2026-03-25 17:17:51 -0400

    Extended Traces: Handle step attributes in userland spans (#3884)

--- pkg/cqrs/base_cqrs/sqlc/postgres/queries.sql.go
+++ pkg/cqrs/base_cqrs/sqlc/postgres/queries.sql.go
@@ -548,7 +548,7 @@ SELECT
     'input_span_id', CASE WHEN input IS NOT NULL THEN span_id ELSE NULL END
   )) AS span_fragments
 FROM spans
-WHERE run_id = CAST($1 AS CHAR(26)) AND account_id = $2
+WHERE run_id = CAST($1 AS CHAR(26)) AND account_id = $2 AND name != 'userland'
 GROUP BY dynamic_span_id, run_id, trace_id, parent_span_id
 HAVING
   SUM(((attributes#>>'{}')::json->>'_inngest.step.id' = $3::text)::int) > 0
@@ -1016,7 +1016,7 @@ SELECT
     'input_span_id', CASE WHEN input IS NOT NULL THEN span_id ELSE NULL END
   )) AS span_fragments
 FROM spans b
-WHERE b.run_id = CAST($1 AS CHAR(26)) AND b.account_id = $2
+WHERE b.run_id = CAST($1 AS CHAR(26)) AND b.account_id = $2 AND b.name != 'userland'
 GROUP BY dynamic_span_id, run_id, trace_id, parent_span_id
 HAVING
   SUM(((attributes#>>'{}')::json->>'_inngest.step.id' = $3::text)::int) > 0
@@ -1490,7 +1490,7 @@ WHERE span_id IN (
   SELECT
     parent_span_id
   FROM spans execSpans
-  WHERE execSpans.run_id = CAST($1 AS CHAR(26)) AND execSpans.account_id = $2
+  WHERE execSpans.run_id = CAST($1 AS CHAR(26)) AND execSpans.account_id = $2 AND name != 'userland'
   GROUP BY dynamic_span_id, parent_span_id
   HAVING
     SUM(((attributes#>>'{}')::json->>'_inngest.step.id' = $3::text)::int) > 0
@@ -1498,7 +1498,7 @@ WHERE span_id IN (
     SUM((name = 'executor.execution')::int) > 0
   ORDER BY MIN(start_time)
   LIMIT 1
-)
+) AND name != 'userland'
 GROUP BY dynamic_span_id, run_id, trace_id, parent_span_id
 HAVING SUM((name = 'executor.step.discovery')::int) > 0
 UNION ALL
@@ -1517,7 +1517,7 @@ SELECT
     'input_span_id', CASE WHEN input IS NOT NULL THEN span_id ELSE NULL END
   )) AS span_fragments
 FROM spans
-WHERE run_id = CAST($1 AS CHAR(26)) AND account_id = $2
+WHERE run_id = CAST($1 AS CHAR(26)) AND account_id = $2 AND name != 'userland'
 GROUP BY dynamic_span_id, run_id, trace_id, parent_span_id
 HAVING
   SUM(((attributes#>>'{}')::json->>'_inngest.step.id' = $3::text)::int) > 0
