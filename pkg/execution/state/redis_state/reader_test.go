84ad0a5a7c6e42c5ac589836a7d862f5cf7d7c8f
Bruno Scheufler <git@brunoscheufler.com>
2026-04-01 14:05:34 +0200

    Potentially fix flaky test

--- pkg/execution/state/redis_state/reader_test.go
+++ pkg/execution/state/redis_state/reader_test.go
@@ -906,7 +906,7 @@ func TestItemsByBacklog(t *testing.T) {
 	defer rc.Close()
 
 	ctx := context.Background()
-	clock := clockwork.NewFakeClock()
+	clock := clockwork.NewFakeClockAt(time.Now().Truncate(time.Minute))
 
 	acctId, fnID, wsID := uuid.New(), uuid.New(), uuid.New()
 
@@ -949,7 +949,7 @@ func TestItemsByBacklog(t *testing.T) {
 			from:          clock.Now(),
 			until:         clock.Now().Add(7 * time.Second).Truncate(time.Second),
 			interval:      time.Second,
-			expectedItems: 7,
+			expectedItems: 8,
 		},
 		{
 			name:          "with batch size",
