7a4a15b787923f63f72faaca4a9bb4e265aecc2d
Tony HB <tonyhb@gmail.com>
2026-04-20 13:21:06 -0700

    dump updates

--- pkg/db/postgres/schema.sql
+++ pkg/db/postgres/schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.11
--- Dumped by pg_dump version 16.11
+-- Dumped from database version 16.13
+-- Dumped by pg_dump version 16.13
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
