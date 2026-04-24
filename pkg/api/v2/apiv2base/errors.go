f4768da01d16eb1ce214a57e56ac7b270b643c5a
Aaron Harper <aaron@inngest.com>
2026-04-24 13:19:26 -0400

    Add /apps/{app_id}/syncs

--- pkg/api/v2/apiv2base/errors.go
+++ pkg/api/v2/apiv2base/errors.go
@@ -17,10 +17,12 @@ const (
 	ErrorInvalidRequest     = "invalid_request"
 	ErrorMissingField       = "missing_field"
 	ErrorInvalidFieldFormat = "invalid_field_format"
+	ErrorEnvNameAmbiguous   = "env_name_ambiguous"
 
 	// 401 Unauthorized errors
 	ErrorAuthorizationHeaderMissing = "authorization_header_missing"
 	ErrorInvalidSigningKey          = "invalid_signing_key"
+	ErrorInvalidAPIKey              = "invalid_api_key"
 
 	// 403 Forbidden errors
 	ErrorAccessDenied = "access_denied"
@@ -35,6 +37,7 @@ const (
 	// 422 Unprocessable Entity errors
 	ErrorFunctionDebounced = "function_debounced"
 	ErrorFunctionSkipped   = "function_skipped"
+	ErrorAppSyncFailed     = "app_sync_failed"
 
 	// 404 Not Found errors
 	// ErrorNotFound is the generic not found error code used across multiple APIs.
