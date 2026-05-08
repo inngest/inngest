b7f6dc93429a5aeaaad1e70da2d8851805e4c1d6
Darwin D Wu <darwin67@users.noreply.github.com>
2026-05-08 16:24:56 -0700

    test(release): split release note tests

---
+++ tools/release-notes/test_helpers_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func assertContains(t *testing.T, haystack, needle string) {
+	t.Helper()
+	if !strings.Contains(haystack, needle) {
+		t.Fatalf("expected output to contain %q:\n%s", needle, haystack)
+	}
+}
