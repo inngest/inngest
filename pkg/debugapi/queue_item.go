124bbd71ae33cd704559354a432b6918aaa99b00
Bruno Scheufler <git@brunoscheufler.com>
2026-05-25 15:23:02 +0200

    Fix shards

--- pkg/debugapi/queue_item.go
+++ pkg/debugapi/queue_item.go
@@ -15,17 +15,17 @@ import (
 )
 
 func (d *debugAPI) GetQueueItem(ctx context.Context, req *pb.QueueItemRequest) (*pb.QueueItemResponse, error) {
-	shardName := consts.DefaultQueueShardName
-	if req.QueueShard != "" {
-		shardName = req.QueueShard
-	}
-
-	shard, err := d.shards.ByName(shardName)
-	if err != nil {
-		return nil, fmt.Errorf("could not find queue shard %q", shardName)
-	}
-
 	if itemID := req.GetItemId(); itemID != "" {
+		shardName := consts.DefaultQueueShardName
+		if req.QueueShard != "" {
+			shardName = req.QueueShard
+		}
+
+		shard, err := d.shards.ByName(shardName)
+		if err != nil {
+			return nil, fmt.Errorf("could not find queue shard %q", shardName)
+		}
+
 		queueItem, err := d.queue.LoadQueueItem(ctx, shard.Name(), itemID)
 		if err != nil {
 			if errors.Is(err, queue.ErrQueueItemNotFound) {
@@ -55,6 +55,19 @@ func (d *debugAPI) GetQueueItem(ctx context.Context, req *pb.QueueItemRequest) (
 	scope, err := debugScope(req.GetFunctionId(), req.GetAccountId(), req.GetEnvId())
 	if err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	var shard queue.QueueShard
+	if req.QueueShard != "" {
+		shard, err = d.shards.ByName(req.QueueShard)
+		if err != nil {
+			return nil, fmt.Errorf("could not find queue shard %q", req.QueueShard)
+		}
+	} else {
+		shard, err = d.shards.Resolve(ctx, scope.AccountID, nil)
+		if err != nil {
+			return nil, fmt.Errorf("could not resolve queue shard for account %q: %w", scope.AccountID, err)
+		}
 	}
 
 	items, err := d.queue.ItemsByRunID(ctx, shard, scope, runID)
