e657fdf57e03f5bccf4c136e172b2d04171c1ed7
Albert Chae <albertchae@users.noreply.github.com>
2026-08-03 14:00:48 -0700

    chore(support): add GraphQL codegen to support app
    This mostly copies dashboard's settings, but because support uses a
    much smaller subset of the gql surface we have strictScalars: false

--- ui/apps/support/src/data/envs.ts
+++ ui/apps/support/src/data/envs.ts
@@ -1,8 +1,25 @@
 import { queryOptions } from "@tanstack/react-query";
 import { createServerFn } from "@tanstack/react-start";
 import { getAuthHeaders, inngestGQLAPI } from "./gqlApi";
-import type { GetEnvironmentBySlugQuery } from "@/gql/graphql";
-import { GetEnvironmentBySlugDocument } from "@/gql/graphql";
+import { graphql } from "@/gql";
+
+const GetEnvironmentBySlugDocument = graphql(`
+  query GetEnvironmentBySlug($slug: String!) {
+    envBySlug(slug: $slug) {
+      id
+      name
+      slug
+      parentID
+      test
+      type
+      createdAt
+      lastDeployedAt
+      isArchived
+      isAutoArchiveEnabled
+      webhookSigningKey
+    }
+  }
+`);
 
 export const envQueryOptions = (slug: string) =>
   queryOptions({
@@ -14,7 +31,7 @@ export const fetchEnvBySlug = createServerFn({ method: "GET" })
   .inputValidator((data: { slug: string }) => data)
   .handler(async ({ data }) => {
     console.info(`gql api fetching env by slug ${data.slug}...`);
-    return await inngestGQLAPI.request<GetEnvironmentBySlugQuery>(
+    return await inngestGQLAPI.request(
       GetEnvironmentBySlugDocument,
       {
         slug: data.slug,
