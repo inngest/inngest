58ec3213db8687496ea2535d277036a7cb2a324a
Linell Bonnette <tlbonnette@gmail.com>
2026-09-08 13:12:34 -0700

    chore: add skip reason to UI

--- ui/packages/components/src/utils/skipReasons.ts
+++ ui/packages/components/src/utils/skipReasons.ts
@@ -3,6 +3,7 @@ const skipReasonLabels: Record<string, string> = {
   FunctionPaused: 'Function is paused',
   FunctionDrained: 'Function is draining',
   FunctionBacklogSizeLimitHit: 'Backlog limit reached',
+  AccountExecutionCapHit: 'Execution cap reached', // TODO: better wordin'
 };
 
 export function formatSkipReason(reason?: string): string {
