8d0eda45d3b0ab9343a2a2de7a3823a064e1e99f
Jakob Evangelista <79593812+jakobevangelista@users.noreply.github.com>
2026-09-08 11:43:52 -0700

    Revert "feat(dashboard): add sandbox dashboard (#4784)"
    This reverts commit 839525f63e90441335e26620ac174ebf611b385d.

--- ui/apps/dashboard/src/routes/_authed/env/$envSlug/sandboxes/index.tsx
+++ ui/apps/dashboard/src/routes/_authed/env/$envSlug/sandboxes/index.tsx
@@ -2,31 +2,20 @@ import { Header } from '@inngest/components/Header/Header';
 import { ClientOnly, createFileRoute } from '@tanstack/react-router';
 
 import SandboxesEmptyState from '@/components/Sandboxes/SandboxesEmptyState';
-import { SandboxesList } from '@/components/Sandboxes/SandboxesList';
-import { getSandboxAPIEnabled } from '@/queries/server/featureFlags';
 
 export const Route = createFileRoute('/_authed/env/$envSlug/sandboxes/')({
   component: SandboxesPage,
-  loader: async () => ({
-    sandboxAPIEnabled: await getSandboxAPIEnabled(),
-  }),
 });
 
 function SandboxesPage() {
-  const { sandboxAPIEnabled } = Route.useLoaderData();
-
   return (
     <>
       <Header breadcrumb={[{ text: 'Sandboxes' }]} />
-      {sandboxAPIEnabled ? (
-        <SandboxesList />
-      ) : (
-        <div className="bg-canvasBase h-full w-full overflow-y-auto">
-          <ClientOnly>
-            <SandboxesEmptyState />
-          </ClientOnly>
-        </div>
-      )}
+      <div className="bg-canvasBase h-full w-full overflow-y-auto">
+        <ClientOnly>
+          <SandboxesEmptyState />
+        </ClientOnly>
+      </div>
     </>
   );
 }
