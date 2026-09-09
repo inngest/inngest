374e11f6b0cb75e1a76f7a611660b6511798beb4
Muzammil <muzammil@inngest.com>
2026-09-08 20:22:27 -0700

    adding batch generation

--- pkg/execution/batch/batch.go
+++ pkg/execution/batch/batch.go
@@ -218,7 +218,8 @@ func (o *ScheduleBatchOpts) JobID() string {
 type ScheduleBatchPayload struct {
 	BatchID                    ulid.ULID  `json:"batchID"`
 	BatchPointer               string     `json:"batchPointer"`
-	BatchCluster               string     `json:"batchCluster,omitempty"` // empty for jobs created before function-level batch routing.
+	BatchCluster               string     `json:"batchCluster,omitempty"`    // empty for jobs created before function-level batch routing.
+	BatchGeneration            string     `json:"batchGeneration,omitempty"` // empty is the legacy generation-zero namespace.
 	AccountID                  uuid.UUID  `json:"acctID"`
 	WorkspaceID                uuid.UUID  `json:"wsID"`
 	AppID                      uuid.UUID  `json:"appID"`
